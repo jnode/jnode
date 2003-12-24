6522294c08f3db64f67565f78722b8e9abdfcc24
vchira_2000 <vchira_2000@407389f7-7c16-0410-9eee-ad85acb55d7f>
2003-12-24 17:19:45 +0000

    short FontManagerTest
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@98 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ gui/src/test/org/jnode/test/gui/FontManagerTest.java
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ */package org.jnode.test.gui;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+
+/**
+ * @author Valentin Chira
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class FontManagerTest {
+
+	public static void main(String[] args) 
+	{
+		Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+		// foants loaded
+		
+		System.out.println("" + fonts.length);
+		
+		for(int i = 0;i< fonts.length;i++)
+		{
+			System.out.println(fonts[i].getName());
+		}
+	}
+}
