17b5324836aa17cdf5a17fc1a414d999055db702
mdenty <mdenty@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-02-06 20:47:19 +0000

    Some rewritings - typo
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@188 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/LoadkeysCommand.java
+++ shell/src/shell/org/jnode/shell/command/LoadkeysCommand.java
@@ -1,11 +1,7 @@
-/*
- * $Id$
- */
 package org.jnode.shell.command;
 
 import java.io.InputStream;
 import java.io.PrintStream;
-
 import org.jnode.driver.Device;
 import org.jnode.driver.DeviceManager;
 import org.jnode.driver.input.KeyboardAPI;
@@ -18,93 +14,78 @@ import org.jnode.shell.help.ParsedArguments;
 import org.jnode.shell.help.Syntax;
 
 /**
- * @author Mark DENTY
+ * @author Marc DENTY
+ * @version Feb 2004
+ * @since 0.15
  */
 public class LoadkeysCommand {
-
-    static String PS2_KEYBOARD_DEV = "ps2keyboard";
-
-    static final OptionArgument COUNTRY = new OptionArgument("action",
-            "country parameter",
-            new OptionArgument.Option[] { new OptionArgument.Option("",
-                    "Specify country")});
-
-    static final OptionArgument REGION = new OptionArgument("action",
-            "region parameter",
-            new OptionArgument.Option[] { new OptionArgument.Option("",
-                    "Specify region")});
-
-    static final Parameter PARAM_COUNTRY = new Parameter(COUNTRY,
-            Parameter.MANDATORY);
-
-    static final Parameter PARAM_REGION = new Parameter(REGION,
-            Parameter.OPTIONAL);
-
-    public static Help.Info HELP_INFO = new Help.Info(
-            "loadkeys",
-            new Syntax[] { new Syntax(
-                    "change the current keyboard layout\n\tExample : loadkeys FR fr",
-                    new Parameter[] { PARAM_COUNTRY, PARAM_REGION})});
-
-    public static void main(String[] args) throws Exception {
-        new LoadkeysCommand().execute(args, System.in, System.out, System.err);
-    }
-
-    /**
-     * Execute this command
-     */
-    public void execute(String[] args, InputStream in, PrintStream out,
-            PrintStream err) throws Exception {
-        String classI10N = "org.jnode.driver.input.l10n.KeyboardInterpreter_";
-
-        ParsedArguments cmdLine = HELP_INFO.parse(args);
-        if (!PARAM_COUNTRY.isSatisfied()) {
-            err.println("You *MUST* specify a country !");
-            return;
-        }
-
-        String country = COUNTRY.getValue(cmdLine).toUpperCase();
-        String region = REGION.getValue(cmdLine);
-        if (region != null) {
-            region = region.toLowerCase();
-        }
-
-        out.print("Searching for " + country
-                + (region == null ? "" : "_" + region) + "...");
-        KeyboardInterpreter interpreter = null;
-        try {
-            if (region == null) {
-                interpreter = (KeyboardInterpreter) Thread.currentThread()
-                        .getContextClassLoader().loadClass(classI10N + country)
-                        .newInstance();
-            } else {
-                try {
-                    interpreter = (KeyboardInterpreter) Thread.currentThread()
-                            .getContextClassLoader().loadClass(
-                                    classI10N + country + "_" + region)
-                            .newInstance();
-                } catch (ClassNotFoundException e) {
-                    // try to load without region
-                    err.print("Class not found : " + classI10N + country + "_"
-                            + region + ", trying to load :" + classI10N
-                            + country);
-                    interpreter = (KeyboardInterpreter) Thread.currentThread()
-                            .getContextClassLoader().loadClass(
-                                    classI10N + country).newInstance();
-                }
-            }
-        } catch (ClassNotFoundException e) {
-            err.println(" Failed, not found");
-            e.printStackTrace(err);
-        }
-
-        DeviceManager dm;
-
-        dm = (DeviceManager) InitialNaming.lookup(DeviceManager.NAME);
-        Device kb = dm.getDevice(/* PS2Constants. */PS2_KEYBOARD_DEV);
-        KeyboardAPI api = (KeyboardAPI) kb.getAPI(KeyboardAPI.class);
-        api.setKbInterpreter(interpreter);
-        out.println(" Done.");
-    }
-
+	
+	//FIXME: why can't I import org.jnode.driver.ps2.PS2Constants (compilation error)
+	public static String PS2_KEYBOARD_DEV = "ps2keyboard";
+		static final OptionArgument COUNTRY =
+		new OptionArgument(
+		"action",
+		"country parameter",
+		new OptionArgument.Option[] { new OptionArgument.Option("", "Specify country")});
+	static final OptionArgument REGION =
+		new OptionArgument(
+		"action",
+		"region parameter",
+		new OptionArgument.Option[] { new OptionArgument.Option("", "Specify region")});
+	
+	static final Parameter PARAM_COUNTRY = new Parameter(COUNTRY, Parameter.MANDATORY);
+	static final Parameter PARAM_REGION  = new Parameter(REGION, Parameter.OPTIONAL);
+	
+	public static Help.Info HELP_INFO =
+		new Help.Info(
+		"loadkeys",
+		new Syntax[] {
+			new Syntax(
+				"change the current keyboard layout\n\tExample : loadkeys FR fr",
+				new Parameter[] { PARAM_COUNTRY, PARAM_REGION })
+		});
+	
+	public static void main(String[] args) throws Exception {
+		new LoadkeysCommand().execute(args,  System.in, System.out, System.err);
+	}
+	
+	/**
+	 * Execute this command
+	 */
+	protected void execute(String[] args, InputStream in, PrintStream out, PrintStream err) throws Exception {
+		String classI10N = "org.jnode.driver.input.l10n.KeyboardInterpreter_";
+		
+		ParsedArguments cmdLine = HELP_INFO.parse(args);
+		if(!PARAM_COUNTRY.isSatisfied()) {
+			err.println("You *MUST* specify a country !");
+		}
+		
+		String country = COUNTRY.getValue(cmdLine).toUpperCase();
+		String region = REGION.getValue(cmdLine);
+		if(region != null) {
+			region = region.toLowerCase();
+		}
+		
+		out.print("Searching for "+country+ (region==null ? "":"_"+region) +"...");
+		KeyboardInterpreter interpreter = null;
+		try {
+			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			if(region == null) {
+				interpreter = (KeyboardInterpreter)cl.loadClass(classI10N+country).newInstance();
+			} else {
+				interpreter = (KeyboardInterpreter)cl.loadClass(classI10N+country+"_"+region).newInstance();
+			}
+		} catch (ClassNotFoundException e) {
+			err.println(" Failed, not found");
+			e.printStackTrace(err);
+		}
+		
+		DeviceManager dm;
+		
+		dm = (DeviceManager)InitialNaming.lookup(DeviceManager.NAME);
+		Device kb = dm.getDevice(PS2_KEYBOARD_DEV);
+		KeyboardAPI api = (KeyboardAPI)kb.getAPI(KeyboardAPI.class);
+		api.setKbInterpreter(interpreter);
+		out.println(" Done.");
+	}
 }
