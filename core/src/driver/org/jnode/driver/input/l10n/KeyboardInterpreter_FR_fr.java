710920e9698e8962716b2b9f4f08aae7614d8b10
mdenty <mdenty@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-02-06 21:13:26 +0000

    Add the FR_fr version
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@193 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/driver/org/jnode/driver/input/l10n/KeyboardInterpreter_FR_fr.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Mar 6, 2003
+ * $Id$
+ */
+package org.jnode.driver.input.l10n;
+
+/**
+ * KeyboardInterpreter_FR_fr.java
+ *
+ * @author Marc DENTY
+ */
+public class KeyboardInterpreter_FR_fr extends KeyboardInterpreter_FR {
+}
+
