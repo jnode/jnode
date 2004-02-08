f3e57914c1d48cc39a625f85abf106ae39dea5b5
mdenty <mdenty@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-02-08 17:54:55 +0000

    Added generic support for dead keys
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@205 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/driver/org/jnode/driver/input/DeadKeyException.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Mar 5, 2003
+ * $Id$
+ */
+
+package org.jnode.driver.input;
+
+/**
+ * DeadKeyException.java
+ * This is tag Exception is thrown when a keyPress generates a dead key
+ *
+ * @author Created by Marc DENTY
+ * @since 0.15
+ */
+
+public class DeadKeyException extends Exception {
+}
+
