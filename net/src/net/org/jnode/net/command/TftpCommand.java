6e02cc2c25ce73c670a6eaf569efa71045b970b3
markhale <markhale@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-02-09 22:19:13 +0000

    Initial import.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@213 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ net/src/net/org/jnode/net/command/TftpCommand.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ */
+package org.jnode.net.command;
+
+import org.jnode.net.ipv4.tftp.TFTPClient;
+import org.jnode.shell.help.Help;
+import org.jnode.shell.help.Syntax;
+import org.jnode.shell.help.OptionArgument;
+import org.jnode.shell.help.Argument;
+import org.jnode.shell.help.Parameter;
+import org.jnode.shell.help.ParsedArguments;
+
+/**
+ * @author markhale
+ */
+public class TftpCommand {
+	
+	private static final OptionArgument.Option[] COMMAND_OPTIONS = new OptionArgument.Option[] {
+		new OptionArgument.Option("put", "transfer a file to a server"),
+		new OptionArgument.Option("get", "transfer a file from a server")
+	};
+	private static final HostArgument ARG_SERVER = new HostArgument("hostname", "the hostname of the TFTP server");
+	private static final OptionArgument ARG_COMMAND = new OptionArgument("command", "must be either PUT or GET", COMMAND_OPTIONS);
+	private static final Argument ARG_FILENAME = new Argument("filename", "the file to transfer");
+	
+	public static Help.Info HELP_INFO = new Help.Info(
+		"tftp",
+		new Syntax[] {
+			new Syntax(
+				"Start the TFTP client as an interactive session",
+				new Parameter[] {
+					new Parameter(ARG_SERVER, Parameter.OPTIONAL)
+				}
+			),
+			new Syntax(
+				"Execute the TFTP client non-interactively",
+				new Parameter[] {
+					new Parameter(ARG_SERVER, Parameter.MANDATORY),
+					new Parameter(ARG_COMMAND, Parameter.MANDATORY),
+					new Parameter(ARG_FILENAME, Parameter.MANDATORY)
+				}
+			)
+		}
+	);
+	
+	public static void main(String[] args) throws Exception {
+		ParsedArguments cmdLine = HELP_INFO.parse(args);
+		TFTPClient.main(args);
+		System.out.println();
+	}
+}
+
