8b01347e89167cecc503c146f22b5664768c27ce
markhale <markhale@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-02-17 20:51:12 +0000

    Initial import.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@232 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ net/src/net/org/jnode/net/command/DhcpCommand.java
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ */
+package org.jnode.net.command;
+
+import org.jnode.driver.Device;
+import org.jnode.net.ipv4.dhcp.DHCPClient;
+import org.jnode.shell.help.DeviceArgument;
+import org.jnode.shell.help.Help;
+import org.jnode.shell.help.Parameter;
+import org.jnode.shell.help.ParsedArguments;
+
+/**
+ * @author markhale
+ */
+public class DhcpCommand {
+
+        static final DeviceArgument ARG_DEVICE = new DeviceArgument("device", "the device to boot from");
+
+	public static Help.Info HELP_INFO = new Help.Info(
+		"dhcp",
+		"Try to configure the given device using DHCP",
+		new Parameter[]{
+			new Parameter(ARG_DEVICE, Parameter.MANDATORY)
+		}
+	);
+
+	public static void main(String[] args)
+	throws Exception {
+		ParsedArguments cmdLine = HELP_INFO.parse(args);
+
+		Device dev = ARG_DEVICE.getDevice(cmdLine);
+
+		System.out.println("Trying to configure " + dev.getId() + "...");
+		final DHCPClient client = new DHCPClient();
+		client.configureDevice(dev);
+	}
+
+}
