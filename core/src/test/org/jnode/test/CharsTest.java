57b9e28dcbb51b8a13c1e8cf2465c2befadbdcc6
vchira_2000 <vchira_2000@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-02-21 13:54:23 +0000

    refactor the console manager..
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@245 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/test/org/jnode/test/CharsTest.java
+++ core/src/test/org/jnode/test/CharsTest.java
@@ -3,7 +3,7 @@ package org.jnode.test;
 /**
  * Print all (8-bit) chars to the console, for debugging purposes.
  * This demonstrates the support for 8-bit chars introduced to
- * the output driver ({@link org.jnode.driver.console.x86.TextConsole}).
+ * the output driver ({@link org.jnode.driver.console.x86.ScrollableShellConsole}).
  * 
  * @author Bengt Bäverman
  * @since 2003-08
