02686a429aac82d18e83cb8ebf8afa02ea27a33e
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-02-29 07:57:32 +0000

    Changed bootdisk creation task - Added fileset support - Removed kernelfile, initjar, menufile attributes - changed build.xml accordingly
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@259 407389f7-7c16-0410-9eee-ad85acb55d7f

--- builder/src/builder/org/jnode/build/BootFloppyBuilder.java
+++ builder/src/builder/org/jnode/build/BootFloppyBuilder.java
@@ -7,14 +7,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
 import org.jnode.driver.ApiNotFoundException;
 import org.jnode.driver.Device;
 import org.jnode.driver.DriverException;
 import org.jnode.driver.block.BlockDeviceAPI;
 import org.jnode.driver.block.FileDevice;
 import org.jnode.fs.FSDirectory;
+import org.jnode.fs.FSEntry;
 import org.jnode.fs.FSFile;
 import org.jnode.fs.FileSystemException;
 import org.jnode.fs.fat.FatFileSystem;
@@ -28,249 +33,282 @@ import org.jnode.util.FileUtils;
  */
 public class BootFloppyBuilder extends Task {
 
-	private File destFile;
-	private File kernelFile;
-	private File menuFile;
-	private File initJarFile;
-	private String stage1ResourceName;
-	private String stage2ResourceName;
+    private File destFile;
 
-	/**
-	 * Build the boot floppy
-	 * 
-	 * @throws BuildException
-	 */
-	public void execute() throws BuildException {
+    private String stage1ResourceName;
 
-		try {
-			if (isExecuteNeeded()) {
-				createImage();
-			}
-		} catch (Throwable ex) {
-			ex.printStackTrace(System.err);
-			throw new BuildException(ex);
-		}
-	}
+    private String stage2ResourceName;
 
-	protected boolean isExecuteNeeded() {
-		long lmDest = destFile.lastModified();
-		long lmKernel = (kernelFile == null) ? 0 : kernelFile.lastModified();
-		long lmMenu = (menuFile == null) ? 0 : menuFile.lastModified();
-		long lmInitJar = (initJarFile == null) ? 0 : initJarFile.lastModified();
-		
-		return (lmKernel > lmDest) || (lmMenu > lmDest) || (lmInitJar > lmDest);
-	}
+    private ArrayList fileSets = new ArrayList();
 
-	/**
-	 * Create the actual bootfloppy
-	 * 
-	 * @throws IOException
-	 * @throws DriverException
-	 * @throws FileSystemException
-	 */
-	public void createImage() throws IOException, DriverException, FileSystemException {
+    /**
+     * Build the boot floppy
+     * 
+     * @throws BuildException
+     */
+    public void execute() throws BuildException {
 
-		final FileDevice newFd = new FileDevice(destFile, "rw");
-		try {
-			newFd.setLength(getDeviceLength());
-			formatDevice(newFd);
-			final Device sysDev = getSystemDevice(newFd);
-			final BlockDeviceAPI sysDevApi = (BlockDeviceAPI) sysDev.getAPI(BlockDeviceAPI.class);
-			copySystemFiles(sysDev);
-			sysDevApi.flush();
-		} catch (ApiNotFoundException ex) {
-			throw new IOException("BlockDeviceAPI not found on device", ex);
-		} finally {
-			newFd.close();
-		}
-	}
+        try {
+            if (isExecuteNeeded()) {
+                createImage();
+            }
+        } catch (Throwable ex) {
+            ex.printStackTrace(System.err);
+            throw new BuildException(ex);
+        }
+    }
 
-	/**
-	 * Format the given device
-	 * 
-	 * @param dev
-	 * @throws IOException
-	 */
-	protected void formatDevice(Device dev) throws IOException {
-		GrubFatFormatter ff = createFormatter();
-		try {
-			ff.format((BlockDeviceAPI) dev.getAPI(BlockDeviceAPI.class));
-		} catch (ApiNotFoundException ex) {
-			throw new IOException("Cannot find BlockDeviceAPI", ex);
-		}
-	}
+    protected boolean isExecuteNeeded() {
+        final long lmDest = destFile.lastModified();
+        return (getLastModified() > lmDest);
+    }
 
-	/**
-	 * Gets the device the system files must be copied onto. This enabled a disk to be formatted
-	 * with partitions.
-	 * 
-	 * @param rootDevice
-	 * @return BlockDevice
-	 */
-	protected Device getSystemDevice(Device rootDevice) {
-		return rootDevice;
-	}
+    /**
+     * Create the actual bootfloppy
+     * 
+     * @throws IOException
+     * @throws DriverException
+     * @throws FileSystemException
+     */
+    public void createImage() throws IOException, DriverException,
+            FileSystemException {
 
-	/**
-	 * Copy the system files to the given device
-	 * 
-	 * @param device
-	 * @throws IOException
-	 * @throws FileSystemException
-	 */
-	protected void copySystemFiles(Device device) throws IOException, FileSystemException {
-		final FatFileSystem fs = new FatFileSystem(device);
+        final FileDevice newFd = new FileDevice(destFile, "rw");
+        try {
+            newFd.setLength(getDeviceLength());
+            formatDevice(newFd);
+            final Device sysDev = getSystemDevice(newFd);
+            final BlockDeviceAPI sysDevApi = (BlockDeviceAPI) sysDev
+                    .getAPI(BlockDeviceAPI.class);
+            copySystemFiles(sysDev);
+            sysDevApi.flush();
+        } catch (ApiNotFoundException ex) {
+            throw new IOException("BlockDeviceAPI not found on device", ex);
+        } finally {
+            newFd.close();
+        }
+    }
 
-		final FSDirectory dir = fs.getRootEntry().getDirectory();
-		final FSDirectory bDir = dir.addDirectory("boot").getDirectory();
-		final FSDirectory bgDir = bDir.addDirectory("grub").getDirectory();
+    /**
+     * Format the given device
+     * 
+     * @param dev
+     * @throws IOException
+     */
+    protected void formatDevice(Device dev) throws IOException {
+        GrubFatFormatter ff = createFormatter();
+        try {
+            ff.format((BlockDeviceAPI) dev.getAPI(BlockDeviceAPI.class));
+        } catch (ApiNotFoundException ex) {
+            throw new IOException("Cannot find BlockDeviceAPI", ex);
+        }
+    }
 
-		if (kernelFile != null) {
-			addFile(dir, kernelFile, kernelFile.getName());
-		}
-		if (menuFile != null) {
-			addFile(bgDir, menuFile, "menu.lst");
-		}
-		if (initJarFile != null) {
-			addFile(dir, initJarFile, "full.jgz");
-		}
-		
-		fs.close();
-	}
+    /**
+     * Gets the device the system files must be copied onto. This enabled a
+     * disk to be formatted with partitions.
+     * 
+     * @param rootDevice
+     * @return BlockDevice
+     */
+    protected Device getSystemDevice(Device rootDevice) {
+        return rootDevice;
+    }
 
-	/**
-	 * Add a given file to a given directory with a given filename.
-	 * 
-	 * @param dir
-	 * @param src
-	 * @param fname
-	 * @throws IOException
-	 */
-	private void addFile(FSDirectory dir, File src, String fname) throws IOException {
+    /**
+     * Copy the system files to the given device
+     * 
+     * @param device
+     * @throws IOException
+     * @throws FileSystemException
+     */
+    protected void copySystemFiles(Device device) throws IOException,
+            FileSystemException {
+        final FatFileSystem fs = new FatFileSystem(device);
 
-		long size = src.length();
-		/*
-		 * log.info( "Adding " + src + " as " + fname + " size " + (size / 1024) + "Kb");
-		 */
+        for (Iterator i = fileSets.iterator(); i.hasNext();) {
+            final FileSet fset = (FileSet) i.next();
+            processFileSet(fs, fset);
+        }
 
-		final byte[] buf = new byte[(int) size];
-		InputStream is = new FileInputStream(src);
-		FileUtils.copy(is, buf);
-		is.close();
+        fs.close();
+    }
 
-		final FSFile fh = dir.addFile(fname).getFile();
-		fh.setLength(size);
-		fh.write(0, buf, 0, buf.length);
+    private void processFileSet(FatFileSystem fs, FileSet fset) throws IOException {
+        final DirectoryScanner ds = fset.getDirectoryScanner(getProject());
+        final String[] dirs = ds.getIncludedDirectories();
+        for (int i = 0; i < dirs.length; i++) {
+            getOrCreateDir(fs, dirs[i]);
+        }
+        final String[] files = ds.getIncludedFiles();
+        for (int i = 0; i < files.length; i++) {
+            final String fn = files[ i];
+            final int idx = fn.lastIndexOf(File.separatorChar);
+            final FSDirectory dir;
+            final String name;
+            if (idx >= 0) {
+                dir = getOrCreateDir(fs, fn.substring(0, idx));
+                name = fn.substring(idx + 1);
+            } else {
+                dir = getOrCreateDir(fs, "");
+                name = fn;
+            }
+            final File f = new File(ds.getBasedir(), fn);
+            addFile(dir, f, name);
+        }
+    }
+    
+    /**
+     * Gets the last modification date of all parameters.
+     * @return
+     */
+    protected long getLastModified() {
+        long lm = 0l;
+        for (Iterator i = fileSets.iterator(); i.hasNext();) {
+            final FileSet fset = (FileSet) i.next();
+            lm = Math.max(lm, getLastModified(fset));
+        }
+        return lm;        
+    }
 
-		log("Added " + src + " as " + fname + " size " + (size / 1024) + "Kb");
-	}
+    private long getLastModified(FileSet fset) {
+        final DirectoryScanner ds = fset.getDirectoryScanner(getProject());
+        final File baseDir = ds.getBasedir();
+        long lm = 0l;
+        final String[] dirs = ds.getIncludedDirectories();
+        for (int i = 0; i < dirs.length; i++) {
+            lm = Math.max(lm, new File(baseDir, dirs[i]).lastModified());
+        }
+        final String[] files = ds.getIncludedFiles();
+        for (int i = 0; i < files.length; i++) {
+            lm = Math.max(lm, new File(baseDir, files[i]).lastModified());
+        }
+        return lm;
+        
+    }
 
-	/**
-	 * Returns the destFile.
-	 * 
-	 * @return File
-	 */
-	public File getDestFile() {
-		return destFile;
-	}
+    private FSDirectory getOrCreateDir(FatFileSystem fs, String dirName)
+            throws IOException {
+        FSDirectory dir = fs.getRootDir();
+        while (dirName.length() > 0) {
+            final int idx = dirName.indexOf(File.separatorChar);
+            final String part;
+            if (idx >= 0) {
+                part = dirName.substring(0, idx);
+                dirName = dirName.substring(idx + 1);
+            } else {
+                part = dirName;
+                dirName = "";
+            }
+            FSEntry entry;
+            try {
+                entry = dir.getEntry(part);
+            } catch (IOException ex) {
+                // Ignore
+                entry = null;
+            }
+            if (entry == null) {
+                entry = dir.addDirectory(part);                
+            }
+            dir = entry.getDirectory();
+        }
+        return dir;
+    }
 
-	/**
-	 * Returns the kernelFile.
-	 * 
-	 * @return File
-	 */
-	public File getKernelFile() {
-		return kernelFile;
-	}
+    /**
+     * Add a given file to a given directory with a given filename.
+     * 
+     * @param dir
+     * @param src
+     * @param fname
+     * @throws IOException
+     */
+    private void addFile(FSDirectory dir, File src, String fname)
+            throws IOException {
 
-	/**
-	 * Returns the menuFile.
-	 * 
-	 * @return File
-	 */
-	public File getMenuFile() {
-		return menuFile;
-	}
+        long size = src.length();
+        /*
+         * log.info( "Adding " + src + " as " + fname + " size " + (size /
+         * 1024) + "Kb");
+         */
 
-	/**
-	 * Sets the destFile.
-	 * 
-	 * @param destFile
-	 *            The destFile to set
-	 */
-	public void setDestFile(File destFile) {
-		this.destFile = destFile;
-	}
+        final byte[] buf = new byte[ (int) size];
+        InputStream is = new FileInputStream(src);
+        FileUtils.copy(is, buf);
+        is.close();
 
-	/**
-	 * Sets the kernelFile.
-	 * 
-	 * @param kernelFile
-	 *            The kernelFile to set
-	 */
-	public void setKernelFile(File kernelFile) {
-		this.kernelFile = kernelFile;
-	}
+        final FSFile fh = dir.addFile(fname).getFile();
+        fh.setLength(size);
+        fh.write(0, buf, 0, buf.length);
 
-	/**
-	 * Sets the menuFile.
-	 * 
-	 * @param menuFile
-	 *            The menuFile to set
-	 */
-	public void setMenuFile(File menuFile) {
-		this.menuFile = menuFile;
-	}
+        log("Added " + src + " as " + fname + " size " + (size / 1024) + "Kb");
+    }
 
-	protected GrubFatFormatter createFormatter() throws IOException {
-		return new GrubFatFormatter(0, stage1ResourceName, stage2ResourceName);
-	}
+    /**
+     * Returns the destFile.
+     * 
+     * @return File
+     */
+    public File getDestFile() {
+        return destFile;
+    }
 
-	protected long getDeviceLength() {
-		return 1440 * 1024;
-	}
-	/**
-	 * @return Returns the stage1ResourceName.
-	 */
-	public final String getStage1ResourceName() {
-		return this.stage1ResourceName;
-	}
+    /**
+     * Sets the destFile.
+     * 
+     * @param destFile
+     *            The destFile to set
+     */
+    public void setDestFile(File destFile) {
+        this.destFile = destFile;
+    }
 
-	/**
-	 * @param stage1ResourceName
-	 *            The stage1ResourceName to set.
-	 */
-	public final void setStage1ResourceName(String stage1ResourceName) {
-		this.stage1ResourceName = stage1ResourceName;
-	}
+    protected GrubFatFormatter createFormatter() throws IOException {
+        return new GrubFatFormatter(0, stage1ResourceName, stage2ResourceName);
+    }
 
-	/**
-	 * @return Returns the stage2ResourceName.
-	 */
-	public final String getStage2ResourceName() {
-		return this.stage2ResourceName;
-	}
+    protected long getDeviceLength() {
+        return 1440 * 1024;
+    }
 
-	/**
-	 * @param stage2ResourceName
-	 *            The stage2ResourceName to set.
-	 */
-	public final void setStage2ResourceName(String stage2ResourceName) {
-		this.stage2ResourceName = stage2ResourceName;
-	}
+    /**
+     * @return Returns the stage1ResourceName.
+     */
+    public final String getStage1ResourceName() {
+        return this.stage1ResourceName;
+    }
 
-	/**
-	 * @return Returns the initJarFile.
-	 */
-	public final File getInitJarFile() {
-		return this.initJarFile;
-	}
+    /**
+     * @param stage1ResourceName
+     *            The stage1ResourceName to set.
+     */
+    public final void setStage1ResourceName(String stage1ResourceName) {
+        this.stage1ResourceName = stage1ResourceName;
+    }
 
-	/**
-	 * @param initJarFile The initJarFile to set.
-	 */
-	public final void setInitJarFile(File initJarFile) {
-		this.initJarFile = initJarFile;
-	}
+    /**
+     * @return Returns the stage2ResourceName.
+     */
+    public final String getStage2ResourceName() {
+        return this.stage2ResourceName;
+    }
 
+    /**
+     * @param stage2ResourceName
+     *            The stage2ResourceName to set.
+     */
+    public final void setStage2ResourceName(String stage2ResourceName) {
+        this.stage2ResourceName = stage2ResourceName;
+    }
+
+    /**
+     * Add a fileset to this task.
+     * 
+     * @return
+     */
+    public FileSet createFileset() {
+        final FileSet fs = new FileSet();
+        fileSets.add(fs);
+        return fs;
+    }
 }
