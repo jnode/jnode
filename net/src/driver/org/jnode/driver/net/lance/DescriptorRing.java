c0aee74df6903f68145677dcc521b01db4306303
chriscole <chriscole@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-03-18 02:29:59 +0000

    Completion of Lance driver for VMWare 4
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@297 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ net/src/driver/org/jnode/driver/net/lance/DescriptorRing.java
@@ -0,0 +1,37 @@
+/*
+ * $Id$
+ */
+package org.jnode.driver.net.lance;
+
+import org.jnode.system.MemoryResource;
+import org.jnode.vm.Address;
+
+/**
+ * @author Chris Cole
+ *
+ */
+public class DescriptorRing {
+	protected final MemoryResource mem;
+	protected final int offset;
+	protected final int length;
+	protected int currentDescriptor;
+	
+	public DescriptorRing(MemoryResource mem, int offset, int length) {
+		this.mem = mem;
+		this.offset = offset;
+		this.length = length;
+	}
+	
+	public int getSize() {
+		return length * Descriptor.MESSAGE_DESCRIPTOR_SIZE;
+	}
+	
+	public int getLength() {
+		return length;
+	}
+	
+	public int getAddressAs32() {
+		return Address.as32bit(Address.add(mem.getAddress(), offset));
+	}
+
+}
