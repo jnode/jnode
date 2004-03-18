041be234ee0d72cfb4224239cb34c59bbf242756
chriscole <chriscole@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-03-18 01:46:18 +0000

    Completion of Lance driver for VMWare 4
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@294 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ net/src/driver/org/jnode/driver/net/lance/BufferManager.java
@@ -0,0 +1,136 @@
+package org.jnode.driver.net.lance;
+
+import java.io.PrintStream;
+
+import org.jnode.net.SocketBuffer;
+import org.jnode.net.ethernet.EthernetAddress;
+import org.jnode.system.MemoryResource;
+import org.jnode.system.ResourceManager;
+import org.jnode.system.ResourceNotFreeException;
+import org.jnode.system.ResourceOwner;
+import org.jnode.util.NumberUtils;
+import org.jnode.vm.Address;
+
+/**
+ * 
+ * @author Chris Cole
+ *
+ */
+public class BufferManager {
+	static final public int DATA_BUFFER_SIZE = 1544;
+
+	/** MemoryResource to hold initialization block, descriptor rings, and data buffers */
+	private MemoryResource mem;
+
+	private final InitializationBlock32Bit initBlock;
+	private final RxDescriptorRing rxRing;
+	private final TxDescriptorRing txRing;
+	
+	private final int size;
+
+	public BufferManager(
+		int rxRingLength,
+		int txRingLength,
+		int mode,
+		EthernetAddress physicalAddr,
+		long logicalAddr,
+		ResourceManager rm,
+		ResourceOwner owner) {
+
+		// Compute the required size for the memory resource
+		size =
+			InitializationBlock32Bit.INIT_BLOCK_SIZE
+				+ ((rxRingLength + txRingLength)
+					* (Descriptor.MESSAGE_DESCRIPTOR_SIZE
+						+ DATA_BUFFER_SIZE));
+						
+		
+
+		// Get the memory
+		try {
+			mem =
+				rm.claimMemoryResource(
+					owner,
+					null,
+					size,
+					ResourceManager.MEMMODE_NORMAL);
+		} catch (ResourceNotFreeException e) {
+			System.out.println("buffer memory resouce not free exception");
+		}
+
+		// define the offsets into the memory resource for the entities
+		final int rxRingOffset = InitializationBlock32Bit.INIT_BLOCK_SIZE;
+
+		final int txRingOffset =
+			rxRingOffset + (rxRingLength * Descriptor.MESSAGE_DESCRIPTOR_SIZE);
+
+		final int rxDataBufferOffset =
+			txRingOffset + (txRingLength * Descriptor.MESSAGE_DESCRIPTOR_SIZE);
+
+		final int txDataBufferOffset =
+			rxDataBufferOffset + (rxRingLength * DATA_BUFFER_SIZE);
+
+		// Create and initialize the receive ring
+		rxRing =
+			new RxDescriptorRing(
+				mem,
+				rxRingOffset,
+				rxRingLength,
+				rxDataBufferOffset);
+
+		// Create and initialize the transmit ring
+		txRing =
+			new TxDescriptorRing(
+				mem,
+				txRingOffset,
+				txRingLength,
+				txDataBufferOffset);
+
+		// Create and initialize the initializtion block
+		initBlock =
+			new InitializationBlock32Bit(
+				mem,
+				0,
+				(short) mode,
+				physicalAddr,
+				logicalAddr,
+				rxRing,
+				txRing);
+	}
+
+	/**
+	 * Gets the address of the initdata structure as a 32-bit int
+	 */
+	public final int getInitDataAddressAs32Bit() {
+		return Address.as32bit(mem.getAddress());
+	}
+
+	public void transmit(SocketBuffer buf) {
+		final int len = buf.getSize();
+		if (len > DATA_BUFFER_SIZE) {
+			System.out.println("Length must be <= " + DATA_BUFFER_SIZE);
+		}
+
+		txRing.transmit(buf);
+	}
+
+	public SocketBuffer getPacket() {
+		return rxRing.getPacket();
+	}
+
+	public void dumpData(PrintStream out) {
+		initBlock.dumpData(out);
+		rxRing.dumpData(out);
+		txRing.dumpData(out);
+	}
+
+	public void dumpDataRaw(PrintStream out) {
+		for (int i = 0; i < size; i++) {
+			out.println(
+				"0x"
+					+ NumberUtils.hex((short) i)
+					+ " : 0x"
+					+ NumberUtils.hex(mem.getShort(i)));
+		}
+	}
+}
