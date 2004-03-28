71c92d29460ebc257de3289cde4226bcfbe76f41
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-03-28 12:49:31 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@347 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/driver/org/jnode/driver/RemovableDeviceAPI.java
+++ core/src/driver/org/jnode/driver/RemovableDeviceAPI.java
@@ -3,6 +3,8 @@
  */
 package org.jnode.driver;
 
+import java.io.IOException;
+
 /**
  * API that must be implemented by removable devices.
  * 
@@ -10,4 +12,42 @@ package org.jnode.driver;
  */
 public interface RemovableDeviceAPI extends DeviceAPI {
 
+    /**
+     * Can this device be locked.
+     * @return
+     */
+    public boolean canLock();
+    
+    /**
+     * Can this device be ejected.
+     * @return
+     */
+    public boolean canEject();
+    
+    /**
+     * Lock the device.
+     * @throws IOException
+     */
+    public void lock()
+    throws IOException;
+    
+    /**
+     * Unlock the device.
+     * @throws IOException
+     */
+    public void unlock()
+    throws IOException;
+    
+    /**
+     * Is this device locked.
+     * @return
+     */
+    public boolean isLocked();
+    
+    /**
+     * Eject this device.
+     * @throws IOException
+     */
+    public void eject()
+    throws IOException;
 }
