020c96065afa2dcb83cf18b4bcab66d28527b540
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-03-28 10:32:25 +0000

    Added geometry parameter to bootdisk builder thanks to Sebastian Lohmeier - monochromata <sebastian@monochromata.de> Refactored build a bit bootdisk files are not replaced to x86 dir CDROM files are not in cdroms directory
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@346 407389f7-7c16-0410-9eee-ad85acb55d7f

--- builder/src/builder/org/jnode/build/BootDiskBuilder.java
+++ builder/src/builder/org/jnode/build/BootDiskBuilder.java
@@ -7,7 +7,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
+import org.apache.tools.ant.Project;
 import org.jnode.driver.ApiNotFoundException;
 import org.jnode.driver.Device;
 import org.jnode.driver.DriverException;
@@ -26,122 +28,156 @@ import org.jnode.fs.util.Geometry;
  */
 public class BootDiskBuilder extends BootFloppyBuilder {
 
-	private File plnFile;
-	int bytesPerSector = 512;
-	int spc = 1;
-	private Geometry geom = new Geometry(64, 16, 32);
-	private MappedBlockDeviceSupport part0;
-	
-	public BootDiskBuilder() {
-	}
+    private File plnFile;
 
-	/**
-	 * Create the actual bootfloppy
-	 * @throws IOException
-	 * @throws DriverException
-	 * @throws FileSystemException
-	 */
-	public void createImage() throws IOException, DriverException, FileSystemException {
-		super.createImage();
-		
-		FileWriter fw = new FileWriter(plnFile);
-		PrintWriter pw = new PrintWriter(fw);
-		pw.println("DRIVETYPE ide");
-		pw.println("CYLINDERS " + geom.getCylinders());
-		pw.println("HEADS     " + geom.getHeads());
-		pw.println("SECTORS   " + geom.getSectors());
-		pw.println("CAPACITY  " + geom.getTotalSectors());
-		pw.println("ACCESS    \"" + getDestFile().getCanonicalPath() + "\" 0 102400");
-		pw.flush();
-		fw.flush();
-		pw.close();
-		fw.close();
-		System.out.println("Wrote " + plnFile);
-	}
-	
-	/**
-	 * Format the given device
-	 * @param device
-	 * @throws IOException
-	 */
-	protected void formatDevice(Device device) throws IOException {
+    int bytesPerSector = 512;
 
-		/* Format the MBR & partitiontable */		
-		GrubBootSector mbr = (GrubBootSector)(createFormatter().getBootSector());
-		
-		mbr.getPartition(0).clear();
-		mbr.getPartition(1).clear();
-		mbr.getPartition(2).clear();
-		mbr.getPartition(3).clear();
-		
-		IBMPartitionTableEntry pte = mbr.getPartition(0);
-		pte.setBootIndicator(true);
-		pte.setStartLba(1);
-		pte.setNrSectors(geom.getTotalSectors() - 1);
-		pte.setSystemIndicator(IBMPartitionTypes.PARTTYPE_DOS_FAT16_LT32M);
-		pte.setStartCHS(geom.getCHS(pte.getStartLba()));
-		pte.setEndCHS(geom.getCHS(pte.getStartLba() + pte.getNrSectors() - 1));
-		
-		/*System.out.println("partition table:");
-		for (int i = 0; i < 4; i++) {
-			System.out.println("" + i + " " + mbr.getPartition(i));
-		}*/
+    int spc = 1;
 
-		/* Format partition 0 */
-		part0 = new MappedBlockDeviceSupport(device, pte.getStartLba() * bytesPerSector, pte.getNrSectors() * bytesPerSector);
-		GrubFatFormatter ff = createFormatter();
-		ff.setInstallPartition(0x0000FFFF);
-		ff.format(part0);
-		GrubBootSector part0bs = (GrubBootSector)ff.getBootSector();  
-		
-		/* Fixup stage2 sector in MBR */
-		mbr.setStage2Sector(pte.getStartLba() + part0bs.getStage2Sector());
-		try {
-			mbr.write((BlockDeviceAPI)device.getAPI(BlockDeviceAPI.class));
-		} catch (ApiNotFoundException ex) {
-			throw new IOException("BlockDeviceAPI not found on device", ex);
-		}
-		//System.out.println("mbr stage2 sector=" + mbr.getStage2Sector());
-	}
-	
-	/**
-	 * @see org.jnode.build.BootFloppyBuilder#createFormatter()
- 	 * @return The formatter
-	 * @throws IOException
-	 */
-	protected GrubFatFormatter createFormatter() throws IOException {
-		return new GrubFatFormatter(bytesPerSector, spc, geom, Fat.FAT16, 1, getStage1ResourceName(), getStage2ResourceName());
-	}
+    private Geometry geom = new Geometry(64, 16, 32);
 
-	/**
-	 * @see org.jnode.build.BootFloppyBuilder#getDeviceLength()
-	 * @return The device length
-	 */
-	protected long getDeviceLength() {
-		return geom.getTotalSectors() * bytesPerSector;
-	}
+    private MappedBlockDeviceSupport part0;
 
-	/**
-	 * @return File
-	 */
-	public File getPlnFile() {
-		return plnFile;
-	}
+    public BootDiskBuilder() {
+    }
 
-	/**
-	 * Sets the plnFile.
-	 * @param plnFile The plnFile to set
-	 */
-	public void setPlnFile(File plnFile) {
-		this.plnFile = plnFile;
-	}
+    /**
+     * Create the actual bootfloppy
+     * 
+     * @throws IOException
+     * @throws DriverException
+     * @throws FileSystemException
+     */
+    public void createImage() throws IOException, DriverException,
+            FileSystemException {
+        super.createImage();
 
-	/**
-	 * @param rootDevice
-	 * @see org.jnode.build.BootFloppyBuilder#getSystemDevice(Device)
-	 * @return The device
-	 */
-	protected Device getSystemDevice(Device rootDevice) {
-		return part0;
-	}
+        FileWriter fw = new FileWriter(plnFile);
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println("DRIVETYPE ide");
+        pw.println("CYLINDERS " + geom.getCylinders());
+        pw.println("HEADS     " + geom.getHeads());
+        pw.println("SECTORS   " + geom.getSectors());
+        pw.println("CAPACITY  " + geom.getTotalSectors());
+        pw.println("ACCESS    \"" + getDestFile().getCanonicalPath()
+                + "\" 0 102400");
+        pw.flush();
+        fw.flush();
+        pw.close();
+        fw.close();
+        System.out.println("Wrote " + plnFile);
+    }
+
+    /**
+     * Format the given device
+     * 
+     * @param device
+     * @throws IOException
+     */
+    protected void formatDevice(Device device) throws IOException {
+
+        /* Format the MBR & partitiontable */
+        GrubBootSector mbr = (GrubBootSector) (createFormatter()
+                .getBootSector());
+
+        mbr.getPartition(0).clear();
+        mbr.getPartition(1).clear();
+        mbr.getPartition(2).clear();
+        mbr.getPartition(3).clear();
+
+        IBMPartitionTableEntry pte = mbr.getPartition(0);
+        pte.setBootIndicator(true);
+        pte.setStartLba(1);
+        pte.setNrSectors(geom.getTotalSectors() - 1);
+        pte.setSystemIndicator(IBMPartitionTypes.PARTTYPE_DOS_FAT16_LT32M);
+        pte.setStartCHS(geom.getCHS(pte.getStartLba()));
+        pte.setEndCHS(geom.getCHS(pte.getStartLba() + pte.getNrSectors() - 1));
+
+        /*
+         * System.out.println("partition table:"); for (int i = 0; i < 4; i++) {
+         * System.out.println("" + i + " " + mbr.getPartition(i)); }
+         */
+
+        /* Format partition 0 */
+        part0 = new MappedBlockDeviceSupport(device, pte.getStartLba()
+                * bytesPerSector, pte.getNrSectors() * bytesPerSector);
+        GrubFatFormatter ff = createFormatter();
+        ff.setInstallPartition(0x0000FFFF);
+        ff.format(part0);
+        GrubBootSector part0bs = (GrubBootSector) ff.getBootSector();
+
+        /* Fixup stage2 sector in MBR */
+        mbr.setStage2Sector(pte.getStartLba() + part0bs.getStage2Sector());
+        try {
+            mbr.write((BlockDeviceAPI) device.getAPI(BlockDeviceAPI.class));
+        } catch (ApiNotFoundException ex) {
+            throw new IOException("BlockDeviceAPI not found on device", ex);
+        }
+        //System.out.println("mbr stage2 sector=" + mbr.getStage2Sector());
+    }
+
+    /**
+     * @see org.jnode.build.BootFloppyBuilder#createFormatter()
+     * @return The formatter
+     * @throws IOException
+     */
+    protected GrubFatFormatter createFormatter() throws IOException {
+        return new GrubFatFormatter(bytesPerSector, spc, geom, Fat.FAT16, 1,
+                getStage1ResourceName(), getStage2ResourceName());
+    }
+
+    /**
+     * @see org.jnode.build.BootFloppyBuilder#getDeviceLength()
+     * @return The device length
+     */
+    protected long getDeviceLength() {
+        return geom.getTotalSectors() * bytesPerSector;
+    }
+
+    /**
+     * @return File
+     */
+    public File getPlnFile() {
+        return plnFile;
+    }
+
+    /**
+     * Sets the plnFile.
+     * 
+     * @param plnFile
+     *            The plnFile to set
+     */
+    public void setPlnFile(File plnFile) {
+        this.plnFile = plnFile;
+    }
+
+    /**
+     * @param rootDevice
+     * @see org.jnode.build.BootFloppyBuilder#getSystemDevice(Device)
+     * @return The device
+     */
+    protected Device getSystemDevice(Device rootDevice) {
+        return part0;
+    }
+
+    /**
+     * Used by ant to set the geometry property.
+     * 
+     * @param geometryString String in the format ' <cylinder>/ <heads>/ <sectors>', e.ög.
+     *            '64/16/32'.
+     */
+    public void setGeometry(String geometryString) {
+        try {
+            log("Setting bootdisk geometry to " + geometryString, Project.MSG_VERBOSE);
+            StringTokenizer tokenizer = new StringTokenizer(geometryString, "/");
+            geom = new Geometry(Integer.parseInt(tokenizer.nextToken()),
+                    Integer.parseInt(tokenizer.nextToken()), Integer
+                            .parseInt(tokenizer.nextToken()));
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    "Invalid geometry "
+                            + geometryString
+                            + ". Must correspond to pattern '<cylinders>/<heads>/<sectors>' e.g. '64/16/32'.");
+        }
+    }
 }
