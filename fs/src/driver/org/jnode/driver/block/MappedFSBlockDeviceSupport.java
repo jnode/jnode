af3e25629c7bca20276f004befd3b45af9fa6102
Ewout Prangsma <ewout@prangsma.net>
2004-03-30 17:25:47 +0000

    Fixed build problem
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@367 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/driver/org/jnode/driver/block/MappedFSBlockDeviceSupport.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ */
+package org.jnode.driver.block;
+
+import java.io.IOException;
+
+import org.jnode.driver.ApiNotFoundException;
+import org.jnode.driver.Device;
+import org.jnode.fs.partitions.PartitionTableEntry;
+
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public class MappedFSBlockDeviceSupport extends MappedBlockDeviceSupport implements FSBlockDeviceAPI {
+
+    private final FSBlockDeviceAPI parentApi;
+    
+    /**
+     * @param parent
+     * @param offset
+     * @param length
+     * @throws IOException
+     */
+    public MappedFSBlockDeviceSupport(Device parent, long offset, long length)
+            throws IOException {
+        super(parent, offset, length);
+        try {
+            this.parentApi = (FSBlockDeviceAPI) parent.getAPI(FSBlockDeviceAPI.class);
+        } catch (ApiNotFoundException ex) {
+            throw new IOException(ex);
+        }
+        registerAPI(FSBlockDeviceAPI.class, this);
+    }
+    /**
+     * @see org.jnode.driver.block.FSBlockDeviceAPI#getPartitionTableEntry()
+     */
+    public PartitionTableEntry getPartitionTableEntry() {
+        return parentApi.getPartitionTableEntry();
+    }
+    
+    /**
+     * @see org.jnode.driver.block.FSBlockDeviceAPI#getSectorSize()
+     */
+    public int getSectorSize() throws IOException {
+        return parentApi.getSectorSize();
+    }
+}
