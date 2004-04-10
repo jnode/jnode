99a2569768c06796a8cc49238e8840fc53fd25da
Levente Santha <lsantha@jnode.org>
2004-04-10 18:00:42 +0000

    Implementing code generation for ConditionalBranchQuad.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@405 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/compiler/ir/quad/ConditionalBranchQuad.java
+++ core/src/core/org/jnode/vm/compiler/ir/quad/ConditionalBranchQuad.java
@@ -8,143 +8,286 @@ import org.jnode.vm.compiler.ir.CodeGenerator;
 import org.jnode.vm.compiler.ir.IRBasicBlock;
 import org.jnode.vm.compiler.ir.Operand;
 import org.jnode.vm.compiler.ir.Variable;
+import org.jnode.vm.compiler.ir.Location;
+import org.jnode.vm.compiler.ir.RegisterLocation;
+import org.jnode.vm.compiler.ir.StackLocation;
+import org.jnode.vm.compiler.ir.Constant;
 
 /**
  * @author Madhu Siddalingaiah
- * 
+ * @author Levente Sántha
  */
 public class ConditionalBranchQuad extends BranchQuad {
-	private final static String[] CONDITION_MAP = {
-		"== 0", "!= 0", "< 0", ">= 0", "> 0", "<= 0",
-		"!= null", "== null",
-		"==",   "!=",   "<",   ">=",   ">",   "<=",
-		"=",   "!="
-	};
+    private final static String[] CONDITION_MAP = {
+        "== 0", "!= 0", "< 0", ">= 0", "> 0", "<= 0",
+        "!= null", "== null",
+        "==", "!=", "<", ">=", ">", "<=",
+        "=", "!="
+    };
 
-	public final static int IFEQ = 0;
-	public final static int IFNE = 1;
-	public final static int IFLT = 2;
-	public final static int IFGE = 3;
-	public final static int IFGT = 4;
-	public final static int IFLE = 5;
+    public final static int IFEQ = 0;
+    public final static int IFNE = 1;
+    public final static int IFLT = 2;
+    public final static int IFGE = 3;
+    public final static int IFGT = 4;
+    public final static int IFLE = 5;
 
-	public final static int IFNONNULL = 6;
-	public final static int IFNULL = 7;
+    public final static int IFNONNULL = 6;
+    public final static int IFNULL = 7;
 
-	public final static int IF_ICMPEQ = 8;
-	public final static int IF_ICMPNE = 9;
-	public final static int IF_ICMPLT = 10;
-	public final static int IF_ICMPGE = 11;
-	public final static int IF_ICMPGT = 12;
-	public final static int IF_ICMPLE = 13;
+    public final static int IF_ICMPEQ = 8;
+    public final static int IF_ICMPNE = 9;
+    public final static int IF_ICMPLT = 10;
+    public final static int IF_ICMPGE = 11;
+    public final static int IF_ICMPGT = 12;
+    public final static int IF_ICMPLE = 13;
 
-	public final static int IF_ACMPEQ = 14;
-	public final static int IF_ACMPNE = 15;
+    public final static int IF_ACMPEQ = 14;
+    public final static int IF_ACMPNE = 15;
 
-	private int condition;
-	private Operand[] refs;
-	private Operand operand2;
-	private Operand operand1;
+    private int condition;
+    private boolean commutative;
+    private Operand[] refs;
+    private Operand operand2;
+    private Operand operand1;
+    private static final int MODE_CC = (Operand.MODE_CONSTANT << 8) | Operand.MODE_CONSTANT;
+    private static final int MODE_CR = (Operand.MODE_CONSTANT << 8) | Operand.MODE_REGISTER;
+    private static final int MODE_CS = (Operand.MODE_CONSTANT << 8) | Operand.MODE_STACK;
+    private static final int MODE_RC = (Operand.MODE_REGISTER << 8) | Operand.MODE_CONSTANT;
+    private static final int MODE_RR = (Operand.MODE_REGISTER << 8) | Operand.MODE_REGISTER;
+    private static final int MODE_RS = (Operand.MODE_REGISTER << 8) | Operand.MODE_STACK;
+    private static final int MODE_SC = (Operand.MODE_STACK << 8) | Operand.MODE_CONSTANT;
+    private static final int MODE_SR = (Operand.MODE_STACK << 8) | Operand.MODE_REGISTER;
+    private static final int MODE_SS = (Operand.MODE_STACK << 8) | Operand.MODE_STACK;
 
-	/**
-	 * @param address
-	 * @param targetAddress
-	 */
-	public ConditionalBranchQuad(int address, IRBasicBlock block,
-		int varIndex1, int condition, int varIndex2, int targetAddress) {
+    /**
+     * @param address
+     * @param targetAddress
+     */
+    public ConditionalBranchQuad(int address, IRBasicBlock block,
+                                 int varIndex1, int condition, int varIndex2, int targetAddress) {
 
-		super(address, block, targetAddress);
-		if (condition < IF_ICMPEQ || condition > IF_ACMPNE) {
-			throw new IllegalArgumentException("can't use that condition here");
-		}
-		this.operand1 = getOperand(varIndex1);
-		this.condition = condition;
-		this.operand2 = getOperand(varIndex2);
-		refs = new Operand[] { operand1, operand2 };
-	}
+        super(address, block, targetAddress);
+        if (condition < IF_ICMPEQ || condition > IF_ACMPNE) {
+            throw new IllegalArgumentException("can't use that condition here");
+        }
+        this.operand1 = getOperand(varIndex1);
+        this.condition = condition;
+        this.commutative = condition == IF_ICMPEQ ||
+                           condition == IF_ICMPNE ||
+                           condition == IF_ACMPEQ ||
+                           condition == IF_ACMPNE;
+        this.operand2 = getOperand(varIndex2);
+        refs = new Operand[]{operand1, operand2};
+    }
 
-	public ConditionalBranchQuad(int address, IRBasicBlock block,
-		int varIndex, int condition, int targetAddress) {
+    public ConditionalBranchQuad(int address, IRBasicBlock block,
+                                 int varIndex, int condition, int targetAddress) {
 
-		super(address, block, targetAddress);
-		if (condition < IFEQ || condition > IFNULL) {
-			throw new IllegalArgumentException("can't use that condition here");
-		}
-		this.operand1 = getOperand(varIndex);
-		this.condition = condition;
-		refs = new Operand[] { operand1 };
-	}
+        super(address, block, targetAddress);
+        if (condition < IFEQ || condition > IFNULL) {
+            throw new IllegalArgumentException("can't use that condition here");
+        }
+        this.operand1 = getOperand(varIndex);
+        this.condition = condition;
+        this.commutative = condition == IF_ICMPEQ ||
+                           condition == IF_ICMPNE ||
+                           condition == IF_ACMPEQ ||
+                           condition == IF_ACMPNE;
+        refs = new Operand[]{operand1};
+    }
 
-	/**
-	 * @see org.jnode.vm.compiler.ir.quad.Quad#getDefinedOp()
-	 */
-	public Operand getDefinedOp() {
-		return null;
-	}
+    /**
+     * @see org.jnode.vm.compiler.ir.quad.Quad#getDefinedOp()
+     */
+    public Operand getDefinedOp() {
+        return null;
+    }
 
-	/**
-	 * @see org.jnode.vm.compiler.ir.quad.Quad#getReferencedOps()
-	 */
-	public Operand[] getReferencedOps() {
-		return refs;
-	}
+    /**
+     * @see org.jnode.vm.compiler.ir.quad.Quad#getReferencedOps()
+     */
+    public Operand[] getReferencedOps() {
+        return refs;
+    }
 
-	/**
-	 * @return
-	 */
-	public Operand getOperand1() {
-		return operand1;
-	}
+    /**
+     * @return
+     */
+    public Operand getOperand1() {
+        return operand1;
+    }
 
-	/**
-	 * @return
-	 */
-	public Operand getOperand2() {
-		return operand2;
-	}
+    /**
+     * @return
+     */
+    public Operand getOperand2() {
+        return operand2;
+    }
 
-	/**
-	 * @return
-	 */
-	public int getCondition() {
-		return condition;
-	}
+    /**
+     * @return
+     */
+    public int getCondition() {
+        return condition;
+    }
 
-	public String toString() {
-		if (condition >= IF_ICMPEQ) {
-			return getAddress() + ": if " + operand1.toString() + " " +
-				CONDITION_MAP[condition] + " " + operand2.toString() +
-				" goto " + getTargetAddress();
-		} else {
-			return getAddress() + ": if " + operand1.toString() + " " +
-				CONDITION_MAP[condition] + " goto " + getTargetAddress();
-		}
-	}
+    public String toString() {
+        if (condition >= IF_ICMPEQ) {
+            return getAddress() + ": if " + operand1.toString() + " " +
+                    CONDITION_MAP[condition] + " " + operand2.toString() +
+                    " goto " + getTargetAddress();
+        } else {
+            return getAddress() + ": if " + operand1.toString() + " " +
+                    CONDITION_MAP[condition] + " goto " + getTargetAddress();
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.jnode.vm.compiler.ir.Quad#doPass2(org.jnode.util.BootableHashMap)
-	 */
-	public void doPass2(BootableHashMap liveVariables) {
-		operand1 = operand1.simplify();
-		if (operand1 instanceof Variable) {
-			Variable v = (Variable) operand1;
-			v.setLastUseAddress(this.getAddress());
-			liveVariables.put(v, v);
-		}
-		if (operand2 != null) {
-			operand2 = operand2.simplify();
-			if (operand2 instanceof Variable) {
-				Variable v = (Variable) operand2;
-				v.setLastUseAddress(this.getAddress());
-				liveVariables.put(v, v);
-			}
-		}
-	}
+    /* (non-Javadoc)
+     * @see org.jnode.vm.compiler.ir.Quad#doPass2(org.jnode.util.BootableHashMap)
+     */
+    public void doPass2(BootableHashMap liveVariables) {
+        operand1 = operand1.simplify();
+        if (operand1 instanceof Variable) {
+            Variable v = (Variable) operand1;
+            v.setLastUseAddress(this.getAddress());
+            liveVariables.put(v, v);
+        }
+        if (operand2 != null) {
+            operand2 = operand2.simplify();
+            if (operand2 instanceof Variable) {
+                Variable v = (Variable) operand2;
+                v.setLastUseAddress(this.getAddress());
+                liveVariables.put(v, v);
+            }
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.jnode.vm.compiler.ir.Quad#generateCode(org.jnode.vm.compiler.ir.CodeGenerator)
-	 */
-	public void generateCode(CodeGenerator cg) {
-		cg.generateCodeFor(this);
-	}
+    /* (non-Javadoc)
+     * @see org.jnode.vm.compiler.ir.Quad#generateCode(org.jnode.vm.compiler.ir.CodeGenerator)
+     */
+    public void generateCode(CodeGenerator cg) {
+        //cg.generateCodeFor(this);
+        if (condition >= IF_ICMPEQ)
+            generateCodeForBinary(cg);
+        else
+            generateCodeForUnary(cg);
+    }
+
+
+    public void generateCodeForUnary(CodeGenerator cg) {
+        if (operand1 instanceof Variable) {
+            Location varLoc = ((Variable) operand1).getLocation();
+            if (varLoc instanceof RegisterLocation) {
+                RegisterLocation vregLoc = (RegisterLocation) varLoc;
+                cg.generateCodeFor(this, condition, vregLoc.getRegister());
+            } else if (varLoc instanceof StackLocation) {
+                StackLocation stackLoc = (StackLocation) varLoc;
+                cg.generateCodeFor(this, condition, stackLoc.getDisplacement());
+            } else {
+                throw new IllegalArgumentException("Unknown location: " + varLoc);
+            }
+        } else if (operand1 instanceof Constant) {
+            // this probably won't happen, is should be folded earlier
+            Constant con = (Constant) operand1;
+            cg.generateCodeFor(this, condition, con);
+        } else {
+            throw new IllegalArgumentException("Unknown operand: " + operand1);
+        }
+    }
+
+    /**
+     * Code generation is complicated by the permutations of addressing modes.
+     * This is not as nice as it could be, but it could be worse!
+     *
+     * @see org.jnode.vm.compiler.ir.quad.Quad#generateCode(org.jnode.vm.compiler.ir.CodeGenerator)
+     */
+    public void generateCodeForBinary(CodeGenerator cg) {
+        cg.checkLabel(getAddress());
+        int op1Mode = operand1.getAddressingMode();
+        int op2Mode = operand2.getAddressingMode();
+
+        Object reg2 = null;
+        if (op1Mode == Operand.MODE_REGISTER) {
+            Variable var = (Variable) operand1;
+            RegisterLocation regLoc = (RegisterLocation) var.getLocation();
+            reg2 = regLoc.getRegister();
+        }
+
+        Object reg3 = null;
+        if (op2Mode == Operand.MODE_REGISTER) {
+            Variable var = (Variable) operand2;
+            RegisterLocation regLoc = (RegisterLocation) var.getLocation();
+            reg3 = regLoc.getRegister();
+        }
+
+        int disp2 = 0;
+        if (op1Mode == Operand.MODE_STACK) {
+            Variable var = (Variable) operand1;
+            StackLocation stackLoc = (StackLocation) var.getLocation();
+            disp2 = stackLoc.getDisplacement();
+        }
+
+        int disp3 = 0;
+        if (op2Mode == Operand.MODE_STACK) {
+            Variable var = (Variable) operand2;
+            StackLocation stackLoc = (StackLocation) var.getLocation();
+            disp3 = stackLoc.getDisplacement();
+        }
+
+        Constant c2 = null;
+        if (op1Mode == Operand.MODE_CONSTANT) {
+            c2 = (Constant) operand1;
+        }
+
+        Constant c3 = null;
+        if (op2Mode == Operand.MODE_CONSTANT) {
+            c3 = (Constant) operand2;
+        }
+
+        int aMode = (op1Mode << 8) | op2Mode;
+        switch (aMode) {
+            case MODE_CC:
+                cg.generateCodeFor(this, c2, condition, c3);
+                break;
+            case MODE_CR:
+                if (commutative && !cg.supports3AddrOps()) {
+                    cg.generateCodeFor(this, reg3, condition, c2);
+                } else {
+                    cg.generateCodeFor(this, c2, condition, reg3);
+                }
+                break;
+            case MODE_CS:
+                cg.generateCodeFor(this, c2, condition, disp3);
+                break;
+            case MODE_RC:
+                cg.generateCodeFor(this, reg2, condition, c3);
+                break;
+            case MODE_RR:
+                if (commutative && !cg.supports3AddrOps()) {
+                    cg.generateCodeFor(this, reg3, condition, reg2);
+                } else {
+                    cg.generateCodeFor(this, reg2, condition, reg3);
+                }
+                break;
+            case MODE_RS:
+                cg.generateCodeFor(this, reg2, condition, disp3);
+                break;
+            case MODE_SC:
+                cg.generateCodeFor(this, disp2, condition, c3);
+                break;
+            case MODE_SR:
+                if (commutative && !cg.supports3AddrOps()) {
+                    cg.generateCodeFor(this, reg3, condition, disp2);
+                } else {
+                    cg.generateCodeFor(this, disp2, condition, reg3);
+                }
+                break;
+            case MODE_SS:
+                cg.generateCodeFor(this, disp2, condition, disp3);
+                break;
+            default:
+                throw new IllegalArgumentException("Undefined addressing mode: " + aMode);
+        }
+    }
 }
