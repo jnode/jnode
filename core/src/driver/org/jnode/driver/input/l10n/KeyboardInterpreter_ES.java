5999d95da0da89b7d25129d248bdb2dd94837a8e
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-04-17 20:26:43 +0000

    New structure to the key layout. Broken, but please impl. this, maybe look in the danish/french versions.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@461 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/driver/org/jnode/driver/input/l10n/KeyboardInterpreter_ES.java
+++ core/src/driver/org/jnode/driver/input/l10n/KeyboardInterpreter_ES.java
@@ -3,33 +3,28 @@
  */
 package org.jnode.driver.input.l10n;
 
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-
-import org.jnode.driver.input.DeadKeyException;
 import org.jnode.driver.input.KeyboardInterpreter;
-import org.jnode.driver.input.UnsupportedKeyException;
+import org.jnode.driver.input.Keys;
 
 /*
  * @author sauron
  */
-public class KeyboardInterpreter_ES extends KeyboardInterpreter {
 
-    protected int lastDeadVK = -1;
+public class KeyboardInterpreter_ES extends KeyboardInterpreter
+{
 
-    protected int lastFlags = -1;
+//    protected int lastDeadVK = -1;
 
-    public KeyboardInterpreter_ES() {
-        super();
-    }
+//    protected int lastFlags = -1;
 
+/*
     protected char interpretExtendedScanCode(int scancode, int vk,
             boolean released) throws UnsupportedKeyException, DeadKeyException {
         // TODO
         //throw new UnsupportedKeyException();
         boolean deadKey = false;
 
-        switch (vk) {
+       switch (vk) {
         case KeyEvent.VK_DEAD_CIRCUMFLEX:
             lastDeadVK = KeyEvent.VK_DEAD_CIRCUMFLEX;
             lastFlags = getFlags();
@@ -164,8 +159,15 @@ public class KeyboardInterpreter_ES extends KeyboardInterpreter {
         }
 
     }
+*/
+  protected void initKeys(Keys keys)
+  {
+    // todo look at the danish/french version for hits to implement this
+  }
 
-    protected void initVkMap(int[] vkMap, char[] lcharMap, char[] ucharMap,
+
+/*
+  protected void initVkMap(int[] vkMap, char[] lcharMap, char[] ucharMap,
             char[] altGrCharMap) {
         vkMap[ 0] = KeyEvent.VK_UNDEFINED;
         vkMap[ 1] = KeyEvent.VK_ESCAPE;
@@ -422,6 +424,6 @@ public class KeyboardInterpreter_ES extends KeyboardInterpreter {
         altGrCharMap[ 41] = '\\';
         altGrCharMap[ 43] = '}';
     }
-
+*/
 }
 
