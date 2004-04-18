7892f2822e228519096d881a10cc106073e5179b
blindidiot <blindidiot@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-04-18 22:34:01 +0000

    Fixed a bug in set32()
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@477 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/util/DosUtils.java
+++ fs/src/fs/org/jnode/fs/util/DosUtils.java
@@ -70,7 +70,7 @@ public class DosUtils {
 		data[offset] = (byte)(value & 0xFF);
 		data[offset+1] = (byte)((value >> 8) & 0xFF);
 		data[offset+2] = (byte)((value >> 16) & 0xFF);
-		data[offset+3] = (byte)((value >> 32) & 0xFF);
+		data[offset+3] = (byte)((value >> 24) & 0xFF);
 	}
 	
 	/**
