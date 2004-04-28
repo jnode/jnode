e27dbcce5c18532bb7e2ab862b1b582ad8646324
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-04-28 16:10:12 +0000

    implementation of a table of entries whose name are case insensitive
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@515 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/fs/org/jnode/fs/FSEntryTableIgnoreCase.java
@@ -0,0 +1,35 @@
+/**
+ * $Id$
+ */
+package org.jnode.fs;
+
+import java.util.List;
+
+import org.jnode.fs.spi.AbstractFileSystem;
+
+/**
+ * A child class of FSEntryTable that ignore the case of entry names.
+ *  
+ * @author Fabien DUMINY
+ */
+public class FSEntryTableIgnoreCase extends FSEntryTable {
+	/**
+	 * Construct a FSEntryTableIgnoreCase from a list of FSEntry 
+	 * @param fs
+	 * @param entryList
+	 */
+	public FSEntryTableIgnoreCase(AbstractFileSystem fs, List/*<FSEntry>*/ entryList) {
+		super(fs, entryList);
+	}
+	
+	/**
+	 * To Ignore case, we convert all entry names to upper case
+	 */
+	protected String normalizeName(String name)
+	{
+		if(name == null)
+			return null;
+		
+		return name.toUpperCase();
+	}
+}
