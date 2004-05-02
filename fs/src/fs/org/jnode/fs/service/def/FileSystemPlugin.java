6153faaeac577f4afd87680f2abad21efa0ad314
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-05-02 07:19:29 +0000

    Unregister filesystems in its own security context
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@543 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/service/def/FileSystemPlugin.java
+++ fs/src/fs/org/jnode/fs/service/def/FileSystemPlugin.java
@@ -5,6 +5,8 @@ package org.jnode.fs.service.def;
 
 import java.io.VMFileSystemAPI;
 import java.io.VMIOUtils;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.Collection;
 
 import javax.naming.NamingException;
@@ -24,110 +26,121 @@ import org.jnode.plugin.PluginException;
  */
 public class FileSystemPlugin extends Plugin implements FileSystemService {
 
-	/** Manager of fs types */
-	private final FileSystemTypeManager fsTypeManager;
-	/** Manager of mounted filesystems */
-	private final FileSystemManager fsm;
-	/** The FS-API implementation */
-	private final VMFileSystemAPI api;
-	/** The mounter */
-	private FileSystemMounter mounter;
+    /** Manager of fs types */
+    private final FileSystemTypeManager fsTypeManager;
 
-	/**
-	 * Create a new instance
-	 *  
-	 */
-	public FileSystemPlugin(PluginDescriptor descriptor) {
-		super(descriptor);
-		this.fsTypeManager = new FileSystemTypeManager(descriptor.getExtensionPoint("types"));
-		this.fsm = new FileSystemManager();
-		this.api = new FileSystemAPIImpl(fsm);
-	}
+    /** Manager of mounted filesystems */
+    private final FileSystemManager fsm;
 
-	/**
-	 * Gets all registered file system types. All instances of the returned
-	 * collection are instanceof FileSystemType.
-	 */
-	public Collection fileSystemTypes() {
-		return fsTypeManager.fileSystemTypes();
-	}
+    /** The FS-API implementation */
+    private final VMFileSystemAPI api;
 
-	/**
-	 * Register a mounted filesystem
-	 * 
-	 * @param fs
-	 */
-	public void registerFileSystem(FileSystem fs) {
-		fsm.registerFileSystem(fs);
-	}
+    /** The mounter */
+    private FileSystemMounter mounter;
 
-	/**
-	 * Unregister a mounted filesystem
-	 * 
-	 * @param device
-	 */
-	public FileSystem unregisterFileSystem(Device device) {
-		api.rootRemoved(new java.io.File(fsm.getMountPoint(device)));
-		return fsm.unregisterFileSystem(device);
-	}
+    /**
+     * Create a new instance
+     *  
+     */
+    public FileSystemPlugin(PluginDescriptor descriptor) {
+        super(descriptor);
+        this.fsTypeManager = new FileSystemTypeManager(descriptor
+                .getExtensionPoint("types"));
+        this.fsm = new FileSystemManager();
+        this.api = new FileSystemAPIImpl(fsm);
+    }
 
-	/**
-	 * Gets the filesystem registered on the given device.
-	 * 
-	 * @param device
-	 * @return null if no filesystem was found.
-	 */
-	public FileSystem getFileSystem(Device device) {
-		return fsm.getFileSystem(device);
-	}
+    /**
+     * Gets all registered file system types. All instances of the returned
+     * collection are instanceof FileSystemType.
+     */
+    public Collection fileSystemTypes() {
+        return fsTypeManager.fileSystemTypes();
+    }
 
-	/**
-	 * Gets all registered filesystems. All instances of the returned collection
-	 * are instanceof FileSystem.
-	 */
-	public Collection fileSystems() {
-		return fsm.fileSystems();
-	}
+    /**
+     * Register a mounted filesystem
+     * 
+     * @param fs
+     */
+    public void registerFileSystem(FileSystem fs) {
+        fsm.registerFileSystem(fs);
+    }
 
-	/**
-	 * Gets the filesystem API.
-	 */
-	public VMFileSystemAPI getApi() {
-		return api;
-	}
+    /**
+     * Unregister a mounted filesystem
+     * 
+     * @param device
+     */
+    public FileSystem unregisterFileSystem(final Device device) {
+        return (FileSystem) AccessController
+                .doPrivileged(new PrivilegedAction() {
 
-	/**
-	 * Start this plugin
-	 */
-	protected void startPlugin() throws PluginException {
-		try {
-			VMIOUtils.setAPI(getApi(), this);
-			mounter = new FileSystemMounter();
-			InitialNaming.bind(NAME, this);
-			mounter.start();
-		} catch (NamingException ex) {
-			throw new PluginException(ex);
-		}
-	}
+                    public Object run() {
+                        api.rootRemoved(new java.io.File(fsm
+                                .getMountPoint(device)));
+                        return fsm.unregisterFileSystem(device);
+                    }
+                });
+    }
 
-	/**
-	 * Stop this plugin
-	 */
-	protected void stopPlugin() {
-		mounter.stop();
-		InitialNaming.unbind(NAME);
-		VMIOUtils.resetAPI(this);
-		mounter = null;
-	}
+    /**
+     * Gets the filesystem registered on the given device.
+     * 
+     * @param device
+     * @return null if no filesystem was found.
+     */
+    public FileSystem getFileSystem(Device device) {
+        return fsm.getFileSystem(device);
+    }
 
-	/**
-	 * @see org.jnode.fs.service.FileSystemService#getFileSystemTypeForNameSystemTypes(java.lang.String)
-	 */
-	public FileSystemType getFileSystemTypeForNameSystemTypes(String name) throws FileSystemException {
-		FileSystemType result = fsTypeManager.getSystemType(name);
-		if (result == null) {
-			throw new FileSystemException("Not existent FisleSystemType");
-		}
-		return result;
-	}
+    /**
+     * Gets all registered filesystems. All instances of the returned collection
+     * are instanceof FileSystem.
+     */
+    public Collection fileSystems() {
+        return fsm.fileSystems();
+    }
+
+    /**
+     * Gets the filesystem API.
+     */
+    public VMFileSystemAPI getApi() {
+        return api;
+    }
+
+    /**
+     * Start this plugin
+     */
+    protected void startPlugin() throws PluginException {
+        try {
+            VMIOUtils.setAPI(getApi(), this);
+            mounter = new FileSystemMounter();
+            InitialNaming.bind(NAME, this);
+            mounter.start();
+        } catch (NamingException ex) {
+            throw new PluginException(ex);
+        }
+    }
+
+    /**
+     * Stop this plugin
+     */
+    protected void stopPlugin() {
+        mounter.stop();
+        InitialNaming.unbind(NAME);
+        VMIOUtils.resetAPI(this);
+        mounter = null;
+    }
+
+    /**
+     * @see org.jnode.fs.service.FileSystemService#getFileSystemTypeForNameSystemTypes(java.lang.String)
+     */
+    public FileSystemType getFileSystemTypeForNameSystemTypes(String name)
+            throws FileSystemException {
+        FileSystemType result = fsTypeManager.getSystemType(name);
+        if (result == null) { throw new FileSystemException(
+                "Not existent FisleSystemType"); }
+        return result;
+    }
 }
