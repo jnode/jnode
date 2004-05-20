15e8678ffc94b4afdd330f8ce3fcbd6b9d1f7666
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-05-20 21:06:40 +0000

    check to see if it's for one self, if so don't send on the net and just put in into recieved
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@595 407389f7-7c16-0410-9eee-ad85acb55d7f

--- net/src/driver/org/jnode/driver/net/ethernet/spi/AbstractEthernetDriver.java
+++ net/src/driver/org/jnode/driver/net/ethernet/spi/AbstractEthernetDriver.java
@@ -15,55 +15,68 @@ import org.jnode.net.ethernet.EthernetUtils;
 /**
  * @author epr
  */
-public abstract class AbstractEthernetDriver extends AbstractNetDriver implements EthernetConstants {
+public abstract class AbstractEthernetDriver extends AbstractNetDriver implements EthernetConstants
+{
 
-	/**
-	 * Gets the maximum transfer unit, the number of bytes this device can
-	 * transmit at a time.
-	 */
-	public int getMTU() {
-		return ETH_DATA_LEN;
-	}
+  /**
+   * Gets the maximum transfer unit, the number of bytes this device can
+   * transmit at a time.
+   */
+  public int getMTU()
+  {
+    return ETH_DATA_LEN;
+  }
 
-	/**
-	 * @see org.jnode.driver.net.spi.AbstractNetDriver#getDevicePrefix()
-	 */
-	protected final String getDevicePrefix() {
-		return ETH_DEVICE_PREFIX;
-	}
+  /**
+   * @see org.jnode.driver.net.spi.AbstractNetDriver#getDevicePrefix()
+   */
+  protected final String getDevicePrefix()
+  {
+    return ETH_DEVICE_PREFIX;
+  }
 
-	/**
-	 * @see org.jnode.driver.net.spi.AbstractNetDriver#onReceive(org.jnode.net.SocketBuffer)
-	 */
-	public void onReceive(SocketBuffer skbuf) throws NetworkException {
-		// Extract ethernet header
-		final EthernetHeader hdr = new EthernetHeader(skbuf); 
-		skbuf.setLinkLayerHeader(hdr);
-		skbuf.setProtocolID(EthernetUtils.getProtocol(hdr));
-		skbuf.pull(hdr.getLength());
-		// Send to PM
-		super.onReceive(skbuf);
-	}
-	
-	/**
-	 * @see org.jnode.driver.net.spi.AbstractNetDriver#doTransmit(org.jnode.net.SocketBuffer, org.jnode.net.HardwareAddress)
-	 */
-	protected final void doTransmit(SocketBuffer skbuf, HardwareAddress destination)
-	throws NetworkException {
-		skbuf.insert(ETH_HLEN);
-		if (destination != null) {
-			destination.writeTo(skbuf, 0);
-		} else {
-			EthernetAddress.BROADCAST.writeTo(skbuf, 0);
-		}
-		getAddress().writeTo(skbuf, 6);
-		skbuf.set16(12, skbuf.getProtocolID());
-		doTransmitEthernet(skbuf);
-	}
+  /**
+   * @see org.jnode.driver.net.spi.AbstractNetDriver#onReceive(org.jnode.net.SocketBuffer)
+   */
+  public void onReceive(SocketBuffer skbuf) throws NetworkException
+  {
+    // Extract ethernet header
+    final EthernetHeader hdr = new EthernetHeader(skbuf);
+    skbuf.setLinkLayerHeader(hdr);
+    skbuf.setProtocolID(EthernetUtils.getProtocol(hdr));
+    skbuf.pull(hdr.getLength());
+    // Send to PM
+    super.onReceive(skbuf);
+  }
 
-	/**
-	 * @see org.jnode.driver.net.spi.AbstractNetDriver#doTransmit(org.jnode.net.SocketBuffer, org.jnode.net.HardwareAddress)
-	 */
-	protected abstract void doTransmitEthernet(SocketBuffer skbuf)
-	throws NetworkException;
+  /**
+   * @see org.jnode.driver.net.spi.AbstractNetDriver#doTransmit(org.jnode.net.SocketBuffer, org.jnode.net.HardwareAddress)
+   */
+  protected final void doTransmit(SocketBuffer skbuf, HardwareAddress destination)
+      throws NetworkException
+  {
+    skbuf.insert(ETH_HLEN);
+    if (destination != null)
+    {
+      destination.writeTo(skbuf, 0);
+    }
+    else
+    {
+      EthernetAddress.BROADCAST.writeTo(skbuf, 0);
+    }
+    getAddress().writeTo(skbuf, 6);
+    skbuf.set16(12, skbuf.getProtocolID());
+
+// check to see if it's for one self, if so don't send on the net and just put in into recieved
+    if (getAddress().equals(destination))
+      onReceive(skbuf);
+    else
+      doTransmitEthernet(skbuf);
+  }
+
+  /**
+   * @see org.jnode.driver.net.spi.AbstractNetDriver#doTransmit(org.jnode.net.SocketBuffer, org.jnode.net.HardwareAddress)
+   */
+  protected abstract void doTransmitEthernet(SocketBuffer skbuf)
+      throws NetworkException;
 }
