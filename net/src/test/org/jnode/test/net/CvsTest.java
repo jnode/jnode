3e4853707a6a46ebfccbb4a6e61e124be65544bc
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-05-25 14:50:22 +0000

    added javax.crypto,javax.security and gnu.crypto
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@604 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ net/src/test/org/jnode/test/net/CvsTest.java
@@ -0,0 +1,22 @@
+package org.jnode.test.net;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mh
+ * Date: 23-05-2004
+ * Time: 21:45:15
+ * To change this template use File | Settings | File Templates.
+ */
+
+import javax.crypto.*;
+
+public class CvsTest
+{
+
+  public static void main(String[] args)
+  {
+    NullCipher nullCipher = new NullCipher();
+
+    System.out.println(nullCipher);
+  }
+}
