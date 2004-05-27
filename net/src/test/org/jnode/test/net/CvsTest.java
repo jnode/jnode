b7a2044478cbeea8ef25508684a0d83caec16f45
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-05-27 20:26:16 +0000

    added ssh2 support with the use of jcraft's jsch
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@611 407389f7-7c16-0410-9eee-ad85acb55d7f

--- net/src/test/org/jnode/test/net/CvsTest.java
+++ net/src/test/org/jnode/test/net/CvsTest.java
@@ -8,7 +8,9 @@ package org.jnode.test.net;
  * To change this template use File | Settings | File Templates.
  */
 
-import javax.crypto.*;
+import com.jcraft.jsch.JSch;
+
+import javax.crypto.NullCipher;
 
 public class CvsTest
 {
@@ -16,7 +18,10 @@ public class CvsTest
   public static void main(String[] args)
   {
     NullCipher nullCipher = new NullCipher();
-
     System.out.println(nullCipher);
+
+
+    JSch jsch = new JSch();
+    System.out.println(jsch);
   }
 }
