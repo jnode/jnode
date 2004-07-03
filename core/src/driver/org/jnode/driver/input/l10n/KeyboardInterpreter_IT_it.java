53719af898bb0eaa75f2060bd3fee20d9e420e7e
Ewout Prangsma <ewout@prangsma.net>
2004-07-03 16:51:27 +0000

    Added Italian keyboard by Matteo Di Giovinazzo <matteo.digiovinazzo@jugpadova.it>
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@639 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/driver/org/jnode/driver/input/l10n/KeyboardInterpreter_IT_it.java
@@ -0,0 +1,13 @@
+/*
+ * $Id$
+ */
+package org.jnode.driver.input.l10n;
+
+
+
+/**
+ * @author Matteo Di Giovinazzo <matteo.digiovinazzo@jugpadova.it>
+ */
+public class KeyboardInterpreter_IT_it extends KeyboardInterpreter_IT
+{
+}
