cf1005b656ed922213eab449c2fa664f7d99ab50
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-07-14 09:04:56 +0000

    Added (helper) adapter class
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@656 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/driver/org/jnode/driver/input/KeyboardAdapter.java
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ */
+package org.jnode.driver.input;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public class KeyboardAdapter implements KeyboardListener {
+
+    /**
+     * @see org.jnode.driver.input.KeyboardListener#keyPressed(org.jnode.driver.input.KeyboardEvent)
+     */
+    public void keyPressed(KeyboardEvent event) {
+    }
+
+    /**
+     * @see org.jnode.driver.input.KeyboardListener#keyReleased(org.jnode.driver.input.KeyboardEvent)
+     */
+    public void keyReleased(KeyboardEvent event) {
+    }
+}
