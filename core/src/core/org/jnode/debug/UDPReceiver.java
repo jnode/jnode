518708b8a47ba2b4363c6bc0c05c70e3d60c24ba
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-07-25 08:15:51 +0000

    Enable UDP received output to be written to file instead of System.out
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@663 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/debug/UDPReceiver.java
+++ core/src/core/org/jnode/debug/UDPReceiver.java
@@ -3,12 +3,12 @@
  */
 package org.jnode.debug;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.net.SocketException;
 
 /**
  * @author Ewout Prangsma (epr@users.sourceforge.net)
@@ -16,17 +16,20 @@ import java.net.SocketException;
 public class UDPReceiver {
 
 	private final DatagramSocket socket;
+	private FileOutputStream out;
 	
 	public static void main(String[] args) 
 	throws IOException {
 		final int port = (args.length > 0) ? Integer.parseInt(args[0]) : 5612;
+		final String fname = (args.length > 1) ? args[1] : null;
 		final SocketAddress address = new InetSocketAddress(port);
-		new UDPReceiver(address).run();
+		new UDPReceiver(address, fname).run();
 	}
 	
-	public UDPReceiver(SocketAddress address)
-	throws SocketException {
+	public UDPReceiver(SocketAddress address, String fname)
+	throws IOException {
 		this.socket = new DatagramSocket(address);
+		this.out = (fname != null) ? new FileOutputStream(fname, true) : null;
 	}
 	
 	public void run() 
@@ -36,6 +39,10 @@ public class UDPReceiver {
 		while (true) {
 			socket.receive(p);
 			System.out.write(p.getData(), p.getOffset(), p.getLength());
+			if (out != null) {
+				out.write(p.getData(), p.getOffset(), p.getLength());
+				out.flush();
+			}
 		}
 	}
 
