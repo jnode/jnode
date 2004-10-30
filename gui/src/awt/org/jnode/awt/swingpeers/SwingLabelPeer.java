b574e119ca6d8fca5923babe91accd2c7ce17a82
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-10-30 14:19:21 +0000

    Fixed layout
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@817 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/awt/org/jnode/awt/swingpeers/SwingLabelPeer.java
+++ gui/src/awt/org/jnode/awt/swingpeers/SwingLabelPeer.java
@@ -13,37 +13,41 @@ import javax.swing.JLabel;
  * AWT label peer implemented as a {@link javax.swing.JLabel}.
  */
 
-class SwingLabelPeer extends SwingComponentPeer implements LabelPeer, SwingPeer {
-
-	private final Label label;
+class SwingLabelPeer extends SwingComponentPeer implements LabelPeer {
 
 	//
 	// Construction
 	//
 
 	public SwingLabelPeer(SwingToolkit toolkit, Label label) {
-        super(toolkit, label, new JLabel());
-        final JLabel jLabel = (JLabel)jComponent;
-		this.label = label;
+		super(toolkit, label, new SwingLabel(label));
+		final JLabel jLabel = (JLabel) jComponent;
 		SwingToolkit.add(label, jLabel);
 		SwingToolkit.copyAwtProperties(label, jLabel);
 		setText(label.getText());
 	}
 
-    public void setText(String text) {
-        ((JLabel)jComponent).setText(text);
-    }
-
-  	public void setAlignment(int alignment) {
-        //TODO implement it
+	public void setText(String text) {
+		((JLabel) jComponent).setText(text);
 	}
 
-	/**
-	 * @see org.jnode.awt.swingpeers.SwingPeer#getAWTComponent()
-	 */
-	public Component getAWTComponent() {
-		return label;
+	public void setAlignment(int alignment) {
+		//TODO implement it
 	}
 
+	private static class SwingLabel extends JLabel implements SwingPeer {
+		private final Label awtComponent;
+
+		public SwingLabel(Label awtComponent) {
+			this.awtComponent = awtComponent;
+		}
+
+		/**
+		 * @see org.jnode.awt.swingpeers.SwingPeer#getAWTComponent()
+		 */
+		public Component getAWTComponent() {
+			return awtComponent;
+		}
+	}
 
 }
