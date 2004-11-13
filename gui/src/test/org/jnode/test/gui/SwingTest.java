8aa41051bdae5deef9eed56b33dc73fec4be480a
Levente Santha <lsantha@jnode.org>
2004-11-13 13:45:53 +0000

    Some Swing widget testing
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@854 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/test/org/jnode/test/gui/SwingTest.java
+++ gui/src/test/org/jnode/test/gui/SwingTest.java
@@ -4,6 +4,7 @@
 package org.jnode.test.gui;
 
 import java.awt.AWTKeyStroke;
+import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
@@ -11,6 +12,11 @@ import javax.swing.UIDefaults;
 import javax.swing.UIManager;
 import javax.swing.JFrame;
 import javax.swing.JTree;
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 /**
  * @author Ewout Prangsma (epr@users.sourceforge.net)
@@ -26,10 +32,22 @@ public class SwingTest {
 			//AWTKeyStroke ks = AWTKeyStroke.getAWTKeyStroke("SPACE");
 			//System.out.println("ks=" + ks);
 			//JPanel pan = new JPanel();
-            JFrame frame = new JFrame("Swing test");
+            JFrame frame = new JFrame("JFrame test");
             frame.setLocation(100, 100);
             frame.setSize(400, 400);
-            frame.getContentPane().add(new JTree());
+            frame.getContentPane().add(new JButton("JButton test"), BorderLayout.NORTH);
+            JTabbedPane tabs = new JTabbedPane();
+            tabs.add("Tab1", new JButton("JButton1"));
+            tabs.add("Tab2", new JButton("JButton2"));
+            tabs.add("Tab3", new JButton("JButton3"));
+            frame.getContentPane().add(tabs, BorderLayout.CENTER);
+            JMenuBar mb = new JMenuBar();
+            JMenu menu = new JMenu("JMenu test");
+            JMenuItem mi = new JMenuItem("JMenuItem test");
+            mb.add(menu);
+            menu.add(mi);
+            frame.setJMenuBar(mb);
+            frame.validate();
             frame.show();
 		} catch (Exception ex) {
 			ex.printStackTrace();
