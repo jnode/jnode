7fb12d1204f01bd9e709d2a194d556a4535134f6
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2004-11-13 13:24:57 +0000

    Applied classpath patches
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@850 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/classpath/vm/java/lang/VMClassLoader.java
+++ core/src/classpath/vm/java/lang/VMClassLoader.java
@@ -3,25 +3,35 @@
  */
 package java.lang;
 
+import org.jnode.vm.VmSystem;
 import org.jnode.vm.classmgr.VmType;
 
-
 /**
- * JNode specific classloader methods.
- * This class is called by various classpath classes.
+ * JNode specific classloader methods. This class is called by various classpath
+ * classes.
  * 
  * @vm-specific
  * @author Ewout Prangsma (epr@users.sourceforge.net)
  */
 final class VMClassLoader {
 
-    /**
-     * Gets a primitive class of a given type.
-     * 
-     * @param type
-     * @return @see VmType#getPrimitiveClass(char)
-     */
-    final static Class getPrimitiveClass(char type) {
-        return VmType.getPrimitiveClass(type).asClass();
-    }
+	/**
+	 * Gets a primitive class of a given type.
+	 * 
+	 * @param type
+	 * @return
+	 * @see VmType#getPrimitiveClass(char)
+	 */
+	final static Class getPrimitiveClass(char type) {
+		return VmType.getPrimitiveClass(type).asClass();
+	}
+
+	/**
+	 * Gets the system classloader.
+	 * 
+	 * @return
+	 */
+	final static ClassLoader getSystemClassLoader() {
+		return VmSystem.getSystemClassLoader().asClassLoader();
+	}
 }
