9f217870bbec76969ae841678d12127d01acb202
Ewout Prangsma <ewout@prangsma.net>
2004-12-12 17:02:51 +0000

    Extended magic classes implementation
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@914 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/compiler/BaseMagicHelper.java
+++ core/src/core/org/jnode/vm/compiler/BaseMagicHelper.java
@@ -33,29 +33,57 @@ public class BaseMagicHelper {
     protected static final int mTOADDRESS = 13;
     protected static final int mTOEXTENT = 14;
     protected static final int mTOOFFSET = 15;
-    protected static final int mEQUALS = 16;
-    protected static final int mISZERO = 17;
-    protected static final int mISMAX  = 18;
+    protected static final int mTOOBJECTREFERENCE = 16;
+    protected static final int mEQUALS = 17;
+    protected static final int mISZERO = 18;
+    protected static final int mISMAX  = 19;
 
-    protected static final int mEQ = 19;
-    protected static final int mNE = 20;
-    protected static final int mGT = 21;
-    protected static final int mGE = 22;
-    protected static final int mLT = 23;
-    protected static final int mLE = 24;
-    protected static final int mSGT = 25;
-    protected static final int mSGE = 26;
-    protected static final int mSLT = 27;
-    protected static final int mSLE = 28;
+    protected static final int mEQ = 20;
+    protected static final int mNE = 21;
+    protected static final int mGT = 22;
+    protected static final int mGE = 23;
+    protected static final int mLT = 24;
+    protected static final int mLE = 25;
+    protected static final int mSGT = 26;
+    protected static final int mSGE = 27;
+    protected static final int mSLT = 28;
+    protected static final int mSLE = 29;
     
-    protected static final int mFROMINT = 29;
-    protected static final int mFROMINTSIGNEXTEND = 30;
-    protected static final int mFROMINTZEROEXTEND = 31;
-    protected static final int mFROMLONG = 32;
+    protected static final int mFROMINT = 30;
+    protected static final int mFROMINTSIGNEXTEND = 31;
+    protected static final int mFROMINTZEROEXTEND = 32;
+    protected static final int mFROMLONG = 33;
 
-    protected static final int mLSH = 33;
-    protected static final int mRSHL = 34;
-    protected static final int mRSHA = 35;
+    protected static final int mLSH = 34;
+    protected static final int mRSHL = 35;
+    protected static final int mRSHA = 36;
+
+    // Make sure that the method code for loadXYV(Offset) is always
+    // equal to method code of loadXYZ() + 1
+    protected static final int mLOADBYTE = 40;
+    protected static final int mLOADBYTE_OFS = 41;
+    protected static final int mLOADCHAR = 42;
+    protected static final int mLOADCHAR_OFS = 43;
+    protected static final int mLOADSHORT = 44;
+    protected static final int mLOADSHORT_OFS = 45;
+    protected static final int mLOADINT = 46;
+    protected static final int mLOADINT_OFS = 47;
+    protected static final int mLOADFLOAT = 48;
+    protected static final int mLOADFLOAT_OFS = 49;
+    protected static final int mLOADLONG = 50;
+    protected static final int mLOADLONG_OFS = 51;
+    protected static final int mLOADDOUBLE = 52;
+    protected static final int mLOADDOUBLE_OFS = 53;
+    protected static final int mLOADADDRESS = 54;
+    protected static final int mLOADADDRESS_OFS = 55;
+    protected static final int mLOADWORD = 56;
+    protected static final int mLOADWORD_OFS = 57;
+    protected static final int mLOADOBJECTREFERENCE = 58;
+    protected static final int mLOADOBJECTREFERENCE_OFS = 59;
+    
+    private static final int mLOAD_MIN = mLOADBYTE;
+    private static final int mLOAD_MAX = mLOADOBJECTREFERENCE_OFS;
+    
     
     private final BootableHashMap methodNames = new BootableHashMap(); 
 
@@ -74,6 +102,7 @@ public class BaseMagicHelper {
         addMethod("toWord", mTOWORD);
         addMethod("toAddress", mTOADDRESS);
         addMethod("toOffset", mTOOFFSET);
+        addMethod("toObjectReference", mTOOBJECTREFERENCE);
         addMethod("toExtent", mTOEXTENT);
         addMethod("equals", mEQUALS);
         addMethod("isMax", mISMAX);
@@ -95,6 +124,16 @@ public class BaseMagicHelper {
         addMethod("lsh", mLSH);
         addMethod("rsha", mRSHA);
         addMethod("rshl", mRSHL);
+        addMethod("loadByte", mLOADBYTE);
+        addMethod("loadChar", mLOADCHAR);
+        addMethod("loadShort", mLOADSHORT);
+        addMethod("loadInt", mLOADINT);
+        addMethod("loadFloat", mLOADFLOAT);
+        addMethod("loadLong", mLOADLONG);
+        addMethod("loadDouble", mLOADDOUBLE);
+        addMethod("loadAddress", mLOADADDRESS);
+        addMethod("loadObjectReference", mLOADOBJECTREFERENCE);
+        addMethod("loadWord", mLOADWORD);
     }
     
     protected static final int getClass(VmMethod method) {
@@ -114,11 +153,17 @@ public class BaseMagicHelper {
 
     protected final int getMethodCode(VmMethod method) {
         final String mname = method.getName();
-        final Integer mcode = (Integer)methodNames.get(mname);
-        if (mcode == null) {
+        final Integer mcodeInt = (Integer)methodNames.get(mname);
+        if (mcodeInt == null) {
             throw new InternalError("Unknown method " + mname + " in " + method.getDeclaringClass().getName());
         }
-        return mcode.intValue();
+        int mcode = mcodeInt.intValue();
+        if ((mcode >= mLOAD_MIN) && (mcode <= mLOAD_MAX)) {
+            if (method.getNoArguments() == 1) {
+                mcode++; 
+            }
+        }
+        return mcode;
     }
     
     private final void addMethod(String mname, int code) {
