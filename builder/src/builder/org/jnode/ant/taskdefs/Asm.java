bbe2664027307c1a1718d0dc07b59ca9549f88eb
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-01-16 19:20:11 +0000

    Prepared for 64bit support. Important note: jnodesys.gz is renamed to jnode32.gz
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@983 407389f7-7c16-0410-9eee-ad85acb55d7f

--- builder/src/builder/org/jnode/ant/taskdefs/Asm.java
+++ builder/src/builder/org/jnode/ant/taskdefs/Asm.java
@@ -18,7 +18,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
+
 package org.jnode.ant.taskdefs;
 
 import java.io.File;
@@ -38,285 +38,371 @@ import org.apache.tools.ant.taskdefs.MatchingTask;
 import org.apache.tools.ant.taskdefs.condition.Os;
 
 /**
- *  Description of the Class
- *
- * @author     epr
- * @version    $Revision$
+ * Description of the Class
+ * 
+ * @author epr
+ * @version $Revision$
  */
 public class Asm extends MatchingTask {
 
-	private File destdir;
-	private File srcdir;
-	private String outputFormat;
-	private Set includeDirs = new HashSet();
-	private String ext = "o";
-	private File listFile;
+    public static class IncludeDir {
+        private File dir;
 
-	/**
-	 * Description of the Method
-	 *
-	 * @throws BuildException  Description of Exception
-	 */
-	public void execute() throws BuildException {
+        /**
+         * Constructor for IncludeDir.
+         */
+        public IncludeDir() {
+            super();
+        }
 
-		if (srcdir == null) {
-			throw new BuildException("Error: srcdir attribute must be set!", getLocation());
-		} else if (!srcdir.isDirectory()) {
-			throw new BuildException("Error: srcdir directory is not valid!", getLocation());
-		}
+        /**
+         * Returns the dir.
+         * 
+         * @return File
+         */
+        public File getDir() {
+            return dir;
+        }
 
-		if (destdir == null) {
-			throw new BuildException("Error: destdir attribute must be set!", getLocation());
-		} else if (!destdir.isDirectory()) {
-			throw new BuildException("Error: destdir directory is not valid!", getLocation());
-		}
+        /**
+         * Sets the dir.
+         * 
+         * @param dir
+         *            The dir to set
+         */
+        public void setDir(File dir) {
+            this.dir = dir;
+        }
+    }
 
-		try {
-			executeAsm();
-		} catch (IOException ex) {
-			throw new BuildException(ex);
-		}
-	}
+    private static String postFixSlash(String s) {
+        if (!s.endsWith(File.separator)) {
+            return s + File.separator;
+        } else {
+            return s;
+        }
+    }
 
-	/**
-	 *  Sets the Destdir attribute of the JspC object
-	 *
-	 * @param  destDir  The new Destdir value
-	 */
-	public void setDestdir(File destDir) {
-		this.destdir = destDir;
-	}
+    private int bits = 32;
 
-	/**
-	 *  Description of the Method
-	 *
-	 * @throws  BuildException  Description of Exception
-	 * @throws  IOException     Description of the Exception
-	 */
-	protected void executeAsm() throws BuildException, IOException {
+    private File destdir;
 
-		DirectoryScanner scanner = getDirectoryScanner(getSrcdir());
-		String allFiles[] = scanner.getIncludedFiles();
-		Map compileMap = new HashMap();
-		scanDir(srcdir, destdir, allFiles, compileMap);
+    private String ext = "o";
 
-		if (compileMap.size() > 0) {
-			log("Compiling " + compileMap.size() + " source files to " + destdir);
+    private Set includeDirs = new HashSet();
 
-			for (Iterator i = compileMap.keySet().iterator(); i.hasNext();) {
-				File srcFile = (File) i.next();
-				File dstFile = (File) compileMap.get(srcFile);
-				doNasm(srcFile, dstFile);
-			}
-		}
-	}
+    private File listFile;
 
-	/**
-	 *  Scans the directory looking for source files to be compiled. The results
-	 *  are returned in the class variable compileList
-	 *
-	 * @param  srcDir           Description of Parameter
-	 * @param  destDir          Description of Parameter
-	 * @param  files            Description of Parameter
-	 * @param compileMap
-	 * @throws  IOException  Description of the Exception
-	 */
-	protected void scanDir(File srcDir, File destDir, String[] files, Map compileMap) throws IOException {
-		long now = System.currentTimeMillis();
+    private String outputFormat;
 
-		for (int c = 0; c < files.length; c++) {
-			File aFile = new File(srcdir, files[c]);
+    private File srcdir;
 
-			// get the path within the uriroot
-			String fileDirString = aFile.getParentFile().toString();
-			String rootDirString = srcdir.toString();
+    /**
+     * Add an includedir
+     * 
+     * @param dir
+     */
+    public void addConfiguredIncludeDir(IncludeDir dir) {
+        includeDirs.add(dir);
+    }
 
-			int diff = fileDirString.compareTo(rootDirString);
-			String destSubDir = fileDirString.substring(fileDirString.length() - diff, fileDirString.length()).replace('\\', '/');
+    /**
+     * Description of the Method
+     * 
+     * @param srcFile
+     * @param dstFile
+     * @throws BuildException
+     *             Description of Exception
+     * @throws IOException
+     *             Description of the Exception
+     */
+    private void doNasm(File srcFile, File dstFile) throws BuildException,
+            IOException {
 
-			//				  log(destSubDir);
-			File fileDir = new File(destDir.toString() + destSubDir);
+        Execute exec = new Execute();
+        ArrayList cmdLine = new ArrayList();
+        if (bits == 64) {
+            cmdLine.add("yasm");
+        } else if (Os.isFamily("windows")) {
+            cmdLine.add("nasmw.exe");
+        } else {
+            cmdLine.add("nasm");
+        }
 
-			String objName = aFile.getName().substring(0, aFile.getName().indexOf(".asm")) + "." + ext;
-			File objFile = new File(fileDir, objName);
+        cmdLine.add("-o");
+        cmdLine.add(dstFile.toString());
 
-			// check if the file has been modified in the future, error?
-			if (aFile.lastModified() > now) {
-				log("Warning: file modified in the future: " + aFile.getName(), Project.MSG_WARN);
-			}
+        if (bits == 64) {
+            cmdLine.add("-m");
+            cmdLine.add("amd64");
+        }
+        
+        if (outputFormat != null) {
+            cmdLine.add("-f");
+            cmdLine.add(outputFormat);
+        }
 
-			if (!objFile.exists() || aFile.lastModified() > objFile.lastModified()) {
-				if (!objFile.exists()) {
-					log("Compiling " + aFile.getPath() + " because class file " + objFile.getPath() + " does not exist", Project.MSG_VERBOSE);
-				} else {
-					log("Compiling " + aFile.getPath() + " because it is out of date with respect to " + objFile.getPath(), Project.MSG_VERBOSE);
-				}
+        if (listFile != null) {
+            cmdLine.add("-l");
+            cmdLine.add(listFile.toString());
+        }
 
-				compileMap.put(aFile, objFile);
-				log(aFile.toString(), Project.MSG_VERBOSE);
-			}
-		}
-	}
+        for (Iterator i = includeDirs.iterator(); i.hasNext();) {
+            IncludeDir dir = (IncludeDir) i.next();
+            cmdLine.add("-I");
+            cmdLine.add(postFixSlash(dir.getDir().toString()));
+        }
 
-	/**
-	 *  Description of the Method
-	 *
-	 * @param srcFile
-	 * @param dstFile
-	 * @throws  BuildException  Description of Exception
-	 * @throws  IOException     Description of the Exception
-	 */
-	private void doNasm(File srcFile, File dstFile) throws BuildException, IOException {
+        cmdLine.add(srcFile.toString());
 
-		Execute exec = new Execute();
-		ArrayList cmdLine = new ArrayList();
-		if (Os.isFamily("windows")) {
-			cmdLine.add("nasmw.exe");
-		} else {
-			cmdLine.add("nasm");
-		}
+        log("cmdLine=" + cmdLine, Project.MSG_VERBOSE);
 
-		cmdLine.add("-o");
-		cmdLine.add(dstFile.toString());
+        exec.setCommandline((String[]) cmdLine.toArray(new String[cmdLine
+                .size()]));
 
-		if (outputFormat != null) {
-			cmdLine.add("-f");
-			cmdLine.add(outputFormat);
-		}
+        dstFile.getParentFile().mkdirs();
+        int rc = exec.execute();
 
-		if (listFile != null) {
-			cmdLine.add("-l");
-			cmdLine.add(listFile.toString());
-		}
+        if (rc != 0) {
+            throw new BuildException("Asm failed on "
+                    + srcFile.getAbsolutePath());
+        }
+    }
 
-		for (Iterator i = includeDirs.iterator(); i.hasNext();) {
-			IncludeDir dir = (IncludeDir) i.next();
-			cmdLine.add("-I" + postFixSlash(dir.getDir().toString()));
-		}
+    /**
+     * Description of the Method
+     * 
+     * @throws BuildException
+     *             Description of Exception
+     */
+    public void execute() throws BuildException {
 
-		cmdLine.add(srcFile.toString());
+        if (srcdir == null) {
+            throw new BuildException("Error: srcdir attribute must be set!",
+                    getLocation());
+        } else if (!srcdir.isDirectory()) {
+            throw new BuildException("Error: srcdir directory is not valid!",
+                    getLocation());
+        }
 
-		log("cmdLine=" + cmdLine, Project.MSG_VERBOSE);
+        if (destdir == null) {
+            throw new BuildException("Error: destdir attribute must be set!",
+                    getLocation());
+        } else if (!destdir.isDirectory()) {
+            throw new BuildException("Error: destdir directory is not valid!",
+                    getLocation());
+        }
 
-		exec.setCommandline((String[]) cmdLine.toArray(new String[cmdLine.size()]));
+        try {
+            executeAsm();
+        } catch (IOException ex) {
+            throw new BuildException(ex);
+        }
+    }
 
-		dstFile.getParentFile().mkdirs();
-		int rc = exec.execute();
+    /**
+     * Description of the Method
+     * 
+     * @throws BuildException
+     *             Description of Exception
+     * @throws IOException
+     *             Description of the Exception
+     */
+    protected void executeAsm() throws BuildException, IOException {
 
-		if (rc != 0) {
-			throw new BuildException("Asm failed on " + srcFile.getAbsolutePath());
-		}
-	}
-	/**
-	 * Returns the destdir.
-	 * @return File
-	 */
-	public File getDestdir() {
-		return destdir;
-	}
+        DirectoryScanner scanner = getDirectoryScanner(getSrcdir());
+        String allFiles[] = scanner.getIncludedFiles();
+        Map compileMap = new HashMap();
+        scanDir(srcdir, destdir, allFiles, compileMap);
 
-	/**
-	 * Returns the srcdir.
-	 * @return File
-	 */
-	public File getSrcdir() {
-		return srcdir;
-	}
+        if (compileMap.size() > 0) {
+            log("Compiling " + compileMap.size() + " source files to "
+                    + destdir);
 
-	/**
-	 * Sets the srcdir.
-	 * @param srcdir The srcdir to set
-	 */
-	public void setSrcdir(File srcdir) {
-		this.srcdir = srcdir;
-	}
+            for (Iterator i = compileMap.keySet().iterator(); i.hasNext();) {
+                File srcFile = (File) i.next();
+                File dstFile = (File) compileMap.get(srcFile);
+                doNasm(srcFile, dstFile);
+            }
+        }
+    }
 
-	/**
-	 * Add an includedir
-	 * @param dir
-	 */
-	public void addConfiguredIncludeDir(IncludeDir dir) {
-		includeDirs.add(dir);
-	}
+    /**
+     * Gets the number of bits of the target platform.
+     * 
+     * @return 32/64
+     */
+    public final int getBits() {
+        return this.bits;
+    }
 
-	public static class IncludeDir {
-		private File dir;
-		/**
-		 * Constructor for IncludeDir.
-		 */
-		public IncludeDir() {
-			super();
-		}
+    /**
+     * Returns the destdir.
+     * 
+     * @return File
+     */
+    public File getDestdir() {
+        return destdir;
+    }
 
-		/**
-		 * Returns the dir.
-		 * @return File
-		 */
-		public File getDir() {
-			return dir;
-		}
+    /**
+     * Returns the ext.
+     * 
+     * @return String
+     */
+    public String getExtension() {
+        return ext;
+    }
 
-		/**
-		 * Sets the dir.
-		 * @param dir The dir to set
-		 */
-		public void setDir(File dir) {
-			this.dir = dir;
-		}
-	}
-	/**
-	 * Returns the outputFormat.
-	 * @return String
-	 */
-	public String getOutputFormat() {
-		return outputFormat;
-	}
+    /**
+     * Returns the listFile.
+     * 
+     * @return File
+     */
+    public File getListFile() {
+        return listFile;
+    }
 
-	/**
-	 * Sets the outputFormat.
-	 * @param outputFormat The outputFormat to set
-	 */
-	public void setOutputFormat(String outputFormat) {
-		this.outputFormat = outputFormat;
-	}
+    /**
+     * Returns the outputFormat.
+     * 
+     * @return String
+     */
+    public String getOutputFormat() {
+        return outputFormat;
+    }
 
-	/**
-	 * Returns the ext.
-	 * @return String
-	 */
-	public String getExtension() {
-		return ext;
-	}
+    /**
+     * Returns the srcdir.
+     * 
+     * @return File
+     */
+    public File getSrcdir() {
+        return srcdir;
+    }
 
-	/**
-	 * Sets the ext.
-	 * @param ext The ext to set
-	 */
-	public void setExtension(String ext) {
-		this.ext = ext;
-	}
+    /**
+     * Scans the directory looking for source files to be compiled. The results
+     * are returned in the class variable compileList
+     * 
+     * @param srcDir
+     *            Description of Parameter
+     * @param destDir
+     *            Description of Parameter
+     * @param files
+     *            Description of Parameter
+     * @param compileMap
+     * @throws IOException
+     *             Description of the Exception
+     */
+    protected void scanDir(File srcDir, File destDir, String[] files,
+            Map compileMap) throws IOException {
+        long now = System.currentTimeMillis();
 
-	/**
-	 * Returns the listFile.
-	 * @return File
-	 */
-	public File getListFile() {
-		return listFile;
-	}
+        for (int c = 0; c < files.length; c++) {
+            File aFile = new File(srcdir, files[c]);
 
-	/**
-	 * Sets the listFile.
-	 * @param listFile The listFile to set
-	 */
-	public void setListFile(File listFile) {
-		this.listFile = listFile;
-	}
+            // get the path within the uriroot
+            String fileDirString = aFile.getParentFile().toString();
+            String rootDirString = srcdir.toString();
 
-	private static String postFixSlash(String s) {
-		if (!s.endsWith(File.separator)) {
-			return s + File.separator;
-		} else {
-			return s;
-		}
-	}
+            int diff = fileDirString.compareTo(rootDirString);
+            String destSubDir = fileDirString.substring(
+                    fileDirString.length() - diff, fileDirString.length())
+                    .replace('\\', '/');
+
+            // log(destSubDir);
+            File fileDir = new File(destDir.toString() + destSubDir);
+
+            String objName = aFile.getName().substring(0,
+                    aFile.getName().indexOf(".asm"))
+                    + "." + ext;
+            File objFile = new File(fileDir, objName);
+
+            // check if the file has been modified in the future, error?
+            if (aFile.lastModified() > now) {
+                log("Warning: file modified in the future: " + aFile.getName(),
+                        Project.MSG_WARN);
+            }
+
+            if (!objFile.exists()
+                    || aFile.lastModified() > objFile.lastModified()) {
+                if (!objFile.exists()) {
+                    log("Compiling " + aFile.getPath() + " because class file "
+                            + objFile.getPath() + " does not exist",
+                            Project.MSG_VERBOSE);
+                } else {
+                    log("Compiling " + aFile.getPath()
+                            + " because it is out of date with respect to "
+                            + objFile.getPath(), Project.MSG_VERBOSE);
+                }
+
+                compileMap.put(aFile, objFile);
+                log(aFile.toString(), Project.MSG_VERBOSE);
+            }
+        }
+    }
+
+    /**
+     * Sets the number of bits of the target platform.
+     * 
+     * @param bits
+     *            32/64
+     */
+    public final void setBits(int bits) {
+        if ((bits != 32) && (bits != 64)) {
+            throw new IllegalArgumentException("Invalid bits value " + bits);
+        }
+        this.bits = bits;
+    }
+
+    /**
+     * Sets the Destdir attribute of the JspC object
+     * 
+     * @param destDir
+     *            The new Destdir value
+     */
+    public void setDestdir(File destDir) {
+        this.destdir = destDir;
+    }
+
+    /**
+     * Sets the ext.
+     * 
+     * @param ext
+     *            The ext to set
+     */
+    public void setExtension(String ext) {
+        this.ext = ext;
+    }
+
+    /**
+     * Sets the listFile.
+     * 
+     * @param listFile
+     *            The listFile to set
+     */
+    public void setListFile(File listFile) {
+        this.listFile = listFile;
+    }
+
+    /**
+     * Sets the outputFormat.
+     * 
+     * @param outputFormat
+     *            The outputFormat to set
+     */
+    public void setOutputFormat(String outputFormat) {
+        this.outputFormat = outputFormat;
+    }
+
+    /**
+     * Sets the srcdir.
+     * 
+     * @param srcdir
+     *            The srcdir to set
+     */
+    public void setSrcdir(File srcdir) {
+        this.srcdir = srcdir;
+    }
 }
