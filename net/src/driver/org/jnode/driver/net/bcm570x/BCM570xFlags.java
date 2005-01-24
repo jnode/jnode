250bc368c8cb40d59e64406448e85d57c4dad51a
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-01-24 10:48:15 +0000

    start of Broadcom 570x driver
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1010 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ net/src/driver/org/jnode/driver/net/bcm570x/BCM570xFlags.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+ 
+package org.jnode.driver.net.bcm570x;
+
+import org.jnode.driver.net.ethernet.spi.Flags;
+import org.jnode.plugin.ConfigurationElement;
+
+/**
+ * @author Martin Husted Hartvig
+ */
+
+public class BCM570xFlags implements Flags {
+
+    private final String name;
+
+    /**
+     * Create a new instance of the flags
+     */
+    public BCM570xFlags(ConfigurationElement config) {
+        final String name = config.getAttribute("name");
+        if (name != null) {
+            this.name = name;
+        } else {
+            this.name = "Unknown BCM570x";
+        }
+    }
+
+    /**
+     * Create a new instance of the flags
+     * 
+     * @param name
+     */
+
+    public BCM570xFlags(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the name of the device
+     */
+
+    public String getName() {
+        return name;
+    }
+}
+
