049acf9c67e7d0605db81e62d0ad2efcfa9cd48e
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-02-05 19:29:03 +0000

    Fixed source file encodings & file headers
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1065 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/jifs/JIFSDirIterator.java
+++ fs/src/fs/org/jnode/fs/jifs/JIFSDirIterator.java
@@ -1,30 +1,24 @@
 /*
-*	This file is part of 
-*
-*	Jnode Information FileSystem (jifs)
-*
-* jifs is used within JNode (see jnode.sourceforge.net for details).
-* jifs provides files with information about JNode internals.
-* For a new version of jifs see jifs.org.
-* To contact me mail to : mail@jifs.org
-*
-* Copyright (C) 2004  Andreas Haenel
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
-*/
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+ 
 	package org.jnode.fs.jifs;
 
 	import java.io.IOException;
