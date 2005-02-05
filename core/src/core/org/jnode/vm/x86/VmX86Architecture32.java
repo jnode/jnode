d36502b201c4e617dca04de74e3ddf678c51fa7e
Ewout Prangsma <ewout@prangsma.net>
2005-02-05 19:23:12 +0000

    Fixed source file encodings & file headers
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1064 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/x86/VmX86Architecture32.java
+++ core/src/core/org/jnode/vm/x86/VmX86Architecture32.java
@@ -18,6 +18,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
+ 
 package org.jnode.vm.x86;
 
 import org.jnode.vm.VmProcessor;
