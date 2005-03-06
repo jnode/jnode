c8c159d516e3884ec63e93800511ffa0db447478
Levente Santha <lsantha@jnode.org>
2005-03-06 18:35:21 +0000

    Fixed non ASCII characters.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1159 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/awt/org/jnode/awt/swingpeers/SwingPanelPeer.java
+++ gui/src/awt/org/jnode/awt/swingpeers/SwingPanelPeer.java
@@ -18,7 +18,6 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
 package org.jnode.awt.swingpeers;
 
 import javax.swing.JPanel;
