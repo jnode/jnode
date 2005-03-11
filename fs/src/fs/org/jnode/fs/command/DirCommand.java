d7fc356946d72696f285b75a3a0c769323696967
trickkiste <trickkiste@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-03-11 10:37:52 +0000

    fixed the bug at /
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1180 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/command/DirCommand.java
+++ fs/src/fs/org/jnode/fs/command/DirCommand.java
@@ -22,7 +22,11 @@
 package org.jnode.fs.command;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
 
+import org.jnode.shell.Command;
+import org.jnode.shell.CommandLine;
 import org.jnode.shell.help.FileArgument;
 import org.jnode.shell.help.Help;
 import org.jnode.shell.help.Parameter;
@@ -30,8 +34,9 @@ import org.jnode.shell.help.ParsedArguments;
 
 /**
  * @author epr
+ * @author Andreas H\u00e4nel
  */
-public class DirCommand {
+public class DirCommand implements Command{
 
 	static final FileArgument ARG_DIR = new FileArgument("directory", "the directory to list contents of");
 	public static Help.Info HELP_INFO =
@@ -41,38 +46,43 @@ public class DirCommand {
 			new Parameter[] { new Parameter(ARG_DIR, Parameter.OPTIONAL)});
 
 	public static void main(String[] args) throws Exception {
-		ParsedArguments cmdLine = HELP_INFO.parse(args);
-
-		File dir = ARG_DIR.getFile(cmdLine);
-		if (dir == null) {
-			dir = new File(System.getProperty("user.dir"));
-		}
-
-		if (dir.exists() && dir.isDirectory()) {
-			final File[] list = dir.listFiles();
-			printList(list);
-		} else if("/".equals(dir.getCanonicalPath())) {
+		new DirCommand().execute(new CommandLine(args), System.in, System.out, System.err);
+	}
+	
+	public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
+		ParsedArguments cmdLine = HELP_INFO.parse(commandLine.toStringArray());
+		String dir_str = ARG_DIR.getValue(cmdLine);
+		
+		if (((dir_str == null)&&(System.getProperty("user.dir").equals("/")))||(dir_str.equals("/"))){
 			File[] roots = File.listRoots();
-			printList(roots);
-		} else if (dir.exists() && dir.isFile()) {
-		    printList(new File[] { dir });
+			this.printList(roots,out);
 		} else {
-			System.err.println("No such directory " + dir);
+			File dir = ARG_DIR.getFile(cmdLine);
+			if (dir==null) dir = new File(System.getProperty("user.dir"));
+			if (dir.exists() && dir.isDirectory()) {
+				final File[] list = dir.listFiles();
+				this.printList(list, out);
+			} else if (dir.exists() && dir.isFile()) {
+				this.printList(new File[] { dir },out);
+			} else {
+				err.println("No such directory " + dir);
+			}
 		}
+			
 	}
 
-	private static void printList(File[] list) {
+	private void printList(File[] list, PrintStream out) {
 		if (list != null) {
 			for (int i = 0; i < list.length; i++) {
 				File f = list[i];
 				if (f.isDirectory()) {
-					System.out.print("[" + f.getName() + "]");
+					out.print("[" + f.getName() + "]");
 				} else {
-					System.out.print(f.getName() + " " + f.length());
+					out.print(f.getName() + " " + f.length());
 				}
-				System.out.println();
+				out.println();
 			}
-			System.out.println();
+			out.println();
 		}
 	}
 }
