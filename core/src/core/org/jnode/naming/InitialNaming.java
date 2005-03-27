2d896dfd2fad52e81ec4a4107fbb99ebe60a5498
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-03-27 10:59:53 +0000

    Now, InitialNaming is delegating its work to a NameSpace implementation. DefaultNameSpace is the default impl and acts as prev version of InitialNaming. DefaultNameSpace is used if no NameSpace is specified (this is the case under JNode).
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1207 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/naming/InitialNaming.java
+++ core/src/core/org/jnode/naming/InitialNaming.java
@@ -30,6 +30,8 @@ import javax.naming.NameAlreadyBoundException;
 import javax.naming.NameNotFoundException;
 import javax.naming.NamingException;
 
+import org.jnode.system.BootLog;
+
 /**
  * This class provides a namespace that is used by the JNode system.
  * Various services are bound into this namespace.
@@ -46,8 +48,17 @@ import javax.naming.NamingException;
 public class InitialNaming {
 	
 	/** All bound names+services */
-	private static final Map namespace = new HashMap();
+	private static NameSpace namespace;
 
+    public static void setNameSpace(NameSpace namespace)
+    {
+        if(InitialNaming.namespace != null)
+        {
+            throw new SecurityException("namespace can't be modified after first initialization");
+        }
+        InitialNaming.namespace = namespace;
+    }
+    
 	/**
 	 * Bind a given service in the namespace under a given name.
 	 * @param name
@@ -56,16 +67,8 @@ public class InitialNaming {
 	 */
 	public static void bind(Class name, Object service) 
 	throws NamingException, NameAlreadyBoundException {
-	    if (name == null) {
-	        throw new IllegalArgumentException("name == null");
-	    }
-		synchronized (namespace) {
-			if (namespace.containsKey(name)) {
-				throw new NameAlreadyBoundException(name.getName());
-			}
-			namespace.put(name, service);
-		}
-	}
+        getNameSpace().bind(name, service);	
+    }
 	
 	/**
 	 * Unbind a service with a given name from the namespace.
@@ -74,9 +77,7 @@ public class InitialNaming {
 	 * @param name
 	 */
 	public static void unbind(Class name) {
-		synchronized (namespace) {
-			namespace.remove(name);
-		}
+        getNameSpace().unbind(name);
 	}
 
 	/**
@@ -86,21 +87,152 @@ public class InitialNaming {
 	 */
 	public static Object lookup(Class name) 
 	throws NameNotFoundException {
-		final Object result = namespace.get(name);
-		if (result == null) {
-			throw new NameNotFoundException(name.getName());
-		}
-		return result;
+		return getNameSpace().lookup(name);
 	}
 
+    /**
+     * Lookup a service with a given name.
+     * @param name
+     * @param defaultImplClass
+     * @throws NameNotFoundException if the name was not found in this namespace
+     */
+    public static Object lookup(Class name, Class defaultImplClass) 
+    throws NameNotFoundException {
+        if(!getNameSpace().nameSet().contains(name))
+        {
+            Object defaultImpl;
+            try
+            {
+                defaultImpl = defaultImplClass.newInstance();
+                bind(name, defaultImpl);
+            }
+            catch (InstantiationException e)
+            {
+                BootLog.error("can't create default impl for "+name.getName(), e);
+            }
+            catch (IllegalAccessException e)
+            {
+                BootLog.error("can't create default impl for "+name.getName(), e);
+            }
+            catch (NameAlreadyBoundException e)
+            {
+                BootLog.error("unexpected error"+name.getName(), e);
+            }
+            catch (NamingException e)
+            {
+                BootLog.error("unexpected error"+name.getName(), e);
+            }
+        }
+        
+        return getNameSpace().lookup(name);
+    }
+    
 	/**
 	 * Gets a set containing all names (Class) of the bound services.
 	 */
 	public static Set nameSet() {
-		final Set result;
-		synchronized (namespace) {
-			result = new HashSet(namespace.keySet());
-		}
-		return result;
+		return getNameSpace().nameSet();
 	}
+
+    private static NameSpace getNameSpace()
+    {
+        if(namespace == null)
+        {
+            namespace = new DefaultNameSpace();
+        }
+        return namespace;
+    }
+    
+    public interface NameSpace {
+        /**
+         * Bind a given service in the namespace under a given name.
+         * @param name
+         * @param service
+         * @throws NameAlreadyBoundException if the name already exists within this namespace
+         */
+        public void bind(Class name, Object service) 
+        throws NamingException, NameAlreadyBoundException;
+        
+        /**
+         * Unbind a service with a given name from the namespace.
+         * If the name does not exist in this namespace, this method
+         * returns without an error.
+         * @param name
+         */
+        public void unbind(Class name);
+
+        /**
+         * Lookup a service with a given name.
+         * @param name
+         * @throws NameNotFoundException if the name was not found in this namespace
+         */
+        public Object lookup(Class name) throws NameNotFoundException;
+
+        /**
+         * Gets a set containing all names (Class) of the bound services.
+         */
+        public Set nameSet();
+    }
+    
+    static public class DefaultNameSpace implements NameSpace {
+        
+        /** All bound names+services */
+        protected final Map namespace = new HashMap();
+
+        /**
+         * Bind a given service in the namespace under a given name.
+         * @param name
+         * @param service
+         * @throws NameAlreadyBoundException if the name already exists within this namespace
+         */
+        public void bind(Class name, Object service) 
+        throws NamingException, NameAlreadyBoundException {
+            if (name == null) {
+                throw new IllegalArgumentException("name == null");
+            }
+            synchronized (namespace) {
+                if (namespace.containsKey(name)) {
+                    throw new NameAlreadyBoundException(name.getName());
+                }
+                namespace.put(name, service);
+            }
+        }
+        
+        /**
+         * Unbind a service with a given name from the namespace.
+         * If the name does not exist in this namespace, this method
+         * returns without an error.
+         * @param name
+         */
+        public void unbind(Class name) {
+            synchronized (namespace) {
+                namespace.remove(name);
+            }
+        }
+
+        /**
+         * Lookup a service with a given name.
+         * @param name
+         * @throws NameNotFoundException if the name was not found in this namespace
+         */
+        public Object lookup(Class name) 
+        throws NameNotFoundException {
+            final Object result = namespace.get(name);
+            if (result == null) {
+                throw new NameNotFoundException(name.getName());
+            }
+            return result;
+        }
+
+        /**
+         * Gets a set containing all names (Class) of the bound services.
+         */
+        public Set nameSet() {
+            final Set result;
+            synchronized (namespace) {
+                result = new HashSet(namespace.keySet());
+            }
+            return result;
+        }
+    }    
 }
