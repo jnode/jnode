ce67ef418bcdc295ef53a8832c95ab404a70f58f
Ewout Prangsma <ewout@prangsma.net>
2005-04-08 15:09:12 +0000

    Added authentication mode to wireless netdevices
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1259 407389f7-7c16-0410-9eee-ad85acb55d7f

--- net/src/driver/org/jnode/driver/net/WirelessNetDeviceAPI.java
+++ net/src/driver/org/jnode/driver/net/WirelessNetDeviceAPI.java
@@ -3,6 +3,8 @@
  */
 package org.jnode.driver.net;
 
+import org.jnode.net.wireless.AuthenticationMode;
+
 /**
  * Device API for wireless network devices.
  * 
@@ -10,6 +12,19 @@ package org.jnode.driver.net;
  */
 public interface WirelessNetDeviceAPI extends NetDeviceAPI {
 
+    /**
+     * Gets the current authentication mode.
+     * @return
+     */
+    public AuthenticationMode getAuthenticationMode();
+    
+    /**
+     * Sets the current authentication mode.
+     * @param mode
+     */
+    public void setAuthenticationMode(AuthenticationMode mode)
+    throws NetworkException;
+    
     /**
      * Gets the current ESS ID.
      * @return A valid ESSID, or null if not ESSID is present.
