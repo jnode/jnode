0cf9610c5c815b2d6b55a350ac88790f8fda9cad
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-04-09 17:57:00 +0000

    changes due to move of Geometry and CHS
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1272 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/test/org/jnode/test/fs/GeometryTest.java
+++ fs/src/test/org/jnode/test/fs/GeometryTest.java
@@ -23,8 +23,9 @@ package org.jnode.test.fs;
 
 import junit.framework.TestCase;
 
-import org.jnode.fs.util.CHS;
-import org.jnode.fs.util.Geometry;
+import org.jnode.driver.block.CHS;
+import org.jnode.driver.block.Geometry;
+import org.jnode.driver.block.Geometry.GeometryException;
 
 /**
  * @author epr
@@ -39,7 +40,7 @@ public class GeometryTest extends TestCase {
 		super(arg0);
 	}
 
-	public void testLogSec2CHS() {
+	public void testLogSec2CHS() throws GeometryException {
 		Geometry geom = new Geometry(64, 8, 40);
 		long max = geom.getTotalSectors();
 		for (long logSec = 0; logSec < max; logSec++) {
