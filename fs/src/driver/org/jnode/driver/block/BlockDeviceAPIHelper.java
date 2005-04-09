a5d0fe8d51375ba0edd239b43e57d1198c2c2c78
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-04-09 17:44:47 +0000

    added helper class for BlockDeviceAPI implementors. For now it only checks offset, length and alignment of parameters passed to read/write
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1269 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/driver/org/jnode/driver/block/BlockDeviceAPIHelper.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+package org.jnode.driver.block;
+
+import java.io.IOException;
+
+import org.jnode.driver.ide.IDEConstants;
+import org.jnode.driver.ide.disk.IDEDiskDriver;
+
+public class BlockDeviceAPIHelper
+{
+    public static void checkBounds(BlockDeviceAPI api, long devOffset) throws IOException
+    {
+        if(devOffset < 0)
+            throw new IOException("devOffset < 0");
+        
+        if(devOffset >= api.getLength())
+            throw new IOException("devOffset < upper bound");
+    }
+    
+    public static void checkBounds(BlockDeviceAPI api, long devOffset, long length) throws IOException
+    {
+        checkBounds(api, devOffset);
+        
+        if(length <= 0)
+            throw new IOException("length <= 0");
+                
+        checkBounds(api, devOffset+length-1); // don't forget to substract 1 !
+    }
+
+    public static void checkAlignment(int sectorSize, BlockDeviceAPI api, long devOffset, int length) throws IOException
+    {
+        if ((devOffset % sectorSize) != 0) {
+            throw new IOException("Non aligned devOffset not allowed. Size requested = " + devOffset + " Sector size = " + sectorSize);
+        }
+        if ((length % sectorSize) != 0) {
+            throw new IOException("Non aligned length not allowed. Size requested = " + length + " Sector size = " + sectorSize);
+        }
+    }    
+}
