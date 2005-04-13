7c31f1874fd160f89007fc3ea1f5fd5a474b2220
trickkiste <trickkiste@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-04-13 20:29:52 +0000

    jiif command improved, mount on boot
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1305 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/jifs/command/createJIFS.java
+++ fs/src/fs/org/jnode/fs/jifs/command/createJIFS.java
@@ -40,7 +40,15 @@ import org.jnode.fs.service.FileSystemService;
 import org.jnode.driver.DeviceManager;
 import org.jnode.driver.DeviceUtils;
 import org.jnode.driver.DriverException;
+import org.jnode.driver.DeviceNotFoundException;
 import org.jnode.driver.DeviceAlreadyRegisteredException;
+
+import org.jnode.shell.help.Help;
+import org.jnode.shell.help.OptionArgument;
+import org.jnode.shell.help.Parameter;
+import org.jnode.shell.help.ParsedArguments;
+import org.jnode.shell.help.Syntax;
+import org.jnode.shell.help.SyntaxErrorException;
 
 /**
  * Just mounts initial JIFS on /Jifs
@@ -50,9 +58,37 @@ import org.jnode.driver.DeviceAlreadyRegisteredException;
 public class createJIFS implements Command{
 
     private static final Logger log = Logger.getLogger(createJIFS.class);
+    
+    static final OptionArgument ACTION = new OptionArgument("action",
+            "Action to perform", new OptionArgument.Option[] {
+                    new OptionArgument.Option("start", "start the jifs"),
+                    new OptionArgument.Option("stop", "stop the jifs"),
+                    new OptionArgument.Option("restart",  "restart the jifs"),
+					});
+    
+    static final Parameter PARAM_ACTION = new Parameter(ACTION, Parameter.MANDATORY);
+    
+    public static Help.Info HELP_INFO = new Help.Info("jifs",
+            new Syntax[] { new Syntax(
+                    "JIFS - Jnode Information FileSystem",
+                    new Parameter[] { PARAM_ACTION })});
 
-	public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
-		log.info("Create jifs");
+    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
+		ParsedArguments cmdLine = HELP_INFO.parse(commandLine.toStringArray());
+		String Act = ACTION.getValue(cmdLine);
+		if (new String("start").equals(Act)){
+			this.startjifs();
+			}
+		if (new String("stop").equals(Act)){
+			this.stopjifs();
+			}
+		if (new String("restart").equals(Act)){
+			this.restartjifs();
+			}
+	}
+    
+    private void startjifs(){
+		log.info("start jifs");
         try {
          	FileSystemService fSS = (FileSystemService) InitialNaming.lookup(FileSystemService.NAME);
          	FileSystemType type = fSS.getFileSystemTypeForNameSystemTypes(JIFileSystemType.NAME);
@@ -85,4 +121,29 @@ public class createJIFS implements Command{
         	log.error(e);
         }
 	}
+	
+	private void stopjifs(){
+		log.info("stop jifs");
+		try {
+         	FileSystemService fSS = (FileSystemService) InitialNaming.lookup(FileSystemService.NAME);
+    		final DeviceManager dm = DeviceUtils.getDeviceManager();
+    		JIFSDevice dev = (JIFSDevice)dm.getDevice("jifs");
+    		fSS.unregisterFileSystem(dev);
+    		dm.unregister(dev);
+	    } catch (NameNotFoundException e){
+    	   	log.error("filsystemservice / filesystemtype not found");
+        } catch (DeviceNotFoundException ex){
+        	log.info("no jifs present");
+        } catch (DriverException ex){
+        	log.error(ex);
+        }
+	}
+	
+	private void restartjifs(){
+		log.info("restart jifs");
+		stopjifs();
+		startjifs();
+	}
+	
+	
 }
---
+++ fs/src/fs/org/jnode/fs/jifs/def/JIFSPlugin.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+package org.jnode.fs.jifs.def;
+
+import org.apache.log4j.Logger;
+
+import org.jnode.fs.jifs.command.createJIFS;
+import org.jnode.shell.CommandLine;
+import org.jnode.plugin.Plugin;
+import org.jnode.plugin.PluginDescriptor;
+import org.jnode.plugin.PluginException;
+
+/**
+ * @author Andreas Haenel
+ */
+public class JIFSPlugin extends Plugin{
+
+	/** My logger */
+	private final Logger log = Logger.getLogger(getClass());
+	/** Manager of Extensions */
+    private final JIFSExtension jifsExtension;
+	
+	public JIFSPlugin(PluginDescriptor descriptor) {
+        super(descriptor);
+        this.jifsExtension = new JIFSExtension(descriptor
+                .getExtensionPoint("info"));
+        log.info("JIFSPlugin created.");
+    }
+	
+	/**
+     * Start this plugin
+     */
+    protected void startPlugin() throws PluginException {
+    	//create / mount JIFS
+    	try{
+    		createJIFS cJ = new createJIFS();
+    		cJ.execute(new CommandLine("start"),System.in,System.out,System.err);
+    		log.info("JIFSPlugin started.");
+    	} catch (Exception e){
+    		log.error(e);
+    	}
+    }
+
+    /**
+     * Stop this plugin
+     */
+    protected void stopPlugin() {
+//    	unmount / free JIFS
+    	try{
+    		createJIFS cJ = new createJIFS();
+    		cJ.execute(new CommandLine("stop"),System.in,System.out,System.err);
+    		log.info("JIFSPlugin stopped.");
+    	} catch (Exception e){
+    		log.error(e);
+    	}
+    }
+	
+}
