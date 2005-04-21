66b6dd2055479b3fc779556058afa23f0590b600
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-04-21 21:35:51 +0000

    started a javadoc alike doc for the plugins
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1383 407389f7-7c16-0410-9eee-ad85acb55d7f

--- builder/src/builder/org/jnode/build/documentation/PluginDocumentationTask.java
+++ builder/src/builder/org/jnode/build/documentation/PluginDocumentationTask.java
@@ -5,9 +5,20 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.types.FileSet;
 import org.jnode.build.AbstractPluginTask;
 import org.jnode.plugin.model.PluginDescriptorModel;
+import org.jnode.plugin.Library;
+import org.jnode.plugin.ExtensionPoint;
+import org.jnode.plugin.Extension;
+import org.jnode.plugin.ConfigurationElement;
+
+import com.lowagie.text.*;
+import com.lowagie.text.html.HtmlWriter;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.List;
+import java.awt.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,7 +31,7 @@ public class PluginDocumentationTask extends AbstractPluginTask
 {
   private LinkedList<FileSet> descriptorSets = new LinkedList<FileSet>();
   private File todir;
-
+  private final static String DOCHEADER = "JNode plugin documentation";
   public FileSet createDescriptors()
   {
     final FileSet fs = new FileSet();
@@ -118,7 +129,106 @@ public class PluginDocumentationTask extends AbstractPluginTask
 
     descriptors.put(fullId, descriptor);
 
+    final String ext = ".html";
+    Document document = new Document();
 
-//    throw new BuildException(pluginDescriptorModel.toString());
+    try
+    {
+      File file = new File(todir,pluginDescriptorModel.getId()+ext);
+
+      HtmlWriter.getInstance(document, new FileOutputStream(file));
+
+      document.open();
+      Table table = new Table(3);
+      Cell cell;
+
+      table.setBorderWidth(1);
+//      table.setBorderColor(new Color(0, 0, 0));
+      table.setPadding(0);
+      table.setSpacing(0);
+
+      cell = new Cell(DOCHEADER+" for "+pluginDescriptorModel.getId());
+      cell.setHeader(true);
+      cell.setColspan(3);
+      table.addCell(cell);
+      table.endHeaders();
+
+      table.addCell("Name");
+      cell = new Cell(pluginDescriptorModel.getName());
+      cell.setColspan(2);
+      table.addCell(cell);
+
+      table.addCell("Version");
+      cell = new Cell(pluginDescriptorModel.getVersion());
+      cell.setColspan(2);
+      table.addCell(cell);
+
+      table.addCell("Provider");
+      cell = new Cell(pluginDescriptorModel.getProviderName());
+      cell.setColspan(2);
+      table.addCell(cell);
+
+      if (pluginDescriptorModel.getRuntime() != null)
+      {
+        final String lib ="Library";
+        final String exports ="Exports packages";
+        for (Library library : pluginDescriptorModel.getRuntime().getLibraries())
+        {
+          table.addCell(lib);
+          cell = new Cell(library.getName());
+          cell.setColspan(2);
+          table.addCell(cell);
+
+          cell = new Cell(exports);
+          cell.setRowspan(library.getExports().length);
+          table.addCell(cell);
+          for (String export : library.getExports())
+          {
+            cell = new Cell(export);
+            cell.setColspan(2);
+            table.addCell(cell);
+          }
+        }
+      }
+
+      if (pluginDescriptorModel.getExtensions() != null)
+      {
+        final String extens ="Extension";
+        final String premis =" ";
+        for (Extension extension : pluginDescriptorModel.getExtensions())
+        {
+          table.addCell(extens);
+          cell = new Cell(extension.getExtensionPointUniqueIdentifier());
+          cell.setColspan(2);
+          table.addCell(cell);
+
+          cell = new Cell(premis);
+          cell.setRowspan(extension.getConfigurationElements().length);
+          cell.setColspan(1);
+          table.addCell(cell);
+
+          for (ConfigurationElement configurationElement : extension.getConfigurationElements())
+          {
+            cell = new Cell(configurationElement.getAttribute("class")+" name: \""+configurationElement.getAttribute("name")+"\" actions: \""+configurationElement.getAttribute("actions")+"\"");
+            cell.setColspan(2);
+            table.addCell(cell);
+          }
+
+        }
+      }
+
+
+      document.add(table);
+
+      document.close();
+    }
+    catch (FileNotFoundException e)
+    {
+      throw new BuildException(e.getMessage());
+    }
+    catch (DocumentException e)
+    {
+      throw new BuildException(e.getMessage());
+    }
   }
 }
