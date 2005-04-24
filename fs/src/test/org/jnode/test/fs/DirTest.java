bde2443aed782d9e662ef74ecde6b86ecab980c1
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-04-24 06:33:05 +0000

    Added File.list test
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1393 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/test/org/jnode/test/fs/DirTest.java
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ */
+package org.jnode.test.fs;
+
+import java.io.File;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public class DirTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        final String name = (args.length > 0) ? args[0] : "";
+        System.out.println("Creating File(\"" + name + "\")");
+        final File f = new File(name);
+        System.out.println("Created File(\"" + name + "\")");
+        
+        System.out.println("List entries on File(\"" + name + "\")");
+        final String[] entries = f.list();
+        System.out.println("Listed " + entries.length + " entries on File(\"" + name + "\")");
+        
+    }
+
+}
