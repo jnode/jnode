539fb74adc83301955d48a3f24b0d9c1991812df
Ewout Prangsma <ewout@prangsma.net>
2005-04-27 19:08:13 +0000

    Minor size optimizations
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1432 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/classmgr/VmLineNumberMap.java
+++ core/src/core/org/jnode/vm/classmgr/VmLineNumberMap.java
@@ -18,7 +18,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
+
 package org.jnode.vm.classmgr;
 
 import org.jnode.vm.VmSystemObject;
@@ -28,72 +28,74 @@ import org.jnode.vm.VmSystemObject;
  */
 public class VmLineNumberMap extends VmSystemObject {
 
-	/** Number of line number info's in this object */
-	private final int noLineNrs;
-	/**
-	 * Line number table.
-	 * The line-number table should be interpreted as follows:
-	 * Each line number entry consists of two entries in the following
-	 * array.
-	 * Entry i*2+0 = start_pc
-	 * Entry i*2+1 = line_number
-	 * So lnTable.length == noLineNrs*2
-	 */
-	private final char[] lnTable;
+    /**
+     * Line number table. The line-number table should be interpreted as
+     * follows: Each line number entry consists of two entries in the following
+     * array. Entry i*2+0 = start_pc Entry i*2+1 = line_number So lnTable.length ==
+     * noLineNrs*2
+     */
+    private final char[] lnTable;
 
-	public static final int LNT_STARTPC_OFS = 0;
-	public static final int LNT_LINENR_OFS = 1;
-	public static final int LNT_ELEMSIZE = 2;
+    public static final int LNT_STARTPC_OFS = 0;
 
-	public VmLineNumberMap(char[] lnTable) {
-		this.lnTable = lnTable;
-		this.noLineNrs = (lnTable == null) ? 0 : (lnTable.length / LNT_ELEMSIZE);
-	}
+    public static final int LNT_LINENR_OFS = 1;
 
-	/**
-	 * Gets the number of entries of this table
-	 * @return length of the table
-	 */
-	public int getLength() {
-		return noLineNrs;
-	}
+    public static final int LNT_ELEMSIZE = 2;
 
-	/**
-	 * Gets the start PC field of an entry in this table with a given index.
-	 * @param index
-	 * @return int
-	 */
-	public int getStartPCAt(int index) {
-		return lnTable[index * LNT_ELEMSIZE + LNT_STARTPC_OFS];
-	}
+    public VmLineNumberMap(char[] lnTable) {
+        this.lnTable = lnTable;
+    }
 
-	/**
-	 * Gets the line number field of an entry in this table with a given index.
-	 * @param index
-	 * @return int
-	 */
-	public int getLineNrAt(int index) {
-		return lnTable[index * LNT_ELEMSIZE + LNT_LINENR_OFS];
-	}
-	
-	/**
-	 * Gets the linenumber for a given PC.
-	 * @param pc
-	 * @return The line number
-	 */
-	public int findLineNr(int pc) {
-		int ln = 0;
-		final int len = noLineNrs * LNT_ELEMSIZE;
-		for (int i = 0; i < len; i += LNT_ELEMSIZE) {
-			final char startPC = lnTable[i + LNT_STARTPC_OFS];
-			final char lineNr = lnTable[i + LNT_LINENR_OFS];
-			if (startPC <= pc) {
-				ln = lineNr;
-			} else {
-				return ln;
-			}
-		}
-		return ln;
-	}
+    /**
+     * Gets the number of entries of this table
+     * 
+     * @return length of the table
+     */
+    public int getLength() {
+        return (lnTable == null) ? 0 : (lnTable.length / LNT_ELEMSIZE);
+    }
+
+    /**
+     * Gets the start PC field of an entry in this table with a given index.
+     * 
+     * @param index
+     * @return int
+     */
+    public int getStartPCAt(int index) {
+        return lnTable[index * LNT_ELEMSIZE + LNT_STARTPC_OFS];
+    }
+
+    /**
+     * Gets the line number field of an entry in this table with a given index.
+     * 
+     * @param index
+     * @return int
+     */
+    public int getLineNrAt(int index) {
+        return lnTable[index * LNT_ELEMSIZE + LNT_LINENR_OFS];
+    }
+
+    /**
+     * Gets the linenumber for a given PC.
+     * 
+     * @param pc
+     * @return The line number
+     */
+    public int findLineNr(int pc) {
+        int ln = 0;
+        if (lnTable != null) {
+            final int len = lnTable.length;
+            for (int i = 0; i < len; i += LNT_ELEMSIZE) {
+                final char startPC = lnTable[i + LNT_STARTPC_OFS];
+                final char lineNr = lnTable[i + LNT_LINENR_OFS];
+                if (startPC <= pc) {
+                    ln = lineNr;
+                } else {
+                    return ln;
+                }
+            }
+        }
+        return ln;
+    }
 
 }
