979decf04e6d2923a471d89b13c0c17a0fef99ec
Ewout Prangsma <ewout@prangsma.net>
2005-04-30 18:37:36 +0000

    Implemented "include" support in plugin-list files
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1456 407389f7-7c16-0410-9eee-ad85acb55d7f

--- builder/src/builder/org/jnode/build/PluginList.java
+++ builder/src/builder/org/jnode/build/PluginList.java
@@ -18,7 +18,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
+
 package org.jnode.build;
 
 import java.io.File;
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 
 import nanoxml.XMLElement;
 
@@ -42,147 +43,193 @@ import org.jnode.plugin.PluginException;
 /**
  * @author epr
  */
-public class PluginList {
+public final class PluginList {
 
-	private final URL[] descrList;
-	private final URL[] pluginList;
+    private final URL[] descrList;
+
+    private final URL[] pluginList;
+
     private final String name;
-	private Manifest manifest;
 
-	public PluginList(File file, File defaultDir, String targetArch) throws PluginException, MalformedURLException {
+    private Manifest manifest;
 
-		final ArrayList<URL> descrList = new ArrayList<URL>();
-		final ArrayList<URL> pluginList = new ArrayList<URL>();
-		final XMLElement root = new XMLElement(new Hashtable(), true, false);
-		try {
-			final FileReader r = new FileReader(file);
-			try {
-				root.parseFromReader(r);
-			} finally {
-				r.close();
-			}
-		} catch (IOException ex) {
-			throw new PluginException(ex);
-		}
-		if (!root.getName().equals("plugin-list")) {
-			throw new PluginException("plugin-list element expected");
-		}
+    private List<PluginList> includes;
 
-        this.name = (String)root.getAttribute("name");
+    public PluginList(File file, File defaultDir, String targetArch)
+            throws PluginException, MalformedURLException {
+
+        final ArrayList<URL> descrList = new ArrayList<URL>();
+        final ArrayList<URL> pluginList = new ArrayList<URL>();
+        final XMLElement root = new XMLElement(new Hashtable(), true, false);
+        try {
+            final FileReader r = new FileReader(file);
+            try {
+                root.parseFromReader(r);
+            } finally {
+                r.close();
+            }
+        } catch (IOException ex) {
+            throw new PluginException(ex);
+        }
+        if (!root.getName().equals("plugin-list")) {
+            throw new PluginException("plugin-list element expected");
+        }
+
+        this.name = (String) root.getAttribute("name");
         if (name == null) {
             throw new PluginException("name attribute is missing in " + file);
-        }        
-        
-		for (Iterator<?> i = root.getChildren().iterator(); i.hasNext();) {
+        }
 
-			final XMLElement e = (XMLElement) i.next();
-			if (e.getName().equals("plugin")) {
-				final String id = e.getStringAttribute("id");
+        for (Iterator< ? > i = root.getChildren().iterator(); i.hasNext();) {
 
-				final URL descrUrl;
-				final URL pluginUrl;
-				if (id != null) {
-					File f = findPlugin(defaultDir, id);
-					pluginUrl = f.toURL();
-					descrUrl = new URL("jar:" + pluginUrl + "!/plugin.xml");
-				} else {
-					throw new PluginException("id attribute expected on " + e.getName());
-				}
+            final XMLElement e = (XMLElement) i.next();
+            if (e.getName().equals("plugin")) {
+                final String id = e.getStringAttribute("id");
 
-                if(pluginList.contains(pluginUrl))
-                {
-                    throw new PluginException("can't use the same id("+id+") for multiple plugins");
+                final URL descrUrl;
+                final URL pluginUrl;
+                if (id != null) {
+                    File f = findPlugin(defaultDir, id);
+                    pluginUrl = f.toURL();
+                    descrUrl = new URL("jar:" + pluginUrl + "!/plugin.xml");
+                } else {
+                    throw new PluginException("id attribute expected on "
+                            + e.getName());
                 }
-                
-				descrList.add(descrUrl);
-				pluginList.add(pluginUrl);
-			} else if (e.getName().equals("manifest")) {
-				manifest = parseManifest(e);
-			} else {
-				throw new PluginException("Unknown element " + e.getName());
-			}
-		}
-		this.descrList = (URL[]) descrList.toArray(new URL[descrList.size()]);
-		this.pluginList = (URL[]) pluginList.toArray(new URL[pluginList.size()]);
-	}
 
-	private File findPlugin(File dir, final String id) {
-		//System.out.println("Find " + id + " in " + dir);
-		String[] names = dir.list(new FilenameFilter() {
-			/**
-			 * @param dir
-			 * @param name
-			 * @see java.io.FilenameFilter#accept(java.io.File, java.lang.String)
-			 * @return boolean
-			 */
-			public boolean accept(File dir, String name) {
-				return name.startsWith(id + "_") && name.endsWith(".jar");
-			}
-		});
+                if (pluginList.contains(pluginUrl)) {
+                    throw new PluginException("can't use the same id(" + id
+                            + ") for multiple plugins");
+                }
 
-		if (names.length == 0) {
-			throw new IllegalArgumentException("Cannot find plugin " + id + " in " + dir);
-		} else {
-			Arrays.sort(names);
-			return new File(dir, names[names.length - 1]);
-		}
-	}
+                descrList.add(descrUrl);
+                pluginList.add(pluginUrl);
+            } else if (e.getName().equals("manifest")) {
+                manifest = parseManifest(e);
+            } else if (e.getName().equals("include")) {
+                parseInclude(e, file.getParentFile(), defaultDir, targetArch,
+                        descrList, pluginList);
+            } else {
+                throw new PluginException("Unknown element " + e.getName());
+            }
+        }
+        this.descrList = (URL[]) descrList.toArray(new URL[descrList.size()]);
+        this.pluginList = (URL[]) pluginList
+                .toArray(new URL[pluginList.size()]);
+    }
 
-	private Manifest parseManifest(XMLElement me) throws PluginException {
-		Manifest mf = new Manifest();
-		for (Iterator<?> i = me.getChildren().iterator(); i.hasNext();) {
-			final XMLElement e = (XMLElement) i.next();
-			if (e.getName().equals("attribute")) {
-				final String k = e.getStringAttribute("key");
-				final String v = e.getStringAttribute("value");
-				try {
-					mf.addConfiguredAttribute(new Manifest.Attribute(k, v));
-				} catch (ManifestException ex) {
-					throw new PluginException("Error in manifest", ex);
-				}
-			} else {
-				throw new PluginException("Unknown element " + e.getName());
-			}
-		}
-		return mf;
-	}
-	
-	/**
-	 * Gets the maximum last modification date of all URL's
-	 * @return last modification date
-	 * @throws IOException
-	 */
-	public long lastModified() throws IOException {
-		long max = 0;
-		for (int i = 0; i < descrList.length; i++) {
-			final URLConnection conn2 = pluginList[i].openConnection();
-			max = Math.max(max, conn2.getLastModified());
-		}
-		return max;
-	}
+    private File findPlugin(File dir, final String id) {
+        // System.out.println("Find " + id + " in " + dir);
+        String[] names = dir.list(new FilenameFilter() {
+            /**
+             * @param dir
+             * @param name
+             * @see java.io.FilenameFilter#accept(java.io.File,
+             *      java.lang.String)
+             * @return boolean
+             */
+            public boolean accept(File dir, String name) {
+                return name.startsWith(id + "_") && name.endsWith(".jar");
+            }
+        });
 
-	/**
-	 * Gets all URL's to plugin descriptors
-	 * @return URL[]
-	 */
-	public URL[] getDescriptorUrlList() {
-		return descrList;
-	}
+        if (names.length == 0) {
+            throw new IllegalArgumentException("Cannot find plugin " + id
+                    + " in " + dir);
+        } else {
+            Arrays.sort(names);
+            return new File(dir, names[names.length - 1]);
+        }
+    }
 
-	/**
-	 * Gets all URL's to the plugin files (jar format)
-	 * @return URL[]
-	 */
-	public URL[] getPluginList() {
-		return pluginList;
-	}
-	
-	/**
-	 * @return Returns the manifest.
-	 */
-	public final Manifest getManifest() {
-		return this.manifest;
-	}
+    private Manifest parseManifest(XMLElement me) throws PluginException {
+        Manifest mf = this.manifest;
+        if (mf == null) {
+            mf = new Manifest();
+        }
+        for (Iterator< ? > i = me.getChildren().iterator(); i.hasNext();) {
+            final XMLElement e = (XMLElement) i.next();
+            if (e.getName().equals("attribute")) {
+                final String k = e.getStringAttribute("key");
+                final String v = e.getStringAttribute("value");
+                try {
+                    mf.addConfiguredAttribute(new Manifest.Attribute(k, v));
+                } catch (ManifestException ex) {
+                    throw new PluginException("Error in manifest", ex);
+                }
+            } else {
+                throw new PluginException("Unknown element " + e.getName());
+            }
+        }
+        return mf;
+    }
+
+    private void parseInclude(XMLElement e, File curDir, File defaultDir,
+            String targetArch, List<URL> descrList, List<URL> pluginList)
+            throws MalformedURLException, PluginException {
+        File file = new File(e.getStringAttribute("file"));
+        if (!file.isAbsolute()) {
+            file = new File(curDir, file.getPath());
+        }
+
+        final PluginList inc = new PluginList(file, defaultDir, targetArch);
+        
+        if (this.manifest == null) {
+            this.manifest = inc.manifest;
+        } else if (inc.manifest != null) {
+            try {
+                final Manifest merged = new Manifest();
+                merged.merge(inc.manifest);
+                merged.merge(this.manifest);
+                this.manifest = merged;
+            } catch (ManifestException ex) {
+                throw new PluginException(ex);
+            }
+        }
+        
+        descrList.addAll(Arrays.asList(inc.descrList));
+        pluginList.addAll(Arrays.asList(inc.pluginList));
+    }
+
+    /**
+     * Gets the maximum last modification date of all URL's
+     * 
+     * @return last modification date
+     * @throws IOException
+     */
+    public long lastModified() throws IOException {
+        long max = 0;
+        for (int i = 0; i < descrList.length; i++) {
+            final URLConnection conn2 = pluginList[i].openConnection();
+            max = Math.max(max, conn2.getLastModified());
+        }
+        return max;
+    }
+
+    /**
+     * Gets all URL's to plugin descriptors
+     * 
+     * @return URL[]
+     */
+    public URL[] getDescriptorUrlList() {
+        return descrList;
+    }
+
+    /**
+     * Gets all URL's to the plugin files (jar format)
+     * 
+     * @return URL[]
+     */
+    public URL[] getPluginList() {
+        return pluginList;
+    }
+
+    /**
+     * @return Returns the manifest.
+     */
+    public final Manifest getManifest() {
+        return this.manifest;
+    }
 
     /**
      * @return Returns the name.
