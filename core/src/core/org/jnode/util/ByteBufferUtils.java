4be0772adf7584b39db555944871ca9095785b0e
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-05 21:54:56 +0000

    added methods to convert a ByteBuffer to a byte[] and keep the array in sync with the ByteBuffer
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1519 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/util/ByteBufferUtils.java
+++ core/src/core/org/jnode/util/ByteBufferUtils.java
@@ -46,4 +46,44 @@ public class ByteBufferUtils
         
         dest.put(src);
     }
+    
+    public static ByteArray toByteArray(ByteBuffer buf)
+    {
+        return new ByteArray(buf);
+    }
+    
+    public static byte[] toArray(ByteBuffer buf)
+    {
+        byte[] array = new byte[buf.remaining()];
+        buf.get(array);
+        return array;
+    }
+    
+    public static class ByteArray
+    {
+        private ByteBuffer buf;        
+        private int bufPosition;
+        private int bufLimit;
+        private byte[] array;
+        
+        private ByteArray(ByteBuffer buf)
+        {
+            this.buf = buf;
+            this.bufPosition = buf.position();
+            this.bufLimit = buf.limit();
+            this.array = ByteBufferUtils.toArray(buf);
+        }
+        
+        public byte[] toArray()
+        {
+            return array;
+        }
+        
+        public void refreshByteBuffer()
+        {
+            buf.position(bufPosition);
+            buf.limit(bufLimit);
+            buf.put(array);
+        }
+    }
 }
