3162a4897566d632e3432fe38330bd4d115bb07b
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-08 16:48:48 +0000

    added possibility to configure log4j from an url or a file
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1545 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/log4j/Log4jCommand.java
+++ shell/src/shell/org/jnode/shell/command/log4j/Log4jCommand.java
@@ -21,8 +21,18 @@
  
 package org.jnode.shell.command.log4j;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.jnode.shell.help.FileArgument;
 import org.jnode.shell.help.Help;
 import org.jnode.shell.help.OptionArgument;
 import org.jnode.shell.help.Parameter;
@@ -30,12 +40,19 @@ import org.jnode.shell.help.ParsedArguments;
 import org.jnode.shell.help.StringArgument;
 import org.jnode.shell.help.Syntax;
 import org.jnode.shell.help.SyntaxErrorException;
+import org.jnode.shell.help.URLArgument;
 
 /**
  * @author Ewout Prangsma (epr@users.sourceforge.net)
+ * @author Fabien DUMINY (fduminy@jnode.org)
  */
 public class Log4jCommand {
-
+    static final OptionArgument ARG_ACTION = new OptionArgument("action",
+            "action to do on log4j", 
+            new OptionArgument.Option("level", "Change the level of a logger"), 
+            new OptionArgument.Option("url", "Configure log4j from an url"),
+            new OptionArgument.Option("file", "Configure log4j from a file"));
+    
 	static final OptionArgument ARG_LEVEL =
 		new OptionArgument(
 			"level",
@@ -46,25 +63,51 @@ public class Log4jCommand {
 				new OptionArgument.Option("warn", "Warning"),
 				new OptionArgument.Option("error", "Error"),
 				new OptionArgument.Option("fatal", "Fatal")});
-
+   
 	static final StringArgument ARG_LOGGER = new StringArgument("logger", "the name of the logger");
-
+    
+    static final FileArgument ARG_CFG_FILE = new FileArgument("configFile", "the configuration file");
+    static final URLArgument ARG_CFG_URL = new URLArgument("configURL", "the configuration URL");
+    
+    static final Parameter PARAM_ACTION = new Parameter(ARG_ACTION);    
 	static final Parameter PARAM_LOGGER = new Parameter(ARG_LOGGER, Parameter.MANDATORY);
-	static final Parameter PARAM_LEVEL = new Parameter(ARG_LEVEL, Parameter.MANDATORY);
+	static final Parameter PARAM_LEVEL = new Parameter(ARG_LEVEL, Parameter.MANDATORY);        
+    
+    static final Parameter PARAM_CFG_FILE = new Parameter(ARG_CFG_FILE, Parameter.MANDATORY);
+    static final Parameter PARAM_CFG_URL = new Parameter(ARG_CFG_URL, Parameter.MANDATORY);
 
-	public static Help.Info HELP_INFO = new Help.Info("log4j", new Syntax[] { new Syntax("Set the level for a logger", new Parameter[] { PARAM_LOGGER, PARAM_LEVEL })
-	});
+	public static Help.Info HELP_INFO = new Help.Info("log4j", new Syntax[] 
+                { 
+                    new Syntax("Set the level for a logger", new Parameter[] { PARAM_LOGGER, PARAM_LEVEL }),
+                    new Syntax("Set the configuration from a file", new Parameter[] { PARAM_CFG_FILE }),
+                    new Syntax("Set the configuration from an url", new Parameter[] { PARAM_CFG_URL })
+                });
 
-	public static void main(String[] args) throws SyntaxErrorException {
+	public static void main(String[] args) throws SyntaxErrorException, IOException {
 		final ParsedArguments cmdLine = HELP_INFO.parse(args);
-		final String loggerName = ARG_LOGGER.getValue(cmdLine);
-		final String levelName = ARG_LEVEL.getValue(cmdLine);
 		
-		final Logger log = Logger.getLogger(loggerName);
-		final Level level = Level.toLevel(levelName.toUpperCase());
-		
-		log.setLevel(level);
+        if(PARAM_CFG_FILE.isSet(cmdLine))
+        {
+            final File configFile = ARG_CFG_FILE.getFile(cmdLine);
+            final Properties props = new Properties();
+            final FileInputStream fis = new FileInputStream(configFile);
+            props.load(fis);
+            PropertyConfigurator.configure(props);
+            fis.close();
+        }
+        else if(PARAM_CFG_URL.isSet(cmdLine))
+        {
+            final URL configURL = ARG_CFG_URL.getURL(cmdLine);
+            PropertyConfigurator.configure(configURL);
+        }
+        else
+        {
+            final String loggerName = ARG_LOGGER.getValue(cmdLine);
+            final String levelName = ARG_LEVEL.getValue(cmdLine);
+    		final Logger log = Logger.getLogger(loggerName);
+    		final Level level = Level.toLevel(levelName.toUpperCase());
+    		
+    		log.setLevel(level);
+        }
 	}
-	
-	
 }
