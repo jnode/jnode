3a010d021dc0678f7dbe5f96505fa99f222cbcc0
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-15 15:44:20 +0000

    Moved nanohttpd command & classes to jnode-distr
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1602 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ distr/src/apps/org/jnode/apps/httpd/NanoHTTPDCommand.java
@@ -0,0 +1,90 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.jnode.apps.httpd;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import org.jnode.shell.Command;
+import org.jnode.shell.CommandLine;
+
+import fi.iki.elonen.NanoHTTPD;
+
+/**
+ * @author Martin Husted Hartvig (hagar@jnode.org)
+ */
+
+public class NanoHTTPDCommand implements Command
+{
+  public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception
+  {
+    File file = new File("/jnode/index.htm");  // ram disk is fat, so no long extension, I guess
+
+    if (!file.exists())
+    {
+      PrintWriter printWriter = new PrintWriter(new FileOutputStream(file));
+      printWriter.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n<html><body>JNode - Java New Operating System Design Effort</body></html>\n");
+      printWriter.close();
+    }
+
+    NanoHTTPD nanoHTTPD = new NanoHTTPD(80)
+    {
+      public Response serve(String uri, String method, Properties header, Properties parms)
+      {
+        return serveFile(uri, header, new File("/jnode"), true);
+      }
+
+    };
+
+    while (true)
+    {
+      try
+      {
+        Thread.sleep(250);
+      }
+      catch (InterruptedException e)
+      {
+        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+      }
+    }
+
+  }
+
+
+  public static void main(String[] args)
+  {
+    NanoHTTPDCommand nanoHTTPDCommand = new NanoHTTPDCommand();
+
+    try
+    {
+      nanoHTTPDCommand.execute(null, System.in, System.out, System.err);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    }
+  }
+}
