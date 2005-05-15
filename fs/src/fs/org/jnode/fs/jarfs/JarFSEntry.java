ccf52d15eb17249d8c6ef5d1a5bc804a9fe75edb
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-15 17:22:00 +0000

    added jar FS (not tested yet)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1607 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/fs/org/jnode/fs/jarfs/JarFSEntry.java
@@ -0,0 +1,134 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+ 
+package org.jnode.fs.jarfs;
+
+import java.io.IOException;
+import java.util.jar.JarEntry;
+
+import org.jnode.fs.FSAccessRights;
+import org.jnode.fs.FSDirectory;
+import org.jnode.fs.FSEntry;
+import org.jnode.fs.FSFile;
+import org.jnode.fs.FileSystem;
+import org.jnode.fs.ReadOnlyFileSystemException;
+import org.jnode.fs.util.FSUtils;
+
+/**
+ * 
+ * @author Fabien DUMINY (fduminy at users.sourceforge.net)
+ *
+ */
+public final class JarFSEntry implements FSEntry {
+
+    private JarFSEntry parent;
+    private String name;
+    private JarEntry jarEntry;
+    private JarFileSystem fs;
+
+    public JarFSEntry(JarFileSystem fs, JarFSEntry parentEntry, JarEntry jarEntry, String name) {
+        this.fs = fs;
+        this.parent = parentEntry;
+        this.jarEntry = jarEntry;
+        this.name = name;
+    }
+
+    public JarEntry getJarEntry()
+    {
+        return jarEntry;
+    }
+
+    public JarFSEntry getParentFSEntry()
+    {
+        return parent;
+    }
+    
+    /**
+     * @see org.jnode.fs.FSEntry#getName()
+     */
+    public String getName() {
+        return name;
+    }
+
+    public FSDirectory getParent()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public long getLastModified() throws IOException
+    {
+        return jarEntry.getTime();
+    }
+
+    public boolean isFile()
+    {
+        return !jarEntry.isDirectory();
+    }
+
+    public boolean isDirectory()
+    {
+        return jarEntry.isDirectory();
+    }
+
+    public void setName(String newName) throws IOException
+    {
+        throw new ReadOnlyFileSystemException("jar file system is readonly");
+    }
+
+    public void setLastModified(long lastModified) throws IOException
+    {
+        throw new ReadOnlyFileSystemException("jar file system is readonly");
+    }
+
+    public FSFile getFile() throws IOException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public FSDirectory getDirectory() throws IOException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public FSAccessRights getAccessRights() throws IOException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean isDirty() throws IOException
+    {
+        return false;
+    }
+
+    public boolean isValid()
+    {
+        return true;
+    }
+
+    public FileSystem getFileSystem()
+    {
+        return fs;
+    }
+}
---
+++ fs/src/fs/org/jnode/fs/jarfs/JarFileSystemType.java
@@ -0,0 +1,70 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+ 
+package org.jnode.fs.jarfs;
+
+import org.jnode.driver.Device;
+import org.jnode.driver.block.FSBlockDeviceAPI;
+import org.jnode.driver.block.JarFileDevice;
+import org.jnode.fs.FileSystem;
+import org.jnode.fs.FileSystemException;
+import org.jnode.fs.FileSystemType;
+import org.jnode.fs.partitions.PartitionTableEntry;
+
+/**
+ * 
+ * @author Fabien DUMINY (fduminy at users.sourceforge.net)
+ *
+ */
+public class JarFileSystemType implements FileSystemType {
+
+    public static final String NAME = "jar";
+
+    public final String getName() {
+        return NAME;
+    }
+
+    /**
+     * @see org.jnode.fs.FileSystemType#supports(PartitionTableEntry, byte[],
+     *      FSBlockDeviceAPI)
+     */
+    public boolean supports(PartitionTableEntry pte, byte[] firstSector,
+            FSBlockDeviceAPI devApi) {
+        return false;
+    }
+
+    /**
+     * @see org.jnode.fs.FileSystemType#create(Device, boolean)
+     */
+    public FileSystem create(Device device, boolean readOnly) throws FileSystemException {
+        // jar file systems are always readOnly        
+        return new JarFileSystem((JarFileDevice) device);
+    }
+
+    /**
+     * @see org.jnode.fs.FileSystemType#format(org.jnode.driver.Device,
+     *      java.lang.Object)
+     */
+    public FileSystem format(Device device, Object specificOptions)
+            throws FileSystemException {
+        throw new FileSystemException("Not yet implemented");
+    }
+}
