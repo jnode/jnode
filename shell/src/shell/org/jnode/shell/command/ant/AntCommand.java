c8e2ebba7e59f4afe213e1664742d43cd63621b8
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-16 21:01:36 +0000

    now use ant.Main instead of AntLauncher
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1636 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/ant/AntCommand.java
+++ shell/src/shell/org/jnode/shell/command/ant/AntCommand.java
@@ -22,103 +22,69 @@
 package org.jnode.shell.command.ant;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.JarURLConnection;
 import java.net.URL;
+import java.security.Permission;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.jar.JarFile;
 
-import org.apache.tools.ant.launch.Launcher;
+import org.apache.tools.ant.Main;
 import org.apache.tools.ant.launch.Locator;
+import org.jnode.security.JNodeSecurityManager;
+import org.jnode.security.JNodeSecurityManager.SecurityManagerListener;
 import org.jnode.shell.help.ParsedArguments;
 
 /**
- * TODO implement the command when jar file system will be ready
  * @author Fabien DUMINY (fduminy at users.sourceforge.net)
  *
  */
 public class AntCommand
 {
-//    static final ClassNameArgument ARG_CLASS = new ClassNameArgument("classname", "the class to excute");
-//    static final Argument ARG_ARGS = new Argument("arg", "the argument(s) to pass to the class", Argument.MULTI);
-//
-//    public static Help.Info HELP_INFO = new Help.Info(
-//        "java",
-//        "Execute the main method of the given java class",
-//        new Parameter[]{
-//            new Parameter(ARG_CLASS, Parameter.MANDATORY),
-//            new Parameter(ARG_ARGS, Parameter.OPTIONAL)
-//        }
-//    );
-
     public static void main(String[] args)
     throws Exception {
-        //AntCommand.execute(HELP_INFO.parse(args), System.in, System.out, System.err);
+        makeBuildXml("build.xml");
+        
+        //final Set<Permission> permissions = new HashSet<Permission>();  
+//        SecurityManagerListener l = new SecurityManagerListener()
+//        {
+//
+//            public boolean checkPermission(Permission perm)
+//            {
+//                //permissions.add(perm);
+//                return false; // false : don't check the permission
+//            }
+//            
+//        };
+//        
+//        ((JNodeSecurityManager) System.getSecurityManager()).setListener(l);
+        Main.main(args);
+//        ((JNodeSecurityManager) System.getSecurityManager()).setListener(null);
 
-        File sourceJar = getClassSource(Launcher.class);
-        
-        String[] args2 = new String[0];
-        Launcher.main(args2);
-    }
-
-    /**
-     * Execute this command
-     */
-    public static void execute(
-        ParsedArguments cmdLine,
-        InputStream in,
-        PrintStream out,
-        PrintStream err)
-        throws Exception {
-        
+//        System.out.println("permissions:\n");            
+//        for(Permission p : permissions)
+//        {
+//            System.out.println(p);            
+//        }
     }
     
-    // Locator.getClassSource : 
-    public static File getClassSource(Class c) throws IOException {
-        System.out.println("class="+c.getName());
-        String classResource = c.getName().replace('.', '/') + ".class";
-        System.out.println("classResource="+classResource);
-        return getResourceSource(c.getClassLoader(), classResource);
+    public static void makeBuildXml(String file) throws Exception 
+    {
+        FileOutputStream fos = new FileOutputStream(new File(file));
+        PrintWriter pw = new PrintWriter(fos);
+        pw.println("<project name=\"JNode\" default=\"help\" basedir=\".\">");
+        pw.println("<target name=\"help\">");
+        pw.println("<echo>");
+        pw.println("echo task is working");
+        pw.println("</echo>");
+        pw.println("</target>");
+        pw.println("</project>");
+        pw.close();
+        fos.close();
     }
-    
-    // Locator.getResourceSource : 
-    public static File getResourceSource(ClassLoader c, String resource) throws IOException {
-        if (c == null) {
-            c = Locator.class.getClassLoader();
-        }
-        System.out.println("ClassLoader="+((c != null) ? c.getClass().getName() : ""));
-        
-        URL url = null;
-        if (c == null) {
-            url = ClassLoader.getSystemResource(resource);
-            System.out.println("url(1)="+url);            
-        } else {
-            url = c.getResource(resource);
-            System.out.println("url(2)="+url);
-        }
-        
-        // Create a URL that refers to an entry in the jar file
-        url = new URL("jar:file:/c:/almanac/my.jar!/com/mycompany/MyClass.class");
-    
-        // Get the jar file
-        JarURLConnection conn = (JarURLConnection)url.openConnection();
-        JarFile jarfile = conn.getJarFile();
-        
-        if (url != null) {
-            String u = url.toString();
-            if (u.startsWith("jar:file:")) {
-                int pling = u.indexOf("!");
-                String jarName = u.substring(4, pling);
-                System.out.println("jarName="+jarName);
-                return new File(Locator.fromURI(jarName));
-            } else if (u.startsWith("file:")) {
-                int tail = u.indexOf(resource);
-                String dirName = u.substring(0, tail);
-                System.out.println("dirName="+u);
-                return new File(Locator.fromURI(dirName));
-            }
-        }
-        return null;
-    }    
 }
