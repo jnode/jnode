26d5148a7651ca9d1d10415aa95e6b3e5388ffe7
Ewout Prangsma <ewout@prangsma.net>
2005-05-25 12:27:22 +0000

    Renamed org.jnode.driver.usb.storage to org.jnode.driver.block.usb.storage
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1708 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/driver/org/jnode/driver/block/usb/storage/USBStorageConstants.java
@@ -0,0 +1,66 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+ 
+package org.jnode.driver.block.usb.storage;
+
+/**
+ * @author flesire
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public interface USBStorageConstants {
+    /* Sub Classes */
+    public final static int US_SC_RBC	=0x01;		/* Typically, flash devices */
+    public final static int US_SC_8020	=0x02;		/* CD-ROM */
+    public final static int US_SC_QIC	=0x03;		/* QIC-157 Tapes */
+    public final static int US_SC_UFI	=0x04;		/* Floppy */
+    public final static int US_SC_8070	=0x05;		/* Removable media */
+    public final static int US_SC_SCSI	=0x06;		/* Transparent */
+    public final static int US_SC_ISD200    =0x07;		/* ISD200 ATA */
+    
+    public final static int US_SC_MIN	=US_SC_RBC;
+    public final static int US_SC_MAX	=US_SC_ISD200;
+    public final static int US_SC_DEVICE	=0xff;		/* Use device's value */
+
+    /* Protocols */
+    public final static int US_PR_CBI	=0x00;		/* Control/Bulk/Interrupt */
+    public final static int US_PR_CB	=0x01;		/* Control/Bulk w/o interrupt */
+    public final static int US_PR_BULK	=0x50;		/* bulk only */
+    public final static int US_PR_SCM_ATAPI	=0x80;		/* SCM-ATAPI bridge */
+    public final static int US_PR_EUSB_SDDR09	=0x81;	/* SCM-SCSI bridge for SDDR-09 */
+    public final static int US_PR_SDDR55	=0x82;		/* SDDR-55 (made up) */
+    public final static int US_PR_DPCM_USB  =0xf0;		/* Combination CB/SDDR09 */
+    public final static int US_PR_FREECOM   =0xf1;		/* Freecom */
+    public final static int US_PR_DATAFAB   =0xf2;		/* Datafab chipsets */
+    public final static int US_PR_JUMPSHOT  =0xf3;		/* Lexar Jumpshot */
+    public final static int US_PR_DEVICE	=0xff;		/* Use device's value */
+    
+    /* Transport */
+    public final static int CB_RESET_CMD_SIZE = 	12;
+    
+    public final static int  US_BULK_CB_WRAP_LEN = 31;
+    public final static int  US_BULK_CB_SIGN	 = 0x43425355;	/*spells out USBC */
+    public final static int  US_BULK_FLAG_IN	 = 1;	
+    public final static int  US_BULK_FLAG_OUT	 = 0;
+
+
+}
---
+++ fs/src/driver/org/jnode/driver/block/usb/storage/USBStorageDeviceToDriverMapper.java
@@ -0,0 +1,101 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+ 
+package org.jnode.driver.block.usb.storage;
+
+import org.apache.log4j.Logger;
+import org.jnode.driver.Device;
+import org.jnode.driver.DeviceToDriverMapper;
+import org.jnode.driver.Driver;
+import org.jnode.driver.usb.InterfaceDescriptor;
+import org.jnode.driver.usb.USBConfiguration;
+import org.jnode.driver.usb.USBConstants;
+import org.jnode.driver.usb.USBDevice;
+import org.jnode.driver.usb.USBInterface;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public class USBStorageDeviceToDriverMapper implements DeviceToDriverMapper, USBStorageConstants, USBConstants {
+	/** My logger */
+	private static final Logger log = Logger.getLogger(USBStorageDeviceToDriverMapper.class);
+	/**
+	 * @see org.jnode.driver.DeviceToDriverMapper#findDriver(org.jnode.driver.Device)
+	 */
+	public Driver findDriver(Device device) {
+		if (!(device instanceof USBDevice)) {
+			return null;
+		}
+		final USBDevice dev = (USBDevice) device;
+		if (dev.getDescriptor().getDeviceClass() != USB_CLASS_PER_INTERFACE) {
+			return null;
+		}
+		final USBConfiguration conf = dev.getConfiguration(0);
+		final USBInterface intf = conf.getInterface(0);
+		final InterfaceDescriptor descr = intf.getDescriptor();
+		if (descr.getInterfaceClass() != USB_CLASS_MASS_STORAGE) {
+			return null;
+		}
+		log.debug("Found mass storage: " + descr);
+		switch (descr.getInterfaceSubClass()) {
+			case US_SC_RBC :
+				// 				TODO Implement driver.
+				log.info("Driver for subclass" + descr.getInterfaceSubClass() + "Not yet implemented");
+				return null;
+			case US_SC_8020 :
+				//				TODO Implement driver.
+				log.info("Driver for subclass" + descr.getInterfaceSubClass() + "Not yet implemented");
+				return null;
+			case US_SC_QIC :
+				//				TODO Implement driver.
+				log.info("Driver for subclass" + descr.getInterfaceSubClass() + "Not yet implemented");
+				return null;
+			case US_SC_UFI :
+				//				TODO Implement driver.
+				log.info("Driver for subclass" + descr.getInterfaceSubClass() + "Not yet implemented");
+				return null;
+			case US_SC_8070 :
+				//				TODO Implement driver.
+				log.info("Driver for subclass" + descr.getInterfaceSubClass() + "Not yet implemented");
+				return null;
+			case US_SC_SCSI :
+				return new USBStorageSCSIDriver();
+			case US_SC_ISD200 :
+				//				TODO Implement driver.
+				log.info("Driver for subclass" + descr.getInterfaceSubClass() + "Not yet implemented");
+				return null;
+			default :
+				return null;
+		}
+	}
+	/**
+	 * Gets the matching level of this mapper. The mappers are queried in order
+	 * of match level. This will ensure the best available driver for a device.
+	 * 
+	 * @return One of the MATCH_xxx constants.
+	 * @see #MATCH_DEVICE_REVISION
+	 * @see #MATCH_DEVICE
+	 * @see #MATCH_DEVCLASS
+	 */
+	public int getMatchLevel() {
+		return MATCH_DEVCLASS;
+	}
+}
