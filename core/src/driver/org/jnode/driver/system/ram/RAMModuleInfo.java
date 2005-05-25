59aef149730dbe5f9e3d6cbf7b03689461744cdb
Ewout Prangsma <ewout@prangsma.net>
2005-05-25 11:33:44 +0000

    Renamed org.jnode.driver.ram to org.jnode.driver.system.ram
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1699 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/driver/org/jnode/driver/system/ram/RAMModuleInfo.java
@@ -0,0 +1,57 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+ 
+package org.jnode.driver.system.ram;
+
+/**
+ * RAMModuleInfo.
+ * 
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Licence: GNU LGPL
+ * </p>
+ * <p>
+ * </p>
+ * 
+ * @author Francois-Frederic Ozog
+ * @version 1.0
+ */
+
+public class RAMModuleInfo {
+
+	//private String brand;
+	//private int type;
+	//private long size;
+	//private double speed;
+	//private double refreshRate;
+
+	public RAMModuleInfo() {
+	}
+
+	public boolean isOccupied() {
+		return true;
+	}
+}
