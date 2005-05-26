af7d301b9b6f786c8b209b11cb89b160c5abf869
markhale <markhale@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-26 21:55:54 +0000

    Implemented some SocketOptions in TCPSocketImpl.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1724 407389f7-7c16-0410-9eee-ad85acb55d7f

--- net/src/net/org/jnode/net/ipv4/tcp/TCPInChannel.java
+++ net/src/net/org/jnode/net/ipv4/tcp/TCPInChannel.java
@@ -18,7 +18,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
+
 package org.jnode.net.ipv4.tcp;
 
 import java.net.SocketException;
@@ -33,200 +33,217 @@ import org.jnode.net.ipv4.IPv4Header;
  */
 public class TCPInChannel {
 
-	/** My logger */
-	private static final Logger log = Logger.getLogger(TCPInChannel.class);
-	/** Segments that have been received, but are out of order */
-	private final LinkedList<TCPInSegment> futureSegments = new LinkedList<TCPInSegment>();
-	/** The next expected sequence number */
-	private int rcv_next;
-	/** The input data buffer */
-	private final TCPDataBuffer dataBuffer;
-	/** The control block I belong to */
-	private final TCPControlBlock controlBlock;
-	/** Has a FIN been received? */
-	private boolean finReceived;
+    /**
+     * My logger
+     */
+    private static final Logger log = Logger.getLogger(TCPInChannel.class);
+    /**
+     * Segments that have been received, but are out of order
+     */
+    private final LinkedList<TCPInSegment> futureSegments = new LinkedList<TCPInSegment>();
+    /**
+     * The next expected sequence number
+     */
+    private int rcv_next;
+    /**
+     * The input data buffer
+     */
+    private final TCPDataBuffer dataBuffer;
+    /**
+     * The control block I belong to
+     */
+    private final TCPControlBlock controlBlock;
+    /**
+     * Has a FIN been received?
+     */
+    private boolean finReceived;
 
-	/**
-	 * Create a new instance
-	 *  
-	 */
-	public TCPInChannel(TCPControlBlock controlBlock) {
-		this.controlBlock = controlBlock;
-		this.dataBuffer = new TCPDataBuffer(TCPConstants.TCP_BUFFER_SIZE);
-		this.finReceived = false;
-	}
+    /**
+     * Create a new instance
+     */
+    public TCPInChannel(TCPControlBlock controlBlock) {
+        this.controlBlock = controlBlock;
+        this.dataBuffer = new TCPDataBuffer(TCPConstants.TCP_BUFFER_SIZE);
+        this.finReceived = false;
+    }
 
-	/**
-	 * Initialize the initial sequence nr from the foreign side.
-	 * 
-	 * @param hdr
-	 */
-	public void initISN(TCPHeader hdr) {
-		this.rcv_next = hdr.getSequenceNr() + 1;
-	}
+    /**
+     * Initialize the initial sequence nr from the foreign side.
+     *
+     * @param hdr
+     */
+    public void initISN(TCPHeader hdr) {
+        this.rcv_next = hdr.getSequenceNr() + 1;
+    }
 
-	/**
-	 * Process received data
-	 * 
-	 * @param ipHdr
-	 * @param hdr
-	 * @param skbuf
-	 */
-	public void processData(IPv4Header ipHdr, TCPHeader hdr, SocketBuffer skbuf) throws SocketException {
-		final int seqNr = hdr.getSequenceNr();
-		// Check the seq-nr
-		if (TCPUtils.SEQ_LT(seqNr, rcv_next)) {
-			// Ignore segment, we've already got it
-			log.debug("Ignoring segment because we already got it");
-			return;
-		}
-		
-		if (hdr.isFlagFinishedSet()) {
-			finReceived = true;
-		}
+    /**
+     * Process received data
+     *
+     * @param ipHdr
+     * @param hdr
+     * @param skbuf
+     */
+    public void processData(IPv4Header ipHdr, TCPHeader hdr, SocketBuffer skbuf) throws SocketException {
+        final int seqNr = hdr.getSequenceNr();
+        // Check the seq-nr
+        if (TCPUtils.SEQ_LT(seqNr, rcv_next)) {
+            // Ignore segment, we've already got it
+            log.debug("Ignoring segment because we already got it");
+            return;
+        }
 
-		if (seqNr == rcv_next) {
-			// This segment is the first expected segment
-			if (processNextSegment(hdr, skbuf)) {
-				// See if we have the next segment already in the list
-				TCPInSegment seg;
-				while ((seg = findNextSegment()) != null) {
-					// Next segment was in the list
-					if (processNextSegment(seg.hdr, seg.skbuf)) {
-						// Segment fully processed, remove it from the list
-						futureSegments.remove(seg);
-					} else {
-						// No space left, stop sending data to application
-						break;
-					}
-				}
-			}
-		}
-	}
+        if (hdr.isFlagFinishedSet()) {
+            finReceived = true;
+        }
 
-	/**
-	 * Process the given segment (that must be the next expected segment). The data will be send to
-	 * the input data buffer, if there is enough space left in the buffer.
-	 * 
-	 * @param hdr
-	 * @param skbuf
-	 * @return True if the segment has been fully processed, false otherwise.
-	 * @throws SocketException
-	 */
-	private synchronized boolean processNextSegment(TCPHeader hdr, SocketBuffer skbuf) throws SocketException {
-		final int seqNr = hdr.getSequenceNr();
-		if (seqNr != rcv_next) {
-			throw new IllegalArgumentException("hdr.seqNr != rcv_next");
-		}
+        if (seqNr == rcv_next) {
+            // This segment is the first expected segment
+            if (processNextSegment(hdr, skbuf)) {
+                // See if we have the next segment already in the list
+                TCPInSegment seg;
+                while ((seg = findNextSegment()) != null) {
+                    // Next segment was in the list
+                    if (processNextSegment(seg.hdr, seg.skbuf)) {
+                        // Segment fully processed, remove it from the list
+                        futureSegments.remove(seg);
+                    } else {
+                        // No space left, stop sending data to application
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
-		// This segment is the first expected segment
-		// Sent it to the application if there is enough space
-		final int dataLength = hdr.getDataLength();
-		if (dataLength > dataBuffer.getFreeSize()) {
-			// Not enough free space, ignore this segment, it will be retransmitted.
-			log.debug("nextSegment dropped due to lack of space");
-			return false;
-		} else {
-			// Enough space, save
-			if (dataLength > 0) {
-				dataBuffer.add(skbuf, 0, dataLength);
-				// Update rcv_next
-				rcv_next += dataLength;
-			}
-			final boolean fin = hdr.isFlagFinishedSet();
-			final boolean syn = hdr.isFlagSynchronizeSet();
-			if (syn || fin) {
-				// SYN & FIN take up 1 seq-nr
-				rcv_next++;
-			}
-			if ((dataLength > 0) || fin) {
-				// And ACK it
-				controlBlock.sendACK(0, rcv_next);
-			}
-			// Notify threads blocked in read
-			notifyAll();
-			// We've processed it fully
-			return true;
-		}
-	}
+    /**
+     * Process the given segment (that must be the next expected segment). The data will be send to
+     * the input data buffer, if there is enough space left in the buffer.
+     *
+     * @param hdr
+     * @param skbuf
+     * @return True if the segment has been fully processed, false otherwise.
+     * @throws SocketException
+     */
+    private synchronized boolean processNextSegment(TCPHeader hdr, SocketBuffer skbuf) throws SocketException {
+        final int seqNr = hdr.getSequenceNr();
+        if (seqNr != rcv_next) {
+            throw new IllegalArgumentException("hdr.seqNr != rcv_next");
+        }
 
-	/**
-	 * Find the segment that has sequnce-nr equal to rcv_next.
-	 * 
-	 * @return The segment or null if not found
-	 */
-	private TCPInSegment findNextSegment() {
-		for (TCPInSegment seg : futureSegments) {
-			if (seg.getSeqNr() == rcv_next) {
-				return seg;
-			}
-		}
-		return null;
-	}
+        // This segment is the first expected segment
+        // Sent it to the application if there is enough space
+        final int dataLength = hdr.getDataLength();
+        if (dataLength > dataBuffer.getFreeSize()) {
+            // Not enough free space, ignore this segment, it will be retransmitted.
+            log.debug("nextSegment dropped due to lack of space");
+            return false;
+        } else {
+            // Enough space, save
+            if (dataLength > 0) {
+                dataBuffer.add(skbuf, 0, dataLength);
+                // Update rcv_next
+                rcv_next += dataLength;
+            }
+            final boolean fin = hdr.isFlagFinishedSet();
+            final boolean syn = hdr.isFlagSynchronizeSet();
+            if (syn || fin) {
+                // SYN & FIN take up 1 seq-nr
+                rcv_next++;
+            }
+            if ((dataLength > 0) || fin) {
+                // And ACK it
+                controlBlock.sendACK(0, rcv_next);
+            }
+            // Notify threads blocked in read
+            notifyAll();
+            // We've processed it fully
+            return true;
+        }
+    }
 
-	/**
-	 * Return the number of available bytes in the input buffer.
-	 */
-	public int available() {
-		return dataBuffer.getUsed();
-	}
+    /**
+     * Find the segment that has sequnce-nr equal to rcv_next.
+     *
+     * @return The segment or null if not found
+     */
+    private TCPInSegment findNextSegment() {
+        for (TCPInSegment seg : futureSegments) {
+            if (seg.getSeqNr() == rcv_next) {
+                return seg;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * Read data from the input buffer up to len bytes long. Block until there is data available.
-	 * 
-	 * @param dst
-	 * @param off
-	 * @param len
-	 * @return The number of bytes read
-	 */
-	public synchronized int read(byte[] dst, int off, int len) throws SocketException {
-		while ((dataBuffer.getUsed() == 0) && !controlBlock.isReset() && !isEOF()) {
-			try {
-				wait();
-			} catch (InterruptedException ex) {
-				// Ignore
-			}
-		}
-		if (controlBlock.isReset()) {
-			throw new SocketException("Connection reset");
-		} else if (isEOF()) {
-			return -1;
-		} else {
-			return dataBuffer.read(dst, off, len);
-		}
-	}
+    /**
+     * Return the number of available bytes in the input buffer.
+     */
+    public int available() {
+        return dataBuffer.getUsed();
+    }
 
-	/**
-	 * Notify a connection reset
-	 */
-	public synchronized void notifyConnectionReset() {
-		notifyAll();
-	}
-	/**
-	 * @return Returns the rcv_next.
-	 */
-	public final int getRcvNext() {
-		return this.rcv_next;
-	}
+    public int getBufferSize() {
+        return dataBuffer.getLength();
+    }
 
-	/**
-	 * Has the End of the InputChannel been reached?
-	 * @return True if EOF has been reached, false otherwise
-	 */
-	protected final boolean isEOF() {
-		if (!finReceived) {
-			// Other site has not closed
-			return false;
-		}
-		if (dataBuffer.getUsed() > 0) {
-			// Still data in databuffer
-			return false;
-		}
-		if (!futureSegments.isEmpty()) {
-			// Still future segments
-			return false;
-		}
-		// TODO No other requirements here?????
-		return true;
-	}
+    /**
+     * Read data from the input buffer up to len bytes long. Block until there is data available.
+     *
+     * @param dst
+     * @param off
+     * @param len
+     * @return The number of bytes read
+     */
+    public synchronized int read(byte[] dst, int off, int len) throws SocketException {
+        while ((dataBuffer.getUsed() == 0) && !controlBlock.isReset() && !isEOF()) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                // Ignore
+            }
+        }
+        if (controlBlock.isReset()) {
+            throw new SocketException("Connection reset");
+        } else if (isEOF()) {
+            return -1;
+        } else {
+            return dataBuffer.read(dst, off, len);
+        }
+    }
+
+    /**
+     * Notify a connection reset
+     */
+    public synchronized void notifyConnectionReset() {
+        notifyAll();
+    }
+
+    /**
+     * @return Returns the rcv_next.
+     */
+    public final int getRcvNext() {
+        return this.rcv_next;
+    }
+
+    /**
+     * Has the End of the InputChannel been reached?
+     *
+     * @return True if EOF has been reached, false otherwise
+     */
+    protected final boolean isEOF() {
+        if (!finReceived) {
+            // Other site has not closed
+            return false;
+        }
+        if (dataBuffer.getUsed() > 0) {
+            // Still data in databuffer
+            return false;
+        }
+        if (!futureSegments.isEmpty()) {
+            // Still future segments
+            return false;
+        }
+        // TODO No other requirements here?????
+        return true;
+    }
 }
--- net/src/net/org/jnode/net/ipv4/tcp/TCPOutChannel.java
+++ net/src/net/org/jnode/net/ipv4/tcp/TCPOutChannel.java
@@ -18,7 +18,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
+
 package org.jnode.net.ipv4.tcp;
 
 import java.net.SocketException;
@@ -32,189 +32,217 @@ import org.jnode.net.ipv4.IPv4Header;
  * @author Ewout Prangsma (epr@users.sourceforge.net)
  */
 public class TCPOutChannel {
-	
-	/** My logger */
-	private static final Logger log = Logger.getLogger(TCPOutChannel.class);
-	/** The protocol */
-	private final TCPProtocol tcp;
-	/** All unacked segments */
-	private final LinkedList<TCPOutSegment> unackedSegments = new LinkedList<TCPOutSegment>();
-	/** The outgoing databuffer */
-	private final TCPDataBuffer dataBuffer;
-	/** Send unacknowledged */
-	private int snd_unack;
-	/** Send next seq-nr */
-	private int snd_next;
-	/** Highest seq-nr sent; used to recognize retransmits */
-	private int snd_max;
-	/** Maximum segment size (determined by the foreign part of the connection */
-	private int mss;
-	/** The control block I belong to */
-	private final TCPControlBlock controlBlock;
-	/** Number of ticks before a retransmit timeout */
-	private int timeoutTicks = 6;
-	
-	/**
-	 * Create a new instance
-	 */
-	public TCPOutChannel(TCPProtocol tcp, TCPControlBlock controlBlock, int isn) {
-		this.tcp = tcp;
-		this.controlBlock = controlBlock;
-		this.dataBuffer = new TCPDataBuffer(TCPConstants.TCP_BUFFER_SIZE);
-		this.snd_unack = isn;
-		this.snd_next = isn;
-		this.snd_max = isn;
-		this.mss = TCPConstants.TCP_DEFAULT_MSS;
-	}
-	
-	/**
-	 * Process an ack-nr.
-	 * Remove all segments that have been acknowledged and remove
-	 * the occupied data from the databuffer.
-	 * @param ackNr
-	 */
-	public synchronized void processAck(int ackNr) {
-		// Is the ack valid?
-		if (snd_unack == ackNr) {
-			// Not a new ack, ignore it
-			return;
-		} else if (!TCPUtils.SEQ_LT(snd_unack, ackNr)) {
-			// snd_unack < ackNr violated
-			log.debug("snd_unack < ackNr violated");
-			return;
-		}
-		if (!TCPUtils.SEQ_LE(ackNr, snd_max)) {
-			// ackNr <= snd_max violated
-			log.debug("ackNr <= snd_max violated");
-			return;
-		}
-		
-		// The ackNr is valid
-		final int diff = ackNr - snd_unack;
-		snd_unack = ackNr;
-		// Remove data from the databuffer
-		dataBuffer.pull(diff);
-		for (Iterator<TCPOutSegment> i = unackedSegments.iterator(); i.hasNext(); ) {
-			final TCPOutSegment seg = (TCPOutSegment)i.next();
-			final int seqNr = seg.getSeqNr();
-			if (TCPUtils.SEQ_LT(seqNr, ackNr)) {
-				// Remove the segment
-				i.remove();
-			} else {
-				// Adjust the dataOffset
-				seg.adjustDataOffset(diff);
-			}
-		}
-		// Notify any blocked threads
-		notifyAll();
-	}
-	
-	/**
-	 * Process timeout handling
-	 */
-	public void timeout() 
-	throws SocketException {
-		for (TCPOutSegment seg : unackedSegments) {
-			seg.timeout(tcp);
-		}
-	}
-	
-	/**
-	 * Send a TCP segment containing no data
-	 * @param ipHdr
-	 * @param hdr
-	 */
-	public void send(IPv4Header ipHdr, TCPHeader hdr)
-	throws SocketException {
-		// Check the datalength
-		if (hdr.getDataLength() != 0) {
-			throw new IllegalArgumentException("dataLength must be 0");
-		}
-		// Do the actual send
-		sendHelper(ipHdr, hdr, 0);
-	}
 
-	/**
-	 * Send a TCP segment containing the given data.
-	 * This method blocks until there is enough space in the output buffer
-	 * to hold the data.
-	 * @param ipHdr
-	 * @param hdr
-	 * @param data
-	 * @param offset
-	 * @param length Must be smaller or equal to mss. 
-	 */
-	public synchronized void send(IPv4Header ipHdr, TCPHeader hdr, byte[] data, int offset, int length)
-	throws SocketException {
-		log.debug("outChannel.send(ipHdr,hdr,data," + offset + ", " + length + ")");
-		// Check for maximum datalength
-		if (length > mss) {
-			throw new IllegalArgumentException("dataLength must be <= mss");
-		}
-		// Wait until there is space in the output buffer
-		while ((length > dataBuffer.getFreeSize()) && !controlBlock.isReset()) {
-			try {
-				wait();
-			} catch (InterruptedException ex) {
-				// Ignore
-			}
-		}
-		if (controlBlock.isReset()) {
-			throw new SocketException("Connection reset");
-		}
-		// Add to databuffer
-		final int bufOfs = dataBuffer.add(data, offset, length);
-		// Update tcp header
-		hdr.setDataLength(length);
-		// Do the actual send
-		sendHelper(ipHdr, hdr, bufOfs);
-	}
+    /**
+     * My logger
+     */
+    private static final Logger log = Logger.getLogger(TCPOutChannel.class);
+    /**
+     * The protocol
+     */
+    private final TCPProtocol tcp;
+    /**
+     * All unacked segments
+     */
+    private final LinkedList<TCPOutSegment> unackedSegments = new LinkedList<TCPOutSegment>();
+    /**
+     * The outgoing databuffer
+     */
+    private final TCPDataBuffer dataBuffer;
+    /**
+     * Send unacknowledged
+     */
+    private int snd_unack;
+    /**
+     * Send next seq-nr
+     */
+    private int snd_next;
+    /**
+     * Highest seq-nr sent; used to recognize retransmits
+     */
+    private int snd_max;
+    /**
+     * Maximum segment size (determined by the foreign part of the connection
+     */
+    private int mss;
+    /**
+     * The control block I belong to
+     */
+    private final TCPControlBlock controlBlock;
+    /**
+     * Number of ticks before a retransmit timeout
+     */
+    private int timeoutTicks = 6;
 
-	/**
-	 * Do the actual sending and adjusting of sequence number.
-	 * @param ipHdr
-	 * @param hdr
-	 * @param dataOffset
-	 */
-	private final void sendHelper(IPv4Header ipHdr, TCPHeader hdr, int dataOffset) 
-	throws SocketException {
-		// Adjust the sequence numbers
-		hdr.setSequenceNr(snd_next);
-		if (hdr.isFlagSynchronizeSet() || hdr.isFlagFinishedSet()) {
-			snd_next++;
-			//snd_unack++;
-		} else {
-			snd_next += hdr.getDataLength();
-		}
-		snd_max = snd_next;
-		// Create & send the segment
-		final TCPOutSegment seg = new TCPOutSegment(ipHdr, hdr, dataBuffer, 0, timeoutTicks);
-		seg.send(tcp);
-		if (!seg.isAckOnly() && !hdr.isFlagSynchronizeSet()) {
-			log.debug("Adding segment " + seg.getSeqNr() + " to unacklist");
-			unackedSegments.add(seg);
-		}		
-	}
+    /**
+     * Create a new instance
+     */
+    public TCPOutChannel(TCPProtocol tcp, TCPControlBlock controlBlock, int isn) {
+        this.tcp = tcp;
+        this.controlBlock = controlBlock;
+        this.dataBuffer = new TCPDataBuffer(TCPConstants.TCP_BUFFER_SIZE);
+        this.snd_unack = isn;
+        this.snd_next = isn;
+        this.snd_max = isn;
+        this.mss = TCPConstants.TCP_DEFAULT_MSS;
+    }
 
-	/**
-	 * Notify a connection reset
-	 */
-	public synchronized void notifyConnectionReset() {
-		notifyAll();
-	}
-	
-	/**
-	 * @return Returns the mss.
-	 */
-	public final int getMss() {
-		return this.mss;
-	}
+    /**
+     * Process an ack-nr.
+     * Remove all segments that have been acknowledged and remove
+     * the occupied data from the databuffer.
+     *
+     * @param ackNr
+     */
+    public synchronized void processAck(int ackNr) {
+        // Is the ack valid?
+        if (snd_unack == ackNr) {
+            // Not a new ack, ignore it
+            return;
+        } else if (!TCPUtils.SEQ_LT(snd_unack, ackNr)) {
+            // snd_unack < ackNr violated
+            log.debug("snd_unack < ackNr violated");
+            return;
+        }
+        if (!TCPUtils.SEQ_LE(ackNr, snd_max)) {
+            // ackNr <= snd_max violated
+            log.debug("ackNr <= snd_max violated");
+            return;
+        }
 
-	/**
-	 * @param mss The mss to set.
-	 */
-	public final void setMss(int mss) {
-		this.mss = mss;
-	}
+        // The ackNr is valid
+        final int diff = ackNr - snd_unack;
+        snd_unack = ackNr;
+        // Remove data from the databuffer
+        dataBuffer.pull(diff);
+        for (Iterator<TCPOutSegment> i = unackedSegments.iterator(); i.hasNext();) {
+            final TCPOutSegment seg = (TCPOutSegment) i.next();
+            final int seqNr = seg.getSeqNr();
+            if (TCPUtils.SEQ_LT(seqNr, ackNr)) {
+                // Remove the segment
+                i.remove();
+            } else {
+                // Adjust the dataOffset
+                seg.adjustDataOffset(diff);
+            }
+        }
+        // Notify any blocked threads
+        notifyAll();
+    }
+
+    /**
+     * Process timeout handling
+     */
+    public void timeout()
+            throws SocketException {
+        for (TCPOutSegment seg : unackedSegments) {
+            seg.timeout(tcp);
+        }
+    }
+
+    public int getBufferSize() {
+        return dataBuffer.getLength();
+    }
+
+    /**
+     * Send a TCP segment containing no data
+     *
+     * @param ipHdr
+     * @param hdr
+     */
+    public void send(IPv4Header ipHdr, TCPHeader hdr)
+            throws SocketException {
+        // Check the datalength
+        if (hdr.getDataLength() != 0) {
+            throw new IllegalArgumentException("dataLength must be 0");
+        }
+        // Do the actual send
+        sendHelper(ipHdr, hdr, 0);
+    }
+
+    /**
+     * Send a TCP segment containing the given data.
+     * This method blocks until there is enough space in the output buffer
+     * to hold the data.
+     *
+     * @param ipHdr
+     * @param hdr
+     * @param data
+     * @param offset
+     * @param length Must be smaller or equal to mss.
+     */
+    public synchronized void send(IPv4Header ipHdr, TCPHeader hdr, byte[] data, int offset, int length)
+            throws SocketException {
+        log.debug("outChannel.send(ipHdr,hdr,data," + offset + ", " + length + ")");
+        // Check for maximum datalength
+        if (length > mss) {
+            throw new IllegalArgumentException("dataLength must be <= mss");
+        }
+        // Wait until there is space in the output buffer
+        while ((length > dataBuffer.getFreeSize()) && !controlBlock.isReset()) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                // Ignore
+            }
+        }
+        if (controlBlock.isReset()) {
+            throw new SocketException("Connection reset");
+        }
+        // Add to databuffer
+        final int bufOfs = dataBuffer.add(data, offset, length);
+        // Update tcp header
+        hdr.setDataLength(length);
+        // Do the actual send
+        sendHelper(ipHdr, hdr, bufOfs);
+    }
+
+    /**
+     * Do the actual sending and adjusting of sequence number.
+     *
+     * @param ipHdr
+     * @param hdr
+     * @param dataOffset
+     */
+    private final void sendHelper(IPv4Header ipHdr, TCPHeader hdr, int dataOffset)
+            throws SocketException {
+        // Adjust the sequence numbers
+        hdr.setSequenceNr(snd_next);
+        if (hdr.isFlagSynchronizeSet() || hdr.isFlagFinishedSet()) {
+            snd_next++;
+            //snd_unack++;
+        } else {
+            snd_next += hdr.getDataLength();
+        }
+        snd_max = snd_next;
+        // Create & send the segment
+        final TCPOutSegment seg = new TCPOutSegment(ipHdr, hdr, dataBuffer, 0, timeoutTicks);
+        seg.send(tcp);
+        if (!seg.isAckOnly() && !hdr.isFlagSynchronizeSet()) {
+            log.debug("Adding segment " + seg.getSeqNr() + " to unacklist");
+            unackedSegments.add(seg);
+        }
+    }
+
+    /**
+     * Notify a connection reset
+     */
+    public synchronized void notifyConnectionReset() {
+        notifyAll();
+    }
+
+    /**
+     * @return Returns the mss.
+     */
+    public final int getMss() {
+        return this.mss;
+    }
+
+    /**
+     * @param mss The mss to set.
+     */
+    public final void setMss(int mss) {
+        this.mss = mss;
+    }
 
 }
