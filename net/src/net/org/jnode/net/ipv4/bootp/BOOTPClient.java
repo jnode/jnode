46e38df156afde0b4f7722470a1f1421e8ad5bf1
markhale <markhale@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-26 02:52:42 +0000

    Factored out system-dependent code.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1720 407389f7-7c16-0410-9eee-ad85acb55d7f

--- net/src/net/org/jnode/net/ipv4/bootp/BOOTPClient.java
+++ net/src/net/org/jnode/net/ipv4/bootp/BOOTPClient.java
@@ -22,17 +22,12 @@
 package org.jnode.net.ipv4.bootp;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.Inet4Address;
-import java.net.MulticastSocket;
-import java.net.NetworkInterface;
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 
 import javax.naming.NameNotFoundException;
 
-import org.apache.log4j.Logger;
 import org.jnode.driver.ApiNotFoundException;
 import org.jnode.driver.Device;
 import org.jnode.driver.net.NetDeviceAPI;
@@ -47,27 +42,19 @@ import org.jnode.net.ipv4.config.IPv4ConfigurationService;
  * @author markhale
  * @author Martin Husted Hartvig (hagar@jnode.org)
  */
-public class BOOTPClient {
+public class BOOTPClient extends AbstractBOOTPClient {
 
-    /**
-     * My logger
-     */
-    private static final Logger log = Logger.getLogger(BOOTPClient.class);
-
-    private static final int RECEIVE_TIMEOUT = 10 * 1000; // 10 seconds
-
-    public static final int SERVER_PORT = 67;
-
-    public static final int CLIENT_PORT = 68;
-
-    protected MulticastSocket socket;
+    private Device device;
+    private NetDeviceAPI api;
 
     /**
      * Configure the given device using BOOTP
-     * 
+     *
      * @param device
      */
     public final void configureDevice(final Device device) throws IOException {
+        this.device = device;
+
         final SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkPermission(new NetPermission("bootpClient"));
@@ -77,100 +64,31 @@ public class BOOTPClient {
             AccessController.doPrivileged(new PrivilegedExceptionAction() {
                 public Object run() throws IOException {
                     // Get the API.
-                    final NetDeviceAPI api;
                     try {
                         api = (NetDeviceAPI) device.getAPI(NetDeviceAPI.class);
                     } catch (ApiNotFoundException ex) {
-                        throw new NetworkException(
-                                "Device is not a network device", ex);
+                        throw new NetworkException("Device is not a network device", ex);
                     }
 
-                    // Open a socket
-                    socket = new MulticastSocket(CLIENT_PORT);
-                    try {
-                        // Prepare the socket
-                        socket.setBroadcast(true);
-                        socket.setNetworkInterface(NetworkInterface
-                                .getByName(device.getId()));
-                        socket.setSoTimeout(RECEIVE_TIMEOUT);
+                    configureDevice(device.getId(), api.getAddress());
 
-                        // Create the BOOTP header
-                        final Inet4Address myIP = null; // any address
-                        final int transactionID = (int) (System
-                                .currentTimeMillis() & 0xFFFFFFFF);
-                        BOOTPHeader hdr = new BOOTPHeader(
-                                BOOTPHeader.BOOTREQUEST, transactionID, 0,
-                                myIP, api.getAddress());
-
-                        // Send the packet
-                        final DatagramPacket packet = createRequestPacket(hdr);
-                        packet.setAddress(IPv4Address.BROADCAST_ADDRESS);
-                        packet.setPort(SERVER_PORT);
-                        socket.send(packet);
-
-                        boolean configured;
-                        do {
-                            // Wait for a response
-                            socket.receive(packet);
-
-                            // Process the response
-                            configured = processResponse(device, api,
-                                    transactionID, packet);
-                        } while (!configured);
-
-                    } finally {
-                        socket.close();
-                    }
                     return null;
                 }
             });
         } catch (PrivilegedActionException ex) {
             throw (IOException) ex.getException();
         }
-    }
 
-    /**
-     * Create a BOOTP request packet
-     */
-    protected DatagramPacket createRequestPacket(BOOTPHeader hdr)
-            throws IOException {
-        return new BOOTPMessage(hdr).toDatagramPacket();
-    }
-
-    /**
-     * Process a BOOTP response
-     * 
-     * @param packet
-     * @return true if the device has been configured, false otherwise
-     */
-    protected boolean processResponse(Device device, NetDeviceAPI api,
-            int transactionID, DatagramPacket packet) throws IOException {
-
-        final BOOTPHeader hdr = new BOOTPHeader(packet);
-        if (hdr.getOpcode() != BOOTPHeader.BOOTREPLY) {
-            // Not a response
-            return false;
-        }
-        if (hdr.getTransactionID() != transactionID) {
-            // Not for me
-            return false;
-        }
-
-        configureNetwork(device, hdr);
-
-        return true;
+        this.api = null;
+        this.device = null;
     }
 
     /**
      * Performs the actual configuration of a network device based on the
      * settings in a BOOTP header.
      */
-    protected void configureNetwork(Device device, BOOTPHeader hdr)
-            throws NetworkException {
-        log.info("Got Client IP address  : " + hdr.getClientIPAddress());
-        log.info("Got Your IP address    : " + hdr.getYourIPAddress());
-        log.info("Got Server IP address  : " + hdr.getServerIPAddress());
-        log.info("Got Gateway IP address : " + hdr.getGatewayIPAddress());
+    protected void doConfigure(BOOTPHeader hdr) throws IOException {
+        super.doConfigure(hdr);
 
         final IPv4ConfigurationService cfg;
         try {
@@ -181,9 +99,8 @@ public class BOOTPClient {
         cfg.configureDeviceStatic(device, new IPv4Address(hdr
                 .getYourIPAddress()), null, false);
 
-        final IPv4Address networkAddress;
         final IPv4Address serverAddr = new IPv4Address(hdr.getServerIPAddress());
-        networkAddress = serverAddr.and(serverAddr.getDefaultSubnetmask());
+        final IPv4Address networkAddress = serverAddr.and(serverAddr.getDefaultSubnetmask());
 
         if (hdr.getGatewayIPAddress().isAnyLocalAddress()) {
             // cfg.addRoute(new IPv4Address(hdr.getServerIPAddress()), null,
---
+++ net/src/net/org/jnode/net/ipv4/dhcp/AbstractDHCPClient.java
@@ -0,0 +1,115 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+package org.jnode.net.ipv4.dhcp;
+
+import org.apache.log4j.Logger;
+import org.jnode.net.ipv4.IPv4Address;
+import org.jnode.net.ipv4.bootp.AbstractBOOTPClient;
+import org.jnode.net.ipv4.bootp.BOOTPHeader;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+/**
+ * System independent base class.
+ * Implementations should override doConfigure.
+ *
+ * @author markhale
+ */
+public class AbstractDHCPClient extends AbstractBOOTPClient {
+
+    private static final Logger log = Logger.getLogger(AbstractDHCPClient.class);
+
+    /**
+     * Create a DHCP discovery packet
+     */
+    protected DatagramPacket createRequestPacket(BOOTPHeader hdr)
+            throws IOException {
+        DHCPMessage msg = new DHCPMessage(hdr, DHCPMessage.DHCPDISCOVER);
+        return msg.toDatagramPacket();
+    }
+
+    protected boolean processResponse(int transactionID, DatagramPacket packet) throws IOException {
+        DHCPMessage msg = new DHCPMessage(packet);
+        BOOTPHeader hdr = msg.getHeader();
+        if (hdr.getOpcode() != BOOTPHeader.BOOTREPLY) {
+            // Not a response
+            return false;
+        }
+        if (hdr.getTransactionID() != transactionID) {
+            // Not for me
+            return false;
+        }
+
+        // debug the DHCP message
+        if (log.isDebugEnabled()) {
+            log.debug("Got Client IP address  : " + hdr.getClientIPAddress());
+            log.debug("Got Your IP address    : " + hdr.getYourIPAddress());
+            log.debug("Got Server IP address  : " + hdr.getServerIPAddress());
+            log.debug("Got Gateway IP address : " + hdr.getGatewayIPAddress());
+            for (int n = 1; n < 255; n++) {
+                byte[] value = msg.getOption(n);
+                if (value != null) {
+                    if (value.length == 1) {
+                        log.debug("Option " + n + " : " + (int) (value[0]));
+                    } else if (value.length == 2) {
+                        log.debug("Option " + n + " : "
+                                + ((value[0] << 8) | value[1]));
+                    } else if (value.length == 4) {
+                        log.debug("Option " + n + " : "
+                                + InetAddress.getByAddress(value).toString());
+                    } else {
+                        log.debug("Option " + n + " : " + new String(value));
+                    }
+                }
+            }
+        }
+
+        switch (msg.getMessageType()) {
+            case DHCPMessage.DHCPOFFER:
+                byte[] serverID = msg
+                        .getOption(DHCPMessage.SERVER_IDENTIFIER_OPTION);
+                byte[] requestedIP = hdr.getYourIPAddress().getAddress();
+                hdr = new BOOTPHeader(BOOTPHeader.BOOTREQUEST, transactionID, 0,
+                        hdr.getClientIPAddress(), hdr.getClientHwAddress());
+                msg = new DHCPMessage(hdr, DHCPMessage.DHCPREQUEST);
+                msg.setOption(DHCPMessage.REQUESTED_IP_ADDRESS_OPTION, requestedIP);
+                msg.setOption(DHCPMessage.SERVER_IDENTIFIER_OPTION, serverID);
+                packet = msg.toDatagramPacket();
+                packet.setAddress(IPv4Address.BROADCAST_ADDRESS);
+                packet.setPort(SERVER_PORT);
+                socket.send(packet);
+                break;
+            case DHCPMessage.DHCPACK:
+                doConfigure(msg);
+                return true;
+            case DHCPMessage.DHCPNAK:
+                break;
+        }
+        return false;
+    }
+
+    protected void doConfigure(DHCPMessage msg) throws IOException {
+        doConfigure(msg.getHeader());
+    }
+}
