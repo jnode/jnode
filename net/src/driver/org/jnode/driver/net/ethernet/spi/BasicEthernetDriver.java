c6348c34e7d1235338edf0797ffe80b7d043910e
markhale <markhale@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-27 18:33:32 +0000

    Transmit time-out turned into a static final.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1726 407389f7-7c16-0410-9eee-ad85acb55d7f

--- net/src/driver/org/jnode/driver/net/ethernet/spi/BasicEthernetDriver.java
+++ net/src/driver/org/jnode/driver/net/ethernet/spi/BasicEthernetDriver.java
@@ -18,7 +18,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
+
 package org.jnode.driver.net.ethernet.spi;
 
 import org.apache.log4j.Logger;
@@ -36,46 +36,55 @@ import org.jnode.util.TimeoutException;
  */
 
 public abstract class BasicEthernetDriver extends AbstractEthernetDriver {
-    
-	/** The device flags */
-	protected Flags flags;
-	/** The actual device driver */
-	private AbstractDeviceCore abstractDeviceCore;
-	/** My logger */
-	protected final Logger log = Logger.getLogger(getClass());
 
-	/**
-	 * @see org.jnode.driver.net.spi.AbstractNetDriver#doTransmit(SocketBuffer, HardwareAddress)
-	 */
-	protected void doTransmitEthernet(SocketBuffer skbuf, HardwareAddress destination) throws NetworkException {
-		try {
-			// Pad
-			if (skbuf.getSize() < ETH_ZLEN) {
-				skbuf.append(ETH_ZLEN - skbuf.getSize());
-			}
+    private static final long TRANSMIT_TIMEOUT = 5000;
 
-			abstractDeviceCore.transmit(skbuf, destination, 5000);
-		} catch (InterruptedException ex) {
-			throw new NetworkException("Interrupted", ex);
-		} catch (TimeoutException ex) {
-			throw new NetworkException("Timeout", ex);
-		}
-	}
+    /**
+     * My logger
+     */
+    protected static final Logger log = Logger.getLogger(BasicEthernetDriver.class);
 
-	/**
-	 * Gets the hardware address of this device
-	 */
-	public HardwareAddress getAddress() {
-		return abstractDeviceCore.getHwAddress();
-	}
+    /**
+     * The device flags
+     */
+    protected Flags flags;
+    /**
+     * The actual device driver
+     */
+    private AbstractDeviceCore abstractDeviceCore;
 
-	/**
-	 * @see org.jnode.driver.Driver#startDevice()
-	 */
-	protected void startDevice() throws DriverException {
-		try {
-		    log.info("Starting " + flags.getName());
-			abstractDeviceCore = newCore(getDevice(), flags);
+    /**
+     * @see org.jnode.driver.net.spi.AbstractNetDriver#doTransmit(SocketBuffer, HardwareAddress)
+     */
+    protected void doTransmitEthernet(SocketBuffer skbuf, HardwareAddress destination) throws NetworkException {
+        try {
+            // Pad
+            if (skbuf.getSize() < ETH_ZLEN) {
+                skbuf.append(ETH_ZLEN - skbuf.getSize());
+            }
+
+            abstractDeviceCore.transmit(skbuf, destination, TRANSMIT_TIMEOUT);
+        } catch (InterruptedException ex) {
+            throw new NetworkException("Interrupted", ex);
+        } catch (TimeoutException ex) {
+            throw new NetworkException("Timeout", ex);
+        }
+    }
+
+    /**
+     * Gets the hardware address of this device
+     */
+    public HardwareAddress getAddress() {
+        return abstractDeviceCore.getHwAddress();
+    }
+
+    /**
+     * @see org.jnode.driver.Driver#startDevice()
+     */
+    protected void startDevice() throws DriverException {
+        try {
+            log.info("Starting " + flags.getName());
+            abstractDeviceCore = newCore(getDevice(), flags);
             try {
                 abstractDeviceCore.initialize();
             } catch (DriverException ex) {
@@ -83,42 +92,41 @@ public abstract class BasicEthernetDriver extends AbstractEthernetDriver {
                 throw ex;
             }
 
-			super.startDevice();
-		} catch (ResourceNotFreeException ex) {
-			throw new DriverException("Cannot claim " + flags.getName() + " resources", ex);
-		}
-	}
+            super.startDevice();
+        } catch (ResourceNotFreeException ex) {
+            throw new DriverException("Cannot claim " + flags.getName() + " resources", ex);
+        }
+    }
 
-	/**
-	 * @see org.jnode.driver.Driver#stopDevice()
-	 */
-	protected void stopDevice() throws DriverException {
-		super.stopDevice();
+    /**
+     * @see org.jnode.driver.Driver#stopDevice()
+     */
+    protected void stopDevice() throws DriverException {
+        super.stopDevice();
 
-		abstractDeviceCore.disable();
-		abstractDeviceCore.release();
-		abstractDeviceCore = null;
-	}
+        abstractDeviceCore.disable();
+        abstractDeviceCore.release();
+        abstractDeviceCore = null;
+    }
 
-	/**
-	 * Create a new RTL8139Core instance
-	 */
-	protected abstract AbstractDeviceCore newCore(Device device, Flags flags) throws DriverException, ResourceNotFreeException;
+    /**
+     * Create a new RTL8139Core instance
+     */
+    protected abstract AbstractDeviceCore newCore(Device device, Flags flags) throws DriverException, ResourceNotFreeException;
 
-	/**
-	 *
-	 * Get the flags for this device
-	 *
-	 * @return The flags
-	 */
-	public Flags getFlags() {
-		return flags;
-	}
+    /**
+     * Get the flags for this device
+     *
+     * @return The flags
+     */
+    public Flags getFlags() {
+        return flags;
+    }
 
     /**
      * @return Returns the device core.
      */
     protected final AbstractDeviceCore getDeviceCore() {
         return abstractDeviceCore;
-    }   
+    }
 }
