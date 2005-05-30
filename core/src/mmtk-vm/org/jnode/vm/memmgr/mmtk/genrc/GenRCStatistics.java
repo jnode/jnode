5346b619ecdaf760090ac751d64b0845ef0fbf21
Ewout Prangsma <ewout@prangsma.net>
2005-05-30 10:09:10 +0000

    Create classes & build structures for MMTk GenRC based heap manager
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1732 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/mmtk-vm/org/jnode/vm/memmgr/mmtk/genrc/GenRCStatistics.java
@@ -0,0 +1,13 @@
+/*
+ * $Id$
+ */
+package org.jnode.vm.memmgr.mmtk.genrc;
+
+import org.jnode.vm.memmgr.mmtk.BaseMmtkGCStatistics;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+final class GenRCStatistics extends BaseMmtkGCStatistics {
+
+}
