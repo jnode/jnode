8ef4e932a44e4b16321fc9533d0d0558c28af06b
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-05-31 07:47:35 +0000

    Create classes & build structures for MMTk MarkSweep based heap manager
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1733 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/mmtk-vm/org/jnode/vm/memmgr/mmtk/ms/Plan.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ */
+package org.jnode.vm.memmgr.mmtk.ms;
+
+import org.jnode.vm.Unsafe;
+import org.jnode.vm.memmgr.HeapHelper;
+import org.mmtk.plan.MarkSweep;
+import org.vmmagic.pragma.InlinePragma;
+import org.vmmagic.pragma.Uninterruptible;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public abstract class Plan extends MarkSweep implements Uninterruptible {
+
+    /**
+     * <code>true</code> if built with GCSpy
+     */
+    public static final boolean WITH_GCSPY = false;
+
+    /** The heap helper */
+    private final HeapHelper heapHelper;
+    
+    /**
+     * Initialize this instance.
+     * @param helper
+     */
+    public Plan(HeapHelper helper) {
+        this.heapHelper = helper;
+    }
+    
+    /**
+     * Gets the plan instance associated with the current processor.
+     * 
+     * @return the plan instance for the current processor
+     */
+    public static Plan getInstance() 
+    throws InlinePragma {
+        return (Plan)Unsafe.getCurrentProcessor().getHeapData();
+    }
+
+    /**
+     * @return Returns the heapHelper.
+     */
+    public final HeapHelper getHeapHelper() {
+        return heapHelper;
+    }    
+}
---
+++ core/src/mmtk-vm/org/jnode/vm/memmgr/mmtk/ms/PlanConstants.java
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ */
+package org.jnode.vm.memmgr.mmtk.ms;
+
+import org.mmtk.plan.MarkSweepConstants;
+import org.vmmagic.pragma.Uninterruptible;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public abstract class PlanConstants extends MarkSweepConstants implements
+        Uninterruptible {
+
+}
