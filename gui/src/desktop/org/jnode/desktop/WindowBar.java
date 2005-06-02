ed3776d2af096c16bcc5ca4a945f750fd9c740ca
Ewout Prangsma <ewout@prangsma.net>
2005-06-02 10:06:21 +0000

    Added Ctrl-Alt-F5 hotkey to force a refresh of the gui
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1741 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/desktop/org/jnode/desktop/WindowBar.java
+++ gui/src/desktop/org/jnode/desktop/WindowBar.java
@@ -23,9 +23,10 @@ package org.jnode.desktop;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JInternalFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -40,29 +41,32 @@ public class WindowBar extends JPanel {
 	private final static Logger log = Logger.getLogger(WindowBar.class);
 
 	private final JList list;
-
-	private final ArrayList<JInternalFrame> frames;
+    private final DefaultListModel model;
+    private final Map<JInternalFrame, FrameWrapper> wrappers;
 
 	public WindowBar() {
 		this.list = new JList();
-		this.frames = new ArrayList<JInternalFrame>();
-		list.setModel(new WindowListModel());
+        this.model = new DefaultListModel();
+        this.wrappers = new HashMap<JInternalFrame, FrameWrapper>();
+		list.setModel(model);
 
 		add(list, BorderLayout.CENTER);
 	}
 
 	public void addFrame(JInternalFrame frame) {
 		log.debug("addFrame " + frame.getTitle());
-		frames.add(frame);
-		revalidate();
-		forceRepaint();
+        final FrameWrapper wrapper = new FrameWrapper(frame);
+        wrappers.put(frame, wrapper);
+        model.addElement(wrapper);
 	}
 
 	public void removeFrame(JInternalFrame frame) {
 		log.debug("removeFrame " + frame.getTitle());
-		frames.remove(frame);
-		revalidate();
-		forceRepaint();
+        final FrameWrapper wrapper = wrappers.get(frame);
+        if (wrapper != null) {
+            model.removeElement(wrapper);
+            wrappers.remove(frame);
+        }
 	}
 
 	public void setActiveFrame(JInternalFrame frame) {
@@ -75,22 +79,19 @@ public class WindowBar extends JPanel {
 			p.repaint();
 		}
 	}
+    
+    private static class FrameWrapper {
+        private final JInternalFrame frame;
 
-	private class WindowListModel extends AbstractListModel {
-
-		/**
-		 * @see javax.swing.ListModel#getElementAt(int)
-		 */
-		public Object getElementAt(int index) {
-			final JInternalFrame frame = (JInternalFrame)frames.get(index);
-			return frame.getTitle();
-		}
-
-		/**
-		 * @see javax.swing.ListModel#getSize()
-		 */
-		public int getSize() {
-			return frames.size();
-		}
-	}
+        /**
+         * @param frame
+         */
+        public FrameWrapper(JInternalFrame frame) {
+            this.frame = frame;
+        }
+        
+        public String toString() {
+            return frame.getTitle();
+        }
+    }
 }
