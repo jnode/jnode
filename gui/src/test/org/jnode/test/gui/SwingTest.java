42990a535b5946aa634d4a4e56aebc911f51ed10
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-06-02 18:37:29 +0000

    no message
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1753 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/test/org/jnode/test/gui/SwingTest.java
+++ gui/src/test/org/jnode/test/gui/SwingTest.java
@@ -25,7 +25,9 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 /**
  * @author Ewout Prangsma (epr@users.sourceforge.net)
@@ -34,28 +36,22 @@ public class SwingTest {
 
 	public static void main(String[] args) {
 		try {
-			//UIDefaults defs = UIManager.getDefaults();
-			//System.out.println("Defs=" + defs);
-			
-			//KeyStroke.getKeyStroke(' ');
-			//AWTKeyStroke ks = AWTKeyStroke.getAWTKeyStroke("SPACE");
-			//System.out.println("ks=" + ks);
-			//JPanel pan = new JPanel();
             JFrame frame = new JFrame("JFrame test");
+            frame.getRootPane().setDoubleBuffered(false);
             frame.setLocation(100, 100);
             frame.setSize(400, 400);
             frame.getContentPane().add(new JButton("JButton test"), BorderLayout.NORTH);
-            JTabbedPane tabs = new JTabbedPane();
-            tabs.add("Tab1", new JButton("JButton1"));
-            tabs.add("Tab2", new JButton("JButton2"));
-            tabs.add("Tab3", new JButton("JButton3"));
+            //JTabbedPane tabs = new JTabbedPane();
+            //tabs.add("Tab1", new JButton("JButton1"));
+            //tabs.add("Tab2", new JButton("JButton2"));
+            //tabs.add("Tab3", new JButton("JButton3"));
             //frame.getContentPane().add(tabs, BorderLayout.CENTER);
-//            JMenuBar mb = new JMenuBar();
-//            JMenu menu = new JMenu("JMenu test");
-//            JMenuItem mi = new JMenuItem("JMenuItem test");
-//            mb.add(menu);
-//            menu.add(mi);
-//            frame.setJMenuBar(mb);
+            JMenuBar mb = new JMenuBar();
+            JMenu menu = new JMenu("JMenu test");
+            JMenuItem mi = new JMenuItem("JMenuItem test");
+            mb.add(menu);
+            menu.add(mi);
+            frame.setJMenuBar(mb);
             frame.validate();
             frame.show();
 		} catch (Exception ex) {
