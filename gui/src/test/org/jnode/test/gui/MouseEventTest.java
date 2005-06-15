55728442e59756a4bda5595283e638d3051829f0
Levente Santha <lsantha@jnode.org>
2005-06-15 20:19:15 +0000

    Added MouseEventTest.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1786 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ gui/src/test/org/jnode/test/gui/MouseEventTest.java
@@ -0,0 +1,79 @@
+package org.jnode.test.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+/**
+ * @author Levente S\u00e1ntha
+ */
+public class MouseEventTest {
+    public static void main(String[] argv) {
+        Frame f = new Frame("Mouse event test");
+        f.add(createLabel("WEST", Color.GREEN, true), BorderLayout.WEST);
+        f.add(createLabel("EAST", Color.BLUE, true), BorderLayout.EAST);
+        f.add(createLabel("NORTH", Color.RED, true), BorderLayout.NORTH);
+        f.add(createLabel("SOUTH", Color.MAGENTA, true), BorderLayout.SOUTH);
+        f.add(createLabel("CENTER",  Color.WHITE, true), BorderLayout.CENTER);
+
+        f.setSize(300, 300);
+        f.setLocation(0, 0);
+        f.validate();
+        f.setVisible(true);
+    }
+
+    private static Label createLabel(String name, Color color, boolean withListeners) {
+        Label label = new Label(name);
+        label.setAlignment(Label.CENTER);
+        label.setName(name);
+        label.setBackground(color);
+        if (withListeners) {
+            label.addMouseListener(new MouseListener() {
+                public void mouseClicked(MouseEvent event) {
+                    printEvent(event);
+                }
+
+                public void mousePressed(MouseEvent event) {
+                    printEvent(event);
+                }
+
+                public void mouseReleased(MouseEvent event) {
+                    printEvent(event);
+                }
+
+                public void mouseEntered(MouseEvent event) {
+                    printEvent(event);
+                }
+
+                public void mouseExited(MouseEvent event) {
+                    printEvent(event);
+                }
+            });
+            label.addMouseMotionListener(new MouseMotionListener() {
+                public void mouseDragged(MouseEvent event) {
+                    printEvent(event);
+                }
+
+                public void mouseMoved(MouseEvent event) {
+                    printEvent(event);
+                }
+            });
+            label.addMouseWheelListener(new MouseWheelListener() {
+                public void mouseWheelMoved(MouseWheelEvent event) {
+                    printEvent(event);
+                }
+            });
+        }
+        return label;
+    }
+
+    private static void printEvent(MouseEvent event) {
+        System.out.println(event);
+    }
+}
