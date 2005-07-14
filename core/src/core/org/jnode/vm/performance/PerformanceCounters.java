42cd004fda3655e69e710db0b582e500356d8a04
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-07-14 07:57:00 +0000

    Added some performance events
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@1909 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/performance/PerformanceCounters.java
+++ core/src/core/org/jnode/vm/performance/PerformanceCounters.java
@@ -19,6 +19,24 @@ public abstract class PerformanceCounters {
     public abstract Set<PerformanceCounterEvent> getAvailableEvents();
     
     /**
+     * Gets the performance counter event with the given id, if that is 
+     * available on thecurrent processor.
+     * 
+     * @return The event, or null if not found.
+     */
+    public abstract PerformanceCounterEvent getAvailableEvent(String id);
+    
+    /**
+     * Gets the performance counter event with the given id, if that is 
+     * available on thecurrent processor.
+     * 
+     * @return The event, or null if not found.
+     */
+    public final PerformanceCounterEvent getAvailableEvent(PresetEvent preset) {
+        return getAvailableEvent(preset.name());
+    }
+    
+    /**
      * Gets the number of performance counters that are available on the
      * current processor.
      * @return
