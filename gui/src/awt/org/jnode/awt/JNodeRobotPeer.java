8d133f20d752c2b56841af045c416f564d8046bb
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-08-27 17:26:34 +0000

    implementation for generating keyboard and mouse events
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2022 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/awt/org/jnode/awt/JNodeRobotPeer.java
+++ gui/src/awt/org/jnode/awt/JNodeRobotPeer.java
@@ -5,7 +5,23 @@ package org.jnode.awt;
 
 import java.awt.GraphicsDevice;
 import java.awt.Rectangle;
+import java.awt.event.InputEvent;
+import java.awt.image.PixelGrabber;
 import java.awt.peer.RobotPeer;
+import java.util.Collection;
+
+import org.jnode.awt.image.JNodeImage;
+import org.jnode.driver.ApiNotFoundException;
+import org.jnode.driver.Device;
+import org.jnode.driver.DeviceNotFoundException;
+import org.jnode.driver.DeviceUtils;
+import org.jnode.driver.input.KeyboardAPI;
+import org.jnode.driver.input.KeyboardEvent;
+import org.jnode.driver.input.KeyboardInterpreter;
+import org.jnode.driver.input.KeyboardListener;
+import org.jnode.driver.input.PointerAPI;
+import org.jnode.driver.input.PointerEvent;
+import org.jnode.shell.help.ParsedArguments;
 
 public class JNodeRobotPeer<toolkitT extends JNodeToolkit> extends
         JNodeGenericPeer<toolkitT, GraphicsDevice> implements RobotPeer {
@@ -19,14 +35,38 @@ public class JNodeRobotPeer<toolkitT extends JNodeToolkit> extends
     }
 
     /**
+     * Return the color of the pixel at the given screen coordinates.
+     *
+     * @param x the x coordinate of the pixel
+     * @param y the y coordinate of the pixel
+     *
+     * @return the RGB color of the pixel at screen coodinates <code>(x, y)</code>
      * @see java.awt.peer.RobotPeer#getRGBPixel(int, int)
-     */
+     */    
     public int getRGBPixel(int x, int y) {
-        // TODO Auto-generated method stub
-        return 0;
+        //JNodeToolkit.createImage -> JNodeImage.getPixelColor
+        //getToolkitImpl().getAwtContext().getAwtRoot().getGraphics().copyArea(x, y, width, height, dx, dy)setColor(null);
+        final JNodeImage screen = null; //TODO get screen image
+        final int[] pixel = new int[1];
+        final PixelGrabber pg = new PixelGrabber(screen, x, y, 1, 1, pixel, 0, 1);
+        try 
+        { 
+          pg.grabPixels(); 
+        }
+        catch (InterruptedException e)  { }
+        
+        return pixel[0];
     }
 
     /**
+     * Create an array of RGB colors containing pixels read from the screen.  The
+     * image does not include the mouse pointer.
+     *
+     * @param screenRect the rectangle of pixels to capture, in screen
+     * coordinates
+     *
+     * @return an array of RGB colors containing the requested pixels
+     *
      * @see java.awt.peer.RobotPeer#getRGBPixels(java.awt.Rectangle)
      */
     public int[] getRGBPixels(Rectangle screen) {
@@ -38,46 +78,110 @@ public class JNodeRobotPeer<toolkitT extends JNodeToolkit> extends
     }
 
     /**
-     * @see java.awt.peer.RobotPeer#keyPress(int)
+     * Press a key.
+     *
+     * @param keycode key to press, a {@link java.awt.event.KeyEvent} VK_ constant
+     * @exception IllegalArgumentException if keycode is not a valid key
+     * @see java.awt.peer.RobotPeer#keyPress(int) 
      */
     public void keyPress(int keycode) {
-        // TODO Auto-generated method stub
-
+        sendKeyboardEvent(keycode, true);
     }
 
     /**
+     * Release a key.
+     *
+     * @param keycode key to release, a {@link java.awt.event.KeyEvent} VK_ 
+     *                constant
+     *
+     * @exception IllegalArgumentException if keycode is not a valid key
      * @see java.awt.peer.RobotPeer#keyRelease(int)
      */
     public void keyRelease(int keycode) {
-        // TODO Auto-generated method stub
-
+        sendKeyboardEvent(keycode, false);        
     }
 
     /**
+     * Move the mouse pointer to absolute coordinates (x, y).
+     *
+     * @param x the destination x coordinate
+     * @param y the destination y coordinate
+     * 
      * @see java.awt.peer.RobotPeer#mouseMove(int, int)
      */
     public void mouseMove(int x, int y) {
-        // TODO implement me
+        getToolkitImpl().getMouseHandler().mouseMove(x, y);
     }
 
     /**
+     * Press one or more mouse buttons.
+     *
+     * @param buttons the buttons to press; a bitmask of one or more of
+     * these {@link InputEvent} fields:
+     *
+     * <ul>
+     *   <li>BUTTON1_MASK</li>
+     *   <li>BUTTON2_MASK</li>
+     *   <li>BUTTON3_MASK</li>
+     * </ul>
+     * 
      * @see java.awt.peer.RobotPeer#mousePress(int)
      */
     public void mousePress(int buttons) {
-        // TODO implement me
+        getToolkitImpl().getMouseHandler().mousePress(buttons);        
     }
 
     /**
+     * Release one or more mouse buttons.
+     *
+     * @param buttons the buttons to release; a bitmask of one or more
+     * of these {@link InputEvent} fields:
+     *
+     * <ul>
+     *   <li>BUTTON1_MASK</li>
+     *   <li>BUTTON2_MASK</li>
+     *   <li>BUTTON3_MASK</li>
+     * </ul>
+     * 
      * @see java.awt.peer.RobotPeer#mouseRelease(int)
      */
     public void mouseRelease(int buttons) {
-        // TODO implement me
+        getToolkitImpl().getMouseHandler().mouseRelease(buttons);        
     }
 
     /**
+     * Rotate the mouse scroll wheel.
+     *
+     * @param wheelAmt number of steps to rotate mouse wheel.  negative
+     * to rotate wheel up (away from the user), positive to rotate wheel
+     * down (toward the user).
+     *
+     * @since 1.4
+     * 
      * @see java.awt.peer.RobotPeer#mouseWheel(int)
      */
     public void mouseWheel(int wheelAmt) {
-        // TODO implement me
+        getToolkitImpl().getMouseHandler().mouseWheel(wheelAmt);
+    }
+    
+    /**
+     * 
+     * @param keycode
+     * @param pressed true=key pressed, false=key released
+     */
+    final private void sendKeyboardEvent(int keycode, boolean pressed)
+    {
+        final KeyboardHandler kbHandler = getToolkitImpl().getKeyboardHandler();
+        final KeyboardAPI api = kbHandler.getKeyboardAPI();
+        final KeyboardInterpreter kbInt = api.getKbInterpreter();            
+        final KeyboardEvent event = kbInt.interpretKeycode(keycode);
+        if(event != null)
+        {
+            if(pressed)
+                kbHandler.keyPressed(event);
+            else
+                kbHandler.keyReleased(event);
+        }
+        // simply ignore keys with invalid/unknown keycodes
     }
 }
