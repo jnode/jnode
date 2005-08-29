ab154da5c93918778bdc550fc61488e3677dc493
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-08-29 20:58:29 +0000

    added some components for mouse event tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2031 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/test/org/jnode/test/gui/RobotTest.java
+++ gui/src/test/org/jnode/test/gui/RobotTest.java
@@ -1,15 +1,23 @@
 package org.jnode.test.gui;
 
-import java.awt.Canvas;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Frame;
-import java.awt.BorderLayout;
-import java.awt.Robot;
 import java.awt.AWTException;
-import java.awt.Rectangle;
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Robot;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 /**
  * @author Levente S\u00e1ntha
@@ -42,20 +50,54 @@ public class RobotTest extends Canvas {
         }
     }
 
-    public static void main(String[] argv) throws AWTException {
-        Frame f = new Frame();
-        RobotTest t = new RobotTest();
+    public static void main(String[] argv) throws AWTException {        
+        final Frame f = new Frame();
+        final JButton b = new JButton("Click me");
+        final JTextField tf = new JTextField();
+        final RobotTest t = new RobotTest();
+        f.add(tf, BorderLayout.NORTH);
         f.add(t, BorderLayout.CENTER);
+        f.add(b, BorderLayout.SOUTH);
         f.pack();
         f.setLocation(0,0);
         f.setVisible(true);
+        
         Robot r = new Robot();
         r.setAutoDelay(50);
         r.delay(1000);
-        image = r.createScreenCapture(new Rectangle(0,0,200,200));
+//        image = r.createScreenCapture(new Rectangle(0,0,200,200));
         t.repaint();
-        for(int i = 0; i < 400; i++){
-            r.mouseMove(i, i);
+//        for(int i = 0; i < 400; i++){
+//            r.mouseMove(i, i);
+//        }
+
+        b.addActionListener(new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent event)
+                    {
+                        tf.setText("Clicked !");
+                    }                    
+                });
+        
+        moveToCenterOfComponent(r, b);
+        r.mousePress(InputEvent.BUTTON1_MASK);
+        r.mouseRelease(InputEvent.BUTTON1_MASK);
+                
+        Point p = f.getLocationOnScreen();
+        p.translate(f.getWidth()/2, 5);
+        r.mouseMove((int) p.getX(), (int) p.getY());
+        r.mousePress(InputEvent.BUTTON1_MASK);
+        for(int i = 0; i < 100; i++)
+        {
+            r.mouseMove((int) p.getX()+i, (int) p.getY()+i);
         }
+        r.mouseRelease(InputEvent.BUTTON1_MASK);
+    }
+    
+    private static final void moveToCenterOfComponent(Robot r, Component c)
+    {
+        Point p = c.getLocationOnScreen();
+        p.translate(c.getWidth()/2, c.getHeight()/2);
+        r.mouseMove((int) p.getX(), (int) p.getY());        
     }
 }
