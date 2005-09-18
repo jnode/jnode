5b5f438e81f733127f55ada6eadb42d87a20a8dd
Ewout Prangsma <ewout@prangsma.net>
2005-09-18 20:42:34 +0000

    Added test to verify the working on the JUnit test code in JNode and the test command
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2056 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/test/org/jnode/test/JUnitTest.java
@@ -0,0 +1,13 @@
+/*
+ * $Id$
+ */
+package org.jnode.test;
+
+import junit.framework.TestCase;
+
+public class JUnitTest extends TestCase {
+
+    public void testOne() {
+        System.out.println("This is testOne");
+    }
+}
