d798ec777888b30a1bf12e457a9db700f7e3ddf1
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-10-06 16:27:49 +0000

    - added handling of classpath-bugfix with their bugzilla id (reported in classpath-bugfix.html) - added options to avoid reporting diffs only due to empty lines, tab chars or space characters.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2104 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ builder/src/builder/org/jnode/ant/taskdefs/classpath/Flags.java
@@ -0,0 +1,74 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2005 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+package org.jnode.ant.taskdefs.classpath;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * @author Fabien DUMINY (fduminy@jnode.org)
+ */
+public class Flags
+{
+    private int flags;
+    private SortedSet<String> bugIDs = new TreeSet<String>();
+
+    public Flags()
+    {
+        this(0);
+    }
+    
+    public Flags(int flags)
+    {
+        this.flags = flags;
+    }
+    
+    public void set(int flag)
+    {
+        flags |= flag;
+    }
+    
+    public Flags mask(int mask)
+    {
+        return new Flags(this.flags & mask);
+    }
+
+    public boolean isSet(int flag)
+    {
+        return ((flags & flag) != 0);
+    }
+
+    public int asInt()
+    {
+        return flags;
+    }
+
+    public void addBugID(String id)
+    {
+        bugIDs.add(id);
+    }
+    
+    public String[] getBugIDs()
+    {
+        return bugIDs.toArray(new String[bugIDs.size()]);
+    }
+}
