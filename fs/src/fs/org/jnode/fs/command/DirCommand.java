0c47c20cefbed0985292d78100b61542f8c5ffb1
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2005-11-10 18:02:20 +0000

    Improved command output.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2129 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/command/DirCommand.java
+++ fs/src/fs/org/jnode/fs/command/DirCommand.java
@@ -18,12 +18,8 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
- 
-package org.jnode.fs.command;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.PrintStream;
+package org.jnode.fs.command;
 
 import org.jnode.shell.Command;
 import org.jnode.shell.CommandLine;
@@ -32,69 +28,90 @@ import org.jnode.shell.help.Help;
 import org.jnode.shell.help.Parameter;
 import org.jnode.shell.help.ParsedArguments;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+
 /**
  * @author epr
  * @author Andreas H\u00e4nel
  * @author Martin Husted Hartvig (hagar@jnode.org)
+ * @author Levente S\u00e1ntha
  */
-public class DirCommand implements Command{
+public class DirCommand implements Command {
+    private static final int LEFT_MARGIN = 14;
+    private static final SimpleDateFormat df = new SimpleDateFormat("yyyy MMM dd hh:mm");
 
-	static final FileArgument ARG_DIR = new FileArgument("directory", "the directory to list contents of");
-	public static Help.Info HELP_INFO =
-		new Help.Info(
-			"dir",
-			"List the entries of the given directory",
-			new Parameter[] { new Parameter(ARG_DIR, Parameter.OPTIONAL)});
 
-	public static void main(String[] args) throws Exception {
-		new DirCommand().execute(new CommandLine(args), System.in, System.out, System.err);
-	}
-	
-	public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
-		ParsedArguments cmdLine = HELP_INFO.parse(commandLine.toStringArray());
+    static final FileArgument ARG_DIR = new FileArgument("directory", "the directory to list contents of");
+    public static Help.Info HELP_INFO =
+            new Help.Info(
+                    "dir",
+                    "List the entries of the given directory",
+                    new Parameter[]{new Parameter(ARG_DIR, Parameter.OPTIONAL)});
 
-        String dir_str = ARG_DIR.getValue(cmdLine);
-        
-		String userDir = System.getProperty("user.dir");
-        
-//		if ( ((dir_str == null) && isRoot(userDir)) ||
-//             ((dir_str != null) && isRoot(dir_str)) ){
-//            File[] roots = File.listRoots();
-//			this.printList(roots,out);
-//		} else {
-			File dir = ARG_DIR.getFile(cmdLine);
-			if (dir==null) dir = new File(userDir);
-			if (dir.exists() && dir.isDirectory()) {
-				final File[] list = dir.listFiles();
-				this.printList(list, out);
-			} else if (dir.exists() && dir.isFile()) {
-				this.printList(new File[] { dir },out);
-			} else {
-				err.println("No such directory " + dir);
-			}
-//		}
-			
-	}
-
-    private boolean isRoot(String dir)
-    {
-        return (dir != null) && dir.equals(File.separator);
+    public static void main(String[] args) throws Exception {
+        new DirCommand().execute(new CommandLine(args), System.in, System.out, System.err);
     }
-    
-	private void printList(File[] list, PrintStream out) {
-		if (list != null) {
-			for (int i = 0; i < list.length; i++) {
-				File f = list[i];
-				if (f.isDirectory()) {
-					out.print("[" + f.getName() + "]");
-				} else {
-					if(!f.exists())
-						continue;
-					out.print(f.getName() + " " + f.length());
-				}
-				out.println();
-			}
-			out.println();
-		}
-	}
+
+    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
+        ParsedArguments cmdLine = HELP_INFO.parse(commandLine.toStringArray());
+        String userDir = System.getProperty("user.dir");
+        File dir = ARG_DIR.getFile(cmdLine);
+        if (dir == null) dir = new File(userDir);
+        if (dir.exists() && dir.isDirectory()) {
+            final File[] list = dir.listFiles();
+            this.printList(list, out);
+        } else if (dir.exists() && dir.isFile()) {
+            this.printList(new File[]{dir}, out);
+        } else {
+            err.println("No such directory " + dir);
+        }
+    }
+
+    private void printList(File[] list, PrintStream out) {
+        if (list != null) {
+            Arrays.sort(list, new Comparator<File>() {
+                public int compare(File f1, File f2) {
+                    boolean b1 = f1.isDirectory();
+                    boolean b2 = f2.isDirectory();
+                    return b1 == b2 ? f1.getName().compareTo(f2.getName()) :
+                            b1 & !b2 ? -1 : 1;
+                }
+            });
+            StringBuilder sb = new StringBuilder();
+            Date lastModified = new Date();
+            for (int i = 0; i < list.length; i++) {
+                File f = list[i];
+                if (f.exists()) {
+                    sb.setLength(0);
+                    lastModified.setTime(f.lastModified());
+                    if (f.isFile()) {
+                        String ln = String.valueOf(f.length());
+                        int cnt = LEFT_MARGIN - ln.length();
+                        for (int j = 0; j < cnt; j++, sb.append(' '))
+                            ;
+                        sb.append(ln);
+                        sb.append("B   ");
+                        sb.append(df.format(lastModified));
+                        sb.append("   ");
+                        sb.append(f.getName());
+                    } else {
+                        for (int j = 0; j < LEFT_MARGIN + 4; j++, sb.append(' '))
+                            ;
+                        sb.append(df.format(lastModified));
+                        sb.append("   [");
+                        sb.append(f.getName());
+                        sb.append(']');
+                    }
+                    out.println(sb.toString());
+                }
+            }
+            out.println();
+        }
+    }
 }
