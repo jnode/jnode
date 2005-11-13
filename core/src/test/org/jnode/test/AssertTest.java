b698f06bcd67076c533a9b1eefeccf154c11e30b
Ewout Prangsma <ewout@prangsma.net>
2005-11-13 12:45:47 +0000

    Added assert test
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2136 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/test/org/jnode/test/AssertTest.java
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ */
+package org.jnode.test;
+
+public class AssertTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        assert false : "Hello world";
+    }
+
+}
