0cf53559c2daa9dffe014de274da2f163282cb06
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-02-13 14:49:52 +0000

    Fixed the udpout command: missed java.net.SocketPermission, resolve. Plus minor clean up.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2287 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/debug/UDPOutputCommand.java
+++ shell/src/shell/org/jnode/shell/command/debug/UDPOutputCommand.java
@@ -21,97 +21,132 @@
  
 package org.jnode.shell.command.debug;
 
+import org.apache.log4j.Logger;
+import org.jnode.debug.UDPAppender;
+import org.jnode.debug.UDPOutputStream;
+import org.jnode.shell.Command;
+import org.jnode.shell.CommandLine;
+import org.jnode.shell.help.*;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
-import org.apache.log4j.Logger;
-import org.jnode.debug.UDPAppender;
-import org.jnode.debug.UDPOutputStream;
-
 /**
  * @author Ewout Prangsma (epr@users.sourceforge.net)
+ * @author Martin Husted Hartvig (hagar@jnode.org)
  */
-public class UDPOutputCommand {
+public class UDPOutputCommand  implements Command {
 
-	public static void main(String[] args) 
-	throws IOException {
-		final int port = (args.length > 1) ? Integer.parseInt(args[1]) : 5612;
-		final SocketAddress address = new InetSocketAddress(args[0], port);
-		UDPOutputStream udpOut = new UDPOutputStream(address);
-		DupOutputStream dupOut = new DupOutputStream(System.out, udpOut);
-		PrintStream ps = new PrintStream(dupOut);
-		System.setOut(ps);		
-		System.setErr(ps);		
-		
-		final Logger root = Logger.getRootLogger();
-		root.addAppender(new UDPAppender(udpOut, null));
-		
-	}
-	
-	static class DupOutputStream extends OutputStream {
-		
-		private final OutputStream os1;
-		private final OutputStream os2;
-		
-		public DupOutputStream(OutputStream os1, OutputStream os2) {
-			this.os1 = os1;
-			this.os2 = os2;
-		}
-		
-			/**
-		 * @see java.io.OutputStream#close()
-		 * @throws IOException
-			 */
-		public void close() throws IOException {
-			os1.close();
-			os2.close();
-		}
+  private static final URLArgument ARG_ADDRESS = new URLArgument(
+          "url", "connect url to the udpreceiver");
 
-		/**
-		 * @see java.io.OutputStream#flush()
-		 * @throws IOException
-		 */
-		public void flush() throws IOException {
-			os1.flush();
-			os2.flush();
-		}
+  private static final IntegerArgument ARG_PORT = new IntegerArgument(
+          "port", "port to connect on");
 
-		/**
-		 * @param b
-		 * @param off
-		 * @param len
-		 * @see java.io.OutputStream#write(byte[], int, int)
-		 * @throws IOException
-		 * @throws NullPointerException
-		 * @throws IndexOutOfBoundsException
-		 */
-		public void write(byte[] b, int off, int len) throws IOException, NullPointerException, IndexOutOfBoundsException {
-			os1.write(b, off, len);
-			os2.write(b, off, len);
-		}
 
-		/**
-		 * @param b
-		 * @see java.io.OutputStream#write(byte[])
-		 * @throws IOException
-		 * @throws NullPointerException
-		 */
-		public void write(byte[] b) throws IOException, NullPointerException {
-			os1.write(b);
-			os2.write(b);
-		}
+  private static final Parameter PARAM_ADDRESS = new Parameter(
+          ARG_ADDRESS, Parameter.MANDATORY);
 
-		/**
-		 * @param b
-		 * @see java.io.OutputStream#write(int)
-		 * @throws IOException
-		 */
-		public void write(int b) throws IOException {
-			os1.write(b);
-			os2.write(b);
-		}
-	}
+  private static final Parameter PARAM_PORT = new Parameter(
+          ARG_PORT, Parameter.OPTIONAL);
+
+  public static Help.Info HELP_INFO = new Help.Info("udpout",
+          "send data from System.out and System.err to remote host",
+          new Parameter[] { PARAM_ADDRESS , PARAM_PORT });
+
+
+  public void execute(CommandLine commandLine, InputStream in,
+                      PrintStream out, PrintStream err) throws Exception {
+
+    final ParsedArguments args = HELP_INFO.parse(commandLine.toStringArray());
+
+    final int port;
+
+    if (PARAM_PORT.isSet(args)) {
+        port = ARG_PORT.getInteger(args);
+    }
+    else
+    {
+      port = 5612;
+    }
+
+    final SocketAddress address = new InetSocketAddress(ARG_ADDRESS.getURL(args).getHost(), port);
+    UDPOutputStream udpOut = new UDPOutputStream(address);
+
+    DupOutputStream dupOut = new DupOutputStream(System.out, udpOut);
+    PrintStream ps = new PrintStream(dupOut);
+    System.setOut(ps);
+    System.setErr(ps);
+
+    final Logger root = Logger.getRootLogger();
+    root.addAppender(new UDPAppender(udpOut, null));
+  }
+
+  static class DupOutputStream extends OutputStream {
+
+    private final OutputStream os1;
+    private final OutputStream os2;
+
+    public DupOutputStream(OutputStream os1, OutputStream os2) {
+      this.os1 = os1;
+      this.os2 = os2;
+    }
+
+      /**
+     * @see java.io.OutputStream#close()
+     * @throws IOException
+       */
+    public void close() throws IOException {
+      os1.close();
+      os2.close();
+    }
+
+    /**
+     * @see java.io.OutputStream#flush()
+     * @throws IOException
+     */
+    public void flush() throws IOException {
+      os1.flush();
+      os2.flush();
+    }
+
+    /**
+     * @param b
+     * @param off
+     * @param len
+     * @see java.io.OutputStream#write(byte[], int, int)
+     * @throws IOException
+     * @throws NullPointerException
+     * @throws IndexOutOfBoundsException
+     */
+    public void write(byte[] b, int off, int len) throws IOException, NullPointerException, IndexOutOfBoundsException {
+      os1.write(b, off, len);
+      os2.write(b, off, len);
+    }
+
+    /**
+     * @param b
+     * @see java.io.OutputStream#write(byte[])
+     * @throws IOException
+     * @throws NullPointerException
+     */
+    public void write(byte[] b) throws IOException, NullPointerException {
+      os1.write(b);
+      os2.write(b);
+    }
+
+    /**
+     * @param b
+     * @see java.io.OutputStream#write(int)
+     * @throws IOException
+     */
+    public void write(int b) throws IOException {
+      os1.write(b);
+      os2.write(b);
+    }
+  }
 }
