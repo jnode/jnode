49e67a57661a7f7f43b962be72ca9fbbcd5aaf39
Ewout Prangsma <ewout@prangsma.net>
2006-02-18 13:41:43 +0000

    Added test class
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2296 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/test/org/jnode/test/ConvertTest.java
@@ -0,0 +1,23 @@
+/*
+ * $Id$
+ */
+package org.jnode.test;
+
+public class ConvertTest {
+    
+    public static void main(String[] argv) {
+        System.out.println(test1());
+        System.out.println(test2());
+    }
+
+    public static boolean test1() {
+        double a = 0.5;
+        long b = (long) a;
+        return b == a;
+    }
+
+    public static boolean test2() {
+        double a = 0.5;
+        return (long) a == a;
+    }
+}
