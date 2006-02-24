b4081a69a25f9bde52fbe18de8242a3d43ec81c8
Levente Santha <lsantha@jnode.org>
2006-02-24 07:41:23 +0000

    Fix for read(...) by Giuseppe Vitillaro.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2311 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/driver/org/jnode/driver/block/BlockAlignmentSupport.java
+++ fs/src/driver/org/jnode/driver/block/BlockAlignmentSupport.java
@@ -18,137 +18,200 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
-package org.jnode.driver.block;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
+package org.jnode.driver.block;
 
 import org.apache.log4j.Logger;
 import org.jnode.util.ByteBufferUtils;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * @author epr
  */
 public class BlockAlignmentSupport implements BlockDeviceAPI {
+    /**
+     * My logger
+     */
+    private static final Logger log = Logger.getLogger(BlockAlignmentSupport.class);
+    private final BlockDeviceAPI parentApi;
+    private int alignment;
 
-	/** My logger */
-	private static final Logger log = Logger.getLogger(BlockAlignmentSupport.class);
-	private final BlockDeviceAPI parentApi;
-	private int alignment;
 
-	public BlockAlignmentSupport(BlockDeviceAPI parentApi, int alignment) {
-		this.parentApi = parentApi;
-		this.alignment = alignment;
-		if (alignment < 0) {
-			throw new IllegalArgumentException("alignment < 0");
-		}
-	}
+    public BlockAlignmentSupport(BlockDeviceAPI parentApi, int alignment) {
+        this.parentApi = parentApi;
+        this.alignment = alignment;
+        if (alignment < 0) {
+            throw new IllegalArgumentException("alignment < 0");
+        }
+    }
 
-	/**
-	 * @see org.jnode.driver.block.BlockDeviceAPI#getLength()
-	 * @return The length
-	 * @throws IOException
-	 */
-	public long getLength() throws IOException {
-		return parentApi.getLength();
-	}
 
-	/**
-	 * @param devOffset
-	 * @param dest
-	 * @param destOffset
-	 * @param length
-	 * @see org.jnode.driver.block.BlockDeviceAPI#read(long, byte[], int, int)
-	 * @throws IOException
-	 */
-	public void read(long devOffset, ByteBuffer dest)
-	throws IOException {
-      //log.info("Original: devOffset="+ devOffset + " destOffset=" + destOffset + " length=" + length);
-      //log.info("Original: alignment=" + alignment);
-		if (dest.remaining() == 0) {
-			return;
-		}
-		
-		final int ofsMisAlign = (int)(devOffset % alignment);
-		final int lenMisAlign = (dest.remaining() % alignment);
-		
-      //log.info("ofsMisAlign="+ ofsMisAlign + " lenMisAlign=" + lenMisAlign);
-        
-        
-		if ((ofsMisAlign != 0) || (lenMisAlign != 0)) {
-			// final byte[] buf = new byte[length + ofsMisAlign + (alignment - lenMisAlign)];
-         final ByteBuffer buf = ByteBuffer.allocate((dest.remaining() / alignment)  + alignment);
-        // log.info("temp buf =" + buf.length);
-			parentApi.read(devOffset - ofsMisAlign, buf);
-			if (ofsMisAlign != 0) {
-                ByteBufferUtils.buffercopy(buf, alignment - ofsMisAlign, dest, dest.position(), dest.remaining());
-			} else {
-                ByteBufferUtils.buffercopy(buf, 0, dest, dest.position(), dest.remaining());
-			}
-		} else {
-			// Aligned call, pass on
-         //log.info("aligned call");
-			parentApi.read(devOffset, dest);
-		}
-	}
+    /**
+     * @return The length
+     * @throws IOException
+     * @see BlockDeviceAPI#getLength()
+     */
+    public long getLength() throws IOException {
+        return parentApi.getLength();
+    }
 
-	/**
-	 * @param devOffset
-	 * @param src
-	 * @param srcOffset
-	 * @param length
-	 * @see org.jnode.driver.block.BlockDeviceAPI#write(long, byte[], int, int)
-	 * @throws IOException
-	 */
-	public void write(long devOffset, ByteBuffer src)
-	throws IOException {
-		
-		if (src.remaining() == 0) {
-			return;
-		}
-		
-		final int ofsMisAlign = (int)(devOffset % alignment);
-		final int lenMisAlign = (src.remaining() % alignment);
-		
-		if ((ofsMisAlign != 0) || (lenMisAlign != 0)) {
-			log.warn("Very expensive misaligned write called!");
-			final ByteBuffer buf = ByteBuffer.allocate(src.remaining() + ofsMisAlign + (alignment - lenMisAlign));
-			// TODO: This is very expensive, make it cheaper!
-			parentApi.read(devOffset - ofsMisAlign, buf);
-			if (ofsMisAlign != 0) {
-				ByteBufferUtils.buffercopy(src, src.position(), buf, alignment - ofsMisAlign, src.remaining());
-			} else {
-                ByteBufferUtils.buffercopy(src, src.position(), buf, 0, src.remaining());
-			}
+    /*
+    * gvt comment: only a picture let to understand this little evil ;-)
+    *
+    *  offset - ofsR |-----------|                     length - lenR
+    *                |           |                     offset - ofsR
+    *                |^^^^^^^^^^^| offset
+    *                |-----------|                     are integer
+    *  (--- is the   |           |  (^^^ are           multiples of
+    *    Buffer )    |           |    Data)            "alignment"
+    *                |-----------|
+    *                |           |
+    *  alignment +   |^^^^^^^^^^^| offset + length
+    *  length - lenR |-----------|
+    *
+    *
+    *                ofsR = offset % alignment
+    *  let me set:
+    *                lenR = length % alignment
+    *
+    *  so (offset - ofsR) and (length - lenR) are integer multiples of
+    *  alignment, i.e. n*alignment and m*aligment
+    *
+    *  if the I/O operation is done at (offset - ofsR) position, with
+    *  a length of [alignment + (length - lenR)], then, by definition
+    *  the operation is "alignment" aligned ... ;-) ...
+    *
+    *  the "real" data will be in the buffer at "ofsR" position in any
+    *  case ... I hope ... ;-) ...
+    *
+    *
+    *  as observed from ewout, this way to go, is very "expensive"
+    *  in the "write" side of the I/O ... we have to read the block.
+    *  copy the data to be written and only then we can write the block
+    *  back on the device
+    *
+    *  probably we can use a "write through" buffer here: the idea
+    *  is to define a buffer (with a length that is a multiple of alignment)
+    *  and then we can read and write "through" this buffer
+    */
+
+
+    /**
+     * @param offset
+     * @param dst
+     * @throws IOException
+     * @see BlockDeviceAPI#read(long, ByteBuffer)
+     */
+    public void read(long offset, ByteBuffer dst)
+            throws IOException {
+        final int length = dst.remaining();
+
+        //log.info ( "offset=" + offset + " with length=" + length +
+        //	   " to be " + alignment + " aligned" );
+
+        if (length == 0)
+            return;
+
+        final int ofsR = (int) (offset % alignment);
+        final int lenR = (int) (length % alignment);
+
+        //log.info ( "ofsR=" + ofsR + " lenR=" + lenR );
+
+        if ((ofsR != 0) || (lenR != 0)) {
+            final int buflen = alignment + (length - lenR);
+
+            //log.info ( "buffer length is "  +  buflen +
+            //	       " to be read at "    +  (offset - ofsR) +
+            //	       " and has data at "  +  ofsR );
+
+            ByteBuffer buf = ByteBuffer.allocate(buflen);
+
+            parentApi.read(offset - ofsR, buf);
+
+            ByteBufferUtils.buffercopy
+                    (buf, ofsR, dst, dst.position(), length);
+        } else {
+            //log.info ( "aligned call" );
+            parentApi.read(offset, dst);
+        }
+    }
+
+
+    /**
+     * @param offset
+     * @param src
+     * @throws IOException
+     * @see BlockDeviceAPI#write(long, ByteBuffer)
+     */
+    public void write(long offset, ByteBuffer src)
+            throws IOException {
+        int length = src.remaining();
+
+        //log.info ( "offset=" + offset + " with length=" + length +
+        //           " to be " + alignment + " aligned" );
+
+        if (length == 0)
+            return;
+
+        final int ofsR = (int) (offset % alignment);
+        final int lenR = (int) (length % alignment);
+
+        //log.info ( "ofsR=" + ofsR + " lenR=" + lenR );
+
+        if ((ofsR != 0) || (lenR != 0)) {
+            final int buflen = alignment + (length - lenR);
+
+            //log.info ( "buffer length is "  +  buflen +
+            //	       " to be written at " +  (offset - ofsR) +
+            //	       " and has data at "  +  ofsR );
+
+            ByteBuffer buf = ByteBuffer.allocate(buflen);
+
+            // TODO: this is very expensive, make it cheaper!
+            // it can be done buffering the IO at this level? (gvt)
+            log.warn("Very expensive misaligned write called!");
+
+            parentApi.read(offset - ofsR, buf);
+
+            ByteBufferUtils.buffercopy
+                    (src, src.position(), buf, ofsR, length);
+
             buf.clear();
-			parentApi.write(devOffset - ofsMisAlign, buf);
-		} else {
-			// Aligned call, pass on
-			parentApi.write(devOffset, src);
-		}
-	}
 
-	/**
-	 * @see org.jnode.driver.block.BlockDeviceAPI#flush()
-	 * @throws IOException
-	 */
-	public void flush() throws IOException {
-		parentApi.flush();
-	}
+            parentApi.write(offset - ofsR, buf);
+        } else {
+            //log.info ( "aligned call" );
+            parentApi.write(offset, src);
+        }
 
-	/**
-	 * Gets the alignment value
-	 * @return alignment
-	 */
-	public int getAlignment() {
-		return alignment;
-	} 
-	
-	/**
-	 * @param i
-	 */
-	public void setAlignment(int i) {
-		alignment = i;
-	}
+    }
+
+
+    /**
+     * @throws IOException
+     * @see BlockDeviceAPI#flush()
+     */
+    public void flush() throws IOException {
+        parentApi.flush();
+    }
+
+
+    /**
+     * Gets the alignment value
+     *
+     * @return alignment
+     */
+    public int getAlignment() {
+        return alignment;
+    }
+
+
+    /**
+     * @param i
+     */
+    public void setAlignment(int i) {
+        alignment = i;
+    }
 }
