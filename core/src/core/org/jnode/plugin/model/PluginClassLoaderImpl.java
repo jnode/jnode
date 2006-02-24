6f4575fafa617a7a2f7ba6a4ba18e62981ca577e
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-02-24 20:30:03 +0000

    Added getClassNames
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2314 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/plugin/model/PluginClassLoaderImpl.java
+++ core/src/core/org/jnode/plugin/model/PluginClassLoaderImpl.java
@@ -18,12 +18,11 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
+
 package org.jnode.plugin.model;
 
 import gnu.java.security.action.GetPolicyAction;
 
-import java.io.IOException;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.security.AccessController;
@@ -32,8 +31,10 @@ import java.security.Policy;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.jnode.plugin.PluginClassLoader;
 import org.jnode.plugin.PluginDescriptor;
@@ -48,70 +49,97 @@ final class PluginClassLoaderImpl extends PluginClassLoader {
 
     /** The registry */
     private final PluginRegistryModel registry;
+
     /** The descriptor */
     private final PluginDescriptorModel descriptor;
-	/** The plugin jar file */
-	private final PluginJar jar;
-	/** The classloaders of the prerequisite plugins */
-	private final PluginClassLoaderImpl[] prerequisiteLoaders;
 
-	/**
-	 * Initialize this instance.
-	 * 
-	 * @param jar
-	 */
-	public PluginClassLoaderImpl(PluginRegistryModel registry, PluginDescriptorModel descr, PluginJar jar, PluginClassLoaderImpl[] prerequisiteLoaders) {
-	    this.registry = registry;
-	    this.descriptor = descr;
-		this.jar = jar;
-		this.prerequisiteLoaders = prerequisiteLoaders;
-	}
+    /** The plugin jar file */
+    private final PluginJar jar;
 
-	/**
-	 * Finds the specified class. This method should be overridden by class loader implementations that follow the new delegation model for loading classes, and will be called by the loadClass method
-	 * after checking the parent class loader for the requested class. The default implementation throws ClassNotFoundException.
-	 * 
-	 * @param name
-	 * @return Class
-	 * @throws ClassNotFoundException
-	 * @see java.lang.ClassLoader#findClass(java.lang.String)
-	 */
-	protected final Class findClass(String name) throws ClassNotFoundException {
-		final Class cls = findPluginClass(name);
-		if (cls != null) {
-			return cls;
-		} else {
-			// Not found
-			throw new ClassNotFoundException(name);
-		}
-	}
+    /** The classloaders of the prerequisite plugins */
+    private final PluginClassLoaderImpl[] prerequisiteLoaders;
 
-	/**
-	 * Finds the specified class. This method should be overridden by class loader implementations that follow the new delegation model for loading classes, and will be called by the loadClass method
-	 * after checking the parent class loader for the requested class. The default implementation throws ClassNotFoundException.
-	 * 
-	 * @param name
-	 * @return Class The class, or null if not found.
-	 * @see java.lang.ClassLoader#findClass(java.lang.String)
-	 */
-	private final Class findPluginClass(String name) {
-		// Try the prerequisite loaders first
-		final int max = prerequisiteLoaders.length;
-		for (int i = 0; i < max; i++) {
-			final PluginClassLoaderImpl cl = prerequisiteLoaders[i];
-			if (cl != null) {
-				final Class cls = cl.findPluginClass(name);
-				if (cls != null) {
-					return cls;
-				}
-			}
-		}
-		// Try the loaded classes first
-		final Class loadedCls = findLoadedClass(name);
-		if (loadedCls != null) {
-			return loadedCls;
-		}
-        
+    /**
+     * Initialize this instance.
+     * 
+     * @param jar
+     */
+    public PluginClassLoaderImpl(PluginRegistryModel registry,
+            PluginDescriptorModel descr, PluginJar jar,
+            PluginClassLoaderImpl[] prerequisiteLoaders) {
+        this.registry = registry;
+        this.descriptor = descr;
+        this.jar = jar;
+        this.prerequisiteLoaders = prerequisiteLoaders;
+    }
+
+    /**
+     * Gets the names of the classes contained in this plugin
+     * 
+     * @return
+     */
+    public Set<String> getClassNames() {
+        HashSet<String> classNames = new HashSet<String>();
+        for (String name : jar.resourceNames()) {
+            if (name.endsWith(".class")) {
+                name = name.substring(0, name.length() - 6);
+                classNames.add(name.replace('/', '.'));
+            }
+        }
+        return classNames;
+    }
+
+    /**
+     * Finds the specified class. This method should be overridden by class
+     * loader implementations that follow the new delegation model for loading
+     * classes, and will be called by the loadClass method after checking the
+     * parent class loader for the requested class. The default implementation
+     * throws ClassNotFoundException.
+     * 
+     * @param name
+     * @return Class
+     * @throws ClassNotFoundException
+     * @see java.lang.ClassLoader#findClass(java.lang.String)
+     */
+    protected final Class findClass(String name) throws ClassNotFoundException {
+        final Class cls = findPluginClass(name);
+        if (cls != null) {
+            return cls;
+        } else {
+            // Not found
+            throw new ClassNotFoundException(name);
+        }
+    }
+
+    /**
+     * Finds the specified class. This method should be overridden by class
+     * loader implementations that follow the new delegation model for loading
+     * classes, and will be called by the loadClass method after checking the
+     * parent class loader for the requested class. The default implementation
+     * throws ClassNotFoundException.
+     * 
+     * @param name
+     * @return Class The class, or null if not found.
+     * @see java.lang.ClassLoader#findClass(java.lang.String)
+     */
+    private final Class findPluginClass(String name) {
+        // Try the prerequisite loaders first
+        final int max = prerequisiteLoaders.length;
+        for (int i = 0; i < max; i++) {
+            final PluginClassLoaderImpl cl = prerequisiteLoaders[i];
+            if (cl != null) {
+                final Class cls = cl.findPluginClass(name);
+                if (cls != null) {
+                    return cls;
+                }
+            }
+        }
+        // Try the loaded classes first
+        final Class loadedCls = findLoadedClass(name);
+        if (loadedCls != null) {
+            return loadedCls;
+        }
+
         // Look for it in the fragments
         ByteBuffer b = null;
         FragmentDescriptorModel fragment = null;
@@ -122,23 +150,23 @@ final class PluginClassLoaderImpl extends PluginClassLoader {
                 break;
             }
         }
-        
-		// Look for it in our own jar
+
+        // Look for it in our own jar
         if (b == null) {
             b = loadClassData(jar, name);
         }
-		if (b != null) {
-		    // We're are now going to use one of my classes, 
-		    // so make sure that my plugin has been started.
-		    try {
-	            startPlugin();
+        if (b != null) {
+            // We're are now going to use one of my classes,
+            // so make sure that my plugin has been started.
+            try {
+                startPlugin();
                 if (fragment != null) {
                     fragment.startPlugin(registry);
                 }
-	        } catch (PluginException ex) {
-	            BootLog.error("Error starting plugin", ex);
-	        }
-            
+            } catch (PluginException ex) {
+                BootLog.error("Error starting plugin", ex);
+            }
+
             // Define package (if needed)
             final int lastDotIndex = name.lastIndexOf('.');
             if (lastDotIndex > 0) {
@@ -156,27 +184,30 @@ final class PluginClassLoaderImpl extends PluginClassLoader {
                             sealed);
                 }
             }
-            
-		    final URL sourceUrl = jar.getResource(name.replace('.', '/') + ".class");
-		    final CodeSource cs = new CodeSource(sourceUrl, null);
-		    final Policy policy = (Policy)AccessController.doPrivileged(GetPolicyAction.getInstance());
-		    final ProtectionDomain pd = new ProtectionDomain(cs, policy.getPermissions(cs));
-			final Class cls = defineClass(name, b, pd);
-			resolveClass(cls);
-			return cls;
-		} else {
-			return null;
-		}
-	}
 
-	/**
-	 * Does this classloader contain the specified class.
-	 * 
-	 * @return boolean
-	 */
-	protected final boolean containsClass(String name) {
+            final URL sourceUrl = jar.getResource(name.replace('.', '/')
+                    + ".class");
+            final CodeSource cs = new CodeSource(sourceUrl, null);
+            final Policy policy = (Policy) AccessController
+                    .doPrivileged(GetPolicyAction.getInstance());
+            final ProtectionDomain pd = new ProtectionDomain(cs, policy
+                    .getPermissions(cs));
+            final Class cls = defineClass(name, b, pd);
+            resolveClass(cls);
+            return cls;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Does this classloader contain the specified class.
+     * 
+     * @return boolean
+     */
+    protected final boolean containsClass(String name) {
         final String resName = name.replace('.', '/') + ".class";
-		if (jar.containsResource(resName)) {
+        if (jar.containsResource(resName)) {
             return true;
         }
         for (ResourceLoader l : descriptor.fragments()) {
@@ -185,28 +216,29 @@ final class PluginClassLoaderImpl extends PluginClassLoader {
             }
         }
         return false;
-	}
+    }
 
-	/**
-	 * Finds the resource with the given name. Class loader implementations should override this method to specify where to find resources.
-	 * 
-	 * @param name
-	 * @return URL
-	 * @see java.lang.ClassLoader#findResource(java.lang.String)
-	 */
-	protected final URL findResource(String name) {
-		// Try the prerequisite loaders first
-		final int max = prerequisiteLoaders.length;
-		for (int i = 0; i < max; i++) {
-			final PluginClassLoaderImpl cl = prerequisiteLoaders[i];
-			if (cl != null) {
-				final URL url = cl.findResource(name);
-				if (url != null) {
-					return url;
-				}
-			}
-		}
-        
+    /**
+     * Finds the resource with the given name. Class loader implementations
+     * should override this method to specify where to find resources.
+     * 
+     * @param name
+     * @return URL
+     * @see java.lang.ClassLoader#findResource(java.lang.String)
+     */
+    protected final URL findResource(String name) {
+        // Try the prerequisite loaders first
+        final int max = prerequisiteLoaders.length;
+        for (int i = 0; i < max; i++) {
+            final PluginClassLoaderImpl cl = prerequisiteLoaders[i];
+            if (cl != null) {
+                final URL url = cl.findResource(name);
+                if (url != null) {
+                    return url;
+                }
+            }
+        }
+
         // Try the fragments
         URL url = null;
         FragmentDescriptorModel fragment = null;
@@ -217,107 +249,109 @@ final class PluginClassLoaderImpl extends PluginClassLoader {
                 break;
             }
         }
-        
-		// Not found, try my own plugin
-		//System.out.println("Try resource " + name + " on " + jar.getDescriptor().getId());
+
+        // Not found, try my own plugin
+        // System.out.println("Try resource " + name + " on " +
+        // jar.getDescriptor().getId());
         if (url == null) {
             url = jar.getResource(name);
         }
-		if (url != null) {
-		    try {
-		        startPlugin();
+        if (url != null) {
+            try {
+                startPlugin();
                 if (fragment != null) {
                     fragment.startPlugin(registry);
                 }
-		    } catch (PluginException ex) {
-		        BootLog.error("Cannot start plugin", ex);
-		    }
-		}
-		return url;
-	}
-	
+            } catch (PluginException ex) {
+                BootLog.error("Cannot start plugin", ex);
+            }
+        }
+        return url;
+    }
+
     public Enumeration getResources(String name) {
-    	System.err.println("getResources "+name);
-    	final List<URL> urls = new ArrayList<URL>();
-    	
-    	//
-		// Try the prerequisite loaders first
-		final int max = prerequisiteLoaders.length;
-		for (int i = 0; i < max; i++) {
-			final PluginClassLoaderImpl cl = prerequisiteLoaders[i];
-			if (cl != null) {
-				final URL url = cl.findResource(name);
-				if (url != null) {
-					System.err.println("adding "+url);
-					if(!urls.contains(url)) urls.add(url);
-				}
-			}
-		}
-        
+        System.err.println("getResources " + name);
+        final List<URL> urls = new ArrayList<URL>();
+
+        //
+        // Try the prerequisite loaders first
+        final int max = prerequisiteLoaders.length;
+        for (int i = 0; i < max; i++) {
+            final PluginClassLoaderImpl cl = prerequisiteLoaders[i];
+            if (cl != null) {
+                final URL url = cl.findResource(name);
+                if (url != null) {
+                    System.err.println("adding " + url);
+                    if (!urls.contains(url))
+                        urls.add(url);
+                }
+            }
+        }
+
         // Try the fragments
         URL url = null;
         for (FragmentDescriptorModel fragment : descriptor.fragments()) {
             url = fragment.getResource(name);
             if (url != null) {
-    		    try {
-    		        startPlugin();
+                try {
+                    startPlugin();
                     fragment.startPlugin(registry);
-    		    } catch (PluginException ex) {
-    		        BootLog.error("Cannot start plugin", ex);
-    		    }
-    		    System.err.println("adding "+url);
-    		    if(!urls.contains(url)) urls.add(url);
+                } catch (PluginException ex) {
+                    BootLog.error("Cannot start plugin", ex);
+                }
+                System.err.println("adding " + url);
+                if (!urls.contains(url))
+                    urls.add(url);
             }
         }
-        
-		// Not found, try my own plugin
-		//System.out.println("Try resource " + name + " on " + jar.getDescriptor().getId());
+
+        // Not found, try my own plugin
+        // System.out.println("Try resource " + name + " on " +
+        // jar.getDescriptor().getId());
         url = jar.getResource(name);
         if (url != null) {
-		    try {
-		        startPlugin();
-		    } catch (PluginException ex) {
-		        BootLog.error("Cannot start plugin", ex);
-		    }
-		    System.err.println("adding "+url);
-		    if(!urls.contains(url)) urls.add(url);
+            try {
+                startPlugin();
+            } catch (PluginException ex) {
+                BootLog.error("Cannot start plugin", ex);
+            }
+            System.err.println("adding " + url);
+            if (!urls.contains(url))
+                urls.add(url);
         }
-    	//
-    	
-    	return new Enumeration<URL>()
-    	{
-    		private Iterator<URL> it = urls.iterator();
+        //
 
-			public boolean hasMoreElements() {
-				return it.hasNext();
-			}
+        return new Enumeration<URL>() {
+            private Iterator<URL> it = urls.iterator();
 
-			public URL nextElement() {
-				return it.next();
-			}    		
-    	};
+            public boolean hasMoreElements() {
+                return it.hasNext();
+            }
+
+            public URL nextElement() {
+                return it.next();
+            }
+        };
     }
-    
-	/**
-	 * Try to load the data of a class with a given name.
-	 * 
-	 * @param name
-	 * @return The loaded class data or null if not found.
-	 */
-	private final ByteBuffer loadClassData(ResourceLoader loader, String name) {
-		return loader.getResourceAsBuffer(name.replace('.', '/') + ".class");
-	}
-	
-	/**
-	 * Make sure that the plugin gets started.
-	 * This method ensures that this classloader can be used to start
-	 * the plugin.
-	 */
-	private final void startPlugin() 
-	throws PluginException {
+
+    /**
+     * Try to load the data of a class with a given name.
+     * 
+     * @param name
+     * @return The loaded class data or null if not found.
+     */
+    private final ByteBuffer loadClassData(ResourceLoader loader, String name) {
+        return loader.getResourceAsBuffer(name.replace('.', '/') + ".class");
+    }
+
+    /**
+     * Make sure that the plugin gets started. This method ensures that this
+     * classloader can be used to start the plugin.
+     */
+    private final void startPlugin() throws PluginException {
         descriptor.startPlugin(registry);
-	}
-	
+    }
+
     /**
      * @see org.jnode.plugin.PluginClassLoader#getDeclaringPluginDescriptor()
      */
