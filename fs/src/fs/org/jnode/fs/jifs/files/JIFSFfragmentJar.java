7fb58aef465f086502dca423fbb2fce7372fe86d
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-03-11 20:46:11 +0000

    Added support for accessing the in-memory plugin jar files via JIFS, see /jifs/lib/.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2363 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/fs/org/jnode/fs/jifs/files/JIFSFfragmentJar.java
@@ -0,0 +1,52 @@
+package org.jnode.fs.jifs.files;
+
+import org.jnode.fs.FSDirectory;
+import org.jnode.plugin.PluginManager;
+import org.jnode.plugin.model.PluginDescriptorModel;
+import org.jnode.naming.InitialNaming;
+
+import javax.naming.NameNotFoundException;
+
+/**
+ * File, which contains information about the plugin with the same name.
+ *
+ * @author Levente S\u00e1ntha
+ */
+public class JIFSFfragmentJar extends JIFSFpluginJar {
+    private String pluginId;
+    private String fragmentId;
+    public JIFSFfragmentJar() {
+        return;
+    }
+
+    /**
+     *
+     */
+    public JIFSFfragmentJar(String pluginId, String fragmentId, FSDirectory parent) {
+        super(fragmentId, parent);
+        this.pluginId = pluginId;
+        this.fragmentId = fragmentId;
+        refresh();
+    }
+
+    public void refresh(){
+        super.refresh();
+        try{
+            final PluginManager mgr = InitialNaming.lookup(PluginManager.NAME);
+            PluginDescriptorModel pdm = (PluginDescriptorModel) mgr.getRegistry().getPluginDescriptor(pluginId);
+            if(pdm != null){
+                isvalid = false;
+                for(PluginDescriptorModel fdm : pdm.fragments()){
+                    if(fdm.getId().equals(fragmentId)){
+                        buffer = pdm.getJarFile().getBuffer();
+                        isvalid = buffer != null;
+                    }
+                }
+            } else {
+                isvalid = false;
+            }
+        } catch (NameNotFoundException e){
+            System.err.println(e);
+        }
+    }
+}
