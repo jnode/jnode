bc9b3d992728c061fa0ff75bb7fc8f6a2a90ca4b
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-03-11 20:31:21 +0000

    Added VmExit.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2359 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/core/org/jnode/vm/VmExit.java
@@ -0,0 +1,17 @@
+package org.jnode.vm;
+
+/**
+ * @author Levente S\u00e1ntha
+ */
+public class VmExit extends RuntimeException {
+    private int status;
+
+    public VmExit(int status) {
+        super("VM exit, status: " + status);
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
