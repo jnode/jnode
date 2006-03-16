f3dc6feb3ddb3ee9fa375dd7c3551cba6527801f
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-03-16 19:44:36 +0000

    Added tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2381 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/test/org/jnode/test/IfNullTest.java
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ */
+package org.jnode.test;
+
+public class IfNullTest {
+
+    final static Object nullVariable = null;
+    final static Object nonNullVariable = new Object();
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        TestNullVariable();
+        TestNonNullVariable();
+    }
+    
+    static void TestNullVariable() {
+        if (nullVariable == null) {
+            System.out.println("nullVariable == null");
+        } else {
+            System.out.println("nullVariable != null");            
+        }    
+    }
+
+    static void TestNonNullVariable() {
+        if (nonNullVariable == null) {
+            System.out.println("nonNullVariable == null");
+        } else {
+            System.out.println("nonNullVariable != null");            
+        }    
+    }
+
+}
