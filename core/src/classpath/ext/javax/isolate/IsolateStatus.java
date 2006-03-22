5906146db73cb5101c5bd224bf266e14d28c0bf0
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-03-22 20:14:59 +0000

    Getting closer to the latest spec
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2401 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/classpath/ext/javax/isolate/IsolateStatus.java
@@ -0,0 +1,140 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package javax.isolate;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ */
+public class IsolateStatus {
+
+    /**
+     * Defines the types of IsolateEvent the can be generated.
+     * 
+     * @author Ewout Prangsma (epr@users.sourceforge.net)
+     */
+    public static enum Type {
+        /** The starting state */
+        STARTING,
+
+        /** The stopping state */
+        EXITING,
+
+        /** The terminated state */
+        TERMINATED;
+    }
+
+    /**
+     * Provides the reason for the termination of an isolate.
+     * 
+     * @author Ewout Prangsma (epr@users.sourceforge.net)
+     */
+    public static enum ExitReason {
+        /** The last non-deamon thread exited */
+        IMPLICIT_EXIT,
+
+        /** Another isolate invoked Isolate.exit */
+        OTHER_EXIT,
+
+        /** Another isolate invoed Isolated.halt */
+        OTHER_HALT,
+
+        /**
+         * The isolate invoked System.exit, Runtime.exit or Isolate.exit by
+         * itself.
+         */
+        SELF_EXIT,
+
+        /**
+         * The isolate invoked Runtime.halt or Isolate.halt by itself.
+         */
+        SELF_HALT,
+
+        /** The last non-deamon thread exited due to an uncaught exception */
+        UNCAUGHT_EXCEPTION;
+    }
+    
+    private final Isolate source;
+    private final Type type;
+    private final ExitReason exitReason;
+    private final int exitStatus;
+    
+    /**
+     * Initialize this instance.
+     * @param source
+     * @param type
+     * @param exitReason
+     */
+    IsolateStatus(Isolate source, Type type, ExitReason exitReason, int exitStatus) {
+        this.source = source;
+        this.type = type;
+        this.exitReason = exitReason;
+        this.exitStatus = exitStatus;
+    }
+    
+    /**
+     * Are this and the given object equal.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object other) {
+        return super.equals(other);
+    }
+    
+    /**
+     * Gets the exit reason.
+     * @return
+     */
+    public ExitReason getExitReason() {
+        return exitReason;
+    }
+    
+    /**
+     * Gets the exit status.
+     * @return
+     */
+    public int getExitStatus() {
+        return exitStatus;
+    }
+    
+    /**
+     * Gets the source of this event.
+     * @return
+     */
+    public Isolate getIsolate() {
+        return source;
+    }
+    
+    /**
+     * Gets the isolate the started the source isolate.
+     * @return
+     */
+    public Isolate getStarter() {
+        return null;
+    }
+    
+    /**
+     * Gets the type of this event.
+     * @return
+     */
+    public Type getType() {
+        return type;
+    }
+}
