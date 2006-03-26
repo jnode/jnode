0ab9bac8468c8731439522a100e22113398a7bb8
epr <epr@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-03-26 12:31:15 +0000

    Implemented stream bindings
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2434 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/classpath/ext/javax/isolate/StreamBindings.java
+++ core/src/classpath/ext/javax/isolate/StreamBindings.java
@@ -3,6 +3,75 @@
  */
 package javax.isolate;
 
-public final class StreamBindings {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.net.Socket;
 
+import org.jnode.vm.VmStreamBindings;
+
+public final class StreamBindings {
+    
+    private final VmStreamBindings bindings = new VmStreamBindings();
+    
+    /**
+     * Gets the bindings implementation
+     * @return
+     */
+    final VmStreamBindings getBindings() {
+        return bindings;
+    }
+
+    /**
+     * Bind the stderr of a new isolate to the given file stream.
+     * @param stream
+     */
+    public StreamBindings setErr(FileOutputStream stream) {
+        bindings.setErr(stream);
+        return this;
+    }
+
+    /**
+     * Bind the stderr of a new isolate to the given socket.
+     * @param socket
+     */
+    public StreamBindings setErr(Socket socket) {
+        bindings.setErr(socket);
+        return this;
+    }
+    
+    /**
+     * Bind the stdout of a new isolate to the given file stream.
+     * @param stream
+     */
+    public StreamBindings setOut(FileOutputStream stream) {
+        bindings.setOut(stream);
+        return this;
+    }
+
+    /**
+     * Bind the stdout of a new isolate to the given socket.
+     * @param socket
+     */
+    public StreamBindings setOut(Socket socket) {
+        bindings.setOut(socket);
+        return this;
+    }
+
+    /**
+     * Bind the stdin of a new isolate to the given stream.
+     * @param stream
+     */
+    public StreamBindings setIn(FileInputStream stream) {
+        bindings.setIn(stream);
+        return this;
+    }
+    
+    /**
+     * Bind the stdin of a new isolate to the given stream.
+     * @param socket
+     */
+    public StreamBindings setIn(Socket socket) {
+        bindings.setIn(socket);
+        return this;
+    }
 }
