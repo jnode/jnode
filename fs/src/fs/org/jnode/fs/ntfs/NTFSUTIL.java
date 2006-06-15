5f1b7ee30841d00e618b994bb9b6eccb8c702fa8
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-06-15 07:02:20 +0000

    Patch from Daniel Noll :
    NTFSEntry.getLastModified() was returning zero, so we implemented it.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2519 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/ntfs/NTFSUTIL.java
+++ fs/src/fs/org/jnode/fs/ntfs/NTFSUTIL.java
@@ -21,7 +21,6 @@
  
 package org.jnode.fs.ntfs;
 
-import java.text.DateFormat;
 import java.util.Date;
 
 /**
@@ -29,13 +28,38 @@ import java.util.Date;
  */
 public class NTFSUTIL {
 
-    public static Date getDateForNTFSTimes(long _100ns) {
-        long timeoffset = Math.abs((369 * 365 + 89) * 24 * 3600 * 10000000);
-        long time = (Math.abs(_100ns) - timeoffset);
+    /**
+     * Converts a 64-bit NTFS time value (number of 100-nanosecond intervals since January 1, 1601 UTC)
+     * to a Java time value (number of milliseconds since January 1, 1970 UTC.)
+     *
+     * @param filetime the FILETIME value.
+     * @return the number of milliseconds since 1970.
+     */
+    public static long filetimeToMillis(long filetime) {
+        // Move the starting epoch to January 1, 1970.
+        filetime -= 116444736000000000L;
 
-        System.out.println("hours" + ((Math.abs(time) / 1000) / 60) / 60);
-        Date date = new Date(time);
-        System.out.println(DateFormat.getInstance().format(date));
-        return date;
+        // Now convert the time into milliseconds, rather than 100-nanosecond units.
+        if (filetime < 0)
+        {
+            filetime = -1 - ((-filetime - 1) / 10000);
+        }
+        else
+        {
+            filetime = filetime / 10000;
+        }
+
+        return filetime;
+    }
+
+    /**
+     * Converts a 64-bit NTFS time value (number of 100-nanosecond intervals since January 1, 1601 UTC)
+     * to a Java {@link Date}.
+     *
+     * @param filetime the FILETIME value.
+     * @return the Java date.
+     */
+    public static Date filetimeToDate(long filetime) {
+        return new Date(filetimeToMillis(filetime));
     }
 }
