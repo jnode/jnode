3a66fbdd7aa8ebdfaab43610332f1a4d60081b57
Levente Santha <lsantha@jnode.org>
2006-08-27 19:30:46 +0000

    classpath patches
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2647 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/classpath/gnu/gnu/java/security/util/SimpleList.java
+++ core/src/classpath/gnu/gnu/java/security/util/SimpleList.java
@@ -43,25 +43,19 @@ import java.util.Collection;
 import java.util.Iterator;
 
 /**
- * A simple way to create immutable n-tuples. This class can be created with
- * up to four elements specified via one of the constructors, or with a
- * collection of arbitrary size.
+ * A simple way to create immutable n-tuples. This class can be created with up
+ * to four elements specified via one of the constructors, or with a collection
+ * of arbitrary size.
  */
-public final class SimpleList extends AbstractList
+public final class SimpleList
+    extends AbstractList
 {
-
-  // Fields.
-  // ------------------------------------------------------------------------
-
   private final Object[] elements;
-
-  // Constructors.
-  // ------------------------------------------------------------------------
 
   /**
    * Create a singleton list.
    *
-   * @param e1 The first element.
+   * @param element The first element.
    */
   public SimpleList(final Object element)
   {
@@ -124,9 +118,9 @@ public final class SimpleList extends AbstractList
   }
 
   /**
-   * Create an n-tuple of arbitrary size. Even if the supplied collection has
-   * no natural order, the created n-tuple will have the order that the
-   * elements are returned by the collection's iterator.
+   * Create an n-tuple of arbitrary size. Even if the supplied collection has no
+   * natural order, the created n-tuple will have the order that the elements
+   * are returned by the collection's iterator.
    *
    * @param c The collection.
    */
@@ -135,13 +129,8 @@ public final class SimpleList extends AbstractList
     elements = new Object[c.size()];
     int i = 0;
     for (Iterator it = c.iterator(); it.hasNext() && i < elements.length;)
-      {
         elements[i++] = it.next();
       }
-  }
-
-  // Instance methods.
-  // ------------------------------------------------------------------------
 
   public int size()
   {
@@ -153,14 +142,9 @@ public final class SimpleList extends AbstractList
   public Object get(int index)
   {
     if (elements == null)
-      {
         throw new IndexOutOfBoundsException("list is empty");
-      }
     if (index < 0 || index >= elements.length)
-      {
-        throw new IndexOutOfBoundsException("index=" + index + ", size="
-                                            + size());
-      }
+      throw new IndexOutOfBoundsException("index=" + index + ", size=" + size());
     return elements[index];
   }
 
