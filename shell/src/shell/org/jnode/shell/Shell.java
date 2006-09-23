641776c223d3c8d33de43c09972be11def09e428
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-09-23 17:34:27 +0000

    added getConsole()
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2755 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/Shell.java
+++ shell/src/shell/org/jnode/shell/Shell.java
@@ -22,25 +22,32 @@
 package org.jnode.shell;
 
 import org.jnode.shell.alias.AliasManager;
+import org.jnode.driver.console.Console;
 
 /**
  * @author epr
  */
 public interface Shell {
 
-	/**
-	 * Gets the alias manager of this shell
-	 */
-	public AliasManager getAliasManager();
-
-	/** 
-	 * Gets the CommandHistory object associated with this shell.
-	 */
-	public CommandHistory getCommandHistory();
+    /**
+     * Gets the alias manager of this shell
+     */
+    public AliasManager getAliasManager();
 
     /**
-	 * Prints a list of choices for command line completion.
-	 */
-	public void list(String[] items);
+     * Gets the CommandHistory object associated with this shell.
+     */
+    public CommandHistory getCommandHistory();
+
+    /**
+     * Prints a list of choices for command line completion.
+     */
+    public void list(String[] items);
+
+    /**
+     * Returns the console where the shell is running.
+     * @return the console
+     */
+    public Console getConsole();
 
 }
