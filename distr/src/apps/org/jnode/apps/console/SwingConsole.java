4f2ef6afd9a9eb86f14501f895b147fb7fba4f90
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-10-01 09:36:47 +0000

    restriced to one running instance
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2808 407389f7-7c16-0410-9eee-ad85acb55d7f

--- distr/src/apps/org/jnode/apps/console/SwingConsole.java
+++ distr/src/apps/org/jnode/apps/console/SwingConsole.java
@@ -8,18 +8,40 @@ import org.jnode.shell.CommandShell;
 import org.jnode.shell.ShellManager;
 import org.jnode.naming.InitialNaming;
 
+import javax.swing.JFrame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * @author Levente S\u00e1ntha
  */
 public class SwingConsole {
-
+    private static JFrame frame;
     public static void main(String[] argv) throws Exception {
+        synchronized(SwingConsole.class){
+            if(frame != null){
+                System.out.println("SwingConsole is running. Only one SwingConsole can run at this time.");
+                return;
+            }
+        }
         ShellManager sm = InitialNaming.lookup(ShellManager.NAME);
         TextScreenConsoleManager manager = (TextScreenConsoleManager) sm.getCurrentShell().getConsole().getManager();
         SwingTextScreenConsoleManager cm = new SwingTextScreenConsoleManager();
         cm.setParent(manager);
         new Thread(new CommandShell((TextConsole) cm.createConsole(null,
-                ConsoleManager.CreateOptions.TEXT | ConsoleManager.CreateOptions.SCROLLABLE))).
+                ConsoleManager.CreateOptions.TEXT | ConsoleManager.CreateOptions.SCROLLABLE)), "SwingConsoleCommandShell").
                 start();
+
+        synchronized(SwingConsole.class){
+            frame = cm.getFrame();
+        }
+
+        frame.addWindowListener(new WindowAdapter(){
+            public void windowClosed(WindowEvent e) {
+                synchronized(SwingConsole.class){
+                    frame = null;
+                }
+            }
+        });
     }
 }
