94cb4b9d3b31ae10336ccdef749840c75f725673
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-10-01 10:01:16 +0000

    added command for new editor
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2816 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ distr/src/apps/org/jnode/apps/editor/LeedCommand.java
@@ -0,0 +1,29 @@
+package org.jnode.apps.editor;
+
+import org.jnode.shell.help.FileArgument;
+import org.jnode.shell.help.Help;
+import org.jnode.shell.help.Parameter;
+import org.jnode.shell.help.ParsedArguments;
+
+import java.io.File;
+
+/**
+ * @author Levente S\u00e1ntha
+ */
+public class LeedCommand {
+    static final FileArgument ARG_EDIT = new FileArgument("file", "the file to edit");
+    public static Help.Info HELP_INFO = new Help.Info(
+            "leed", "LEvi's EDitor\n   Ctrl-S - save\n   Ctrl-Q - quit",
+            new Parameter[]{new Parameter(ARG_EDIT, Parameter.MANDATORY)}
+    );
+
+    public static void main(String[] args) throws Exception {
+        final ParsedArguments cmdLine = HELP_INFO.parse(args);
+        final File file = ARG_EDIT.getFile(cmdLine);
+        if (file.isDirectory()) {
+            System.err.println(file + " is a directory");
+        } else {
+            TextEditor.main(args);
+        }
+    }
+}
