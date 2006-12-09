7044546a33fd96bffc188e029bc0e2fe5736d9e9
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2006-12-09 10:29:22 +0000

    removed unused imports
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2878 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/test/org/jnode/test/gui/HTMLTest.java
+++ gui/src/test/org/jnode/test/gui/HTMLTest.java
@@ -23,12 +23,7 @@ package org.jnode.test.gui;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JEditorPane;
-import javax.swing.JTextPane;
-import javax.swing.text.html.HTMLDocument;
-import javax.swing.text.html.HTMLEditorKit;
-import javax.swing.table.DefaultTableModel;
 
 /**
  * @author Levente S\u00e1ntha
