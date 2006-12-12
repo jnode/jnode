e787d262e2e12ead691291cd1b3107ec1b0cd9f4
Levente Santha <lsantha@jnode.org>
2006-12-12 22:20:48 +0000

    test env.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@2904 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ distr/src/apps/org/jnode/apps/editor/EditEmu.java
@@ -0,0 +1,31 @@
+package org.jnode.apps.editor;
+
+import org.jnode.driver.console.ConsoleManager;
+import org.jnode.driver.console.swing.SwingTextScreenConsoleManager;
+import org.jnode.driver.console.textscreen.TextScreenConsole;
+import org.jnode.test.gui.Emu;
+
+import java.io.File;
+
+/**
+ * @author Levente S\u00e1ntha
+ */
+public class EditEmu extends Emu {
+    public static void main(String[] argv) throws Exception{
+        initEnv();
+
+
+        if(argv.length == 0){
+            System.out.println("No file specified");
+            return;
+        }
+
+        SwingTextScreenConsoleManager cm = new SwingTextScreenConsoleManager();
+        final TextScreenConsole console = cm.createConsole(null,
+                ConsoleManager.CreateOptions.TEXT | ConsoleManager.CreateOptions.NO_SYSTEM_OUT_ERR_IN);
+
+        TextEditor te = new TextEditor(console);
+        File f = new File(argv[0]);
+        te.loadFile(f);
+    }
+}
