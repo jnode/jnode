1a688c3f4f4cb73b5dfd6cb959bdaf540b615898
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-02-11 22:12:18 +0000

    for size computation : - created SizeUnit enum - added more units (that can fit in a long value)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3116 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/core/org/jnode/util/SizeUnit.java
@@ -0,0 +1,39 @@
+package org.jnode.util;
+
+import java.math.BigInteger;
+
+public enum SizeUnit {
+	B(1l, "B"),
+	K(1024l, "K"),
+	M(1024l*1024l, "M"),
+	G(1024l*1024l*1024l, "G"),
+	T(1024l*1024l*1024l*1024l, "T"),
+	P(1024l*1024l*1024l*1024l*1024l, "P"),
+	E(1024l*1024l*1024l*1024l*1024l*1024l, "E");
+	//these units have too big multipliers to fit in a long
+	// (aka they are greater than 2^64) :
+	//Z(1024l*1024l*1024l*1024l*1024l*1024l*1024l, "Z"),
+	//Y(1024l*1024l*1024l*1024l*1024l*1024l*1024l*1024l, "Y");
+	
+	public static final SizeUnit MIN = B;
+	public static final SizeUnit MAX = E;
+	
+	final private long multiplier;
+	final private String unit; 
+
+	private SizeUnit(long multiplier, String unit)
+	{
+		this.multiplier = multiplier;
+		this.unit = unit;
+	}
+
+	public long getMultiplier() {
+		return multiplier;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+	
+	
+}
