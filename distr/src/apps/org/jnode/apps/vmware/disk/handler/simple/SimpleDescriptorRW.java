7b4b50fab6ac1f64401db8fdc83499fcc3219b42
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-03-31 16:56:33 +0000

    new version with some refactoring, more tests ... - working disk type detection - working read/write of headers only - read/write of sectors still buggy
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3149 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ distr/src/apps/org/jnode/apps/vmware/disk/handler/simple/SimpleDescriptorRW.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.jnode.apps.vmware.disk.handler.simple;
+
+import java.io.IOException;
+
+import org.jnode.apps.vmware.disk.ExtentDeclaration;
+import org.jnode.apps.vmware.disk.descriptor.Descriptor;
+import org.jnode.apps.vmware.disk.descriptor.DescriptorRW;
+import org.jnode.apps.vmware.disk.extent.Extent;
+import org.jnode.apps.vmware.disk.handler.FileDescriptor;
+import org.jnode.apps.vmware.disk.handler.UnsupportedFormatException;
+
+/**
+ * Wrote from the 'Virtual Disk Format 1.0' specifications (from VMWare)
+ * 
+ * @author Fabien DUMINY (fduminy at jnode dot org)
+ *
+ */
+final class SimpleDescriptorRW extends DescriptorRW 
+{
+	@Override
+	public Extent createMainExtent(Descriptor desc, ExtentDeclaration extentDecl)
+	{
+		return new Extent(desc, extentDecl);
+	}
+	
+	@Override
+	public Extent createExtent(FileDescriptor fileDescriptor, ExtentDeclaration extentDecl)
+						throws IOException, UnsupportedFormatException 
+	{
+		Descriptor desc = (fileDescriptor == null) ? null : 
+								fileDescriptor.getDescriptor();
+		return createMainExtent(desc, extentDecl);
+	}
+	
+}
