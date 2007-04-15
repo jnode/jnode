1c1b9dfe209093e285f872189686395c60035be3
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-04-15 19:44:40 +0000

    update of JPartition : refactor around stamps-mvc for better separation of model, view and controller
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3161 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ distr/src/apps/org/jnode/apps/jpartition/commands/framework/BaseCommand.java
@@ -0,0 +1,52 @@
+package org.jnode.apps.jpartition.commands.framework;
+
+import org.apache.log4j.Logger;
+
+abstract public class BaseCommand implements Command 
+{
+	private static final Logger log = Logger.getLogger(BaseCommand.class);
+		
+	private CommandStatus status = CommandStatus.NOT_RUNNING;
+	protected String name; 
+	
+	protected BaseCommand(String name)
+	{
+		this.name = name;
+	}
+	
+	final public void execute(CommandProcessor processor) throws CommandException
+	{
+		try
+		{
+			status = CommandStatus.RUNNING;
+			processor.commandStarted(this);
+			
+			doExecute();
+			status = CommandStatus.SUCCESS;		
+		}
+		catch(CommandException e)
+		{
+			log.error("command failed", e);
+			status = CommandStatus.FAILED;
+			throw e;
+		}
+		catch(Throwable t)
+		{
+			log.error("command failed", t);
+			status = CommandStatus.FAILED;
+			throw new CommandException("command failed", t);
+		}
+	}
+	
+	public CommandStatus getStatus() 
+	{
+		return status;
+	}
+	
+	abstract protected void doExecute() throws CommandException;
+	
+	@Override
+	public String toString() {
+		return status + " - " + name;
+	}
+}
---
+++ distr/src/apps/org/jnode/apps/jpartition/commands/framework/CommandProcessor.java
@@ -0,0 +1,118 @@
+package org.jnode.apps.jpartition.commands.framework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import org.apache.log4j.Logger;
+
+public class CommandProcessor {
+	private static final Logger log = Logger.getLogger(CommandProcessor.class);
+		
+	private Stack<Command> commands = new Stack<Command>();
+	private List<CommandProcessorListener> listeners = new ArrayList<CommandProcessorListener>();
+	
+	private boolean running = false;
+	
+	public synchronized void process()
+	{
+		if(!running)
+		{
+			running = true;
+			
+			while(!commands.isEmpty())
+			{
+				Command command = null;
+				try
+				{
+					command = peekCommand();
+					
+					command.execute(this);
+				}
+				catch(CommandException e)
+				{
+					log.error("error in command processing", e);
+					break;
+				}
+				catch(Throwable t)
+				{
+					log.error("unexpected error in command processing", t);
+					break;
+				}
+				finally
+				{
+					if(command != null)
+					{
+						for(CommandProcessorListener l : listeners)
+						{
+							l.commandFinished(this, command);
+						}
+						
+						try {
+							removeCommand();
+						} 
+						catch(Throwable t)
+						{
+							log.error("error in removeCommand", t);
+						}
+					}
+				}
+			}
+		
+			running = false; 
+		}
+	}
+	
+	public void addCommand(Command command)
+	{
+		if(command.getStatus() != CommandStatus.NOT_RUNNING)
+		{
+			throw new IllegalArgumentException("command must be in status NOT_RUNNING");
+		}
+		
+		commands.push(command);
+		
+		for(CommandProcessorListener l : listeners)
+		{
+			l.commandAdded(this, command);
+		}			
+	}
+	
+	protected Command peekCommand() throws Exception
+	{
+		Command command = commands.peek(); 
+		if(command.getStatus() != CommandStatus.NOT_RUNNING)
+		{
+			throw new Exception("command already started : "+command);
+		}
+		return command;
+	}
+
+	protected void removeCommand()
+	{
+		Command command = commands.pop();
+		
+		for(CommandProcessorListener l : listeners)
+		{
+			l.commandRemoved(this, command);
+		}
+	}
+
+	public void commandStarted(Command command)
+	{
+		for(CommandProcessorListener l : listeners)
+		{
+			l.commandStarted(this, command);
+		}				
+	}
+	
+	public void addListener(CommandProcessorListener listener)
+	{
+		listeners.add(listener);
+	}
+	
+	public void removeListener(CommandProcessorListener listener)
+	{
+		listeners.remove(listener);
+	}
+}
