21dca4000c2f9a517f71691ef4918bbb4a6313fa
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-06-16 20:39:39 +0000

    Openjdk integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3260 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/io/NativeConsole.java
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ */
+package java.io;
+
+/**
+ * @author Levente Sántha
+ */
+class NativeConsole {
+    private static String encoding(){
+        //todo implement it
+        return null;
+    }
+    private static boolean echo(boolean on) throws IOException{
+        //todo implement it
+        return true;
+    }
+    private static boolean istty(){
+        //todo implement it
+        return true;
+    }
+}
