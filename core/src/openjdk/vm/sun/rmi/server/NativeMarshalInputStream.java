cb59e75361f381a83f086b2f3e341365928509a1
Levente Santha <lsantha@jnode.org>
2007-06-17 10:09:41 +0000

    Openjdk integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3274 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/sun/rmi/server/NativeMarshalInputStream.java
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ */
+package sun.rmi.server;
+
+/**
+ * @author Levente Sántha
+ */
+class NativeMarshalInputStream {
+    static ClassLoader latestUserDefinedLoader(){
+        //todo implement
+        throw new UnsupportedOperationException();
+    }
+}
