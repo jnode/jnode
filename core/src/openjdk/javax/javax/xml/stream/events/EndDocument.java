56027e7667e99e64ebbda13fc39cb8eeac14540c
Levente Santha <lsantha@jnode.org>
2007-06-25 09:36:18 +0000

    Openjdk integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3307 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/javax/javax/xml/stream/events/EndDocument.java
@@ -0,0 +1,14 @@
+package javax.xml.stream.events;
+
+/**
+ * A marker interface for the end of the document
+ *
+ * @version 1.0
+ * @author Copyright (c) 2003 by BEA Systems. All Rights Reserved.
+ * @since 1.6
+ */
+public interface EndDocument extends XMLEvent {
+  /**
+   * No methods are defined in this interface.  
+   */
+}
