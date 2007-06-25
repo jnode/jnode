0e7ffb17c128cc31413606e3948993f4e7de6d38
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-06-25 09:40:29 +0000

    Openjdk integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3308 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/util/NativeResourceBundle.java
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ */
+package java.util;
+
+import org.jnode.vm.VmSystem;
+
+/**
+ * @author Levente Sántha
+ */
+class NativeResourceBundle {
+    static Class[] getClassContext(){
+        //skip the call to VmSystem.getRealClassContext() 
+        Class[] context = VmSystem.getRealClassContext();
+        Class[] ret = new Class[context.length - 1];
+        System.arraycopy(context, 1, ret, 0, ret.length);
+
+        return context;
+    }
+}
