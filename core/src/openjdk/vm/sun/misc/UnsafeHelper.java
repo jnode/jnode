4c81632d526f34e0463e49275cdf5c56d415d672
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-06-30 13:33:24 +0000

    Support for sun.misc.Unsafe .
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3338 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/sun/misc/UnsafeHelper.java
@@ -0,0 +1,24 @@
+/*
+ * $Id$
+ */
+package sun.misc;
+
+import java.lang.reflect.Field;
+import org.jnode.vm.classmgr.VmInstanceField;
+import org.vmmagic.unboxed.ObjectReference;
+
+/**
+ * @author Levente Sántha
+ */
+class UnsafeHelper {
+    private static final int OFFSET_OF_vmField_IN_Field =
+            ((VmInstanceField) Field.class.getVmClass().getField("vmField")).getOffset();
+    private static final int OFFSET_OF_offset_IN_VmInstanceField =
+            ((VmInstanceField) VmInstanceField.class.getVmClass().getField("offset")).getOffset();
+
+    static long objectFieldOffset(Field f) {
+        return ObjectReference.fromObject(f).toAddress().
+                add(OFFSET_OF_vmField_IN_Field).loadAddress().
+                add(OFFSET_OF_offset_IN_VmInstanceField).loadInt();
+    }
+}
