cfc663616a5f857ada2a9454deefc90c16efaa3b
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-08-11 19:55:49 +0000

    Openjdk integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3391 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/sun/reflect/NativeConstantPool.java
@@ -0,0 +1,68 @@
+/*
+ * $Id$
+ */
+package sun.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+
+/**
+ * @author Levente Sántha
+ */
+public class NativeConstantPool {
+    private static int getSize0(ConstantPool instance, Object constantPoolOop) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Class getClassAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Class getClassAtIfLoaded0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Member getMethodAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Member getMethodAtIfLoaded0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Field getFieldAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Field getFieldAtIfLoaded0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String[] getMemberRefInfoAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static int getIntAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static long getLongAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static float getFloatAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static double getDoubleAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String getStringAt0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static String getUTF8At0(ConstantPool instance, Object constantPoolOop, int index) {
+        throw new UnsupportedOperationException();
+    }
+}
