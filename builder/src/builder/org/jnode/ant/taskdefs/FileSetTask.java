c5a4263e37e586dbc7f78c812598cbf8153014c0
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-08-20 20:44:06 +0000

    - refactored some JNode ant tasks around abstract class FileSetTask - created ant task to add annotations on classes (useful for adding @SharedStatics on openjdk classes without modifying the original source code)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3399 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ builder/src/builder/org/jnode/ant/taskdefs/FileSetTask.java
@@ -0,0 +1,70 @@
+/*
+ * $Id: HeaderTask.java 3379 2007-08-04 10:19:57Z lsantha $
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package org.jnode.ant.taskdefs;
+
+import java.io.*;
+import java.util.ArrayList;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
+
+/**
+ * Abstract class for ant task that process one or more FileSet
+ * The concrete classes only have to implement the <i>process</i> method
+ * for doing the concrete work on a file. 
+ * 
+ * @author Fabien DUMINY (fduminy at jnode.org)
+ *
+ */
+abstract public class FileSetTask extends Task {
+
+	private final ArrayList<FileSet> fileSets = new ArrayList<FileSet>();
+
+	public void addFileSet(FileSet fs) {
+		fileSets.add(fs);
+	}
+
+	public void execute() throws BuildException {
+		// default implementation : simply iterate on all files
+		processFiles();
+	}
+	
+	final protected void processFiles() throws BuildException
+	{
+		final Project project = getProject();
+		try {
+			for (FileSet fs : fileSets) {
+				final String[] files = fs.getDirectoryScanner(project)
+						.getIncludedFiles();
+				final File projectDir = fs.getDir(project);
+				for (String fname : files) {
+					processFile(new File(projectDir, fname));
+				}
+			}
+		} catch (IOException e) {
+			throw new BuildException(e);
+		}		
+	}
+	
+	abstract protected void processFile(File file) throws IOException;
+}
