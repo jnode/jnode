de20ee1b9186cb573f86ea82363a0a4156d0e252
Levente Santha <lsantha@jnode.org>
2007-08-25 19:23:35 +0000

    Openjdk integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3411 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/util/prefs/NativeFileSystemPreferences.java
@@ -0,0 +1,34 @@
+/*
+ * $Id$
+ */
+package java.util.prefs;
+
+/**
+ * @author Levente Sántha
+ */
+public class NativeFileSystemPreferences {
+    /**
+     * @see java.util.prefs.FileSystemPreferences#lockFile0(String, int, boolean)
+     */
+    private static int[] lockFile0(String fileName, int permission, boolean shared){
+        //todo implement it
+        return new int[]{1, 0};
+    }
+
+    /**
+     *
+     * @see java.util.prefs.FileSystemPreferences#unlockFile0(int)
+     */
+    private static int unlockFile0(int lockHandle){
+        //todo implement it
+        return 0;
+    }
+
+    /**
+     * @see java.util.prefs.FileSystemPreferences#chmod(String, int)
+     */
+    private static int chmod(String fileName, int permission){
+        //todo implement it
+        return 0;
+    }
+}
