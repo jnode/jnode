541cf32d5d6063e60d0dad9a9f8ca1f09e175929
Levente Santha <lsantha@jnode.org>
2007-09-06 14:46:22 +0000

    openjdk integration
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3461 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/util/logging/NativeFileHandler.java
@@ -0,0 +1,17 @@
+/*
+ * $Id$
+ */
+package java.util.logging;
+
+/**
+ * @author Levente Sántha
+ */
+public class NativeFileHandler {
+    /**
+     * @see FileHandler#isSetUID() 
+     */
+    private static boolean isSetUID(){
+        //todo implement it
+        return false;
+    }
+}
