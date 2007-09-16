e16267cd0a3b65d8ac4fd1fb71c28a04ddbb3b62
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-09-16 15:25:06 +0000

    fixed that issue : http://www.jnode.org/node/1614 + added extra error messages for the user
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3509 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/command/CatCommand.java
+++ fs/src/fs/org/jnode/fs/command/CatCommand.java
@@ -22,12 +22,15 @@
 package org.jnode.fs.command;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.apache.log4j.Logger;
 import org.jnode.shell.Command;
 import org.jnode.shell.CommandLine;
 import org.jnode.shell.help.Argument;
@@ -40,8 +43,10 @@ import org.jnode.shell.help.argument.FileArgument;
  * @author epr
  * @author Andreas H\u00e4nel
  * @author Stephen Crawley
+ * @author Fabien DUMINY (fduminy@jnode.org)
  */
 public class CatCommand implements Command{
+	//private static final Logger log = Logger.getLogger(CatCommand.class);
 
     static final Argument ARG_FILE = new FileArgument("file",
             "the file (or URL) to print out");
@@ -60,46 +65,101 @@ public class CatCommand implements Command{
     	ParsedArguments cmdLine = HELP_INFO.parse(commandLine.toStringArray());
     	String fileName = ARG_FILE.getValue(cmdLine);
     	InputStream is = null;
+    	boolean isNewFile = false;
     	try {
     		if (fileName == null) {
+    			isNewFile = true;
     			is = in;
     		}
     		else {
     			URL url = openURL(fileName);
-    			try {
-    				is = url.openStream();
+    			if(url != null)
+    			{
+        			try {
+        				is = url.openStream();	
+        			}
+        			catch (IOException ex) {
+        				//log.error("can't read "+fileName, ex);
+        				err.println("Can't access file from url " + fileName);        				
+        			}
     			}
-    			catch (IOException ex) {
-    				/* drop through ... */
+    			else
+    			{
+    		    	// it's not really an error since we can expect a file
+    		    	// instead of an url -> write to out and not to err 
+    		    	//out.println("Not an url -> assuming it's a file.");
+    				
+    				is = openFile(fileName, err);
     			}
+    			
     			if (is == null) {
-    				err.println("Not found " + ARG_FILE.getValue(cmdLine));
+    				// Here, we already have printed an appropriate 
+    				// error message. Simply return an errorcode.
+    				
     				// FIXME ... System.exit(1);
     				return;
     			}
     		}
+    		
+    		boolean isEmpty = true;
     		int len;
     		final byte[] buf = new byte[ 1024];
     		while ((len = is.read(buf)) > 0) {
+    			isEmpty = false;
+    			
     			out.write(buf, 0, len);
     		}
-    		// FIXME ... Why are we adding an extra newline???
-    		out.println();
+    		
+    		if(isEmpty && !isNewFile)
+    		{
+    			out.println("<empty file>");
+    		}
+    		
     		out.flush();
     	}
     	finally {
-    		if (is != null && fileName != null) {
+    		if (is != null && !isNewFile) {
     			is.close();
     		}
     	}
     }
 
-    private URL openURL(String fname) throws MalformedURLException {
+    private URL openURL(String fname) {
+    	URL url = null;
+    	
         try {
-            return new URL(fname);
+            url = new URL(fname);
         } catch (MalformedURLException ex) {
-            return new File(fname).toURL();
+        	//log.error(ex);
         }
+        
+        return url;
+    }
+
+    private InputStream openFile(String fname, PrintStream err) {
+    	InputStream is = null;
+    	
+        try {
+        	File file = new File(fname);
+        	if(!file.exists())
+        	{
+        		err.println("File doesn't exist");
+        	}
+        	else if(file.isDirectory())
+        	{
+        		err.println("Can't print content of a directory");
+        	}
+        	else
+        	{
+        		is = new FileInputStream(file);
+        	}
+        } catch (FileNotFoundException ex) {
+        	// should never happen since we check for existence before
+        }
+        
+        // here, if is is null we should have already printed 
+        // an appropriate error message
+        return is;
     }
 
 }
