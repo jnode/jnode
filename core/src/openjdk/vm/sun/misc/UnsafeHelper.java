478cc772ad3d0f5b128636c16efa2f695afe2d1e
Levente Santha <lsantha@jnode.org>
2007-11-22 21:29:15 +0000

    Fixed unneeded security exception.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3601 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/openjdk/vm/sun/misc/UnsafeHelper.java
+++ core/src/openjdk/vm/sun/misc/UnsafeHelper.java
@@ -4,6 +4,8 @@
 package sun.misc;
 
 import java.lang.reflect.Field;
+import java.security.PrivilegedAction;
+import java.security.AccessController;
 import org.jnode.vm.classmgr.VmInstanceField;
 import org.vmmagic.unboxed.ObjectReference;
 
@@ -11,11 +13,18 @@ import org.vmmagic.unboxed.ObjectReference;
  * @author Levente S\u00e1ntha
  */
 class UnsafeHelper {
-    private static final int OFFSET_OF_vmField_IN_Field =
-            ((VmInstanceField) Field.class.getVmClass().getField("vmField")).getOffset();
-    private static final int OFFSET_OF_offset_IN_VmInstanceField =
-            ((VmInstanceField) VmInstanceField.class.getVmClass().getField("offset")).getOffset();
+    private static final int OFFSET_OF_vmField_IN_Field = AccessController.doPrivileged(new PrivilegedAction<Integer>(){
+        public Integer run() {
+            return ((VmInstanceField) Field.class.getVmClass().getField("vmField")).getOffset();
+        }
+    });
 
+    private static final int OFFSET_OF_offset_IN_VmInstanceField = AccessController.doPrivileged(new PrivilegedAction<Integer>(){
+        public Integer run() {
+            return ((VmInstanceField) VmInstanceField.class.getVmClass().getField("offset")).getOffset();
+        }
+    });
+            
     static long objectFieldOffset(Field f) {
         return ObjectReference.fromObject(f).toAddress().
                 add(OFFSET_OF_vmField_IN_Field).loadAddress().
