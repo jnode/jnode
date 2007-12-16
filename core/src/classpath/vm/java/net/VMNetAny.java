b9ee4752a345d6b1b7659ee352e69fa02d10bf9f
Levente Santha <lsantha@jnode.org>
2007-12-16 14:09:25 +0000

    OpenJDK integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3630 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/classpath/vm/java/net/VMNetAny.java
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ */
+package java.net;
+
+/**
+ * @author Levente S\u00e1ntha
+ */
+public class VMNetAny {
+    /**
+     * Dummy InetAddress, used to bind socket to any (all) network interfaces.
+     */
+    //static InetAddress ANY_IF = new Inet4Address("0.0.0.0", new byte[]{0, 0, 0, 0});
+}
