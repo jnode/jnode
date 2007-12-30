a5aa4a7f1488d2d2cc80e318fd699f545d13e38e
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2007-12-30 21:34:51 +0000

    OpenJDK integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3664 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/sun/font/NativeNativeFont.java
@@ -0,0 +1,57 @@
+package sun.font;
+
+/**
+ * @see sun.font.NativeFont
+ * @author Levente S\u00e1ntha
+ */
+class NativeNativeFont {
+    /**
+     * @see sun.font.NativeFont#haveBitmapFonts(byte[])
+     */
+    private static boolean haveBitmapFonts(byte[] arg1) {
+        //todo implement it
+        return false;
+    }
+    /**
+     * @see sun.font.NativeFont#fontExists(byte[])
+     */
+    private static boolean fontExists(byte[] arg1) {
+        //todo implement it
+        return false;
+    }
+    /**
+     * @see sun.font.NativeFont#getFontMetrics(long)
+     */
+    private static StrikeMetrics getFontMetrics(NativeFont instance, long arg1) {
+        //todo implement it
+        return null;
+    }
+    /**
+     * @see sun.font.NativeFont#getGlyphAdvance(long, int)
+     */
+    private static float getGlyphAdvance(NativeFont instance, long arg1, int arg2) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see sun.font.NativeFont#getGlyphImage(long, int)
+     */
+    private static long getGlyphImage(NativeFont instance, long arg1, int arg2) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see sun.font.NativeFont#getGlyphImageNoDefault(long, int)
+     */
+    private static long getGlyphImageNoDefault(NativeFont instance, long arg1, int arg2) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see sun.font.NativeFont#countGlyphs(byte[], int)
+     */
+    private static int countGlyphs(NativeFont instance, byte[] arg1, int arg2) {
+        //todo implement it
+        return 0;
+    }
+}
---
+++ core/src/openjdk/vm/sun/font/NativeSunLayoutEngine.java
@@ -0,0 +1,22 @@
+package sun.font;
+
+import java.awt.geom.Point2D;
+
+/**
+ * @see sun.font.SunLayoutEngine
+ * @author Levente S\u00e1ntha
+ */
+class NativeSunLayoutEngine {
+    /**
+     * @see sun.font.SunLayoutEngine#initGVIDs()
+     */
+    private static void initGVIDs() {
+        //todo implement it
+    }
+    /**
+     * @see sun.font.SunLayoutEngine#nativeLayout(sun.font.Font2D, sun.font.FontStrike, float[], int, int, char[], int, int, int, int, int, int, int, java.awt.geom.Point2D.Float, sun.font.GlyphLayout.GVData)
+     */
+    private static void nativeLayout(Font2D arg1, FontStrike arg2, float[] arg3, int arg4, int arg5, char[] arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12, int arg13, Point2D.Float arg14, GlyphLayout.GVData arg15) {
+        //todo implement it
+    }
+}
