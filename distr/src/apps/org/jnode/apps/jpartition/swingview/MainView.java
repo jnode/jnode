a73a41715db29644abcdebebe5c61d0668d040e1
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-01-05 08:51:26 +0000

    build fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3685 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ distr/src/apps/org/jnode/apps/jpartition/swingview/MainView.java
@@ -0,0 +1,95 @@
+package org.jnode.apps.jpartition.swingview;
+
+import java.awt.BorderLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+import org.apache.log4j.Logger;
+import org.jnode.apps.jpartition.model.Device;
+import org.jnode.apps.jpartition.model.UserFacade;
+import org.jnode.apps.jpartition.model.UserListener;
+
+public class MainView extends JFrame {
+	private static final Logger log = Logger.getLogger(DeviceView.class);
+
+	final private DefaultComboBoxModel devices;
+	final private DeviceView deviceView;
+
+	public MainView(JComponent cmdProcessorView) throws Exception
+	{
+		setTitle("JPartition");
+		setLayout(new BorderLayout());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		add(cmdProcessorView, BorderLayout.SOUTH);
+
+		deviceView = new DeviceView();
+		add(deviceView, BorderLayout.CENTER);
+
+		devices = new DefaultComboBoxModel(UserFacade.getInstance().getDevices());
+		final JComboBox cboDevices = new JComboBox(devices);
+		cboDevices.addItemListener(new ItemListener()
+		{
+			public void itemStateChanged(ItemEvent event) {
+				boolean selected = (event.getStateChange() == ItemEvent.SELECTED);
+				String item = selected ? String.valueOf(event.getItem()) : null;
+				UserFacade.getInstance().selectDevice(item);
+				deviceView.update();
+			}
+		});
+		add(cboDevices, BorderLayout.NORTH);
+
+		setSize(600, 300);
+		setVisible(true);
+		setLocation(300, 300);
+
+		UserFacade.getInstance().setUserListener(new UserListener(){
+			public void deviceAdded(String name) {
+				devices.addElement(name);
+				deviceView.update();
+			}
+
+			public void deviceRemoved(String name) {
+				devices.removeElement(name);
+				deviceView.update();
+			}
+
+			public void selectionChanged(Device selectedDevice) {
+				cboDevices.setSelectedItem(selectedDevice.getName());
+				deviceView.update();
+			}});
+	}
+
+//	public void deviceStarted(Object device) {
+//    	log.debug("deviceStarted");
+//		devices.addElement(device);
+//	}
+//
+//	public void deviceStop(Object device) {
+//		log.debug("deviceStop");
+//		devices.removeElement(device);
+//	}
+//
+//	public void deviceSelected(Object device) {
+//		log.debug("deviceSelected");
+//		deviceView.clearDevice();
+//		//controller.userSelectDevice(device);
+//	}
+//
+//	public void partitionAdded(Object device) {
+//		log.debug("partitionAdded");
+//		deviceView.clearDevice();
+//		//controller.userSelectDevice(device);
+//	}
+//
+//	public void partitionRemoved(Object device) {
+//		log.debug("partitionRemoved");
+//		deviceView.clearDevice();
+//		//controller.userSelectDevice(device);
+//	}
+}
