6b532d888269496b123168e3997eba55701bc71f
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-01-06 18:07:36 +0000

    integration of a telnet server in jnode (base on telnetd library)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3687 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/driver/org/jnode/driver/textscreen/x86/PcBufferTextScreen.java
+++ core/src/driver/org/jnode/driver/textscreen/x86/PcBufferTextScreen.java
@@ -27,20 +27,10 @@ import org.jnode.vm.Unsafe;
 /**
  * @author Ewout Prangsma (epr@users.sourceforge.net)
  */
-public class PcBufferTextScreen extends AbstractPcTextScreen implements TextScreen {
+public class PcBufferTextScreen extends AbstractPcBufferTextScreen implements TextScreen {
 
     /** My parent */
     private final AbstractPcTextScreen parent;
-
-    /** Actual content buffer */
-    private final char[] buffer;
-
-    /** Temporary buffer that includes the cursor.  This goes to the video device.
-     * Slower, but more likely to be correct than a temporary pointer to the last character
-     * the cursor was over, as in previous versions.*/
-    private final char[]screenBuffer;
-    private int cursorIndex=0;
-    private boolean cursorVisible=true;
 
     /**
      * Initialize this instance.
@@ -49,113 +39,7 @@ public class PcBufferTextScreen extends AbstractPcTextScreen implements TextScre
      */
     public PcBufferTextScreen(int width, int height, AbstractPcTextScreen parent) {
         super(width, height);
-        this.buffer = new char[width * height];
-        this.screenBuffer=new char[buffer.length];
         this.parent = parent;
-    }
-
-    /**
-     * @see org.jnode.driver.textscreen.TextScreen#copyContent(int, int, int)
-     */
-    public void copyContent(int srcOffset, int destOffset, int length) {
-        System.arraycopy(buffer, srcOffset, buffer, destOffset, length);
-    }
-
-    /**
-     * @see org.jnode.driver.textscreen.TextScreen#getChar(int)
-     */
-    public char getChar(int offset) {
-        return (char)(buffer[offset] & 0xFF);
-    }
-
-    /**
-     * @see org.jnode.driver.textscreen.TextScreen#getColor(int)
-     */
-    public int getColor(int offset) {
-        return (char)((buffer[offset] >> 8) & 0xFF);
-    }
-
-    /**
-     * @see org.jnode.driver.textscreen.TextScreen#set(int, char, int, int)
-     */
-    public void set(int offset, char ch, int count, int color) {
-        final char v = (char)((ch & 0xFF) | ((color & 0xFF) << 8));
-        count = Math.min(count, buffer.length - offset);
-        for (int i = 0; i < count; i++) {
-            buffer[offset+i] = v;
-        }
-    }
-
-    /**
-     * @see org.jnode.driver.textscreen.TextScreen#set(int, char[], int, int, int)
-     */
-    public void set(int offset, char[] ch, int chOfs, int length, int color) {
-        color = (color & 0xFF) << 8;
-        length = Math.min(length, buffer.length - offset);
-        for (int i = 0; i < length; i++) {
-            buffer[offset+i] = (char)((ch[chOfs+i] & 0xFF) | color);
-        }
-    }
-
-    /**
-     * @see org.jnode.driver.textscreen.TextScreen#set(int, char[], int, int, int[], int)
-     */
-    public void set(int offset, char[] ch, int chOfs, int length, int[] colors,
-            int colorsOfs) {
-        length = Math.min(length, buffer.length - offset);
-        for (int i = 0; i < length; i++) {
-            buffer[offset+i] = (char)((ch[chOfs+i] & 0xFF) | (colors[colorsOfs+i] & 0xFF) << 8);
-        }
-    }
-
-    /**
-     * Copies the entire screen to the given destination.
-     * For this operation to succeed, the screens involved must be
-     * compatible.
-     * @param dst
-     */
-    public void copyTo( TextScreen dst ) {
-        if( dst instanceof AbstractPcTextScreen ) {
-            char[] toScreen = buffer;
-            if( cursorVisible&&cursorIndex<buffer.length&&cursorIndex>=0 ) {
-                System.arraycopy( buffer, 0, screenBuffer, 0, buffer.length );
-                char origValue = buffer[cursorIndex];
-                //origValue |= 0x7000;//from december 2003 jnode code.
-
-                //exchange the background with the foreground
-                int color = (origValue >>8) & 0xFF;
-                color = ((color >> 4) & 0xF) | ((color << 4) & 0xF0);
-                origValue &= 0x00FF;
-                origValue |= (color << 8) & 0xFF00;
-                
-                screenBuffer[cursorIndex] = origValue;
-                toScreen = screenBuffer;
-            }
-            ( (AbstractPcTextScreen)dst ).copyFrom( toScreen, getTopOffset() );
-        }
-        else {
-            throw new IllegalArgumentException( "Unknown destination type " + dst.getClass().getName() );
-        }
-    }
-
-    /**
-     * Return the offset in the buffer of the first visible row.
-     * @return the offset
-     */
-    protected int getTopOffset() {
-        return 0;
-    }
-
-    /**
-     * Copy the content of the given rawData into this screen.
-     * @param rawData
-     * @param rawDataOffset
-     */
-    public final void copyFrom(char[] rawData, int rawDataOffset) {
-        if (rawDataOffset < 0) {
-            Unsafe.die("Buffer:rawDataOffset = " + rawDataOffset);
-        }
-        System.arraycopy(rawData, rawDataOffset, buffer, getTopOffset(), getWidth() * getHeight());
     }
 
     /**
@@ -165,13 +49,8 @@ public class PcBufferTextScreen extends AbstractPcTextScreen implements TextScre
         copyTo(parent);
     }
 
-    public void setCursor( int x, int y ) {
-        this.cursorIndex=getOffset( x,y);
+    protected void setParentCursor( int x, int y ) {
         parent.setCursor(x, y);
-    }
-
-    public void setCursorVisible( boolean visible ) {
-        this.cursorVisible=visible;
     }
 
     /**
---
+++ distr/src/apps/org/jnode/apps/telnetd/TelnetServerCommand.java
@@ -0,0 +1,32 @@
+package org.jnode.apps.telnetd;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import net.wimpi.telnetd.TelnetD;
+
+
+/**
+ *
+ * @author Fabien DUMINY (fduminy at jnode.org)
+ *
+ */
+public class TelnetServerCommand
+{
+    public static void main(String[] args) throws Exception {
+        try {
+          //1. prepare daemon
+          Properties props = new Properties();
+          InputStream is = TelnetServerCommand.class.getResourceAsStream("telnetd.properties");
+          props.load(is);
+
+          TelnetD daemon = TelnetD.createTelnetD(props);
+
+          //2.start serving/accepting connections
+          daemon.start();
+        } catch (Exception ex) {
+          ex.printStackTrace();
+          System.exit(1);
+        }
+    }
+}
