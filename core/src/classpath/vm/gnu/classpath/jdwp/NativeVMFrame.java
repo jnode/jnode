71fd6cf1710700fbab7ee13453aa33e1441ff9dc
Levente Santha <lsantha@jnode.org>
2008-01-12 19:31:19 +0000

    Added support for hotswapping.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3693 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/classpath/vm/gnu/classpath/jdwp/NativeVMFrame.java
@@ -0,0 +1,20 @@
+package gnu.classpath.jdwp;
+
+/**
+ * @see gnu.classpath.jdwp.VMFrame
+ */
+class NativeVMFrame {
+    /**
+     * @see gnu.classpath.jdwp.VMFrame#getValue(int)
+     */
+    private static Object getValue(VMFrame instance, int arg1) {
+        //todo implement it
+        return null;
+    }
+    /**
+     * @see gnu.classpath.jdwp.VMFrame#setValue(int, java.lang.Object)
+     */
+    private static void setValue(VMFrame instance, int arg1, Object arg2) {
+        //todo implement it
+    }
+}
