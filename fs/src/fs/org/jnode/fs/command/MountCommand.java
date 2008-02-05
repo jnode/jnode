d109046a7136758d995628bd9a4e52f605f8eb50
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-02-05 22:57:41 +0000

    added a variant without parameter to list mounted filesystems
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3764 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/command/MountCommand.java
+++ fs/src/fs/org/jnode/fs/command/MountCommand.java
@@ -23,8 +23,10 @@ package org.jnode.fs.command;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Map;
 
 import org.jnode.driver.Device;
+import org.jnode.driver.block.BlockDeviceAPI;
 import org.jnode.fs.FileSystem;
 import org.jnode.fs.service.FileSystemService;
 import org.jnode.naming.InitialNaming;
@@ -42,7 +44,7 @@ import org.jnode.shell.help.argument.FileArgument;
 public class MountCommand extends AbstractCommand {
 
     private static final DeviceArgument ARG_DEV = new DeviceArgument("device",
-            "the device to mount");
+            "the device to mount", BlockDeviceAPI.class);
 
     private static final FileArgument ARG_DIR = new FileArgument("directory",
             "the mountpoint");
@@ -51,8 +53,8 @@ public class MountCommand extends AbstractCommand {
     "the subdirectory within the filesystem to use as root");
 
     static Help.Info HELP_INFO = new Help.Info("mount", "Mount a filesystem",
-            new Parameter[] { new Parameter(ARG_DEV, Parameter.MANDATORY),
-                    new Parameter(ARG_DIR, Parameter.MANDATORY),
+            new Parameter[] { new Parameter(ARG_DEV, Parameter.OPTIONAL),
+                    new Parameter(ARG_DIR, Parameter.OPTIONAL),
                     new Parameter(ARG_FSPATH, Parameter.OPTIONAL) });
 
     public static void main(String[] args) throws Exception {
@@ -63,22 +65,40 @@ public class MountCommand extends AbstractCommand {
             PrintStream out, PrintStream err) throws Exception {
         ParsedArguments cmdLine = HELP_INFO.parse(commandLine);
 
-        // Get the parameters
-        final Device dev = ARG_DEV.getDevice(cmdLine);
-        final String mountPoint = ARG_DIR.getValue(cmdLine);
-        final String fsPath = ARG_FSPATH.getValue(cmdLine);
+        if(ARG_DEV.getValue(cmdLine) == null)
+        {
+	        // Find the filesystem service
+	        final FileSystemService fss = InitialNaming.lookup(FileSystemService.NAME);
 
-        // Find the filesystem service
-        final FileSystemService fss = InitialNaming.lookup(FileSystemService.NAME);
+	        Map<String, FileSystem<?>> filesystems = fss.getMountPoints();
+        	for(String mountPoint : filesystems.keySet())
+        	{
+        		FileSystem<?> fs = filesystems.get(mountPoint);
+        		Device device = fs.getDevice();
+        		String mode = fs.isReadOnly() ? "ro" : "rw";
+        		String type = fs.getType().getName();
+        		System.out.println(device.getId() + " on " + mountPoint + " type " + type + " (" + mode + ')');
+        	}
+        }
+        else
+        {
+            // Get the parameters
+            final Device dev = ARG_DEV.getDevice(cmdLine);
+	        final String mountPoint = ARG_DIR.getValue(cmdLine);
+	        final String fsPath = ARG_FSPATH.getValue(cmdLine);
 
-        // Find the filesystem
-        final FileSystem fs = fss.getFileSystem(dev);
-        if (fs == null) {
-            err.println("No filesystem found on " + dev.getId());
-            exit(1);
-        } else {
-            // Mount it
-            fss.mount(mountPoint, fs, fsPath);
+	        // Find the filesystem service
+	        final FileSystemService fss = InitialNaming.lookup(FileSystemService.NAME);
+
+	        // Find the filesystem
+	        final FileSystem<?> fs = fss.getFileSystem(dev);
+	        if (fs == null) {
+	            err.println("No filesystem found on " + dev.getId());
+	            exit(1);
+	        } else {
+	            // Mount it
+	            fss.mount(mountPoint, fs, fsPath);
+	        }
         }
     }
 
