b190de8b39a2ccb5774cada037c7470c5babe8e2
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-02-07 15:30:53 +0000

    Fix node names.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3767 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/hfsplus/HFSUnicodeString.java
+++ fs/src/fs/org/jnode/fs/hfsplus/HFSUnicodeString.java
@@ -4,24 +4,30 @@ import org.jnode.util.BigEndian;
 
 
 public class HFSUnicodeString {
-
-	private byte[] data;
+	/** Length of string in characters. */
+	private int length;
+	
+	private String string;
 	
 	public HFSUnicodeString(byte[] src, int offset){
-		data = new byte[src.length];
-		System.arraycopy(src, offset, data, 0, src.length);
+		byte[] data = new byte[2];
+		System.arraycopy(src, offset, data, 0, 2);
+		length = BigEndian.getInt16(data, 0);
+		data = new byte[length*2];
+		System.arraycopy(src, offset+2, data, 0, length*2);
+		char[] result = new char[length];
+		for(int i = 0; i < length; ++i){
+		    result[i] = BigEndian.getChar(data, i*2);
+		}
+		string =  new String(result);
 	}
 
 	public int getLength(){
-		return BigEndian.getInt16(data, 0);
+		return length;
 	}
 	
 	public String getUnicodeString(){
-		char[] result = new char[(getLength())];
-		for(int i = 1; i < result.length; ++i){
-		    result[i] = BigEndian.getChar(data, i*2);
-		}
-		return new String(result);
+		return string;
 	}
 	
 }
