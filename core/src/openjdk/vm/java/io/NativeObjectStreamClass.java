08af5b68cd505d1df2794d0c853b0c86427517ab
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-02-17 14:33:49 +0000

    OpenJDK integration - completed support for object serialization
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3804 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/openjdk/vm/java/io/NativeObjectStreamClass.java
+++ core/src/openjdk/vm/java/io/NativeObjectStreamClass.java
@@ -1,7 +1,11 @@
 package java.io;
 
+import org.jnode.vm.classmgr.VmType;
+import org.jnode.vm.classmgr.VmMethod;
+
 /**
  * @see java.io.ObjectStreamClass
+ * @author Levente S\u00e1ntha
  */
 class NativeObjectStreamClass {
     /**
@@ -13,8 +17,9 @@ class NativeObjectStreamClass {
     /**
      * @see java.io.ObjectStreamClass#hasStaticInitializer(java.lang.Class)
      */
-    private static boolean hasStaticInitializer(Class arg1) {
-        //todo implement it
-        return false;
+    private static boolean hasStaticInitializer(Class clazz) {
+        VmType vmt = clazz.getVmClass();
+        VmMethod met = vmt.getDeclaredMethod("<clinit>", "()V");
+        return met != null && met.isStatic();
     }
 }
