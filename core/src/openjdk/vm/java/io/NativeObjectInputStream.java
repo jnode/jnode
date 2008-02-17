e116b5c325c0ec9ad7ecd42dd240719b9ac7597f
Levente Santha <lsantha@jnode.org>
2008-02-17 14:33:49 +0000

    OpenJDK integration - completed support for object serialization
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3804 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/openjdk/vm/java/io/NativeObjectInputStream.java
+++ core/src/openjdk/vm/java/io/NativeObjectInputStream.java
@@ -1,26 +1,60 @@
 package java.io;
 
+import org.jnode.vm.VmSystem;
+
 /**
  * @see java.io.ObjectInputStream
+ * @author Levente S\u00e1ntha
  */
 class NativeObjectInputStream {
     /**
      * @see java.io.ObjectInputStream#bytesToFloats(byte[], int, float[], int, int)
      */
-    private static void bytesToFloats(byte[] arg1, int arg2, float[] arg3, int arg4, int arg5) {
-        //todo implement it
+    private static void bytesToFloats(byte[] src, int srcpos, float[] dst, int dstpos, int nfloats) {
+        if(src == null) throw new NullPointerException();
+        if(dst == null) throw new NullPointerException();
+        int dstend = dstpos + nfloats;
+        while(dstpos < dstend){
+            int ival =
+                    ((src[srcpos ++ ] & 0xff) << 24) +
+                    ((src[srcpos ++ ] & 0xff) << 16) +
+                    ((src[srcpos ++ ] & 0xff) <<  8) +
+                    ((src[srcpos ++ ] & 0xff)      );
+            dst[dstpos ++ ] = Float.intBitsToFloat(ival);
+        }
     }
     /**
      * @see java.io.ObjectInputStream#bytesToDoubles(byte[], int, double[], int, int)
      */
-    private static void bytesToDoubles(byte[] arg1, int arg2, double[] arg3, int arg4, int arg5) {
-        //todo implement it
+    private static void bytesToDoubles(byte[] src, int srcpos, double[] dst, int dstpos, int ndoubles) {
+        if(src == null) throw new NullPointerException();
+        if(dst == null) throw new NullPointerException();
+        int dstend = dstpos + ndoubles;
+        while(dstpos < dstend){
+            long lval =
+                    ((src[srcpos ++ ] & 0xffL) << 56) +
+                    ((src[srcpos ++ ] & 0xffL) << 48) +
+                    ((src[srcpos ++ ] & 0xffL) << 40) +
+                    ((src[srcpos ++ ] & 0xffL) << 32) +
+                    ((src[srcpos ++ ] & 0xffL) << 24) +
+                    ((src[srcpos ++ ] & 0xffL) << 16) +
+                    ((src[srcpos ++ ] & 0xffL) <<  8) +
+                    ((src[srcpos ++ ] & 0xffL)      );
+            dst[dstpos ++ ] = Double.longBitsToDouble(lval);
+        }
     }
     /**
      * @see java.io.ObjectInputStream#latestUserDefinedLoader()
      */
     private static ClassLoader latestUserDefinedLoader() {
-        //todo implement it
+        Class[] ctx = VmSystem.getClassContext();
+        for(int i = 0; i < ctx.length; i++){
+            Class c = ctx[i];
+            if(!sun.reflect.MethodAccessor.class.isAssignableFrom(c) &&
+            !sun.reflect.ConstructorAccessor.class.isAssignableFrom(c)){
+                return c.getClassLoader();
+            }
+        }
         return null;
     }
 }
--- core/src/openjdk/vm/java/io/NativeObjectOutputStream.java
+++ core/src/openjdk/vm/java/io/NativeObjectOutputStream.java
@@ -2,18 +2,41 @@ package java.io;
 
 /**
  * @see java.io.ObjectOutputStream
+ * @author Levente S\u00e1ntha
  */
 class NativeObjectOutputStream {
     /**
      * @see java.io.ObjectOutputStream#floatsToBytes(float[], int, byte[], int, int)
      */
-    private static void floatsToBytes(float[] arg1, int arg2, byte[] arg3, int arg4, int arg5) {
-        //todo implement it
+    private static void floatsToBytes(float[] src, int srcpos, byte[] dst, int dstpos, int nfloats) {
+        if(src == null) throw new NullPointerException();
+        if(dst == null) throw new NullPointerException();
+        int srcend = srcpos + nfloats;
+        while( srcpos < srcend ){
+            int ival = Float.floatToIntBits(src[srcpos ++]);
+            dst[dstpos ++ ] = (byte) ((ival >> 24) & 0xff);
+            dst[dstpos ++ ] = (byte) ((ival >> 16) & 0xff);
+            dst[dstpos ++ ] = (byte) ((ival >>  8) & 0xff);
+            dst[dstpos ++ ] = (byte) ((ival      ) & 0xff);
+        }
     }
     /**
      * @see java.io.ObjectOutputStream#doublesToBytes(double[], int, byte[], int, int)
      */
-    private static void doublesToBytes(double[] arg1, int arg2, byte[] arg3, int arg4, int arg5) {
-        //todo implement it
+    private static void doublesToBytes(double[] src, int srcpos, byte[] dst, int dstpos, int ndoubles) {
+        if(src == null) throw new NullPointerException();
+        if(dst == null) throw new NullPointerException();
+        int srcend = srcpos + ndoubles;
+        while( srcpos < srcend ){
+            long lval = Double.doubleToLongBits(src[srcpos ++]);
+            dst[dstpos ++ ] = (byte) ((lval >> 56) & 0xff);
+            dst[dstpos ++ ] = (byte) ((lval >> 48) & 0xff);
+            dst[dstpos ++ ] = (byte) ((lval >> 40) & 0xff);
+            dst[dstpos ++ ] = (byte) ((lval >> 32) & 0xff);
+            dst[dstpos ++ ] = (byte) ((lval >> 24) & 0xff);
+            dst[dstpos ++ ] = (byte) ((lval >> 16) & 0xff);
+            dst[dstpos ++ ] = (byte) ((lval >>  8) & 0xff);
+            dst[dstpos ++ ] = (byte) ((lval      ) & 0xff);
+        }
     }
 }
