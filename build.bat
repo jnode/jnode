725fcb6912c27841cd81f4a32451914146977e2a
hagar-wize <hagar-wize@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-02-21 09:58:33 +0000

    increased heap
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3813 407389f7-7c16-0410-9eee-ad85acb55d7f

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 setlocal
-"%JAVA_HOME%\bin\java" -Xmx512M -Xms128M -jar core\lib\ant-launcher.jar -lib "%JAVA_HOME%\lib" -lib core\lib -f all\build.xml %*
+"%JAVA_HOME%\bin\java" -Xmx768M -Xms256M -jar core\lib\ant-launcher.jar -lib "%JAVA_HOME%\lib" -lib core\lib -f all\build.xml %*
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dir=`dirname $0`
-java -Xmx512M -Xms128M -jar $dir/core/lib/ant-launcher.jar -lib $JAVA_HOME/lib -lib $dir/core/lib -f $dir/all/build.xml $*
+java -Xmx768M -Xms256M -jar $dir/core/lib/ant-launcher.jar -lib $JAVA_HOME/lib -lib $dir/core/lib -f $dir/all/build.xml $*
 
 
