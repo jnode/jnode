c09024ae6ae8c05862fb334c33fe37a2dc45db2c
konkubinaten <konkubinaten@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-02-28 20:08:25 +0000

    ~ corrected license of mmtk-vm files
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3819 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/mmtk-vm/org/mmtk/vm/Plan.java
+++ core/src/mmtk-vm/org/mmtk/vm/Plan.java
@@ -1,24 +1,16 @@
 /*
- * $Id$
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
  *
- * JNode.org
- * Copyright (C) 2003-2006 JNode.org
+ *  This file is licensed to You under the Common Public License (CPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
+ *      http://www.opensource.org/licenses/cpl1.0.php
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; If not, write to the Free Software Foundation, Inc., 
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
  */
- 
+
 package org.mmtk.vm;
 
 import org.jnode.vm.annotation.Inline;
