0a06288d08ac5958c69ab14ce9ce0a5d4579ee3f
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-03-24 07:51:28 +0000

    Added first tests for new syntax completion etc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3867 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/test/org/jnode/test/shell/CompletionTest.java
+++ shell/src/test/org/jnode/test/shell/CompletionTest.java
@@ -1,8 +1,29 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2007-2008 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.jnode.test.shell;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 import javax.naming.NameNotFoundException;
 import javax.naming.NamingException;
@@ -16,6 +37,9 @@ import org.jnode.shell.RedirectingInterpreter;
 import org.jnode.shell.ShellUtils;
 import org.jnode.shell.ThreadCommandInvoker;
 import org.jnode.shell.alias.AliasManager;
+import org.jnode.shell.syntax.ArgumentSyntax;
+import org.jnode.shell.syntax.SequenceSyntax;
+import org.jnode.shell.syntax.SyntaxManager;
 import org.jnode.test.shell.syntax.TestAliasManager;
 import org.jnode.test.shell.syntax.TestSyntaxManager;
 
@@ -34,9 +58,20 @@ public class CompletionTest extends TestCase {
     
     private String userDirName = System.getProperty("user.dir");
     private File testDir;
+    private String[] aliasCompletions;
     
+    static {
+        try {
+            Cassowary.initEnv();
+        }
+        catch (NamingException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
     @Override
     protected void setUp() throws Exception {
+        // Setup a temporary home directory for filename completion
         File tempDir = new File(System.getProperty("java.io.tmpdir"));
         testDir = new File(tempDir, "CompletionTestDir");
         testDir.mkdir();
@@ -62,17 +97,8 @@ public class CompletionTest extends TestCase {
         System.setProperty("user.dir", userDirName);
     }
 
-    public static class TestCommandShell extends CommandShell {
+    public class TestCommandShell extends CommandShell {
         
-        static {
-            try {
-                Cassowary.initEnv();
-            }
-            catch (NamingException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-
         public TestCommandShell() throws NameNotFoundException {
             super(new TestAliasManager(), new TestSyntaxManager());
             ShellUtils.getShellManager().registerShell(this);
@@ -85,8 +111,15 @@ public class CompletionTest extends TestCase {
 
             AliasManager am = this.getAliasManager();
             am.add("gc", "org.jnode.shell.command.GcCommand");
+            am.add("cpuid", "org.jnode.shell.command.system.CpuIDCommand");
+            am.add("set", "org.jnode.shell.command.SetCommand");
             am.add("dir", "org.jnode.test.shell.MyDirCommand");
             am.add("cat", "org.jnode.test.shell.MyCatCommand");
+            aliasCompletions = new String[]{"cat ", "cpuid ", "dir ", "gc ", "set "};
+            
+            SyntaxManager sm = this.getSyntaxManager();
+            sm.add("set", new SequenceSyntax(new ArgumentSyntax("key"), new ArgumentSyntax("value")));
+            sm.add("cpuid", new SequenceSyntax());
         }
     }
     
@@ -94,49 +127,86 @@ public class CompletionTest extends TestCase {
         TestCommandShell cs = new TestCommandShell();
         cs.setCommandInterpreter("default");
         
-        checkCompletions(cs.complete(""), new String[]{"cat ", "dir ", "gc "});
-        checkCompletions(cs.complete("di"), new String[]{"dir "});
-        checkCompletions(cs.complete("dir"), new String[]{"dir "});
-        checkCompletions(cs.complete("dir "), new String[]{"Four/", "One ", "Three ", "Two ", });
-        checkCompletions(cs.complete("dir T"), new String[]{"Three ", "Two "});
-        checkCompletions(cs.complete("cat -"), new String[]{"-u "});
-        checkCompletions(cs.complete("cat -u"), new String[]{"-u "});
+        checkCompletions(cs.complete(""), aliasCompletions, 0);
+        checkCompletions(cs.complete("di"), new String[]{"dir "}, 0);
+        checkCompletions(cs.complete("dir"), new String[]{"dir "}, 0);
+        checkCompletions(cs.complete("dir "), new String[]{"Four/", "One ", "Three ", "Two ", }, 4);
+        checkCompletions(cs.complete("dir T"), new String[]{"Three ", "Two "}, 4);
         
         // The default interpreter doesn't recognize '>' '<' or '|' as anything special.
         // Therefore it should just try to complete them as filenames ... and fail.
-        checkCompletions(cs.complete("dir |"), new String[]{});
-        checkCompletions(cs.complete("dir | "), new String[]{}); // dir takes one argument only
-        checkCompletions(cs.complete("cat | "), new String[]{"Four/", "One ", "Three ", "Two ", });
-        checkCompletions(cs.complete("cat | ca"), new String[]{});
-        checkCompletions(cs.complete("cat >"), new String[]{});
-        checkCompletions(cs.complete("cat > "), new String[]{"Four/", "One ", "Three ", "Two ", });
-        checkCompletions(cs.complete("cat <"), new String[]{});
-        checkCompletions(cs.complete("cat < "), new String[]{"Four/", "One ", "Three ", "Two ", });
+        checkCompletions(cs.complete("dir |"), new String[]{}, 4);
+        checkCompletions(cs.complete("dir | "), new String[]{}, 6); // dir takes one argument only
+        checkCompletions(cs.complete("cat | "), new String[]{"Four/", "One ", "Three ", "Two ", }, 6);
+        checkCompletions(cs.complete("cat | ca"), new String[]{}, 6);
+        checkCompletions(cs.complete("cat >"), new String[]{}, 4);
+        checkCompletions(cs.complete("cat > "), new String[]{"Four/", "One ", "Three ", "Two ", }, 6);
+        checkCompletions(cs.complete("cat <"), new String[]{}, 4);
+        checkCompletions(cs.complete("cat < "), new String[]{"Four/", "One ", "Three ", "Two ", }, 6);
+    }
+    
+    public void testOldSyntaxOptions() throws Exception {
+        TestCommandShell cs = new TestCommandShell();
+        cs.setCommandInterpreter("default");
+        
+        // For bug #4418
+        checkCompletions(cs.complete("cat -"), new String[]{"-u "}, -2);
+        checkCompletions(cs.complete("cat -u"), new String[]{"-u "}, -2);
+        
+        // And some more ...
     }
     
     public void testRedirectingInterpreterOldSyntax() throws Exception {
         TestCommandShell cs = new TestCommandShell();
         cs.setCommandInterpreter("redirecting");
         
-        checkCompletions(cs.complete(""), new String[]{"cat ", "dir ", "gc "});
-        checkCompletions(cs.complete("di"), new String[]{"dir "});
-        checkCompletions(cs.complete("dir"), new String[]{"dir "});
-        checkCompletions(cs.complete("dir "), new String[]{"Four/", "One ", "Three ", "Two ", });
-        checkCompletions(cs.complete("dir T"), new String[]{"Three ", "Two "});
-        checkCompletions(cs.complete("cat -"), new String[]{"-u "});
-        checkCompletions(cs.complete("cat -u"), new String[]{"-u "});
-        checkCompletions(cs.complete("dir |"), new String[]{"cat ", "dir ", "gc "});
-        checkCompletions(cs.complete("dir | "), new String[]{"cat ", "dir ", "gc "});
-        checkCompletions(cs.complete("cat |"), new String[]{"cat ", "dir ", "gc "});
-        checkCompletions(cs.complete("cat | "), new String[]{"cat ", "dir ", "gc "});
-        checkCompletions(cs.complete("cat | ca"), new String[]{"cat "});
-        checkCompletions(cs.complete("cat >"), new String[]{"Four/", "One ", "Three ", "Two ", });
-        checkCompletions(cs.complete("cat > "), new String[]{"Four/", "One ", "Three ", "Two ", });
-        checkCompletions(cs.complete("cat <"), new String[]{"Four/", "One ", "Three ", "Two ", });
-        checkCompletions(cs.complete("cat < "), new String[]{"Four/", "One ", "Three ", "Two ", });
+        checkCompletions(cs.complete(""), aliasCompletions, 0);
+        checkCompletions(cs.complete("di"), new String[]{"dir "}, 0);
+        checkCompletions(cs.complete("dir"), new String[]{"dir "}, 0);
+        checkCompletions(cs.complete("dir "), new String[]{"Four/", "One ", "Three ", "Two ", }, 4);
+        checkCompletions(cs.complete("dir T"), new String[]{"Three ", "Two "}, 4);
+        checkCompletions(cs.complete("dir |"), aliasCompletions, 5);
+        checkCompletions(cs.complete("dir | "), aliasCompletions, 6);
+        checkCompletions(cs.complete("cat |"), aliasCompletions, 5);
+        checkCompletions(cs.complete("cat | "), aliasCompletions, 6);
+        checkCompletions(cs.complete("cat | ca"), new String[]{"cat "}, 6);
+        checkCompletions(cs.complete("cat >"), new String[]{"Four/", "One ", "Three ", "Two ", }, 5);
+        checkCompletions(cs.complete("cat > "), new String[]{"Four/", "One ", "Three ", "Two ", }, 6);
+        checkCompletions(cs.complete("cat <"), new String[]{"Four/", "One ", "Three ", "Two ", }, 5);
+        checkCompletions(cs.complete("cat < "), new String[]{"Four/", "One ", "Three ", "Two ", }, 6);
     }
     
-    private void checkCompletions(CompletionInfo ci, String[] expected) {
+    public void testDefaultInterpreterNewSyntax() throws Exception {
+        TestCommandShell cs = new TestCommandShell();
+        cs.setCommandInterpreter("default");
+        
+        final String[] propertyCompletions = getExpectedPropertyNameCompletions();
+
+        checkCompletions(cs.complete("set "), propertyCompletions, 4);
+        checkCompletions(cs.complete("set a"), new String[]{}, 4);
+        checkCompletions(cs.complete("set u"), new String[]{
+            "user.country ", "user.dir ", "user.home ", 
+            "user.language ", "user.name ", "user.timezone ", }, 4);
+        checkCompletions(cs.complete("set a "), new String[]{}, 6);
+        checkCompletions(cs.complete("set a b"), new String[]{}, 6);
+        checkCompletions(cs.complete("set a b "), new String[]{}, 8);
+        
+        checkCompletions(cs.complete("cpuid "), new String[]{}, 6);
+    }
+    
+    /**
+     * Snarf the system property names in the form we expect for
+     * property name completion.
+     */
+    private String[] getExpectedPropertyNameCompletions() {
+        TreeSet<String> tmp = new TreeSet<String>();
+        for (Object key : System.getProperties().keySet()) {
+            tmp.add(key + " ");
+        }
+        return tmp.toArray(new String[tmp.size()]);
+    }
+
+    private void checkCompletions(CompletionInfo ci, String[] expected, int startPos) {
         SortedSet<String> completions = ci.getCompletions();
         if (completions.size() != expected.length) {
             err("Wrong number of completions", expected, completions);
@@ -148,6 +218,7 @@ public class CompletionTest extends TestCase {
             }
             i++;
         }
+        assertEquals(startPos, ci.getCompletionStart());
     }
     
     private void err(String message, String[] expected, Collection<String> actual) {
