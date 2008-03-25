6fd38dface464de616a5411188b53dd66ec1ac54
Levente Santha <lsantha@jnode.org>
2008-03-25 22:10:53 +0000

    AWT and SwingPeer improvements.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3881 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/awt/image/NativeComponentSampleModel.java
@@ -0,0 +1,13 @@
+package java.awt.image;
+
+/**
+ * @see java.awt.image.ComponentSampleModel
+ */
+class NativeComponentSampleModel {
+    /**
+     * @see java.awt.image.ComponentSampleModel#initIDs()
+     */
+    private static void initIDs() {
+        //empty
+    }
+}
---
+++ core/src/openjdk/vm/java/awt/image/NativeSinglePixelPackedSampleModel.java
@@ -0,0 +1,13 @@
+package java.awt.image;
+
+/**
+ * @see java.awt.image.SinglePixelPackedSampleModel
+ */
+class NativeSinglePixelPackedSampleModel {
+    /**
+     * @see java.awt.image.SinglePixelPackedSampleModel#initIDs()
+     */
+    private static void initIDs() {
+        //empty
+    }
+}
