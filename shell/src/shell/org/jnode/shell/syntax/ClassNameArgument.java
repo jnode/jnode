315ac6df651e1a1efb581e0d98e09d9c5e20af46
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-03-27 11:58:20 +0000

    Ported ClassNameArgument to new syntax APIs
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3884 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ shell/src/shell/org/jnode/shell/syntax/ClassNameArgument.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2007-2008 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jnode.shell.syntax;
+
+import org.jnode.shell.CommandLine.Token;
+
+public class ClassNameArgument extends Argument<String> {
+    
+    public ClassNameArgument(String label, int flags, String description) {
+        super(label, flags, new String[0], description);
+    }
+
+    public ClassNameArgument(String label, int flags) {
+        this(label, flags, null);
+    }
+
+    @Override
+    protected void doAccept(Token token) throws CommandSyntaxException {
+        addValue(token.token);
+    }
+  
+    @Override
+    public String toString() {
+        return "ClassNameArgument{" + super.toString() + "}";
+    }
+    
+    @Override
+    protected String argumentKind() {
+        return "class name";
+    }
+    
+    public Class<?> getValueAsClass() throws ClassNotFoundException {
+        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        return cl.loadClass(getValue());
+    }
+}
