cce855f498eb91aa34f7eb800a045aacbea142a4
konkubinaten <konkubinaten@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-03-30 19:19:21 +0000

    added copyright and license header
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3915 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/imageio/jpeg/JPEGDecoder.java
+++ core/src/core/org/jnode/imageio/jpeg/JPEGDecoder.java
@@ -1,9 +1,27 @@
 package org.jnode.imageio.jpeg;
 import java.io.*;
+
 /*
-    Original source: http://webuser.fh-furtwangen.de/~dersch/
-    With the permission of Helmut Dersch.
-*/
+ * Copyright (C) Helmut Dersch
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *
+ * Original source: http://webuser.fh-furtwangen.de/~dersch/
+ * Changed License to LGPL with the friendly permission of Helmut Dersch.
+ */
 
 public class JPEGDecoder{
     private int height;
