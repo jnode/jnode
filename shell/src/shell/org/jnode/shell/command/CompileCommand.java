ea9a22eeb1e6cec53e34a866f1cedae2fb7f126e
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-04-06 05:06:43 +0000

    Recoded 'compile' command to use new syntax mechanism.  (It also does completion in the 'level' argument, though more work is needed ...)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3936 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/CompileCommand.java
+++ shell/src/shell/org/jnode/shell/command/CompileCommand.java
@@ -21,40 +21,79 @@
  
 package org.jnode.shell.command;
 
-import org.jnode.shell.help.Help;
-import org.jnode.shell.help.Parameter;
-import org.jnode.shell.help.ParsedArguments;
-import org.jnode.shell.help.argument.ClassNameArgument;
-import org.jnode.shell.help.argument.IntegerArgument;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.jnode.shell.AbstractCommand;
+import org.jnode.shell.CommandLine;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.ClassNameArgument;
+import org.jnode.shell.syntax.FlagArgument;
+import org.jnode.shell.syntax.IntegerArgument;
+import org.jnode.vm.VmArchitecture;
+import org.jnode.vm.VmMagic;
 import org.jnode.vm.classmgr.VmType;
 
 /**
  * @author epr
+ * @author crawley@jnode.org
  */
-public class CompileCommand {
-
-	static final ClassNameArgument ARG_CLASS = new ClassNameArgument("className", "the class file to compile");
-	static final IntegerArgument ARG_LEVEL = new IntegerArgument("level", "the optimization level");
-	static final IntegerArgument ARG_TEST = new IntegerArgument("test", "If 1, the test compilers are used");
-	static final Parameter PARAM_LEVEL = new Parameter(ARG_LEVEL, Parameter.OPTIONAL);
-	static final Parameter PARAM_TEST = new Parameter(ARG_TEST, Parameter.OPTIONAL);
+public class CompileCommand extends AbstractCommand {
+    // FIXME ... it would be good if something exposed the current maximum 
+    // compiler optimization level via(s) a getter method.
+//    private final VmArchitecture arch = VmMagic.currentProcessor().getArchitecture();
+//    private final Object[] compilers = arch.getCompilers();
+//    private final Object[] testCompilers = arch.getTestCompilers();
+//    private final int maxLevel = Math.max(
+//            compilers.length, (testCompilers == null) ? 0 : testCompilers.length) - 1;
+    
+    // FIXME ... temporary hack until I work out what is causing the System.out stream
+    // to be toasted in the compiler thread.  (The commented out code causes the compiler
+    // to try to print "MagicPermission is not granted ..." to System.out, and a NPE is
+    // being thrown because (I think) the proclet context thinks that the global output
+    // stream is null.)
+    private final int maxLevel = 3;
+    
+	private final ClassNameArgument ARG_CLASS = 
+	    new ClassNameArgument("className", Argument.MANDATORY, "the class file to compile");
+	private final IntegerArgument ARG_LEVEL = 
+	    new IntegerArgument("level", Argument.OPTIONAL, 0, maxLevel, "the optimization level");
+	private final FlagArgument ARG_TEST = 
+	    new FlagArgument("test", Argument.OPTIONAL, "use the test version of the compiler");
 	
-	public static Help.Info HELP_INFO = new Help.Info("compile", "Compile a Java class", new Parameter[] { new Parameter(ARG_CLASS, Parameter.MANDATORY), PARAM_LEVEL, PARAM_TEST});
-
+	public CompileCommand() {
+        super("compile a Java class (bytecodes) to native code");
+        registerArguments(ARG_CLASS, ARG_LEVEL, ARG_TEST);
+    }
+	
 	public static void main(String[] args) throws Exception {
-		final ParsedArguments cmdLine = HELP_INFO.parse(args);
-
-		final String className = ARG_CLASS.getValue(cmdLine);
-		final int level = PARAM_LEVEL.isSet(cmdLine) ? ARG_LEVEL.getInteger(cmdLine) : 0;
-		final boolean test = PARAM_TEST.isSet(cmdLine) ? (ARG_TEST.getInteger(cmdLine) != 0) : false;
+	    new CompileCommand().execute(args);
+	}
+	
+	@Override
+    public void execute(CommandLine commandLine, InputStream in,
+            PrintStream out, PrintStream err) 
+	throws Exception {
+		final String className = ARG_CLASS.getValue();
+		final int level = ARG_LEVEL.isSet() ? ARG_LEVEL.getValue() : 0;
+		final boolean test = ARG_TEST.isSet();
 		
 		final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-		final Class<?> cls = cl.loadClass(className);
+		final Class<?> cls;
+		try {
+		    cls = cl.loadClass(className);
+		}
+		catch (ClassNotFoundException ex) {
+		    err.println("Class '" + className + "' not found");
+		    exit(1);
+		    // not reached
+		    return;  
+		}
 		final VmType<?> type = cls.getVmClass();
 		final long start = System.currentTimeMillis();
 		final int count = type.compileRuntime(level, test);
 		final long end = System.currentTimeMillis();
-		System.out.println("Compiling " + count + " methods took " + (end - start) + "ms");
+		out.println("Compiling " + count + " methods took " + (end - start) + "ms");
 	}
 
 }
