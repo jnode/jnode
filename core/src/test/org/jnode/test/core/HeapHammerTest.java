294cf122af08f6d78d26bff3b63600fb9b6c17de
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-04-20 10:25:10 +0000

    Like it says ... hammer the heap
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3981 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/test/org/jnode/test/core/HeapHammerTest.java
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ *
+ * JNode.org
+ * Copyright (C) 2008 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package org.jnode.test.core;
+
+/**
+ * Hammer the heap by allocating and dropping large numbers of objects (byte arrays)
+ * 
+ * @author crawley@jnode.org
+ */
+public class HeapHammerTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Starting HeapHammerTest ...");
+        for (int i = 0; i < 10000000; i++) {
+            Object dummy = new byte[i % 100];
+        }
+        System.out.println("done");
+    }
+
+}
