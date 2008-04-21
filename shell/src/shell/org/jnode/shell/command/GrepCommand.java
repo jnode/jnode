f250c8fb11cdbb2000c82ccb3349bb8f70f58563
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-04-21 11:57:47 +0000

    Cosmetic: delete unnecessary import
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@3992 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/GrepCommand.java
+++ shell/src/shell/org/jnode/shell/command/GrepCommand.java
@@ -24,7 +24,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
