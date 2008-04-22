7638aebf5c03b96a6d7b4fac3ec3fddfba7a3de5
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-04-22 13:50:36 +0000

    Convert RunCommand
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4002 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/RunCommand.java
+++ shell/src/shell/org/jnode/shell/command/RunCommand.java
@@ -1,3 +1,23 @@
+/*
+ * $Id: JavaCommand.java 3913 2008-03-30 02:36:44Z crawley $
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.jnode.shell.command;
 
 import java.io.File;
@@ -8,27 +28,25 @@ import javax.naming.NameNotFoundException;
 
 import org.jnode.shell.AbstractCommand;
 import org.jnode.shell.CommandLine;
-import org.jnode.shell.CommandShell;
 import org.jnode.shell.Shell;
 import org.jnode.shell.ShellUtils;
-import org.jnode.shell.help.Help;
-import org.jnode.shell.help.Parameter;
-import org.jnode.shell.help.ParsedArguments;
-import org.jnode.shell.help.argument.FileArgument;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.FileArgument;
 
 /**
  * Load and execute a command file.
  *
  * @author Levente S\u00e1ntha
+ * @author crawley@jnode.org
  */
 public class RunCommand extends AbstractCommand {
-
-    static final FileArgument ARG_FILE = new FileArgument("file",
-            "a command file", false);
-
-    public static Help.Info HELP_INFO = new Help.Info("run",
-            "execute a command file", new Parameter[] { new Parameter(
-                    RunCommand.ARG_FILE, Parameter.MANDATORY) });
+    
+    private final FileArgument ARG_FILE = new FileArgument("file", Argument.MANDATORY, "The command file name");
+    
+    public RunCommand() {
+        super("Run a command file");
+        registerArguments(ARG_FILE);
+    }
 
     public static void main(String[] args) throws Exception {
         new RunCommand().execute(args);
@@ -36,27 +54,24 @@ public class RunCommand extends AbstractCommand {
 
     public void execute(CommandLine commandLine, InputStream in,
                         PrintStream out, PrintStream err) throws Exception {
-        ParsedArguments cmdLine = RunCommand.HELP_INFO.parse(commandLine);
-        final File file = RunCommand.ARG_FILE.getFile(cmdLine);
+        final File file = ARG_FILE.getValue();
 
         Shell shell = null;
         try {
             shell = ShellUtils.getShellManager().getCurrentShell();
-        } catch( NameNotFoundException e ) {
+        } catch (NameNotFoundException e) {
             e.printStackTrace();
             exit(2);
         }
 
-        if( shell == null ) {
-            System.err.println( "Shell is null." );
+        if (shell == null) {
+            System.err.println("Shell is null.");
             exit(2);
         }
 
-        if(!(shell instanceof CommandShell)) {
-            System.err.println("Shell wasn't a CommandShell: " + shell.getClass());
-            exit(2);
+        int rc = shell.runCommandFile(file);
+        if (rc != 0) {
+            exit(rc);
         }
-
-        ((CommandShell)shell).executeFile(file);
     }
 }
