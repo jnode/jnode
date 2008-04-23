7085d4df9818fb1d06073739d3bcd10417794897
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-04-23 13:37:21 +0000

    Converted JavaCommand and tidied up the error detection and reporting.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4005 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/JavaCommand.java
+++ shell/src/shell/org/jnode/shell/command/JavaCommand.java
@@ -23,72 +23,102 @@ package org.jnode.shell.command;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.jnode.shell.AbstractCommand;
 import org.jnode.shell.CommandLine;
-import org.jnode.shell.help.Argument;
-import org.jnode.shell.help.Help;
-import org.jnode.shell.help.Parameter;
-import org.jnode.shell.help.ParsedArguments;
-import org.jnode.shell.help.argument.ClassNameArgument;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.ClassNameArgument;
+import org.jnode.shell.syntax.StringArgument;
+
+import java.lang.reflect.Modifier;
 
 /**
+ * This command runs a Java class by calling its 'public static void
+ * main(String[])' method. The class is located by searching the the shell
+ * classpath, searching the plugins, and finally looking in the current
+ * directory.
+ * 
  * @author epr
+ * @author crawley@jnode.org
  */
 public class JavaCommand extends AbstractCommand {
 
-	static final ClassNameArgument ARG_CLASS = new ClassNameArgument("classname", "the class to excute");
-	static final Argument ARG_ARGS = new Argument("arg", "the argument(s) to pass to the class", Argument.MULTI);
-
-	public static Help.Info HELP_INFO = new Help.Info(
-		"java",
-		"Execute the main method of the given java class",
-		new Parameter[]{
-			new Parameter(ARG_CLASS, Parameter.MANDATORY),
-			new Parameter(ARG_ARGS, Parameter.OPTIONAL)
-		}
-	);
-
-	public static void main(String[] args)
-	throws Exception {
+	private final ClassNameArgument ARG_CLASS = 
+	    new ClassNameArgument("className", Argument.MANDATORY, "the class to execute");
+	private final StringArgument ARG_ARGS =
+	    new StringArgument("arg", Argument.OPTIONAL | Argument.MULTIPLE, 
+	            "the argument(s) to pass to the class");
+	
+	public JavaCommand() {
+	    super("Run a Java class via its 'main' method");
+	    registerArguments(ARG_ARGS, ARG_CLASS);
+	}
+	
+	public static void main(String[] args) throws Exception {
 		new JavaCommand().execute(args);
 	}
 
 	/**
-	 * Execute this command
+	 * Execute the command
 	 */
     public void execute(CommandLine cmdLine, InputStream in, PrintStream out, PrintStream err)
 		throws Exception {
 
-    	ParsedArguments args = HELP_INFO.parse(cmdLine);
-
+        // Build our classloader
         final ClassLoader parent_cl = Thread.currentThread().getContextClassLoader();
         JCClassLoader cl = new JCClassLoader(parent_cl, new String[]{"./"});
-
-		Class<?> cls = cl.loadClass(ARG_CLASS.getValue(args));
-
-		Method mainMethod = cls.getMethod("main", new Class[] { String[].class });
-
-		String[] clsArgs = ARG_ARGS.getValues(args);
-		if (clsArgs == null) {
-			clsArgs = new String[0];
-		}
-
-		try {
-			mainMethod.invoke(null, new Object[] { clsArgs });
-		} catch (InvocationTargetException ex) {
-			ex.getTargetException().printStackTrace(err);
-			exit(1);
+        
+        Method mainMethod = null;
+        try {
+            // Find (if necessary load) the class to be executed.
+            Class<?> cls = cl.loadClass(ARG_CLASS.getValue());
+            // Lookup and check the 'main' method.
+            mainMethod = cls.getMethod("main", new Class[]{String[].class});
+            if ((mainMethod.getModifiers() & Modifier.STATIC) == 0) {
+                err.println("The 'main' method for this class is not static");
+                exit(1);
+            }
+            if ((mainMethod.getModifiers() & Modifier.PUBLIC) == 0) {
+                err.println("The 'main' method for this class is not public");
+                exit(1);
+            }
+            String[] mainArgs = ARG_ARGS.isSet() ? ARG_ARGS.getValues() : new String[0];
+            mainMethod.invoke(null, new Object[]{mainArgs});
+        }
+        catch (ClassNotFoundException ex) {
+            err.println("Cannot find the supplied class: " + ARG_CLASS.getValue());
+            exit(1);
+        }
+        catch (NoSuchMethodException ex) {
+            err.println("Cannot the 'void main(String[])' method for class " + ARG_CLASS.getValue());
+            exit(1);
+        }
+        catch (InvocationTargetException ex) {
+            // We unwrap and rethrow any exceptions that were thrown by 'invoke'.  It is
+            // up to the shell to print a stacktrace ... or not.
+			Throwable t = ex.getTargetException();
+			if (t instanceof Exception) {
+			    throw (Exception) t;
+			}
+			else {
+			    throw (Error) t;
+			}
 		}
 	}
 
+    /**
+     * This class loader looks in the supplied list of directories after 
+     * checking the parent class loader.
+     */
     private static class JCClassLoader extends ClassLoader {
-         private String dirs[];
+        private String dirs[];
 
         public JCClassLoader(ClassLoader parent, String[] dir) {
             super(parent);
@@ -101,54 +131,49 @@ public class JavaCommand extends AbstractCommand {
         }
 
         protected URL findResource(String name) {
-            try{
-                System.out.println("Find res: " + name);
+            try {
                 return findResource(name, dirs);
-            }catch(Exception e){
-
+            } catch (MalformedURLException e) {
+                return null;
             }
-            return null;
         }
 
-        private URL findResource(String name, String[] dirs) throws Exception{
-
-            for(int i = 0; i < dirs.length; i++){
+        private URL findResource(String name, String[] dirs) 
+        throws MalformedURLException {
+            for (int i = 0; i < dirs.length; i++) {
                 File d = new File(dirs[i]);
-                if(d.isDirectory()){
-                    System.out.println("Find res: " + name + " in " + d);
+                if (d.isDirectory()) {
                     return findResource(name, d.list());
-                }else{
-                    System.out.println("Find res: " + name + " as " + d);
-                    if(d.getName().equals(name)){
-                        return d.toURL();
-                    }
+                } 
+                else if (d.getName().equals(name)) {
+                    return d.toURI().toURL();
                 }
             }
-
             return null;
         }
 
-         private byte[] loadClassData(String name) throws ClassNotFoundException{
-             String fn = name.replace('.','/');
-             File f = null;
-             for(int i = 0; i < dirs.length; i++){
-                 f = new File(dirs[i] + fn + ".class");
-                 if(f.exists()) break;
-                 f = null;
-             }
-             if(f == null){
-                 throw new ClassNotFoundException(name);
-             }else{
-                 byte[] data = new byte[(int) f.length()];
-                 try{
-                     FileInputStream fis = new FileInputStream(f);
-                     fis.read(data);
-                     return data;
-                 }catch(Exception fnfe){
-                     throw new ClassNotFoundException(name);
-                 }
-             }
-         }
-     }
+        private byte[] loadClassData(String name) throws ClassNotFoundException{
+            String fn = name.replace('.','/');
+            File f = null;
+            for (int i = 0; i < dirs.length; i++) {
+                f = new File(dirs[i] + fn + ".class");
+                if (f.exists()) {
+                    break;
+                }
+                f = null;
+            }
+            if (f == null) {
+                throw new ClassNotFoundException(name);
+            }
+            byte[] data = new byte[(int) f.length()];
+            try {
+                FileInputStream fis = new FileInputStream(f);
+                fis.read(data);
+                return data;
+            } catch (IOException ex) {
+                throw new ClassNotFoundException(name, ex);
+            }
+        }
+    }
 }
 
