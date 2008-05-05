8b929a59b0b4e12e68abb8e76f8d4426df1ce311
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-05-05 04:14:02 +0000

    More improvements to Log4jCommand.  Level arguments are now case insensitive, Logger arguments now have completion, and the command can now change the Level for the root logger.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4056 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ shell/src/shell/org/jnode/shell/syntax/Log4jLoggerArgument.java
@@ -0,0 +1,49 @@
+package org.jnode.shell.syntax;
+
+import java.util.Enumeration;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import org.jnode.driver.console.CompletionInfo;
+import org.jnode.shell.CommandLine.Token;
+
+/**
+ * This Argument class captures log4j logger names, and completes them against 
+ * the set of logger names that are already defined.
+ * 
+ * @author crawley@jnode.org
+ */
+public class Log4jLoggerArgument extends Argument<Logger> {
+
+    public Log4jLoggerArgument(String label, int flags, String description) {
+        super(label, flags, new Logger[0], description);
+    }
+
+    @Override
+    protected String argumentKind() {
+        return "logger";
+    }
+
+    /**
+     * Any token is an acceptable Logger name.
+     */
+    @Override
+    protected void doAccept(Token value) throws CommandSyntaxException {
+        addValue(Logger.getLogger(value.token));
+    }
+
+    /**
+     * Complete against existing logger names.
+     */
+    @Override
+    public void complete(CompletionInfo completion, String partial) {
+        Enumeration<?> en = LogManager.getCurrentLoggers();
+        while (en.hasMoreElements()) {
+            String loggerName = ((Logger) en.nextElement()).getName();
+            if (loggerName.startsWith(partial)) {
+                completion.addCompletion(loggerName);
+            }
+        }
+    }
+}
