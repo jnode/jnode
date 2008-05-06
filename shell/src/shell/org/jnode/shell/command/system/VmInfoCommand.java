b7339773185742bfc1517a663b7f4f41c9f4ffa1
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-05-06 11:33:20 +0000

    Removed the hacky 'main' method from the Vm class and turned it into a normal command class.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4065 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ shell/src/shell/org/jnode/shell/command/system/VmInfoCommand.java
@@ -0,0 +1,74 @@
+/*
+ * $Id: LsIRQ.java 2491 2006-04-23 11:31:22Z epr $
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package org.jnode.shell.command.system;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import org.jnode.shell.AbstractCommand;
+import org.jnode.shell.CommandLine;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.FlagArgument;
+import org.jnode.vm.Vm;
+import org.jnode.vm.scheduler.VmProcessor;
+
+/**
+ * @author Ewout Prangsma (epr@users.sourceforge.net)
+ * @author crawley@jnode.org
+ */
+public class VmInfoCommand extends AbstractCommand {
+    
+    private final FlagArgument FLAG_RESET = 
+        new FlagArgument("reset", Argument.OPTIONAL, "if set, reset the JNode VM counters");
+    
+    public VmInfoCommand() {
+        super("prints JNode VM info");
+        registerArguments(FLAG_RESET);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new VmInfoCommand().execute(args);
+    }
+    
+    @Override
+    public void execute(CommandLine commandLine, InputStream in,
+            PrintStream out, PrintStream err) {
+        final Vm vm = Vm.getVm();
+        if (vm != null && !vm.isBootstrap()) {
+            out.println("JNode VM " + vm.getVersion());
+            vm.dumpStatistics(out);
+            vm.getSharedStatics().dumpStatistics(out);
+            Vm.getHeapManager().dumpStatistics(out);
+            final SecurityManager sm = System.getSecurityManager();
+            out.println("Security manager: " + sm);
+            List<VmProcessor> processors = vm.getProcessors();
+            for (VmProcessor cpu : processors) {
+                out.println("Processor " + processors.indexOf(cpu) + " (" + cpu.getIdString() + ")");
+                cpu.dumpStatistics(out);
+            }
+            if (FLAG_RESET.isSet()) {
+                vm.resetCounters();
+            }
+        }
+    }
+}
