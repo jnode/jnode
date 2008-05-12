a937694beb0cee2bd7f178469ae27b1e68e3a530
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-05-12 15:14:56 +0000

    Argument class for hostname / IP_v4 address
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4091 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ net/src/net/org/jnode/net/syntax/IPv4HostArgument.java
@@ -0,0 +1,39 @@
+package org.jnode.net.syntax;
+
+import java.net.InetAddress;
+import java.util.StringTokenizer;
+
+import org.jnode.driver.console.CompletionInfo;
+import org.jnode.net.ipv4.IPv4Address;
+
+import org.jnode.shell.CommandLine.Token;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.CommandSyntaxException;
+
+/**
+ * This Argument class accepts a host name or IPv4 addresses.  No 
+ * validation or completion is performed,
+ * 
+ * @author crawley@jnode.org
+ */
+public class IPv4HostArgument extends Argument<IPv4Address> {
+
+    public IPv4HostArgument(String label, int flags,
+            String description) {
+        super(label, flags, new IPv4Address[0], description);
+    }
+
+    @Override
+    protected String argumentKind() {
+        return "hostname/IPv4 address";
+    }
+
+    @Override
+    protected IPv4Address doAccept(Token value) throws CommandSyntaxException {
+        try {
+            return new IPv4Address(value.token);
+        } catch (IllegalArgumentException ex) {
+            throw new CommandSyntaxException("Inval;id hostname or IPv4 address");
+        }
+    }
+}
