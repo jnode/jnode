f20594f1c79c1015914ce735481134bcc6110445
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-05-13 14:13:57 +0000

    Converted TftpCommand, refactored a bit and fixed some bugs
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4096 407389f7-7c16-0410-9eee-ad85acb55d7f

--- net/src/net/org/jnode/net/ipv4/tftp/TFTPClient.java
+++ net/src/net/org/jnode/net/ipv4/tftp/TFTPClient.java
@@ -21,16 +21,22 @@
  
 package org.jnode.net.ipv4.tftp;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
  * Console TFTP client.
  * Usage: TFTPClient [hostname [PUT/GET filename]]
+ * 
  * @author markhale
  */
 public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
@@ -46,81 +52,34 @@ public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
     public final static String HELP_CMD = "help";
     public final static String QUIT_CMD = "quit";
 
-    // BufferedReader does not currently function correctly (GNU classpath bug 5558)
-    //private final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    // use DataInputStream instead
-    private final DataInputStream in = new DataInputStream(System.in);
+    private BufferedReader br;
+    private PrintStream out;
     private InetAddress serverAddress;
     private int mode = BINARY_MODE;
     private boolean quit;
     
-
-    public static void main(String[] args) {
-        TFTPClient client = new TFTPClient();
-        if (args.length == 3) { // non-interactive mode
-            if (args[1].equalsIgnoreCase(PUT_CMD)) {
-                if (client.executeCommand(new String[] {CONNECT_CMD, args[0]})) {
-                    client.executeCommand(new String[] {PUT_CMD, args[2]});
-                }
-            }
-            else if (args[1].equalsIgnoreCase(GET_CMD)) {
-                if (client.executeCommand(new String[] {CONNECT_CMD, args[0]})) {
-                    client.executeCommand(new String[] {GET_CMD, args[2]});
-                }
-            } 
-            else {
-                System.out.println("Unrecognised command line.");
-            }
-        } 
-        else { // interactive mode
-            if (args.length == 1) {
-                client.executeCommand(new String[] {CONNECT_CMD, args[0]});
-            }
-            client.run();
-        }
+    
+    public TFTPClient(PrintStream out) {
+        this.out = out;
     }
 
     @SuppressWarnings("deprecation")
-    private void run() {
-        System.out.println("JNode TFTP Client");
+    public void run(InputStream in) throws IOException {
+        // FIXME ... figure out to how to use JNode command argument parsing
+        // (and completion) for our little TFTP interactive command syntax. 
+        this.br = new BufferedReader(new InputStreamReader(in));
+        out.println("JNode TFTP Client");
         do {
-            try {
-                System.out.print("tftp> ");
-                String line = in.readLine();
-                String[] args = parseLine(line);
-                executeCommand(args);
-            } catch (IOException ex) {
+            out.print("tftp> ");
+            String line = br.readLine();
+            if (line == null) {
+                // EOF
+                break;
             }
+            executeCommand(line.trim().split("\\s+"));
         } while (!quit);
     }
     
-    
-    private final static String[] parseLine(String line) {
-        // count arguments
-        int count = 0;
-        int pos = -1;
-        do {
-            count++;
-            pos = line.indexOf(' ', pos+1);
-        } while (pos != -1);
-
-        // parse
-        String[] args = new String[count];
-        count = 0;
-        pos = -1;
-        do {
-            int startPos = pos + 1;
-            pos = line.indexOf(' ', startPos);
-            if (pos != -1) {
-                args[count] = line.substring(startPos, pos);
-            }
-            else {
-                args[count] = line.substring(startPos, line.length());
-            }
-            count++;
-        } while (pos != -1);
-        return args;
-    }
     /**
      * High-level command API.
      * @return true on success.
@@ -134,25 +93,26 @@ public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
         final String cmd = args[0];
         if (cmd.equals(CONNECT_CMD)) { // connect
             if (args.length < 2) {
-                System.out.println("Please specify a host name.");
+                out.println("Please specify a host name.");
             } 
             else {
                 try {
+                    // FIXME ... this is not "connecting"!!
                     serverAddress = InetAddress.getByName(args[1]);
                     serverAddress.getHostName(); // do DNS lookup
                     success = true;
                 } 
                 catch (UnknownHostException ex) {
-                    System.out.println("Unknown host " + args[1] + ".");
+                    out.println("Unknown host " + args[1] + ".");
                 }
             }
         } 
         else if (cmd.equals(GET_CMD)) { // get
             if (serverAddress == null) {
-                System.out.println("Not connected.");
+                out.println("Not connected.");
             } 
             else if (args.length < 2) {
-                System.out.println("Please specify a file name.");
+                out.println("Please specify a file name.");
             } 
             else {
                 String filename = args[1];
@@ -162,7 +122,7 @@ public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
                         open();
                         try {
                             int bytesTransferred = receiveFile(filename, mode, fileOut, serverAddress);
-                            System.out.println(bytesTransferred + " bytes transferred.");
+                            out.println(bytesTransferred + " bytes transferred.");
                         } 
                         finally {
                             close();
@@ -174,16 +134,16 @@ public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
                     success = true;
                 } 
                 catch (IOException ex) {
-                    System.out.println("Error transferring file: " + ex.getMessage());
+                    diagnose(ex, "Error transferring file");
                 }
             }
         } 
         else if (cmd.equals(PUT_CMD)) { // put
             if (serverAddress == null) {
-                System.out.println("Not connected.");
+                out.println("Not connected.");
             } 
             else if (args.length < 2) {
-                System.out.println("Please specify a file name.");
+                out.println("Please specify a file name.");
             } 
             else {
                 String filename = args[1];
@@ -204,7 +164,7 @@ public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
                     success = true;
                 } 
                 catch (IOException ex) {
-                    System.out.println("Error transferring file: " + ex.getMessage());
+                    diagnose(ex, "Error transferring file");
                 }
             }
         } 
@@ -218,58 +178,58 @@ public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
         } 
         else if (cmd.equals(TIMEOUT_CMD)) { // timeout
             if (args.length < 2) {
-                System.out.println("Please specify a timeout value.");
+                out.println("Please specify a timeout value.");
             } 
             else {
                 try {
                     setDefaultTimeout(Integer.parseInt(args[1]));
                     success = true;
-                } catch(NumberFormatException ex) {
-                    System.out.println("Invalid timeout value.");
+                } catch (NumberFormatException ex) {
+                    out.println("Invalid timeout value.");
                 }
             }
         } 
         else if (cmd.equals(RETRIES_CMD)) { // retries
             if (args.length < 2) {
-                System.out.println("Please specify a retries value.");
+                out.println("Please specify a retries value.");
             } 
             else {
                 try {
                     setMaxTimeouts(Integer.parseInt(args[1]));
                     success = true;
-                } catch(NumberFormatException ex) {
-                    System.out.println("Invalid retries value.");
+                } catch (NumberFormatException ex) {
+                    out.println("Invalid retries value.");
                 }
             }
         } 
         else if (cmd.equals(STATUS_CMD)) { // status
             if (serverAddress != null) {
-                System.out.println("Connected to "+serverAddress.getHostName() + ".");
+                out.println("Connected to " + serverAddress.getHostName() + ".");
             }
             else {
-                System.out.println("Not connected.");
+                out.println("Not connected.");
             }
             if (mode == ASCII_MODE) {
-                System.out.print("mode: ASCII");
+                out.print("mode: ASCII");
             }
             else if (mode == BINARY_MODE) {
-                System.out.print("mode: BINARY");
+                out.print("mode: BINARY");
             }
-            System.out.print("   timeout: " + getDefaultTimeout());
-            System.out.println("   retries: " + getMaxTimeouts());
+            out.print("   timeout: " + getDefaultTimeout());
+            out.println("   retries: " + getMaxTimeouts());
             success = true;
         } 
         else if (cmd.equals(HELP_CMD)) { // help
-            System.out.println(ASCII_CMD + " - set mode to ASCII");
-            System.out.println(CONNECT_CMD + " - connect to a tftp server");
-            System.out.println(BINARY_CMD + " - set mode to binary");
-            System.out.println(GET_CMD + " - receive file");
-            System.out.println(HELP_CMD + " - display this help");
-            System.out.println(PUT_CMD + " - send file");
-            System.out.println(QUIT_CMD + " - exit");
-            System.out.println(RETRIES_CMD + " - set retries");
-            System.out.println(STATUS_CMD + " - display current status");
-            System.out.println(TIMEOUT_CMD + " - set timeout");
+            out.println(ASCII_CMD + " - set mode to ASCII");
+            out.println(CONNECT_CMD + " - connect to a tftp server");
+            out.println(BINARY_CMD + " - set mode to binary");
+            out.println(GET_CMD + " - receive file");
+            out.println(HELP_CMD + " - display this help");
+            out.println(PUT_CMD + " - send file");
+            out.println(QUIT_CMD + " - exit");
+            out.println(RETRIES_CMD + " - set retries");
+            out.println(STATUS_CMD + " - display current status");
+            out.println(TIMEOUT_CMD + " - set timeout");
             success = true;
         } 
         else if (cmd.equals(QUIT_CMD)) { // quit
@@ -277,8 +237,14 @@ public class TFTPClient extends org.apache.commons.net.tftp.TFTPClient {
             success = true;
         } 
         else {
-            System.out.println("Unrecognised command.");
+            out.println("Unrecognised command.");
         }
         return success;
     }
+
+    private void diagnose(IOException ex, String message) {
+        String exMessage = ex.getClass().getSimpleName() + " - " + ex.getLocalizedMessage();
+        out.println(message + ": " + exMessage);
+    }
+    
 }
