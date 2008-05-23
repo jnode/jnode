ec9c3faaed0a30fadf3769e9bcbf93007da2537e
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-05-23 15:48:30 +0000

    Converted the format commands
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4118 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/ext2/command/FormatExt2Command.java
+++ fs/src/fs/org/jnode/fs/ext2/command/FormatExt2Command.java
@@ -21,52 +21,55 @@
 
 package org.jnode.fs.ext2.command;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jnode.fs.command.AbstractFormatCommand;
 import org.jnode.fs.ext2.BlockSize;
 import org.jnode.fs.ext2.Ext2FileSystem;
 import org.jnode.fs.ext2.Ext2FileSystemFormatter;
-import org.jnode.fs.fat.FatType;
-import org.jnode.shell.CommandLine;
-import org.jnode.shell.help.Help;
-import org.jnode.shell.help.Parameter;
-import org.jnode.shell.help.ParsedArguments;
-import org.jnode.shell.help.Syntax;
-import org.jnode.shell.help.SyntaxErrorException;
-import org.jnode.shell.help.argument.EnumOptionArgument;
-import org.jnode.shell.help.argument.OptionArgument;
+import org.jnode.fs.jfat.ClusterSize;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.EnumArgument;
+import org.jnode.shell.syntax.MappedArgument;
 
 /**
  * @author gbin
+ * @author crawley@jnode.org
  */
-@SuppressWarnings("unchecked")
 public class FormatExt2Command extends AbstractFormatCommand<Ext2FileSystem> {
-    static final EnumOptionArgument<BlockSize> BS_VAL = new EnumOptionArgument<BlockSize>("blocksize",
-            "block size for ext2 filesystem",
-                    new EnumOptionArgument.EnumOption<BlockSize>("1", "1Kb", BlockSize._1Kb),
-                    new EnumOptionArgument.EnumOption<BlockSize>("2", "2Kb", BlockSize._2Kb),
-                    new EnumOptionArgument.EnumOption<BlockSize>("4", "4Kb", BlockSize._4Kb));
+    
+    private static class BlockSizeArgument extends MappedArgument<BlockSize> {
+        private static final Map<String, BlockSize> MAP = new HashMap<String, BlockSize> ();
+        static {
+            MAP.put("1k", BlockSize._1Kb);
+            MAP.put("2k", BlockSize._2Kb);
+            MAP.put("4k", BlockSize._4Kb);
+        }
+        public BlockSizeArgument() {
+            super("blockSize", Argument.OPTIONAL, new BlockSize[0],
+                    MAP, true, "block size for EXT2 filesystem (default 4k)");
+        }
 
-    static final Parameter PARAM_BS_VAL = new Parameter(BS_VAL,
-            Parameter.OPTIONAL);
+        @Override
+        protected String argumentKind() {
+            return "block size";
+        }
+    }
+    
+    private final BlockSizeArgument ARG_BLOCK_SIZE = new BlockSizeArgument();
 
-    public static Help.Info HELP_INFO = new Help.Info("format",
-            new Syntax[] { new Syntax(
-                    "Format a block device with ext2 filesystem",
-                    new Parameter[] { PARAM_DEVICE, PARAM_BS_VAL }) });
+    public FormatExt2Command() {
+        super("Format a block device with ext2 filesystem");
+        registerArguments(ARG_BLOCK_SIZE);
+    }
 
     public static void main(String[] args) throws Exception {
     	new FormatExt2Command().execute(args);
     }
 
 	@Override
-	protected Ext2FileSystemFormatter getFormatter(ParsedArguments cmdLine) {
-        BlockSize bsize = BS_VAL.getEnum(cmdLine, BlockSize.class);
-        return new Ext2FileSystemFormatter(bsize);
-	}
-
-	@Override
-	protected ParsedArguments parse(CommandLine commandLine)
-			throws SyntaxErrorException {
-		return HELP_INFO.parse(commandLine);
+	protected Ext2FileSystemFormatter getFormatter() {
+        return new Ext2FileSystemFormatter(ARG_BLOCK_SIZE.getValue());
 	}
 }
