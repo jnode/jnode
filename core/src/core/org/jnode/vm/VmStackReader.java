697e235c2261351c4148cda92fdbfee57b98938f
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-05-27 19:18:48 +0000

    - reverted to previous implementation of debugStackTrace(int) - created new method for non-allocation free version, with line numbers
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4137 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/VmStackReader.java
+++ core/src/core/org/jnode/vm/VmStackReader.java
@@ -189,6 +189,35 @@ public abstract class VmStackReader extends VmSystemObject {
      */ 
     @KernelSpace
     public final void debugStackTrace(int max) {
+        Address f = VmMagic.getCurrentFrame();
+        Unsafe.debug("Debug stacktrace: ");
+        boolean first = true;
+        while (isValid(f) && (max > 0)) {
+            if (first) {
+                first = false;
+            } else {
+                Unsafe.debug(", ");
+            }
+            final VmMethod method = getMethod(f);
+            final VmType<?> vmClass = method.getDeclaringClass();
+            Unsafe.debug(vmClass.getName());
+            Unsafe.debug("::");
+            Unsafe.debug(method.getName()); 
+            Unsafe.debug('\n');
+            f = getPrevious(f);
+            max--;
+        }
+        if (isValid(f)) {
+            Unsafe.debug("...");
+        }
+    }
+    
+    /**
+     * Show the current stacktrace using Screen.debug.
+     * TODO that method only exist to have line numbers : find a way to add line numbers to debugStackTrace(max)
+     */ 
+    @KernelSpace
+    public final void debugStackTraceWithLineNumbers(int max) {
     	final VmThread current = VmThread.currentThread();
     	final VmStackFrame[] frames = (VmStackFrame[]) VmThread.getStackTrace(current);
     	if(frames == null)
@@ -209,7 +238,7 @@ public abstract class VmStackReader extends VmSystemObject {
                 Unsafe.debug('\n');
         	}    	
     	}
-    }
+    }    
     
 	/**
 	 * Show the stacktrace of the given thread using Screen.debug.
