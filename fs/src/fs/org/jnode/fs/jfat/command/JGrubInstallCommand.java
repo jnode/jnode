4e16d86b211150a49d37c2955207c532f1a80069
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-06-04 11:00:35 +0000

    Converted JGrubCommand
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4191 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/jfat/command/JGrubInstallCommand.java
+++ fs/src/fs/org/jnode/fs/jfat/command/JGrubInstallCommand.java
@@ -29,40 +29,33 @@ import org.jnode.driver.Device;
 import org.jnode.driver.block.BlockDeviceAPI;
 import org.jnode.shell.AbstractCommand;
 import org.jnode.shell.CommandLine;
-import org.jnode.shell.help.Help;
-import org.jnode.shell.help.Parameter;
-import org.jnode.shell.help.ParsedArguments;
-import org.jnode.shell.help.argument.DeviceArgument;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.DeviceArgument;
 
 /**
  * Grub Installer command for JNode.
- * example of usage : grub hdb0
  *
  * TODO: Add more options for supporting Grub with user specified file
  * TODO: Add more command support for grub installation.
  * @author Tango Devian
  */
 public class JGrubInstallCommand extends AbstractCommand {
-    static final DeviceArgument ARG_DEVICE = new DeviceArgument("device partition", "device where grub stage 2 will be installed", BlockDeviceAPI.class);
+    private final DeviceArgument ARG_DEVICE = new DeviceArgument(
+            "device", Argument.MANDATORY,
+            "device where grub stage 2 will be installed", BlockDeviceAPI.class);
 
-
- static final Help.Info      HELP_INFO  =  new Help.Info("grub",
-         "Install the grub to the specified location.",
-         new Parameter(ARG_DEVICE, Parameter.MANDATORY));
-    /**
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String... args) throws Exception {
+    public JGrubInstallCommand() {
+         super("Install a Grub stage2 loader on a disc device");
+         registerArguments(ARG_DEVICE);
+    }
+         
+    public static void main(String[] args) throws Exception {
         new JGrubInstallCommand().execute(args);
     }
 
-    /**
-     *
-     */
-    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
-        ParsedArguments cmdLine = HELP_INFO.parse(commandLine);
-        Device device = ARG_DEVICE.getDevice(cmdLine);
+    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) 
+    throws GrubException {
+        Device device = ARG_DEVICE.getValue();
 
         JGrub jgrub = new JGrub(out, err, device);
         jgrub.install();
