0373720ca9546931a45216c30ac55e7d6ab75d50
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-06-10 11:15:53 +0000

    Converted JettyCommand + plugin dependency fixes
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4228 407389f7-7c16-0410-9eee-ad85acb55d7f

--- distr/src/apps/org/jnode/apps/jetty/JettyCommand.java
+++ distr/src/apps/org/jnode/apps/jetty/JettyCommand.java
@@ -3,18 +3,17 @@
  */
 package org.jnode.apps.jetty;
 
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.webapp.WebAppContext;
-import org.jnode.shell.AbstractCommand;
-import org.jnode.shell.CommandLine;
-import org.jnode.shell.help.ParsedArguments;
-import org.jnode.shell.help.Help;
-import org.jnode.shell.help.Parameter;
-import org.jnode.shell.help.argument.FileArgument;
-import org.jnode.shell.help.argument.IntegerArgument;
+import java.io.File;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.io.File;
+
+import org.jnode.shell.AbstractCommand;
+import org.jnode.shell.CommandLine;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.FileArgument;
+import org.jnode.shell.syntax.PortNumberArgument;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
 
 /**
  * Starts a Jetty server instance with the specified web application and HTTP port.
@@ -22,26 +21,27 @@ import java.io.File;
  * @author Levente S\u00e1ntha
  */
 public class JettyCommand extends AbstractCommand {
-    static final FileArgument ARG_WEBAPP = new FileArgument("webapp", "web application directory");
-    static final IntegerArgument ARG_PORT = new IntegerArgument("port", "http port");
-
-    private static Parameter PARAM_PORT = new Parameter(ARG_PORT, Parameter.OPTIONAL);
-    public static Help.Info HELP_INFO = new Help.Info("jetty", "start the Jetty web server with a web application on the given port (default 8080)",
-            new Parameter(ARG_WEBAPP, Parameter.MANDATORY),
-            PARAM_PORT);
+    private final FileArgument ARG_WEBAPP = 
+        new FileArgument("webapp", Argument.MANDATORY, "web application directory");
+    
+    private final PortNumberArgument ARG_PORT = 
+        new PortNumberArgument("port", Argument.OPTIONAL, "http port (default 8080)");
+    
+    
+    public JettyCommand() {
+        super("start the Jetty web server running a web application");
+        registerArguments(ARG_PORT, ARG_WEBAPP);
+    }
 
     public static void main(String[] args) throws Exception {
         new JettyCommand().execute(args);
     }
 
-    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
-        ParsedArguments arguments = HELP_INFO.parse(commandLine);
-        File webapp_dir = ARG_WEBAPP.getFile(arguments);
-        int port = 8080;
-        if(PARAM_PORT.isSet(arguments)){
-            port = ARG_PORT.getInteger(arguments);
-        }
-
+    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) 
+        throws Exception {
+        File webapp_dir = ARG_WEBAPP.getValue();
+        int port = ARG_PORT.isSet() ? ARG_PORT.getValue() : 8080;
+        
         Server server = new Server(port);
 
         WebAppContext webapp = new WebAppContext();
