b2e6bc6edeaf31a89fe527d0fe0830553ab99a66
Levente Santha <lsantha@jnode.org>
2008-06-15 17:12:49 +0000

    Codestyle fixes.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4251 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/iso9660/ISO9660DTime.java
+++ fs/src/fs/org/jnode/fs/iso9660/ISO9660DTime.java
@@ -1,9 +1,9 @@
 package org.jnode.fs.iso9660;
 
-import org.jnode.util.LittleEndian;
 import java.util.Calendar;
-import java.util.TimeZone;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import org.jnode.util.LittleEndian;
 
 /**
  * ISO9660 datetime structure.
@@ -12,38 +12,53 @@ import java.util.GregorianCalendar;
  */
 public class ISO9660DTime {
 
-    /** Length of the structure. */
+    /**
+     * Length of the structure.
+     */
     static final int LENGTH = 7;
 
-    /** Number of years since 1900. */
+    /**
+     * Number of years since 1900.
+     */
     private final int/*uint8*/ year;
 
-    /** Month of the year from 1 to 12. */
+    /**
+     * Month of the year from 1 to 12.
+     */
     private final int/*uint8*/ month;
 
-    /** Day of the month from 1 to 31. */
+    /**
+     * Day of the month from 1 to 31.
+     */
     private final int/*uint8*/ day;
 
-    /** Hour of the day from 0 to 23. */
+    /**
+     * Hour of the day from 0 to 23.
+     */
     private final int/*uint8*/ hour;
 
-    /** Minute of the hour from 0 to 59. */
+    /**
+     * Minute of the hour from 0 to 59.
+     */
     private final int/*uint8*/ minute;
 
-    /** Second of the minute from 0 to 59. */
+    /**
+     * Second of the minute from 0 to 59.
+     */
     private final int/*uint8*/ second;
 
-    /** GMT values -48 .. + 52 in 15 minute intervals (e.g. -48 = GMT-12, +48=GMT+12, 0=UTC.) */
+    /**
+     * GMT values -48 .. + 52 in 15 minute intervals (e.g. -48 = GMT-12, +48=GMT+12, 0=UTC.)
+     */
     private final int/*int8*/ gmtOffset;
 
     /**
      * Constructs an ISO9660 datetime structure by reading from the provided byte array.
      *
      * @param buff the byte array from which to read.
-     * @param off offset into the byte array at which to read the struct.
+     * @param off  offset into the byte array at which to read the struct.
      */
-    public ISO9660DTime(byte[] buff, int off)
-    {
+    public ISO9660DTime(byte[] buff, int off) {
         year = LittleEndian.getUInt8(buff, off);
         month = LittleEndian.getUInt8(buff, off + 1);
         day = LittleEndian.getUInt8(buff, off + 2);
@@ -58,8 +73,7 @@ public class ISO9660DTime {
      *
      * @return the date as millis since 1970.
      */
-    public long toJavaMillis()
-    {
+    public long toJavaMillis() {
         // Force use of the Gregorian calendar in UTC and manually offset hours later.
         // Mainly because it isn't convenient to create a TimeZone from a UTC/GMT offset.
         Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
