545c349e6b49956c9acd51c3d0174bc48908848e
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-06-19 20:06:31 +0000

    checkstyle fixes
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4265 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/driver/org/jnode/driver/console/textscreen/TextScreenConsole.java
+++ core/src/driver/org/jnode/driver/console/textscreen/TextScreenConsole.java
@@ -47,337 +47,333 @@ public class TextScreenConsole extends AbstractConsole implements TextConsole {
     /**
      * The screen I'm writing on
      */
-	private final TextScreen screen;
+    private final TextScreen screen;
 
     /**
      * Width of the screen
      */
-	private final int scrWidth;
+    private final int scrWidth;
 
     /**
      * Height of the screen
      */
-	private final int scrHeight;
+    private final int scrHeight;
 
     /**
      * Current tab size
      */
-	private int tabSize = 4;
+    private int tabSize = 4;
 
     /**
      * Current X position
      */
-	private int curX;
+    private int curX;
 
     /**
      * Current Y position
      */
-	private int curY;
-	
-	private InputStream in;
+    private int curY;
 
-	private final ConsolePrintStream out;
+    private InputStream in;
 
-	private final ConsolePrintStream err;
+    private final ConsolePrintStream out;
 
-	private PrintStream savedOut;
+    private final ConsolePrintStream err;
 
-	private PrintStream savedErr;
+    private PrintStream savedOut;
 
-	private boolean cursorVisible = true;
+    private PrintStream savedErr;
 
-	private final boolean claimSystemOutErr;
+    private boolean cursorVisible = true;
+
+    private final boolean claimSystemOutErr;
 
     private VmIsolate myIsolate;
 
     /**
-	 * @param mgr
-	 * @param name
-	 * @param screen
-	 */
-	public TextScreenConsole(ConsoleManager mgr, String name,
-			TextScreen screen, int options) {
-		super(mgr, name);
-		this.screen = screen;
-		this.scrWidth = screen.getWidth();
-		this.scrHeight = screen.getHeight();
-		this.savedOut = this.out = new ConsolePrintStream(new ConsoleOutputStream(
-				this, 0x07));
-		this.savedErr = this.err = new ConsolePrintStream(new ConsoleOutputStream(
-				this, 0x04));
-        //todo crawley, will the console not set System.out/err any more? 
-//		this.claimSystemOutErr = ((options & ConsoleManager.CreateOptions.NO_SYSTEM_OUT_ERR) == 0);
-		this.claimSystemOutErr = false;
+     * @param mgr
+     * @param name
+     * @param screen
+     */
+    public TextScreenConsole(ConsoleManager mgr, String name, TextScreen screen, int options) {
+        super(mgr, name);
+        this.screen = screen;
+        this.scrWidth = screen.getWidth();
+        this.scrHeight = screen.getHeight();
+        this.savedOut = this.out = new ConsolePrintStream(new ConsoleOutputStream(this, 0x07));
+        this.savedErr = this.err = new ConsolePrintStream(new ConsoleOutputStream(this, 0x04));
+        // todo crawley, will the console not set System.out/err any more?
+        // this.claimSystemOutErr = ((options &
+        // ConsoleManager.CreateOptions.NO_SYSTEM_OUT_ERR) == 0);
+        this.claimSystemOutErr = false;
         this.myIsolate = VmIsolate.currentIsolate();
     }
 
-	/**
-	 * Clear the console
-	 * 
-	 * @see org.jnode.driver.console.TextConsole#clear()
-	 */
-	public void clear() {
-		final int size = screen.getWidth() * screen.getHeight();
-		screen.set(0, ' ', size, 0x07);
-		syncScreen(0, size);
-	}
+    /**
+     * Clear the console
+     * 
+     * @see org.jnode.driver.console.TextConsole#clear()
+     */
+    public void clear() {
+        final int size = screen.getWidth() * screen.getHeight();
+        screen.set(0, ' ', size, 0x07);
+        syncScreen(0, size);
+    }
 
-	/**
-	 * Clear a given row
-	 */
-	public void clearRow(int row) {
-		final int size = screen.getWidth();
-		final int offset = screen.getOffset(0, row); 
-		screen.set(offset, ' ', size, 0x07);
-		syncScreen(offset, size);
-	}
+    /**
+     * Clear a given row
+     */
+    public void clearRow(int row) {
+        final int size = screen.getWidth();
+        final int offset = screen.getOffset(0, row);
+        screen.set(offset, ' ', size, 0x07);
+        syncScreen(offset, size);
+    }
 
-	/**
-	 * Append characters to the current line.
-	 *
-	 * @param v
+    /**
+     * Append characters to the current line.
+     * 
+     * @param v
      * @param offset
      * @param lenght
-	 * @param color
-	 */
+     * @param color
+     */
     public void putChar(char v[], int offset, int lenght, int color) {
         int mark = 0;
-        for(int i = 0; i < lenght; i++){
+        for (int i = 0; i < lenght; i++) {
             char c = v[i + offset];
-            if(c == '\n' || c =='\b' || c == '\t' ||
-                    curX + i == scrWidth - 1 || i == lenght - 1){
+            if (c == '\n' || c == '\b' || c == '\t' || curX + i == scrWidth - 1 || i == lenght - 1) {
                 final int ln = i - mark;
-                if(ln > 0){
+                if (ln > 0) {
                     screen.set(screen.getOffset(curX, curY), v, offset + mark, ln, color);
                     curX += ln;
                     if (curX >= scrWidth) {
                         curY++;
                         curX = curX - scrWidth;
-			        }
+                    }
                 }
                 mark = i + 1;
                 doPutChar(c, color);
             }
         }
-		screen.ensureVisible(curY, isFocused()); // synchronize if focused
+        screen.ensureVisible(curY, isFocused()); // synchronize if focused
     }
 
-	/**
-	 * Append a character to the current line.
-	 * 
-	 * @param v
-	 * @param color
-	 */
-	public void putChar(char v, int color) {
-		doPutChar(v, color);
-		screen.ensureVisible(curY, isFocused()); // synchronize if focused
-	}
-	
-	private void doPutChar(char v, int color) {
-		if (v == '\n') {
-			// Goto next line
-			// Clear till eol
+    /**
+     * Append a character to the current line.
+     * 
+     * @param v
+     * @param color
+     */
+    public void putChar(char v, int color) {
+        doPutChar(v, color);
+        screen.ensureVisible(curY, isFocused()); // synchronize if focused
+    }
+
+    private void doPutChar(char v, int color) {
+        if (v == '\n') {
+            // Goto next line
+            // Clear till eol
             screen.set(screen.getOffset(curX, curY), ' ', scrWidth - curX, color);
-			curX = 0;
-			curY++;
-		} else if (v == '\b') {
-			if (curX > 0) {
-				curX--;
-			} else if (curY > 0) {
-				curX = scrWidth - 1;
-				curY--;
-			}
-			setChar(curX, curY, ' ', color);
-		} else if (v == '\t') {
-			putChar(' ', color);
-			while ((curX % tabSize) != 0) {
-				putChar(' ', color);
-			}
-		} else {
-			setChar(curX, curY, v, color);
-			curX++;
-			if (curX >= scrWidth) {
-				curY++;
-				curX = 0;
-			}
-		}
-		while (curY >= scrHeight) {
-			screen.copyContent(scrWidth, 0, (scrHeight - 1) * scrWidth);
-			curY--;
-			clearRow(curY);
-		}
-	}
+            curX = 0;
+            curY++;
+        } else if (v == '\b') {
+            if (curX > 0) {
+                curX--;
+            } else if (curY > 0) {
+                curX = scrWidth - 1;
+                curY--;
+            }
+            setChar(curX, curY, ' ', color);
+        } else if (v == '\t') {
+            putChar(' ', color);
+            while ((curX % tabSize) != 0) {
+                putChar(' ', color);
+            }
+        } else {
+            setChar(curX, curY, v, color);
+            curX++;
+            if (curX >= scrWidth) {
+                curY++;
+                curX = 0;
+            }
+        }
+        while (curY >= scrHeight) {
+            screen.copyContent(scrWidth, 0, (scrHeight - 1) * scrWidth);
+            curY--;
+            clearRow(curY);
+        }
+    }
 
-	/**
-	 * @return Returns the tabSize.
-	 */
-	public int getTabSize() {
-		return tabSize;
-	}
+    /**
+     * @return Returns the tabSize.
+     */
+    public int getTabSize() {
+        return tabSize;
+    }
 
-	/**
+    /**
      * @param tabSize The tabSize to set.
-	 */
-	public void setTabSize(int tabSize) {
-		this.tabSize = tabSize;
-	}
+     */
+    public void setTabSize(int tabSize) {
+        this.tabSize = tabSize;
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getColor(int, int)
-	 */
-	public int getColor(int x, int y) {
-		return screen.getColor(screen.getOffset(x, y));
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getColor(int, int)
+     */
+    public int getColor(int x, int y) {
+        return screen.getColor(screen.getOffset(x, y));
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getChar(int, int)
-	 */
-	public char getChar(int x, int y) {
-		return screen.getChar(screen.getOffset(x, y));
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getChar(int, int)
+     */
+    public char getChar(int x, int y) {
+        return screen.getChar(screen.getOffset(x, y));
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getCursorX()
-	 */
-	public int getCursorX() {
-		return curX;
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getCursorX()
+     */
+    public int getCursorX() {
+        return curX;
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getCursorY()
-	 */
-	public int getCursorY() {
-		return curY;
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getCursorY()
+     */
+    public int getCursorY() {
+        return curY;
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getHeight()
-	 */
-	public int getHeight() {
-		return screen.getHeight();
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getHeight()
+     */
+    public int getHeight() {
+        return screen.getHeight();
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getWidth()
-	 */
-	public int getWidth() {
-		return screen.getWidth();
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getWidth()
+     */
+    public int getWidth() {
+        return screen.getWidth();
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#setChar(int, int, char, int)
-	 */
-	public void setChar(int x, int y, char ch, int color) {
-		int offset = screen.getOffset(x, y);
-		screen.set(offset, ch, 1, color);
-		syncScreen(offset, 1);
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#setChar(int, int, char, int)
+     */
+    public void setChar(int x, int y, char ch, int color) {
+        int offset = screen.getOffset(x, y);
+        screen.set(offset, ch, 1, color);
+        syncScreen(offset, 1);
+    }
 
-	public void setChar(int x, int y, char[] ch, int color) {
-		int offset = screen.getOffset(x, y); 
-		screen.set(offset, ch, 0, ch.length, color);
-		syncScreen(offset, ch.length);
-	}
+    public void setChar(int x, int y, char[] ch, int color) {
+        int offset = screen.getOffset(x, y);
+        screen.set(offset, ch, 0, ch.length, color);
+        syncScreen(offset, ch.length);
+    }
 
-	public void setChar(int x, int y, char[] ch, int cOfset, int cLength,
-			int color) {
-		int offset = screen.getOffset(x, y);
-		screen.set(offset, ch, cOfset, cLength, color);
-		syncScreen(offset, cLength);
-	}
+    public void setChar(int x, int y, char[] ch, int cOfset, int cLength, int color) {
+        int offset = screen.getOffset(x, y);
+        screen.set(offset, ch, cOfset, cLength, color);
+        syncScreen(offset, cLength);
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#setCursor(int, int)
-	 */
-	public void setCursor(int x, int y) {
-		this.curX = x;
-		this.curY = y;
-        int offset = screen.setCursor(x,y);
-		syncScreen(offset, 1);
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#setCursor(int, int)
+     */
+    public void setCursor(int x, int y) {
+        this.curX = x;
+        this.curY = y;
+        int offset = screen.setCursor(x, y);
+        syncScreen(offset, 1);
+    }
 
     private void syncScreen(int offset, int size) {
-		if (isFocused()) {
-			screen.sync(offset, size);
-		}
-	}
+        if (isFocused()) {
+            screen.sync(offset, size);
+        }
+    }
 
-	/**
-	 * Ensure that the given row is visible.
-	 * 
-	 * @param row
-	 */
-	public void ensureVisible(int row) {
-		screen.ensureVisible(row, isFocused()); // synchronize if focused 
-	}
+    /**
+     * Ensure that the given row is visible.
+     * 
+     * @param row
+     */
+    public void ensureVisible(int row) {
+        screen.ensureVisible(row, isFocused()); // synchronize if focused
+    }
 
-	public InputCompleter getCompleter() {
-		if (in instanceof KeyboardInputStream) {
-			return ((KeyboardInputStream) in).getCompleter();
+    public InputCompleter getCompleter() {
+        if (in instanceof KeyboardInputStream) {
+            return ((KeyboardInputStream) in).getCompleter();
         } else {
-			return null;
-		}
-	}
+            return null;
+        }
+    }
 
-	public void setCompleter(InputCompleter completer) {
-		if (in instanceof KeyboardInputStream) {
-			((KeyboardInputStream) in).setCompleter(completer);
-		}
-	}
+    public void setCompleter(InputCompleter completer) {
+        if (in instanceof KeyboardInputStream) {
+            ((KeyboardInputStream) in).setCompleter(completer);
+        }
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getIn()
-	 */
-	public InputStream getIn() {
-		return in;
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getIn()
+     */
+    public InputStream getIn() {
+        return in;
+    }
 
-	void setIn(InputStream in) {
-		this.in = in;
-	}
+    void setIn(InputStream in) {
+        this.in = in;
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getErr()
-	 */
-	public PrintStream getErr() {
-		return err;
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getErr()
+     */
+    public PrintStream getErr() {
+        return err;
+    }
 
-	/**
-	 * @see org.jnode.driver.console.TextConsole#getOut()
-	 */
-	public PrintStream getOut() {
-		return out;
-	}
+    /**
+     * @see org.jnode.driver.console.TextConsole#getOut()
+     */
+    public PrintStream getOut() {
+        return out;
+    }
 
-	/**
-	 * Is the cursor visible.
-	 */
-	public boolean isCursorVisible() {
-		return cursorVisible;
-	}
+    /**
+     * Is the cursor visible.
+     */
+    public boolean isCursorVisible() {
+        return cursorVisible;
+    }
 
-	/**
-	 * Make the cursor visible or not visible.
-	 * 
-	 * @param visible
-	 */
-	public void setCursorVisible(boolean visible) {
-		this.cursorVisible = visible;
+    /**
+     * Make the cursor visible or not visible.
+     * 
+     * @param visible
+     */
+    public void setCursorVisible(boolean visible) {
+        this.cursorVisible = visible;
         int offset = screen.setCursorVisible(visible);
-		syncScreen(offset, 1);
-	}
+        syncScreen(offset, 1);
+    }
 
-	/**
-	 * @see org.jnode.system.event.FocusListener#focusGained(org.jnode.system.event.FocusEvent)
-	 */
-	public void focusGained(FocusEvent event) {
-		super.focusGained(event);
-		syncScreen(0, screen.getWidth() * screen.getHeight());
-		if (in instanceof FocusListener) {
-			((FocusListener) in).focusGained(event);
-		}
-		if (claimSystemOutErr && VmSystem.hasVmIOContext()) {
+    /**
+     * @see org.jnode.system.event.FocusListener#focusGained(org.jnode.system.event.FocusEvent)
+     */
+    public void focusGained(FocusEvent event) {
+        super.focusGained(event);
+        syncScreen(0, screen.getWidth() * screen.getHeight());
+        if (in instanceof FocusListener) {
+            ((FocusListener) in).focusGained(event);
+        }
+        if (claimSystemOutErr && VmSystem.hasVmIOContext()) {
             myIsolate.invokeAndWait(new Runnable() {
                 public void run() {
                     AccessController.doPrivileged(new PrivilegedAction<Object>() {
@@ -389,31 +385,31 @@ public class TextScreenConsole extends AbstractConsole implements TextConsole {
                     });
                 }
             });
-		}
-	}
+        }
+    }
 
-	/**
-	 * @see org.jnode.system.event.FocusListener#focusLost(org.jnode.system.event.FocusEvent)
-	 */
-	public void focusLost(FocusEvent event) {
-		if (in instanceof FocusListener) {
-			((FocusListener) in).focusLost(event);
-		}
-		if (claimSystemOutErr && VmSystem.hasVmIOContext()) {
+    /**
+     * @see org.jnode.system.event.FocusListener#focusLost(org.jnode.system.event.FocusEvent)
+     */
+    public void focusLost(FocusEvent event) {
+        if (in instanceof FocusListener) {
+            ((FocusListener) in).focusLost(event);
+        }
+        if (claimSystemOutErr && VmSystem.hasVmIOContext()) {
             myIsolate.invokeAndWait(new Runnable() {
                 public void run() {
                     savedOut = System.out;
                     savedErr = System.err;
                 }
             });
-		}
-		super.focusLost(event);
-	}
+        }
+        super.focusLost(event);
+    }
 
-	/**
-	 * @return Returns the screen.
-	 */
-	protected final TextScreen getScreen() {
-		return this.screen;
-	}
+    /**
+     * @return Returns the screen.
+     */
+    protected final TextScreen getScreen() {
+        return this.screen;
+    }
 }
