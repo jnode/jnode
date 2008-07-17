4cbedad5ae868846fe8f47af16dbc101d3012199
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-07-17 11:06:09 +0000

    Added native toolkit.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4310 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/awt/NativeToolkit.java
@@ -0,0 +1,13 @@
+package java.awt;
+
+/**
+ * @see java.awt.Toolkit
+ */
+class NativeToolkit {
+    /**
+     * @see java.awt.Toolkit#initIDs()
+     */
+    private static void initIDs() {
+        //empty
+    }
+}
