5920b6e895b859c1b81e120391638aefb83cfb14
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-07-20 14:59:22 +0000

    Fixed VESA driver discovery and initialization.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4329 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ gui/src/driver/org/jnode/driver/video/vesa/VESADeviceToDriverMapper.java
@@ -0,0 +1,47 @@
+package org.jnode.driver.video.vesa;
+
+import org.jnode.driver.bus.pci.PCIDevice;
+import org.jnode.driver.bus.pci.PCIDeviceConfig;
+import org.jnode.driver.DeviceToDriverMapper;
+import org.jnode.driver.Driver;
+import org.jnode.driver.Device;
+import org.jnode.vm.x86.UnsafeX86;
+import org.vmmagic.unboxed.Address;
+
+/**
+ * Custom device Mapper for the VESA driver.
+ * 
+ * @author Levente S\u00e1ntha
+ */
+public class VESADeviceToDriverMapper implements DeviceToDriverMapper {
+
+    public Driver findDriver(Device device) {
+        //PCI device needed
+        if (!(device instanceof PCIDevice))
+            return null;
+
+        //checking display controller device class
+        final PCIDevice pciDev = (PCIDevice) device;
+        final PCIDeviceConfig cfg = pciDev.getConfig();
+        if ((cfg.getBaseClass() & 0xFFFFFF) != 0x03)
+            return null;
+
+        //checking the VESA mode set up by GRUB
+        Address vbeControlInfo = UnsafeX86.getVbeControlInfos();
+        VbeInfoBlock vbeInfoBlock = new VbeInfoBlock(vbeControlInfo);
+        if (vbeInfoBlock.isEmpty())
+            return null;
+
+        Address vbeModeInfo = UnsafeX86.getVbeModeInfos();
+        ModeInfoBlock modeInfoBlock = new ModeInfoBlock(vbeModeInfo);
+        if (modeInfoBlock.isEmpty())
+            return null;
+
+        //OK
+        return new VESADriver();
+    }
+
+    public int getMatchLevel() {
+        return DeviceToDriverMapper.MATCH_DEVICE_PREDEFINED;
+    }
+}
