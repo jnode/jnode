677bdae138367c65f7e057054070d48a1ab10d31
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-07-23 12:33:38 +0000

    Moving 'configure' to the Builder project
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4335 407389f7-7c16-0410-9eee-ad85acb55d7f

--- configure.sh
+++ configure.sh
@@ -4,9 +4,9 @@ DIR=`dirname $CMD`
 
 # Belt and braces classpath to get the most recent version
 # of the 'configure' classes that we can find.
-CP=$DIR/distr/build/classes:\
+CP=$DIR/builder/build/classes:\
 $DIR/all/build/descriptors/jnode-configure.jar:\
-$DIR/distr/lib/jnode-configure-dist.jar:\
-$DIR/distr/lib/nanoxml-2.2.3.jar
+$DIR/builder/lib/jnode-configure-dist.jar:\
+$DIR/builder/lib/nanoxml-2.2.3.jar
 
 java -cp $CP org.jnode.configure.Configure "$@"
