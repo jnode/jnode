84b0ffc7fea6056f3b82bd31078a8e0a8ea2a08a
Levente Santha <lsantha@jnode.org>
2008-07-24 19:34:46 +0000

    Added method setPreferredListener() to PointerAPI and its implementations.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4345 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/driver/org/jnode/driver/input/PointerAPI.java
+++ core/src/driver/org/jnode/driver/input/PointerAPI.java
@@ -22,26 +22,46 @@
 package org.jnode.driver.input;
 
 import org.jnode.driver.DeviceAPI;
+import org.jnode.driver.DriverPermission;
 
 /**
  * Device API implemented by Pointer devices.
  *
  * @author qades
  * @author Ewout Prangsma (epr@users.sourceforge.net)
+ * @author Levente S\u00e1ntha
  */
 public interface PointerAPI extends DeviceAPI {
+    //todo KeboardAPI.SET_PREFERRED_LISTENER_PERMISSION has a similar role
+    //remove duplication
+
+    /**
+     * Permission
+     */
+    public static final DriverPermission SET_PREFERRED_LISTENER_PERMISSION =
+        new DriverPermission("setPreferredListener");
 
     /**
      * Add a pointer listener
      *
-     * @param l
+     * @param listener the pointer listener to be added
      */
-    public void addPointerListener(PointerListener l);
+    public void addPointerListener(PointerListener listener);
 
     /**
      * Remove a pointer listener
      *
-     * @param l
+     * @param listener the pointer listener to be removed
      */
-    public void removePointerListener(PointerListener l);
+    public void removePointerListener(PointerListener listener);
+
+    /**
+     * Claim to be the preferred listener.
+     * The given listener must have been added by addPointerListener.
+     * If there is a security manager, this method will call
+     * <code>checkPermission(new DriverPermission("setPreferredListener"))</code>.
+     *
+     * @param listener the prefered pointer listener
+     */
+    public abstract void setPreferredListener(PointerListener listener);
 }
