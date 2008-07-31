3a7d3233d18fb6f420b35f893d66f77fca4fd2fe
Levente Santha <lsantha@jnode.org>
2008-07-31 20:26:24 +0000

    Fixed syncronization issue, added author tag.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4374 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/driver/org/jnode/driver/textscreen/fb/FbScreenPainter.java
+++ gui/src/driver/org/jnode/driver/textscreen/fb/FbScreenPainter.java
@@ -4,14 +4,15 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-
 import org.jnode.driver.video.Surface;
 
+/**
+ * @author Levente S\u00e1ntha
+ */
 class FbScreenPainter {
     private static final int margin = 5;
     private static final int w = 7;
     private static final int h = 18;
-    
     private final Surface surface;
     private final int sw;
     private final int sh;
@@ -20,25 +21,22 @@ class FbScreenPainter {
     private final Font font;
     private final FbTextScreen screen;
     private final Thread painterThread;
-
     private final Background background;
-        
-    private boolean update;
+    private boolean update = true;
 
     public FbScreenPainter(FbTextScreen screen, Surface g) {
         this.screen = screen;
         this.surface = g;
         sh = h * screen.getHeight() + 2 * margin;
         sw = w * screen.getWidth() + 2 * margin;
-        
+
         //background = new DefaultBackground(Color.BLACK);
         background = new GradientBackground(sw, sh);
-        
+
         buffer = new BufferedImage(sw, sh, BufferedImage.TYPE_INT_ARGB);
         graphics = buffer.getGraphics();
         font = new Font(
-                "-FontForge-Bitstream Vera Sans Mono-Book-R-Normal-SansMono--12-120-75-75-P-69-ISO10646",
-                Font.PLAIN, 12);
+            "-FontForge-Bitstream Vera Sans Mono-Book-R-Normal-SansMono--12-120-75-75-P-69-ISO10646", Font.PLAIN, 12);
 
         painterThread = new Thread(new Runnable() {
             public void run() {
@@ -46,7 +44,7 @@ class FbScreenPainter {
                     try {
                         paintComponent();
                         synchronized (FbScreenPainter.this) {
-                            if (!update) {
+                            if (update) {
                                 update = false;
                                 FbScreenPainter.this.wait();
                             }
@@ -59,30 +57,29 @@ class FbScreenPainter {
         }, "FbScreenPainter");
         painterThread.start();
     }
-        
+
     protected void paintComponent() {
         background.paint(graphics);
-        
+
         graphics.setColor(Color.WHITE);
         graphics.setFont(font);
-        
+
         final char[] textBuffer = screen.getBuffer();
         final int length = screen.getWidth();
         int offset = 0;
         final int x = margin;
         int y = h;
-        
+
         for (int i = 0; i < screen.getHeight(); i++) {
             graphics.drawChars(textBuffer, offset, length, x, y);
-            
+
             offset += length;
             y += h;
         }
         surface.drawCompatibleRaster(buffer.getRaster(), 0, 0, 0, 0, sw, sh, Color.BLACK);
     }
-    
+
     public synchronized void repaint() {
-        //Unsafe.debug("repaint");
         if (!update) {
             update = true;
             notifyAll();
