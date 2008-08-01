372dd1632adff10048d3c8aecee5947d08468b1f
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-08-01 23:20:01 +0000

    speed improvement by pre-computing colors
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4382 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/driver/org/jnode/driver/textscreen/fb/GradientBackground.java
+++ gui/src/driver/org/jnode/driver/textscreen/fb/GradientBackground.java
@@ -5,26 +5,37 @@ import java.awt.Graphics;
 
 
 public class GradientBackground implements Background {
-    private final int width;
+    private final int maxX;
     private final int height;
+    private final Color[] colors;
     
     GradientBackground(int width, int height) {
-        this.width = width;
-        this.height = height;
+        this.maxX = width - 1;
+        this.height = height;        
+        this.colors = new Color[height];
+        
+        final float step = (float) 1.0 / (float) height;        
+        float blue = 0f;
+        Color color = null;
+        Color prevColor = null;
+        for (int y = 0; y < height; y++) {
+            if ((y > 0) && (prevColor.getBlue() == (int) (blue * 255))) {
+                color = prevColor;
+            } else {
+                color = new Color(0f, 0f, blue);
+            }
+            colors[y] = color;
+            
+            blue += step;
+            prevColor = color;
+        }
     }
     
     
     public void paint(Graphics ig) {
-        float step = (float) 1.0 / (float) height;
-        
-        float blue = 0f;
-        Color color = new Color(0f, 0f, blue);
         for (int y = 0; y < height; y++) {
-            ig.setColor(color);        
-            ig.drawLine(0, y, width - 1, y);
-            
-            blue += step;
-            color = new Color(0f, 0f, blue);
+            ig.setColor(colors[y]);        
+            ig.drawLine(0, y, maxX, y);
         }
     }
 
