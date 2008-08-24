1036c59dde7fa5623ff614f9e65c63aa5ed5ea53
Levente Santha <lsantha@jnode.org>
2008-08-24 11:38:12 +0000

    Code style fixes.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4492 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/ext2/command/FormatExt2Command.java
+++ fs/src/fs/org/jnode/fs/ext2/command/FormatExt2Command.java
@@ -23,12 +23,11 @@ package org.jnode.fs.ext2.command;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.jnode.fs.command.AbstractFormatCommand;
 import org.jnode.fs.ext2.BlockSize;
 import org.jnode.fs.ext2.Ext2FileSystem;
-import org.jnode.fs.ext2.Ext2FileSystemFormatter; 
-import org.jnode.shell.syntax.Argument; 
+import org.jnode.fs.ext2.Ext2FileSystemFormatter;
+import org.jnode.shell.syntax.Argument;
 import org.jnode.shell.syntax.MappedArgument;
 
 /**
@@ -39,14 +38,16 @@ public class FormatExt2Command extends AbstractFormatCommand<Ext2FileSystem> {
 
     private static class BlockSizeArgument extends MappedArgument<BlockSize> {
         private static final Map<String, BlockSize> MAP = new HashMap<String, BlockSize>();
+
         static {
             MAP.put("1k", BlockSize._1Kb);
             MAP.put("2k", BlockSize._2Kb);
             MAP.put("4k", BlockSize._4Kb);
         }
+
         public BlockSizeArgument() {
             super("blockSize", Argument.OPTIONAL, new BlockSize[0],
-                    MAP, true, "block size for EXT2 filesystem (default 4k)");
+                MAP, true, "block size for EXT2 filesystem (default 4k)");
         }
 
         @Override
@@ -68,10 +69,10 @@ public class FormatExt2Command extends AbstractFormatCommand<Ext2FileSystem> {
 
     @Override
     protected Ext2FileSystemFormatter getFormatter() {
-    	if(ARG_BLOCK_SIZE.isSet()){
-    		return new Ext2FileSystemFormatter(ARG_BLOCK_SIZE.getValue());
-    	} else {
-    		return new Ext2FileSystemFormatter(BlockSize._4Kb);
-    	}
+        if (ARG_BLOCK_SIZE.isSet()) {
+            return new Ext2FileSystemFormatter(ARG_BLOCK_SIZE.getValue());
+        } else {
+            return new Ext2FileSystemFormatter(BlockSize._4Kb);
+        }
     }
 }
