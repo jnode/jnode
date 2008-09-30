d1406545ddd8b1f5294c394a558bfab4e909ad8b
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-09-30 11:41:19 +0000

    Style fixes
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4589 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/IsolateCommand.java
+++ shell/src/shell/org/jnode/shell/command/IsolateCommand.java
@@ -14,20 +14,16 @@ import java.io.PrintStream;
  * @author Levente S\u00e1ntha
  */
 public class IsolateCommand extends AbstractCommand {
-    /**
-     * The default implementation of the 'execute(...)' entry point complains that
-     * you haven't implemented it.  A command class must override either this method
-     * or (ideally) the 'execute()' entry point method.
-     */
     @Override
-    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
+    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) 
+        throws Exception {
         out.println("      Id " + " Creator " + "State    " + "Main class");
         VmIsolate root = VmIsolate.getRoot();
-        if(root != null)
+        if (root != null) {
             out.println(format(String.valueOf(root.getId()), 8, false) + " " +
                 format("0", 8, false) + " "  +
                 format(String.valueOf(root.getState()), 8, true));
-
+        }
         for (VmIsolate iso : VmIsolate.getVmIsolates()) {
             out.println(format(String.valueOf(iso.getId()), 8, false) + " " +
                 format(String.valueOf(iso.getCreator().getId()), 8, false) + " " +
@@ -37,15 +33,15 @@ public class IsolateCommand extends AbstractCommand {
     }
 
     public String format(String value, int width, boolean left) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < width - value.length(); i++) {
+        StringBuilder sb = new StringBuilder(width);
+        for (int i = 0; i < width - value.length(); i++) {
             sb.append(' ');
         }
-        if(left)
+        if (left) {
             sb.insert(0, value);
-        else
+        } else {
             sb.append(value);
-
+        }
         return sb.toString();
     }
 }
