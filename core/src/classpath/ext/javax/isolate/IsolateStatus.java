189906dc5772397db75b4278528ce020dd46aa7f
Levente Santha <lsantha@jnode.org>
2008-10-12 20:00:40 +0000

    Prorgess with isolates: clarification of exit reason, initial system exit support, child isolates.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4626 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/classpath/ext/javax/isolate/IsolateStatus.java
+++ core/src/classpath/ext/javax/isolate/IsolateStatus.java
@@ -18,49 +18,75 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
+
 package javax.isolate;
 
+import org.jnode.vm.annotation.SharedStatics;
+
 /**
+ * Represents isolate status, capturing such information as isolate state, exit code, and exit reason.
+ * State transitions in the isolate life cycle generate LinkMessages containing instances of this class
+ * which are sent over status links (but can be then forwarded over any link). If the isolate state is EXITED,
+ * the corresponding exit code and exit reason are contained in IsolateStatus.
+ *
  * @author Ewout Prangsma (epr@users.sourceforge.net)
+ * @author Levente S\u00e1ntha
  */
 public class IsolateStatus {
 
     /**
      * Defines the state of an Isolate.
-     * 
+     *
      * @author Ewout Prangsma (epr@users.sourceforge.net)
      */
+    @SharedStatics
     public static enum State {
-        /** Isolate is starting */
+        /**
+         * Isolate is starting
+         */
         STARTING,
 
-        /** Isolate is started */
+        /**
+         * Isolate is started
+         */
         STARTED,
 
-        /** Isolate is exiting */
+        /**
+         * Isolate is exiting
+         */
         EXITING,
 
-        /** Isolate has exited */
+        /**
+         * Isolate has exited
+         */
         EXITED,
 
-        /** State is not known */
-        UNKNOWN;
+        /**
+         * State is not known
+         */
+        UNKNOWN
     }
 
     /**
      * Provides the reason for the termination of an isolate.
-     * 
+     *
      * @author Ewout Prangsma (epr@users.sourceforge.net)
      */
+    @SharedStatics
     public static enum ExitReason {
-        /** The last non-deamon thread exited */
+        /**
+         * The last non-deamon thread exited
+         */
         IMPLICIT_EXIT,
 
-        /** Another isolate invoked Isolate.exit */
+        /**
+         * Another isolate invoked Isolate.exit
+         */
         OTHER_EXIT,
 
-        /** Another isolate invoed Isolated.halt */
+        /**
+         * Another isolate invoed Isolated.halt
+         */
         OTHER_HALT,
 
         /**
@@ -74,55 +100,91 @@ public class IsolateStatus {
          */
         SELF_HALT,
 
-        /** The last non-deamon thread exited due to an uncaught exception */
-        UNCAUGHT_EXCEPTION;
+        /**
+         * The last non-deamon thread exited due to an uncaught exception
+         */
+        UNCAUGHT_EXCEPTION
     }
-    
+
     private final ExitReason exitReason;
     private final State state;
     private final int exitCode;
-    
+
     /**
      * Initialize this instance.
+     *
      * @param state
      * @param exitReason
      * @param exitCode
      */
     protected IsolateStatus(State state, ExitReason exitReason, int exitCode) {
+        if (state == null)
+            throw new IllegalArgumentException();
+
+        if (state.equals(State.EXITED) && exitReason == null)
+            throw new IllegalArgumentException();
+
         this.state = state;
         this.exitReason = exitReason;
         this.exitCode = exitCode;
     }
-    
-    /**
-     * Are this and the given object equal.
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    public boolean equals(Object other) {
-        return super.equals(other);
-    }
-    
+
     /**
      * Gets the exit reason.
+     *
      * @return
      */
     public ExitReason getExitReason() {
         return exitReason;
     }
-    
+
     /**
      * Gets the exit code.
+     *
      * @return
      */
     public int getExitCode() {
         return exitCode;
     }
-    
+
     /**
      * Gets the state of the isolate.
+     *
      * @return
      */
     public State getState() {
         return state;
+    }
+
+    /**
+     * @param o
+     * @return
+     */
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IsolateStatus)) return false;
+
+        IsolateStatus that = (IsolateStatus) o;
+
+        if (!state.equals(that.state)) return false;
+
+        if (state.equals(State.EXITED)) {
+            if (exitCode != that.exitCode) return false;
+            if (exitReason != that.exitReason) return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return
+     */
+    public int hashCode() {
+        int result = state.hashCode();
+        if (state.equals(State.EXITED)) {
+            result = 31 * result + exitReason.hashCode();
+            result = 31 * result + exitCode;
+        }
+        return result;
     }
 }
