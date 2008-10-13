744561ec6864510429243da5f046163c24d11369
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-10-13 12:20:49 +0000

    Removed some dead constant declarations.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4627 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/driver/org/jnode/driver/console/textscreen/KeyboardReader.java
+++ core/src/driver/org/jnode/driver/console/textscreen/KeyboardReader.java
@@ -1,3 +1,23 @@
+/*
+ * $Id: CommandLine.java 4611 2008-10-07 12:55:32Z crawley $
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.jnode.driver.console.textscreen;
 
 import java.awt.event.KeyEvent;
@@ -42,14 +62,6 @@ import org.jnode.util.ConsoleStream;
  */
 public class KeyboardReader extends Reader 
     implements FocusListener, ConsoleStream {
-    
-    /**
-     * This KR code causes the next history line to be selected.
-     */
-    public static final byte KR_HISTORY_DOWN = 16;
-
-    public static final byte CTRL_L = 12;
-    public static final byte CTRL_D = 4;
 
     private boolean eof;
 
