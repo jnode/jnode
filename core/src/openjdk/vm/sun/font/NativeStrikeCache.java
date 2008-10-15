ab7d3094a321848386776a5bc2bfcb8cc28abfa7
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-10-15 18:49:53 +0000

    added dummy implementation of getGlyphCacheDescription (but args1[0] is correct for 32 bits systems)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4634 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/openjdk/vm/sun/font/NativeStrikeCache.java
+++ core/src/openjdk/vm/sun/font/NativeStrikeCache.java
@@ -9,7 +9,19 @@ class NativeStrikeCache {
      * @see sun.font.StrikeCache#getGlyphCacheDescription(long[])
      */
     private static void getGlyphCacheDescription(long[] arg1) {
-        //todo implement it
+        arg1[0] = 4; // nativeAddressSize
+
+		//TODO find right values
+        arg1[1] = 0; // glyphInfoSize
+        arg1[2] = 0; // xAdvanceOffset
+        arg1[3] = 0; // yAdvanceOffset
+        arg1[4] = 10; // widthOffset
+        arg1[5] = 10; // heightOffset
+        arg1[6] = 0; // rowBytesOffset
+        arg1[7] = 0; // topLeftXOffset
+        arg1[8] = 0; // topLeftYOffset
+        arg1[9] = 0; // pixelDataOffset
+        arg1[10] = 0; // invisibleGlyphPtr
     }
     /**
      * @see sun.font.StrikeCache#freeIntPointer(int)
