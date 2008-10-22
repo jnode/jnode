22a7b82b22b985cdca979dbc52c443fe17646515
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-10-22 09:50:43 +0000

    PageCommand now supports line forward (RET), line backward (k), page forward (SP), page backward (b), goto start (>), goto end (<) and help (h).
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4640 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/PageCommand.java
+++ shell/src/shell/org/jnode/shell/command/PageCommand.java
@@ -20,12 +20,14 @@
  */
 package org.jnode.shell.command;
 
+import org.jnode.driver.console.ConsoleManager;
 import org.jnode.driver.console.TextConsole;
+import org.jnode.driver.console.textscreen.TextScreenConsoleManager;
 import org.jnode.driver.input.KeyboardEvent;
 import org.jnode.driver.input.KeyboardListener;
+import org.jnode.naming.InitialNaming;
 import org.jnode.shell.AbstractCommand;
-import org.jnode.shell.Shell;
-import org.jnode.shell.ShellUtils;
+import org.jnode.shell.ShellManager;
 import org.jnode.shell.syntax.Argument;
 import org.jnode.shell.syntax.FileArgument;
 
@@ -36,6 +38,8 @@ import java.io.PipedReader;
 import java.io.PipedWriter;
 import java.io.PrintWriter;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.NameNotFoundException;
 
@@ -54,16 +58,38 @@ public class PageCommand extends AbstractCommand implements KeyboardListener {
     private final FileArgument ARG_FILE = 
         new FileArgument("file", Argument.OPTIONAL, "the file to be paged");
 
-    private PrintWriter out;
     private PrintWriter err;
     private TextConsole console;
-    private int screenHeight;
-    private int screenWidth;
+    
+    private int pageHeight;
+    private int pageWidth;
+    
+    // This is the line number of the top (data source) line displayed on the
+    // screen page.
+    private int topLineNo;
+    private int topSublineNo;
+    
+    
+    // This is the line number of the bottom (data source) line displayed on the
+    // screen page.
+    private int bottomLineNo;
+    private int bottomSublineNo;
+    
+    // This is where we build the screen image prior to sending it to the console
+    private char[] buffer;
     
     // This pipe passes characters from the system thread that calls 
     // our 'keyPressed' event method to the thread that runs the Page command.
     private PipedReader pr;
     private PipedWriter pw;
+
+    private TextScreenConsoleManager manager;
+
+    private int tabSize = 8;
+
+    private int pageSize;
+
+    private LineStore lineStore;
     
     public PageCommand() {
         super("output a file to the console one 'page' at a time");
@@ -79,7 +105,6 @@ public class PageCommand extends AbstractCommand implements KeyboardListener {
 
     @Override
     public void execute() throws Exception {
-        out = getOutput().getPrintWriter();
         err = getError().getPrintWriter();
         Reader r = null;
         boolean opened = false;
@@ -96,7 +121,8 @@ public class PageCommand extends AbstractCommand implements KeyboardListener {
                 r = getInput().getReader();
             }
             setup();
-            page(r);
+            lineStore = new LineStore(r);
+            pager();
             
         } catch (IOException ex) {
             err.println(ex.getMessage());
@@ -114,25 +140,26 @@ public class PageCommand extends AbstractCommand implements KeyboardListener {
     }
 
     private void tearDown() throws IOException {
-        console.removeKeyboardListener(this);
+        manager.unregisterConsole(console);
         pw.close();
         pr.close();
     }
 
     private void setup() throws NameNotFoundException, IOException {
-        Shell shell = ShellUtils.getCurrentShell();
-        try {
-            console = (TextConsole) shell.getConsole();
-        } catch (ClassCastException ex) {
-            err.println("Page is only supported with a TextConsole");
-            exit(1);
-        }
-//        this.screenHeight = console.getHeight();
-//        this.screenWidth = console.getWidth();
-        this.screenHeight = 24;
-        this.screenWidth = 80;
-//        err.println("screen height - " + screenHeight);
-//        err.println("screen width - " + screenWidth);
+        ShellManager sm = InitialNaming.lookup(ShellManager.NAME);
+        manager = (TextScreenConsoleManager) sm.getCurrentShell().getConsole().getManager();
+        console = manager.createConsole(
+            "page",
+            (ConsoleManager.CreateOptions.TEXT |
+                ConsoleManager.CreateOptions.STACKED |
+                ConsoleManager.CreateOptions.NO_LINE_EDITTING |
+                ConsoleManager.CreateOptions.NO_SYSTEM_OUT_ERR));
+        manager.focus(console);
+        
+        pageHeight = console.getDeviceHeight() - 1;
+        pageWidth = console.getDeviceWidth();
+        pageSize = pageHeight * pageWidth;
+        buffer = new char[pageSize];
         
         pw = new PipedWriter();
         pr = new PipedReader();
@@ -142,72 +169,280 @@ public class PageCommand extends AbstractCommand implements KeyboardListener {
     }
 
     /**
-     * Do the paging, reading from our private console input
+     * Do the paging, reading commands from our private console input
      * pipe to figure out what to do next.
      * 
      * @param r the source of data to be paged.
      * @throws IOException
      */
-    private void page(Reader r) throws IOException {
-        BufferedReader br = new BufferedReader(r);
-        
+    private void pager() throws IOException {
         // Output first page.
-        boolean eof = outputPage(br);
+        console.clear();
+        bottomLineNo = -1;
+        boolean exit = false;
+        nextPage();
         
-        // Until we get
-        while (!eof) {
+        // Process commands until we reach the EOF on the data source or
+        // the command pipe.
+        while (!exit) {
+            prompt();
             int ch = pr.read();
+            erasePrompt();
             switch (ch) {
                 case -1:
-                    eof = true;
+                    exit = true;
                     break;
                 case ' ':
-                    eof = outputPage(br);
+                case 'f':
+                    if (lineStore.isLastLineNo(bottomLineNo)) {
+                        exit = true;
+                    } else {
+                        nextPage();
+                    }
+                    break;
+                case 'b':
+                    prevPage();
+                    break;
+                case 'k':
+                case 'y':
+                    prevLine();
                     break;
                 case '\n':
-                    eof = outputLine(br);
+                    if (lineStore.isLastLineNo(bottomLineNo)) {
+                        exit = true;
+                    } else {
+                        nextLine();
+                    }
                     break;
+                case '<':
+                    gotoPage(0);
+                    break;
+                case '>':
+                    gotoLastPage();
+                    break;
+                case 'q':
+                    exit = true;
+                    break;
+                case 'h':
+                    help();
                 default:
                     // ignore
             }
         }
     }
 
-    /**
-     * Output the next line.
-     * @param br the source of the data being paged.
-     * @return <code>true</code> if we reach the EOF.
-     * @throws IOException
-     */
-    private boolean outputLine(BufferedReader br) throws IOException {
-        String line = br.readLine();
-        if (line == null) {
-            return true;
-        } else {
-            out.println(line);
-            return false;
+    private void help() throws IOException {
+        String[] help = new String[] {
+            "Move forwards 1 page: ' ', 'f'",
+            "Move backwards 1 page: 'b'",
+            "Move forwards 1 line: ENTER",
+            "Move backwards 1 line: 'k', 'y'",
+            "Go to start of data: '<'",
+            "Go to end of data: '>'",
+        };
+        StringBuffer sb = new StringBuffer(pageSize);
+        for (int i = 0; i < pageHeight; i++) {
+            if (i < help.length) {
+                sb.append(help[i]);
+            } else {
+                sb.append(' ');
+            }
+            while (sb.length() % pageWidth != 0) {
+                sb.append(' ');
+            }
         }
+        console.setChar(0, 0, sb.toString().toCharArray(), 0, pageSize, 0x7);
+        prompt("Hit any key to continue");
+        int ch = pr.read();
+        prompt();
+        output();
+    }
+
+    private void prompt() {
+        prompt(lineStore.isLastLineNo(bottomLineNo) ? "(END)" :
+                (topLineNo + ", " + bottomLineNo + ": "));
+    }
+
+    private void prompt(String text) {
+        console.clearRow(this.pageHeight);
+        console.setChar(0, this.pageHeight, text.toCharArray(), 0x07);
+        console.setCursor(0, this.pageHeight);
+    }
+
+    private void erasePrompt() {
+        console.clearRow(this.pageHeight);
     }
 
     /**
-     * Output the next page. 
+     * Page forward by one page.  The implementation strategy is to 'prepare'
+     * lines one at a time until we have got (at least) a whole screen full.
+     * The preparation process involves copying characters to 'buffer', padding
+     * lines with spaces, and doing BS and TAB interpretation.  Then we output 
+     * the prepared characters to the screen.  The 'output' process deals with
+     * scrolling if we prepared less than a full screen, or truncation if we
+     * prepared more than a full screen.
      * 
-     * @param br the source of the data being paged.
-     * @return <code>true</code> if we reach the EOF.
      * @throws IOException
      */
-    private boolean outputPage(BufferedReader br) throws IOException {
-        int lineCount = 0;
-        String line = null;
-        while (lineCount < screenHeight && (line = br.readLine()) != null) {
-            lineCount += (line.length() / screenWidth) + 1;
-            out.println(line);
+    private void nextPage() throws IOException {
+        prepare(bottomLineNo + 1);
+        output();
+    }
+    
+    private void prevPage() throws IOException {
+        if (topLineNo > 0) {
+            prepareReverse(topLineNo - 1);
+            output();
         }
-        return line == null;
+    }
+    
+    private void gotoPage(int firstLineNo) throws IOException {
+        prepare(firstLineNo);
+        output();
+    }
+    
+    private void gotoLastPage() throws IOException {
+        prepareReverse(lineStore.getLastLineNo());
+        output();
+    }
+    
+    /**
+     * Page forward by one line.  We use the 'prepare' and 'output'
+     * strategy as described in {@link nextPage}.
+     * 
+     * @throws IOException
+     */
+    private void nextLine() throws IOException {
+        prepare(topLineNo + 1);
+        output();
     }
 
     /**
-     * Capture keyboard input and stuff characters input the
+     * Page backwards by one line.  We use the 'prepare' and 'output'
+     * strategy as described in {@link nextPage}.
+     * 
+     * @throws IOException
+     */
+    private void prevLine() throws IOException {
+        prepare((topLineNo > 0) ? topLineNo - 1 : topLineNo);
+        output();
+    }
+
+    private void output() {
+        console.setChar(0, 0, buffer, 0, pageSize, 0x7);
+    }
+
+    /**
+     * Prepare lines for output by painting them to our private buffer
+     * starting at the supplied bufferLineOffset
+     * 
+     * @param startLineNo
+     */
+    private void prepare(int startLineNo) {
+        int lineNo = startLineNo;
+        RenderCursor cursor = new RenderCursor(buffer);
+        String line = null;
+        while (cursor.getEndOffset() < pageSize) {
+            line = lineStore.getLine(lineNo);
+            if (line == null) {
+                if (startLineNo > 0) {
+                    prepare(startLineNo - 1);
+                    return;
+                }
+                while (cursor.getEndOffset() < pageSize) {
+                    cursor.putChar(' ');
+                }
+                break;
+            }
+            lineNo++;
+            render(line, 0, cursor);
+        }
+        if (lineNo > startLineNo) {
+            topLineNo = startLineNo;
+            bottomLineNo = lineNo - 1;
+        }
+    }
+
+    /**
+     * Prepare lines for output by painting them to our private buffer
+     * starting at the supplied bufferLineOffset
+     * 
+     * @param bufferLineOffset the buffer offset in screen lines to start
+     *        painting
+     */
+    private void prepareReverse(int endLineNo) {
+        int lineNo = endLineNo;
+        RenderCursor cursor = new RenderCursor(buffer);
+        RenderCursor measureCursor = new RenderCursor(null);
+        String line = null;
+        int lineOffset = pageSize;
+        while (lineOffset > 0 && lineNo >= 0) {
+            line = lineStore.getLine(lineNo--);
+            measureCursor.setStartOffset(0);
+            render(line, 0, measureCursor);
+            lineOffset -= measureCursor.getEndOffset();
+            cursor.setStartOffset(lineOffset);
+            render(line, 0, cursor);
+        }
+        if (lineOffset <= 0) {
+            topLineNo = Math.max(0, lineNo);
+            bottomLineNo = endLineNo;
+        } else {
+            prepare(0);
+        }
+    }
+    
+    /**
+     * Prepare lines for output by painting them to our private buffer
+     * starting at the supplied bufferLineOffset
+     * 
+     * @param line the line to be prepared
+     * @param bufferLineOffset the buffer offset in screen lines to start
+     *        painting
+     */
+    private void render(String line, int firstSubline, RenderCursor cursor) {
+        int pos = 0;
+        int len = line.length();
+        for (int i = 0; i < len; i++) {
+            // FIXME - support different renderings, including ones where
+            // control characters are rendered as visible characters?
+            char ch = line.charAt(i);
+            switch (ch) {
+                case '\n':
+                    throw new AssertionError("no newlines expected");
+                case '\r':
+                    // ignore bare CRs.
+                    break;
+                case '\b':
+                    if (pos > 0) {
+                        pos--;
+                    }
+                    break;
+                case '\t':
+                    cursor.putChar(' ');
+                    pos++;
+                    while (pos % pageWidth % tabSize != 0) {
+                        cursor.putChar(' ');
+                        pos++;
+                    }
+                    break;
+                default:
+                    if (ch > ' ' && ch <= '\377' && ch != '\177') {
+                        cursor.putChar(ch);
+                    } else {
+                        cursor.putChar('?');
+                    }
+                    pos++;
+            }
+        }
+        while (pos % pageWidth != 0) {
+            cursor.putChar(' ');
+            pos++;
+        }
+    }
+
+    /**
+     * Capture keyboard input events and write all character data to the
      * private pipe.  The command thread will read them from
      * the other end as required.
      */
@@ -230,5 +465,92 @@ public class PageCommand extends AbstractCommand implements KeyboardListener {
     @Override
     public void keyReleased(KeyboardEvent event) {
         // ignore
+    }
+    
+    /**
+     * This class provides an in-memory buffer of lines from the data source 
+     * being paged.  In the future, this could be enhanced to cut down on memory
+     * usage.  When paging a seekable Reader, it could use seek/tell to record file
+     * offsets rather than actual file lines.  When paging a non-seekable Reader
+     * (e.g. a pipe) it could store the lines read from the reader in a temporary file.
+     * 
+     * @author crawley@jnode.org
+     */
+    private final class LineStore {
+        private final BufferedReader reader;
+        private boolean reachedEOF;
+        private List<String> lines = new ArrayList<String>(100);
+        
+        private LineStore(Reader reader) {
+            this.reader = new BufferedReader(reader);
+        }
+        
+        private String getLine(int lineNo) {
+            if (lineNo < lines.size()) {
+                return lines.get(lineNo);
+            } else if (lineNo > lines.size()) {
+                throw new AssertionError(
+                        "Reading at wrong place (" + lineNo + " > " + lines.size() + ")");
+            } else if (reachedEOF) {
+                return null;
+            } else {
+                try {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        reachedEOF = true;
+                    } else {
+                        lines.add(line);
+                    }
+                    return line;
+                } catch (IOException ex) {
+                    ex.printStackTrace(err);
+                    reachedEOF = true;
+                    return null;
+                }
+            }
+        }
+        
+        private int getLastLineNo() throws IOException {
+            while (!reachedEOF) {
+                String line = reader.readLine();
+                if (line == null) {
+                    reachedEOF = true;
+                } else {
+                    lines.add(line);
+                }
+            }
+            return lines.size() - 1;
+        }
+        
+        private boolean isLastLineNo(int lineNo) {
+            return reachedEOF && lineNo == lines.size() - 1;
+        }
+    }
+    
+    private final class RenderCursor {
+        int pos;
+        int startOffset;
+        char[] buffer;
+        
+        private RenderCursor(char[] buffer) {
+            this.buffer = buffer;
+            this.startOffset = 0;
+        }
+        
+        public void setStartOffset(int startOffset) {
+            this.startOffset = startOffset;
+        }
+
+        private void putChar(char ch) {
+            if (buffer == null || pos + startOffset < 0 || pos + startOffset > buffer.length) {
+                pos++;
+            } else {
+                buffer[pos++ + startOffset] = ch;
+            }
+        }
+        
+        private int getEndOffset() {
+            return pos + startOffset;
+        }
     }
 }
