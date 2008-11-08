fb1b26082c54943b04e7b100e1a70ea9da949dc9
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-11-08 07:34:40 +0000

    Implemented the 'sleep' command
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4676 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ shell/src/shell/org/jnode/shell/command/SleepCommand.java
@@ -0,0 +1,59 @@
+/*
+ * $Id: SetCommand.java 4198 2008-06-05 10:54:46Z crawley $
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package org.jnode.shell.command;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.jnode.shell.AbstractCommand;
+import org.jnode.shell.CommandLine;
+import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.IntegerArgument;
+
+
+/**
+ * Sleep for a given number of seconds.
+ * 
+ * @author crawley@jnode.org
+ */
+
+public class SleepCommand extends AbstractCommand {
+    private IntegerArgument SECONDS_ARG = 
+        new IntegerArgument("seconds", Argument.MANDATORY, "the number of seconds to sleep");
+    
+    public SleepCommand() {
+        super("Sleep for a given number of seconds");
+        registerArguments(SECONDS_ARG);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SleepCommand().execute(args);
+    }
+
+    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) 
+        throws Exception {
+        Integer seconds = SECONDS_ARG.getValue();
+        if (seconds > 0) {
+            Thread.sleep(seconds * 1000);
+        }
+    }
+}
