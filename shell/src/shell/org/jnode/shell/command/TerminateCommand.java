2bdcd662cc31b6c843bb12b3be97ac5e53c5ef40
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-11-10 10:12:31 +0000

    If an Argument is MANDATORY, there is no need to check that it has a value. Also, if a command detects an error, it should call 'exit(N)' where N != 0.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4695 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/TerminateCommand.java
+++ shell/src/shell/org/jnode/shell/command/TerminateCommand.java
@@ -1,13 +1,13 @@
 package org.jnode.shell.command;
 
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.PrintWriter;
+
 import javax.isolate.Isolate;
 import javax.isolate.IsolateStatus;
 import javax.isolate.Link;
 import javax.isolate.LinkMessage;
+
 import org.jnode.shell.AbstractCommand;
-import org.jnode.shell.CommandLine;
 import org.jnode.shell.syntax.Argument;
 import org.jnode.shell.syntax.IntegerArgument;
 import org.jnode.vm.isolate.VmIsolate;
@@ -19,30 +19,22 @@ import org.jnode.vm.isolate.VmIsolate;
  * @author Levente S\u00e1ntha
  */
 public class TerminateCommand extends AbstractCommand {
-    private final IntegerArgument ARG_ID = new IntegerArgument("id", Argument.MANDATORY | Argument.SINGLE,
-        "the identifier of the isolate to terminate");
+    private final IntegerArgument ARG_ID = new IntegerArgument(
+            "id", Argument.MANDATORY | Argument.SINGLE,
+            "the identifier of the isolate to terminate");
 
     public TerminateCommand() {
         super("Terminate an isolate");
         registerArguments(ARG_ID);
     }
 
-    public void execute(CommandLine commandLine, InputStream in, PrintStream out, PrintStream err) throws Exception {
-
-        if (!ARG_ID.isSet()) {
-            err.println("Missing isolate identifier.");
-            return;
-        }
-
+    public void execute() throws Exception {
+        PrintWriter err = getError().getPrintWriter();
+        PrintWriter out = getOutput().getPrintWriter();
         Integer id = ARG_ID.getValue();
-        if (id == null) {
-            err.println("Missing isolate identifier.");
-            return;
-        }
-
         if (id.equals(VmIsolate.getRoot().getId())) {
             err.println("The root isolate cannot be terminated with this command.");
-            return;
+            exit(1);
         }
 
         VmIsolate[] iso_arr = VmIsolate.getVmIsolates();
@@ -56,7 +48,7 @@ public class TerminateCommand extends AbstractCommand {
 
         if (iso == null) {
             err.println("Isolate not found: " + id);
-            return;
+            exit(1);
         }
 
         Link link = iso.newStatusLink();
