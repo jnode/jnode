404341e987470db19348b4afda6b61210b93cb62
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2008-12-30 20:56:15 +0000

    OpenJDK integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4811 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/lang/NativeShutdown.java
@@ -0,0 +1,23 @@
+package java.lang;
+
+/**
+ * @see java.lang.Shutdown
+ *
+ * @author Levente S\u00e1ntha
+ */
+class NativeShutdown {
+    /**
+     * @see java.lang.Shutdown#halt0(int)
+     */
+    private static void halt0(int arg1) {
+        //todo implement it
+        throw new UnsupportedOperationException();
+    }
+    /**
+     * @see java.lang.Shutdown#runAllFinalizers()
+     */
+    private static void runAllFinalizers() {
+        //todo implement it
+        throw new UnsupportedOperationException();
+    }
+}
