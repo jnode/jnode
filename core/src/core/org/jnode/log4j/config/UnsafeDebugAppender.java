8354643a54fee1092a9f57652d4c21e1459348fe
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-01-03 04:28:46 +0000

    Added a log4j appender that writes log messages via Unsafe.debug calls. (It is not yet wired into anything ...)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4817 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/core/org/jnode/log4j/config/UnsafeDebugAppender.java
@@ -0,0 +1,103 @@
+/*
+ * $Id: VirtualConsoleAppender.java 4562 2008-09-18 13:23:59Z crawley $
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jnode.log4j.config;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.naming.NameNotFoundException;
+
+import org.apache.log4j.Layout;
+import org.apache.log4j.WriterAppender;
+import org.jnode.driver.console.ConsoleManager;
+import org.jnode.driver.console.TextConsole;
+import org.jnode.naming.InitialNaming;
+import org.jnode.vm.Unsafe;
+
+/**
+ * Custom Log4j appender class that outputs to the kernel debug 'stream'.
+ * 
+ * @author crawley@jnode.org
+ */
+public class UnsafeDebugAppender extends WriterAppender {
+    
+    private static ConsoleManager mgr;
+    
+    private Writer writer;
+
+    /**
+     * Create an appender for a named JNode console.
+     * @param layout the appender's initial log message layout.
+     * @param name the target console name.
+     * @param toErr if <code>true</code> output to the console's 'err' stream,
+     *       otherwise to it's 'out' stream.
+     */
+    public UnsafeDebugAppender(Layout layout) {
+        super();
+        this.layout = layout;
+        this.writer = new UnsafeDebugWriter();
+        super.setWriter(this.writer);
+    }
+    
+    @Override
+    protected void closeWriter() {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            // ignore
+        }
+    }
+
+    @Override
+    public void activateOptions() {
+        super.setWriter(writer);
+    }
+
+    @Override
+    public synchronized void setWriter(Writer writer) {
+        if (writer != this.writer) {
+            throw new IllegalArgumentException("cannot change the writer");
+        }
+        super.setWriter(writer);
+    }
+    
+    /**
+     * A Writer wrapper for the kernel debug 'stream'.
+     */
+    private class UnsafeDebugWriter extends Writer {
+
+        public UnsafeDebugWriter() {
+        }
+
+        @Override
+        public void close() throws IOException {
+        }
+
+        @Override
+        public void flush() throws IOException {
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            Unsafe.debug(new String(cbuf, off, len));
+        }
+    }
+}
