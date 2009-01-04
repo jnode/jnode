8a7c83ab8b97cda486f9ac7889009c5dcb2b88e7
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-01-04 01:48:18 +0000

    Snapshot of recent progress.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4823 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/bjorne/BjorneTokenizer.java
+++ shell/src/shell/org/jnode/shell/bjorne/BjorneTokenizer.java
@@ -1,3 +1,23 @@
+/*
+ * $Id: Command.java 3772 2008-02-10 15:02:53Z lsantha $
+ *
+ * JNode.org
+ * Copyright (C) 2007-2008 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.jnode.shell.bjorne;
 
 import static org.jnode.shell.bjorne.BjorneToken.RULE_1_CONTEXT;
@@ -148,7 +168,7 @@ public class BjorneTokenizer {
         while (ch == '\t' || ch == ' ') {
             ch = nextCh();
         }
-        int start = pos;
+        int start = pos - 1;
         switch (ch) {
             case EOS:
                 return makeToken(TOK_END_OF_STREAM, start);
