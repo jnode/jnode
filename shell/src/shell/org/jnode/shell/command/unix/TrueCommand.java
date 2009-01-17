fcb9f46cbff139ffd8f5e08df4a724f34bc1b8eb
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-01-17 07:56:28 +0000

    Implemented posix 'false' and 'true'
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4876 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ shell/src/shell/org/jnode/shell/command/unix/TrueCommand.java
@@ -0,0 +1,46 @@
+/*
+ * $Id: EchoCommand.java 4694 2008-11-09 14:20:31Z crawley $
+ *
+ * JNode.org
+ * Copyright (C) 2003-2006 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package org.jnode.shell.command.unix;
+
+import org.jnode.shell.AbstractCommand;
+
+/**
+ * Simply exit with rc of 0; 
+ * 
+ * @author crawley@jnode.org
+ */
+public class TrueCommand extends AbstractCommand {
+
+    public TrueCommand() {
+        super("Exit with zero return code");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new TrueCommand().execute(args);
+    }
+
+    /**
+     * Execute the command
+     */
+    public void execute() throws Exception {
+    }
+}
