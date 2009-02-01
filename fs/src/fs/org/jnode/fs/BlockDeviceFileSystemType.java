c2f9233bccf16ef16c2132e33493ee307aef407d
Levente Santha <lsantha@jnode.org>
2009-02-01 20:29:28 +0000

    Fixed headers.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4961 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/BlockDeviceFileSystemType.java
+++ fs/src/fs/org/jnode/fs/BlockDeviceFileSystemType.java
@@ -1,24 +1,24 @@
 /*
- * $Id: FileSystemType.java 3668 2007-12-30 22:20:07Z fduminy $
+ * $Id: header.txt 2224 2006-01-01 12:49:03Z epr $
  *
  * JNode.org
- * Copyright (C) 2003-2006 JNode.org
+ * Copyright (C) 2003-2009 JNode.org
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but
+ * This library is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; If not, write to the Free Software Foundation, Inc.,
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
+ 
 package org.jnode.fs;
 
 import org.jnode.driver.block.FSBlockDeviceAPI;
--- fs/src/fs/org/jnode/fs/command/DFCommand.java
+++ fs/src/fs/org/jnode/fs/command/DFCommand.java
@@ -1,8 +1,8 @@
 /*
- * $Id: Help.java 3962 2008-04-17 14:33:02Z crawley $
+ * $Id: header.txt 2224 2006-01-01 12:49:03Z epr $
  *
  * JNode.org
- * Copyright (C) 2003-2006 JNode.org
+ * Copyright (C) 2003-2009 JNode.org
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -18,6 +18,7 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+ 
 package org.jnode.fs.command;
 
 import java.io.IOException;
