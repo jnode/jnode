6f54529c71ebb1ea29fef4d6d3c3a1801761b2e6
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-02-01 20:29:28 +0000

    Fixed headers.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@4961 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/partitions/service/PartitionTableService.java
+++ fs/src/fs/org/jnode/partitions/service/PartitionTableService.java
@@ -2,7 +2,7 @@
  * $Id$
  *
  * JNode.org
- * Copyright (C) 2006 JNode.org
+ * Copyright (C) 2003-2009 JNode.org
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -14,10 +14,11 @@
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
  * License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License 
- * along with this library; if not, write to the Free Software Foundation, 
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+ 
 package org.jnode.partitions.service;
 
 import java.util.Collection;
