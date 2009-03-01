276ee75e197c64741b2d3d155dc8fa508c4b0bd3
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-03-01 08:55:23 +0000

    OpenJDK integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5077 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/lang/ref/NativeFinalizer.java
@@ -0,0 +1,13 @@
+package java.lang.ref;
+
+/**
+ * @see java.lang.ref.Finalizer
+ */
+class NativeFinalizer {
+    /**
+     * @see java.lang.ref.Finalizer#invokeFinalizeMethod(java.lang.Object)
+     */
+    private static void invokeFinalizeMethod(Object arg1) {
+        //todo implement it
+    }
+}
