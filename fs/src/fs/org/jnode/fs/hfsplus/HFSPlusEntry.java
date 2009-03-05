2984f52b8fedd52bc7c4dd84b918e3f043468550
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-03-05 16:31:25 +0000

    Don't use AbstractFS classes anymore.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5088 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/hfsplus/HFSPlusEntry.java
+++ fs/src/fs/org/jnode/fs/hfsplus/HFSPlusEntry.java
@@ -17,24 +17,52 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
+
 package org.jnode.fs.hfsplus;
 
+import java.io.IOException;
+
+import org.jnode.fs.FSAccessRights;
+import org.jnode.fs.FSDirectory;
+import org.jnode.fs.FSEntry;
+import org.jnode.fs.FSFile;
+import org.jnode.fs.FileSystem;
 import org.jnode.fs.hfsplus.tree.LeafRecord;
 import org.jnode.fs.spi.AbstractFSEntry;
-import org.jnode.fs.spi.FSEntryTable;
+import org.jnode.fs.spi.UnixFSAccessRights;
 
-public class HFSPlusEntry extends AbstractFSEntry {
+public class HFSPlusEntry implements FSEntry {
 
-    private LeafRecord record;
+    protected HfsPlusFileSystem fs;
+    protected HFSPlusDirectory parent;
+    protected String name;
+    protected LeafRecord record;
+    private int type;
 
-    public HFSPlusEntry(final HfsPlusFileSystem fs, final FSEntryTable table, final HFSPlusDirectory parent,
-            final String name, final LeafRecord record) {
-        super(fs, table, parent, name, getFSEntryType(name, record));
+    protected boolean valid;
+    protected boolean dirty;
+    protected FSAccessRights rights;
+    private long lastModified;
+
+    /**
+     * 
+     * @param fs
+     * @param parent
+     * @param name
+     * @param record
+     */
+    public HFSPlusEntry(HfsPlusFileSystem fs, HFSPlusDirectory parent, String name,
+            LeafRecord record) {
+        this.fs = fs;
+        this.parent = parent;
+        this.name = name;
         this.record = record;
+        this.type = getFSEntryType();
+        this.rights = new UnixFSAccessRights(fs);
+        this.lastModified = System.currentTimeMillis();
     }
 
-    private static int getFSEntryType(final String name, final LeafRecord record) {
+    private int getFSEntryType() {
         int mode = record.getType();
         if ("/".equals(name)) {
             return AbstractFSEntry.ROOT_ENTRY;
@@ -47,7 +75,91 @@ public class HFSPlusEntry extends AbstractFSEntry {
         }
     }
 
-    public final LeafRecord getRecord() {
-        return record;
+    @Override
+    public FSAccessRights getAccessRights() throws IOException {
+        return rights;
     }
+
+    @Override
+    public FSDirectory getDirectory() throws IOException {
+        if (!isFile()) {
+            throw new IOException("It is not a Directory");
+        }
+        return (HFSPlusDirectory) this;
+    }
+
+    @Override
+    public FSFile getFile() throws IOException {
+        if (!isFile()) {
+            throw new IOException("It is not a file");
+        }
+        return (HFSPlusFile) this;
+    }
+
+    @Override
+    public long getLastModified() throws IOException {
+        // TODO Auto-generated method stub
+        return lastModified;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public FSDirectory getParent() {
+        return parent;
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return (type == AbstractFSEntry.DIR_ENTRY || type == AbstractFSEntry.ROOT_ENTRY);
+    }
+
+    @Override
+    public boolean isDirty() throws IOException {
+        return dirty;
+    }
+    
+    public void setDirty() {
+        dirty = true;
+    }
+
+    public void resetDirty() {
+        dirty = false;
+    }
+
+    @Override
+    public boolean isFile() {
+        return (type == AbstractFSEntry.FILE_ENTRY);
+    }
+
+    @Override
+    public void setLastModified(long lastModified) throws IOException {
+        this.lastModified = lastModified;
+    }
+
+    @Override
+    public void setName(String newName) throws IOException {
+        if (type == AbstractFSEntry.ROOT_ENTRY) {
+            throw new IOException("Cannot change name of root directory");
+        }
+        if (parent.getTable().rename(name, newName) < 0) {
+            throw new IOException("Cannot change name");
+        }
+
+        this.name = newName;
+    }
+
+    @Override
+    public FileSystem<?> getFileSystem() {
+        return fs;
+    }
+
+    @Override
+    public boolean isValid() {
+        return valid;
+    }
+
 }
