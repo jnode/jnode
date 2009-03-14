d1970f07468dfc28456f71664343bbd0cd1ede76
Levente Santha <lsantha@jnode.org>
2009-03-14 10:45:16 +0000

    OpenJDK integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5101 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/VmSystem.java
+++ core/src/core/org/jnode/vm/VmSystem.java
@@ -338,6 +338,7 @@ public final class VmSystem {
 
         res.put("swing.handleTopLevelPaint", "false");
         res.put("java.protocol.handler.pkgs", "org.jnode.protocol|gnu.java.net.protocol|gnu.inet");
+        res.put("java.content.handler.pkgs", "gnu.java.net.content");
 
         VmSystemSettings.insertSystemProperties(res);
     }
---
+++ core/src/openjdk/vm/java/net/NativeDatagramPacket.java
@@ -0,0 +1,13 @@
+package java.net;
+
+/**
+ * @see java.net.DatagramPacket
+ */
+class NativeDatagramPacket {
+    /**
+     * @see java.net.DatagramPacket#init()
+     */
+    private static void init() {
+        //nothing to do
+    }
+}
