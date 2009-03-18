8bbedd0fed4ef0b24c36cea7c806e8509c97c27f
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-03-18 22:53:16 +0000

    rewrite BTHeaderRecord and add javadocs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5116 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/hfsplus/tree/BTHeaderRecord.java
+++ fs/src/fs/org/jnode/fs/hfsplus/tree/BTHeaderRecord.java
@@ -17,135 +17,151 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
+
 package org.jnode.fs.hfsplus.tree;
 
 import org.jnode.util.BigEndian;
 
 public class BTHeaderRecord {
     public static final int BT_HEADER_RECORD_LENGTH = 106;
-    private byte[] data;
+    /** The depth of the current B-Tree. */
+    private int treeDepth;
+    /** The root node number */
+    private int rootNode;
+    /** The number of records contains in all leaf nodes. */
+    private int leafRecords;
+    /** The number of the first leaf node. This may be zero. */
+    private int firstLeafNode;
+    /** The number of the last leaf node. This may be zero. */
+    private int lastLeafNode;
+    /** The size in bytes of a node. */
+    private int nodeSize;
+    /** The maximum length of a key. */
+    private int maxKeyLength;
+    /** The total number of free or used nodes in the B-Tree. */
+    private int totalNodes;
+    /** The number of free node in the B-Tree. */
+    private int freeNodes;
+    /**
+     * Ignore for HFS+, clumpSize field from {@code HFSPlusForkData} used
+     * instead.
+     */
+    private int clumpSize;
+    /** The type of the B-Tree. */
+    private int treeType;
+    /** Ignore in HFS+, should be threat as reserved. */
+    private int keyCompareType;
+    /** Various attributes of the B-Tree. */
+    private int attributes;
 
-    public BTHeaderRecord() {
-        data = new byte[BT_HEADER_RECORD_LENGTH];
+    public BTHeaderRecord(int treeDepth, int rootNode, int leafRecords, int firstLeafNode,
+            int lastLeafNode, int nodeSize, int maxKeyLength, int totalNodes, int freeNodes,
+            int clumpsize, int treeType, int keyCompareType, int attributes) {
+        this.treeDepth = treeDepth;
+        this.rootNode = rootNode;
+        this.leafRecords = leafRecords;
+        this.firstLeafNode = firstLeafNode;
+        this.lastLeafNode = lastLeafNode;
+        this.nodeSize = nodeSize;
+        this.maxKeyLength = maxKeyLength;
+        this.totalNodes = totalNodes;
+        this.freeNodes = freeNodes;
+        this.clumpSize = clumpsize;
+        this.treeType = treeType;
+        this.keyCompareType = keyCompareType;
+        this.attributes = attributes;
     }
 
     public BTHeaderRecord(final byte[] src, int offset) {
-        data = new byte[BT_HEADER_RECORD_LENGTH];
+        byte[] data = new byte[BT_HEADER_RECORD_LENGTH];
         System.arraycopy(src, offset, data, 0, BT_HEADER_RECORD_LENGTH);
-    }
-
-    public final int getTreeDepth() {
-        return BigEndian.getInt16(data, 0);
-    }
-
-    public void setTreeDepth(int depth) {
-        BigEndian.setInt16(data, 0, depth);
-    }
-
-    public final int getRootNode() {
-        return BigEndian.getInt32(data, 2);
-    }
-
-    public void setRootNode(int node) {
-        BigEndian.setInt32(data, 2, node);
-    }
-
-    public final int getLeafRecords() {
-        return BigEndian.getInt32(data, 6);
-    }
-
-    public void setLeafRecords(int count) {
-        BigEndian.setInt32(data, 6, count);
-    }
-
-    public final int getFirstLeafNode() {
-        return BigEndian.getInt32(data, 10);
-    }
-
-    public void settFirstLeafNode(int node) {
-        BigEndian.setInt32(data, 10, node);
-    }
-
-    public final int getLastLeafNode() {
-        return BigEndian.getInt32(data, 14);
-    }
-
-    public void setLastLeafNode(int node) {
-        BigEndian.setInt32(data, 14, node);
-    }
-
-    public final int getNodeSize() {
-        return BigEndian.getInt16(data, 18);
-    }
-
-    public void setNodeSize(int size) {
-        BigEndian.setInt16(data, 18, size);
-    }
-
-    public int getMaxKeyLength() {
-        return BigEndian.getInt16(data, 20);
-    }
-
-    public void setMaxKeyLength(int length) {
-        BigEndian.setInt16(data, 20, length);
-    }
-
-    public int getTotalNodes() {
-        return BigEndian.getInt32(data, 22);
-    }
-
-    public void setTotalNodes(int count) {
-        BigEndian.setInt32(data, 22, count);
-    }
-
-    public int getFreeNodes() {
-        return BigEndian.getInt32(data, 26);
-    }
-
-    public void setFreeNodes(int count) {
-        BigEndian.setInt32(data, 26, count);
-    }
-
-    public int getClumpSize() {
-        return BigEndian.getInt32(data, 32);
-    }
-
-    public void setClumpSize(int size) {
-        BigEndian.setInt32(data, 32, size);
-    }
-
-    public int getTreeType() {
-        return BigEndian.getInt8(data, 36);
-    }
-
-    public void setTreeType(int type) {
-        BigEndian.setInt8(data, 36, type);
-    }
-
-    public int getKeyCompareType() {
-        return BigEndian.getInt8(data, 37);
-    }
-
-    public void setKeyCompareType(int type) {
-        BigEndian.setInt8(data, 38, type);
-    }
-
-    public long getAttributes() {
-        return BigEndian.getInt32(data, 39);
-    }
-
-    public void setAttributes(int attrs) {
-        BigEndian.setInt32(data, 39, attrs);
+        treeDepth = BigEndian.getInt16(data, 0);
+        rootNode = BigEndian.getInt32(data, 2);
+        leafRecords = BigEndian.getInt32(data, 6);
+        firstLeafNode = BigEndian.getInt32(data, 10);
+        lastLeafNode = BigEndian.getInt32(data, 14);
+        nodeSize = BigEndian.getInt16(data, 18);
+        maxKeyLength = BigEndian.getInt16(data, 20);
+        totalNodes = BigEndian.getInt16(data, 24);
+        freeNodes = BigEndian.getInt16(data, 28);
+        clumpSize = BigEndian.getInt16(data, 32);
+        treeType = BigEndian.getInt16(data, 36);
+        keyCompareType = BigEndian.getInt16(data, 37);
+        attributes = BigEndian.getInt32(data, 39);
     }
 
     public byte[] getBytes() {
+        byte[] data = new byte[BT_HEADER_RECORD_LENGTH];
+        BigEndian.setInt16(data, 0, treeDepth);
+        BigEndian.setInt32(data, 2, rootNode);
+        BigEndian.setInt32(data, 6, leafRecords);
+        BigEndian.setInt32(data, 10, firstLeafNode);
+        BigEndian.setInt32(data, 14, lastLeafNode);
+        BigEndian.setInt16(data, 18, nodeSize);
+        BigEndian.setInt16(data, 20, maxKeyLength);
+        BigEndian.setInt32(data, 22, totalNodes);
+        BigEndian.setInt32(data, 26, freeNodes);
+        BigEndian.setInt32(data, 32, clumpSize);
+        BigEndian.setInt8(data, 36, treeType);
+        BigEndian.setInt8(data, 38, keyCompareType);
+        BigEndian.setInt32(data, 39, attributes);
         return data;
     }
-    
+
     public final String toString() {
-        return ("Root node:  " + getRootNode() + "\n" + "First leaf: "
-                + getFirstLeafNode() + "\n" + "Last leaf:  "
-                + getLastLeafNode() + "\n" + "node size:  " + getNodeSize() + "\n");
+        return ("Root node:  " + getRootNode() + "\n" + "First leaf: " + getFirstLeafNode() + "\n" +
+                "Last leaf:  " + getLastLeafNode() + "\n" + "node size:  " + getNodeSize() + "\n");
+    }
+
+    public int getTreeDepth() {
+        return treeDepth;
+    }
+
+    public int getRootNode() {
+        return rootNode;
+    }
+
+    public int getLeafRecords() {
+        return leafRecords;
+    }
+
+    public int getFirstLeafNode() {
+        return firstLeafNode;
+    }
+
+    public int getLastLeafNode() {
+        return lastLeafNode;
+    }
+
+    public int getNodeSize() {
+        return nodeSize;
+    }
+
+    public int getMaxKeyLength() {
+        return maxKeyLength;
+    }
+
+    public int getTotalNodes() {
+        return totalNodes;
+    }
+
+    public int getFreeNodes() {
+        return freeNodes;
+    }
+
+    public int getClumpSize() {
+        return clumpSize;
+    }
+
+    public int getTreeType() {
+        return treeType;
+    }
+
+    public int getKeyCompareType() {
+        return keyCompareType;
+    }
+
+    public long getAttributes() {
+        return attributes;
     }
 }
