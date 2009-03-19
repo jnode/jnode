376eb1aeb73f95ff4e81d563acc4a9e781963c6c
Levente Santha <lsantha@jnode.org>
2009-03-19 18:13:36 +0000

    OpenJDK integration.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5119 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/java/net/NativeSocketInputStream.java
@@ -0,0 +1,24 @@
+package java.net;
+
+import java.io.FileDescriptor;
+
+/**
+ * @see java.net.SocketInputStream
+ */
+class NativeSocketInputStream {
+    /**
+     * @see java.net.SocketInputStream#socketRead0(java.io.FileDescriptor, byte[], int, int, int)
+     */
+    private static int socketRead0(SocketInputStream instance, FileDescriptor arg1, byte[] arg2, int arg3, int arg4, int arg5) {
+        //todo implement it
+        //return 0;
+        throw new UnsupportedOperationException();
+    }
+    /**
+     * @see java.net.SocketInputStream#init()
+     */
+    private static void init() {
+        //todo implement it
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ core/src/openjdk/vm/java/net/NativeSocketOutputStream.java
@@ -0,0 +1,23 @@
+package java.net;
+
+import java.io.FileDescriptor;
+
+/**
+ * @see java.net.SocketOutputStream
+ */
+class NativeSocketOutputStream {
+    /**
+     * @see java.net.SocketOutputStream#socketWrite0(java.io.FileDescriptor, byte[], int, int)
+     */
+    private static void socketWrite0(SocketOutputStream instance, FileDescriptor arg1, byte[] arg2, int arg3, int arg4) {
+        //todo implement it
+        throw new UnsupportedOperationException();
+    }
+    /**
+     * @see java.net.SocketOutputStream#init()
+     */
+    private static void init() {
+        //todo implement it
+        throw new UnsupportedOperationException();
+    }
+}
