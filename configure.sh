83f85db748ed2c693fb865e79d56165a69968223
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-03-29 09:40:23 +0000

    Applied patch by 'caless' that fixes shell script portability problems and regression caused by moving the nanoxml JAR file.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5178 407389f7-7c16-0410-9eee-ad85acb55d7f

--- configure.sh
+++ configure.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-CMD=`type -p $0`
-DIR=`dirname $CMD`
+DIR=`dirname $0`
+DIR=`cd $DIR; pwd;`
 
 # Belt and braces classpath to get the most recent version
 # of the 'configure' classes that we can find.
 CP=$DIR/builder/build/classes:\
 $DIR/all/build/descriptors/jnode-configure.jar:\
 $DIR/builder/lib/jnode-configure-dist.jar:\
-$DIR/builder/lib/nanoxml-2.2.3.jar
+$DIR/shell/lib/nanoxml-2.2.3.jar
 
 if [ $# = 0 ] ; then
-    java -cp $CP org.jnode.configure.Configure all/conf-source/script.xml
+    java -cp $CP org.jnode.configure.Configure $DIR/all/conf-source/script.xml
 else
     java -cp $CP org.jnode.configure.Configure "$@"
 fi
