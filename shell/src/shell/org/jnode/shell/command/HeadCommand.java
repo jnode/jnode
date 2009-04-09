600578ce16f40aa6b0f0039c1c212b2744adae13
chrisboertien <chrisboertien@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-04-09 12:11:49 +0000

    Implementation for head and tail commands
    Signed-off-by: Chris <chris.boertien@gmail.com>
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5236 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/command/HeadCommand.java
+++ shell/src/shell/org/jnode/shell/command/HeadCommand.java
@@ -20,13 +20,31 @@
 
 package org.jnode.shell.command;
 
+import org.jnode.shell.syntax.ArgumentBundle;
+import org.jnode.driver.console.CompletionInfo;
+import org.jnode.shell.CommandLine.Token;
+
 import org.jnode.shell.AbstractCommand;
 import org.jnode.shell.syntax.Argument;
+import org.jnode.shell.syntax.ArgumentSnapshot;
 import org.jnode.shell.syntax.FlagArgument;
 import org.jnode.shell.syntax.FileArgument;
-import org.jnode.shell.syntax.IntegerArgument;
+import org.jnode.shell.syntax.StringArgument;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
+ * TODO add support for -c -<int> on stdin
  * @author chris boertien
  */
 public class HeadCommand extends AbstractCommand {
@@ -39,20 +57,219 @@ public class HeadCommand extends AbstractCommand {
     private static final String help_quiet = "never output headers giving file names";
     private static final String help_verbose = "always output headers giving file names";
     
-    private final FileArgument Files = new FileArgument("files", Argument.MULTIPLE | Argument.OPTIONAL, help_files);
-    private final IntegerArgument Lines = 
-        new IntegerArgument("lines", Argument.OPTIONAL | Argument.EXISTING, help_lines);
-    private final IntegerArgument Bytes =
-        new IntegerArgument("bytes", Argument.OPTIONAL | Argument.EXISTING, help_bytes);
-    private final FlagArgument Quiet = new FlagArgument("quiet", Argument.OPTIONAL, help_quiet);
-    private final FlagArgument Verbose = new FlagArgument("verbose", Argument.OPTIONAL, help_verbose);
+    private final FileArgument Files;
+    private final StringArgument Lines;
+    private final StringArgument Bytes;
+    private final FlagArgument Quiet;
+    private final FlagArgument Verbose;
+    
+    private PrintWriter err;
+    
+    private int count;
+    private boolean headers;
+    private boolean useLines;
+    private boolean reverse;
     
     public HeadCommand() {
         super("Print the head of a list of files, or stdin");
+        Files   = new FileArgument("files", Argument.MULTIPLE | Argument.OPTIONAL, help_files);
+        Lines   = new StringArgument("lines", Argument.OPTIONAL | Argument.EXISTING, help_lines);
+        Bytes   = new StringArgument("bytes", Argument.OPTIONAL | Argument.EXISTING, help_bytes);
+        Quiet   = new FlagArgument("quiet", Argument.OPTIONAL, help_quiet);
+        Verbose = new FlagArgument("verbose", Argument.OPTIONAL, help_verbose);
         registerArguments(Files, Lines, Bytes, Quiet, Verbose);
     }
     
     public void execute() {
-        exit(1);
+        File[] files;
+        err      = getError().getPrintWriter();
+        headers  = getHeaders();
+        useLines = getCountType();
+        count    = getCount();
+        reverse  = getReversed();
+        
+        if (!Files.isSet()) {
+            files = new File[1];
+            files[0] = new File("-");
+        } else {
+            files = Files.getValues();
+        }
+        
+        try {
+            for (File file : files) {
+                printHeader(file.getName());
+                if (file.getName().equals("-")) {
+                    if (useLines) {
+                        printStdinLines();
+                    } else {
+                        printStdinBytes();
+                    }
+                } else {
+                    if (useLines) {
+                        printFileLines(file);
+                    } else {
+                        printFileBytes(file);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            exit(1);
+        }
+    }
+    
+    private void printStdinLines() {
+        try {
+            printLines(new LineNumberReader(new InputStreamReader(getInput().getInputStream())));
+        } catch (IOException ex) {
+            err.println(ex);
+        }
+    }
+    
+    private void printStdinBytes() {
+        try {
+            printBytes(getInput().getInputStream(), -1);
+        } catch (IOException ex) {
+            err.println(ex);
+        }
+    }
+    
+    private void printFileLines(File file) {
+        LineNumberReader reader = null;
+        try {
+            reader = new LineNumberReader(new FileReader(file));
+            printLines(reader);
+        } catch (IOException ex) {
+            err.println(ex);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException _) {
+                    // ignore
+                }
+            }
+        }
+    }
+    
+    private void printFileBytes(File file) {
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            printBytes(in, (int) file.length());
+        } catch (IOException ex) {
+            err.println(ex);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException _) {
+                    // ignore
+                }
+            }
+        }
+    }
+    
+    private void printLines(LineNumberReader reader) throws IOException {
+        PrintWriter out = getOutput().getPrintWriter();
+        String line;
+        if (!reverse) {
+            int n = count;
+            while (--n >= 0 && (line = reader.readLine()) != null) {
+                out.println(line);
+            }
+        } else {
+            List<String> lines = new ArrayList<String>();
+            
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            
+            int numLines = lines.size() - count;
+            for (int n = 0; n < numLines; n++) {
+                out.println(lines.get(n));
+            }
+        }
+    }
+    
+    private void printBytes(InputStream in, int size) throws IOException {
+        OutputStream out = getOutput().getOutputStream();
+        byte[] buffer;
+        int len;
+        int n;
+        int bufsize = 8 * 1024;
+        
+        if (!reverse) {
+            n = count;
+        } else if (size != -1) {
+            n = size - count;
+        } else {
+            // FIXME
+            throw new UnsupportedOperationException("Cannot do -count byte reads on stdin yet");
+        }
+        
+        buffer = new byte[Math.min(n, bufsize)];
+        while (n > 0 && (len = in.read(buffer)) > 0) {
+            len = Math.min(n, len);
+            out.write(buffer,0,len);
+            n -= len;
+        }
+    }
+    
+    private void printHeader(String name) {
+        PrintWriter out = getOutput().getPrintWriter();
+        if (headers) {
+            out.println();
+            if (name.equals("-")) {
+                out.println("==> standard input <==");
+            } else {
+                out.println("==> " + name + " <==");
+            }
+        }
+    }
+    
+    private boolean getHeaders() {
+        if (Quiet.isSet()) {
+            return false;
+        } else if (Verbose.isSet()) {
+            return true;
+        } else {
+            return headers = Files.isSet() && Files.getValues().length > 1;
+        }
+    }
+    
+    private boolean getCountType() {
+        return !Bytes.isSet();
+    }
+    
+    private int getCount() {
+        int count = 10;
+        if (Bytes.isSet()) {
+            count = parseInt(Bytes.getValue());
+        }
+        if (Lines.isSet()) {
+            count = parseInt(Lines.getValue());
+        }
+        return count < 0 ? 10 : count;
+    }
+    
+    private boolean getReversed() {
+        if (Bytes.isSet()) {
+            return Bytes.getValue().charAt(0) == '-';
+        } else if (Lines.isSet()) {
+            return Lines.getValue().charAt(0) == '-';
+        }
+        return false;
+    }
+    
+    private int parseInt(String s) {
+        if (s.charAt(0) == '-') {
+            s = s.substring(1);
+        }
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException ex) {
+            return -1;
+        }
     }
 }
