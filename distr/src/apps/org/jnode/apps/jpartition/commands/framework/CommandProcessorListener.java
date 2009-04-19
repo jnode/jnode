fdcbaa853c8393bec0579c156fdf3f2d89f60cd8
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-04-19 19:34:21 +0000

    added javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5315 407389f7-7c16-0410-9eee-ad85acb55d7f

--- distr/src/apps/org/jnode/apps/jpartition/commands/framework/CommandProcessorListener.java
+++ distr/src/apps/org/jnode/apps/jpartition/commands/framework/CommandProcessorListener.java
@@ -20,12 +20,37 @@
  
 package org.jnode.apps.jpartition.commands.framework;
 
+/**
+ * Listener interface for {@link CommandProcessor} events.
+ * @author Fabien DUMINY (fduminy@jnode.org)
+ *
+ */
 public interface CommandProcessorListener {
+    /**
+     * Method called when a {@link Command} has been added to a {@link CommandProcessor}. 
+     * @param processor the command processor
+     * @param command the command
+     */
     void commandAdded(CommandProcessor processor, Command command);
 
+    /**
+     * Method called when a {@link Command} has been removed from a {@link CommandProcessor}. 
+     * @param processor the command processor
+     * @param command the command
+     */
     void commandRemoved(CommandProcessor processor, Command command);
 
+    /**
+     * Method called when a {@link Command} has started. 
+     * @param processor the command processor
+     * @param command the command
+     */
     void commandStarted(CommandProcessor processor, Command command);
 
+    /**
+     * Method called when a {@link Command} has finished. 
+     * @param processor the command processor
+     * @param command the command
+     */
     void commandFinished(CommandProcessor processor, Command command);
 }
