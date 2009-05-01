8fae331b1d1af7cc852a064811b14d2434c21ff9
chrisboertien <chrisboertien@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-05-01 07:09:28 +0000

    Improved df impl
    Support for -h/-H/-B/-k switches
    
    Signed-off-by: chrisboertien <chris.boertien@gmail.com>
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5376 407389f7-7c16-0410-9eee-ad85acb55d7f

--- cli/src/commands/org/jnode/command/file/DFCommand.java
+++ cli/src/commands/org/jnode/command/file/DFCommand.java
@@ -26,6 +26,8 @@ import java.util.Map;
 
 import javax.naming.NameNotFoundException;
 
+import java.io.File;
+
 import org.jnode.driver.Device;
 import org.jnode.driver.DeviceManager;
 import org.jnode.fs.FileSystem;
@@ -34,6 +36,10 @@ import org.jnode.naming.InitialNaming;
 import org.jnode.shell.AbstractCommand;
 import org.jnode.shell.syntax.Argument;
 import org.jnode.shell.syntax.DeviceArgument;
+import org.jnode.shell.syntax.FileArgument;
+import org.jnode.shell.syntax.FlagArgument;
+import org.jnode.shell.syntax.IntegerArgument;
+import org.jnode.util.NumberUtils;
 
 /**
  * The DF command prints disk usage information for devices with filesystems.
@@ -41,13 +47,21 @@ import org.jnode.shell.syntax.DeviceArgument;
  * @author galatnm@jnode.org
  * @author crawley@jnode.org
  * @author Levente S\u00e1ntha
+ * @author chris boertien
  */
 public class DFCommand extends AbstractCommand {
     
     private static final String help_device = "The device for which disk usage information should be displayed";
+    private static final String help_path = "Display disk usage info for the file system that contains this path";
+    private static final String help_read_dec = "Print output in human readable decimal form (1000)";
+    private static final String help_read_bin = "Print output in human readable binary form (1024)";
+    private static final String help_all = "Show all file systems, even pseudo file systems";
+    private static final String help_block_1k = "Same as -B 1024";
+    private static final String help_block = "Print output with a specified block size";
     private static final String help_super = "Print file system usage information";
     private static final String str_id = "ID";
     private static final String str_size = "Size";
+    private static final String str_blocks = "blocks";
     private static final String str_used = "Used";
     private static final String str_free = "Free";
     private static final String str_mount = "Mount";
@@ -55,44 +69,110 @@ public class DFCommand extends AbstractCommand {
     private static final String str_unknown = "unknown";
     private static final String err_get_info = "\tError getting disk usage information for %s on %s : %s%n";
     
+    private static final int OUT_BINARY  = 1;
+    private static final int OUT_DECIMAL = 2;
+    private static final int OUT_BLOCKS  = 3;
+    
+    private static final int DEFAULT_BLOCK_SIZE = 1024;
+    
     private final DeviceArgument argDevice;
-
+    private final FileArgument argPath;
+    private final FlagArgument argReadDec;
+    private final FlagArgument argReadBin;
+    private final FlagArgument argAll;
+    private final FlagArgument argBlock1k;
+    private final IntegerArgument argBlock;
+    
+    private StringBuilder line;
+    private FileSystemService fss;
+    private DeviceManager dm;
+    private Map<String, String> mountPoints;
+    private PrintWriter out;
+    private int outputType;
+    private int blockSize;
+    private boolean all;
+    
     public DFCommand() {
         super(help_super);
-        argDevice = new DeviceArgument("device", Argument.OPTIONAL | Argument.EXISTING, help_device);
-        registerArguments(argDevice);
+        argDevice  = new DeviceArgument("device", Argument.EXISTING, help_device);
+        argPath    = new FileArgument("path", Argument.EXISTING, help_path);
+        argReadDec = new FlagArgument("human-read-dec", 0, help_read_dec);
+        argReadBin = new FlagArgument("human-read-bin", 0, help_read_bin);
+        argAll     = new FlagArgument("show-all", 0, help_all);
+        argBlock1k = new FlagArgument("block-size-1k", 0, help_block_1k);
+        argBlock   = new IntegerArgument("block-size", 0, help_block);
+        registerArguments(argDevice, argPath, argReadDec, argReadBin, argAll, argBlock1k, argBlock);
     }
-
-    public void execute() throws NameNotFoundException {
-        final FileSystemService fss = InitialNaming.lookup(FileSystemService.NAME);
-        final Map<String, String> mountPoints = fss.getDeviceMountPoints();
-        PrintWriter out = getOutput().getPrintWriter(false);
-        format(out, str_id, true);
-        format(out, str_size, false);
-        format(out, str_used, false);
-        format(out, str_free, false);
-        out.println(str_mount);
-        out.println();
-        if (argDevice.isSet()) {
-            final Device dev = argDevice.getValue();
-            FileSystem<?> fs = fss.getFileSystem(dev);
-            if (fs == null) {
-                out.println(str_no_fs);
-            } else {
-                displayInfo(out, dev, fs, mountPoints.get(fs.getDevice().getId()));
-            }
+    
+    public void execute() throws NameNotFoundException, IOException {
+        parseOptions();
+        fss         = InitialNaming.lookup(FileSystemService.NAME);
+        dm          = InitialNaming.lookup(DeviceManager.NAME);
+        mountPoints = fss.getDeviceMountPoints();
+        out         = getOutput().getPrintWriter(true);
+        line        = new StringBuilder();
+        
+        Device device = null;
+        
+        printHeader();
+        
+        if (argPath.isSet()) {
+            device = getDeviceForPath(argPath.getValue());
+        } else if (argDevice.isSet()) {
+            device = argDevice.getValue();
         } else {
-            final DeviceManager dm = InitialNaming.lookup(DeviceManager.NAME);
             for (Device dev : dm.getDevices()) {
                 FileSystem<?> fs = fss.getFileSystem(dev);
                 if (fs != null) {
                     displayInfo(out, dev, fs, mountPoints.get(fs.getDevice().getId()));
                 }
             }
+            out.flush();
+            exit(0);
         }
-        out.flush();
+        if (device != null) {
+            FileSystem<?> fs = fss.getFileSystem(device);
+            if (fs == null) {
+                out.println(str_no_fs);
+            } else {
+                displayInfo(out, device, fs, mountPoints.get(fs.getDevice().getId()));
+            }
+            out.flush();
+            exit(0);
+        }
     }
-
+    
+    private Device getDeviceForPath(File file) throws IOException {
+        String path = file.getCanonicalPath();
+        String mp = null;
+        for (String mountPoint : fss.getMountPoints().keySet()) {
+            if (path.startsWith(mountPoint)) {
+                if (mp != null) {
+                    if (!mp.startsWith(mountPoint)) {
+                        continue;
+                    }
+                }
+                mp = mountPoint;
+            }
+        }
+        if (mp == null) {
+            throw new AssertionError("No fs device for " + path);
+        }
+        return fss.getMountPoints().get(mp).getDevice();
+    }
+    
+    private void printHeader() {
+        format(out, str_id, true);
+        if (outputType == OUT_BLOCKS) {
+            format(out, String.format("%s-%s", NumberUtils.toBinaryByte(blockSize), str_blocks), false);
+        } else {
+            format(out, str_size, false);
+        }
+        format(out, str_used, false);
+        format(out, str_free, false);
+        out.println(str_mount);
+    }
+    
     /**
      * @param out
      * @param dev
@@ -101,27 +181,27 @@ public class DFCommand extends AbstractCommand {
      */
     private void displayInfo(PrintWriter out, Device dev, FileSystem<?> fs, String mountPoint) {
         try {
-
             String str = dev.getId();
+            long total = fs.getTotalSpace();
+            long free = fs.getFreeSpace();
+            
             format(out, str, true);
-
-            final long total = fs.getTotalSpace();
-            str = total < 0 ? str_unknown : String.valueOf(total);
+            
+            str = total < 0 ? str_unknown : valueOf(total, true);
             format(out, str, false);
-
-            final long free = fs.getFreeSpace();
-            str = total < 0 ? str_unknown : String.valueOf(total - free);
+            
+            str = total < 0 ? str_unknown : valueOf(total - free, true);
             format(out, str, false);
-
-            str = free < 0 ? str_unknown : String.valueOf(free);
+            
+            str = free < 0 ? str_unknown : valueOf(free, false);
             format(out, str, false);
-
+            
             out.println(mountPoint);
         } catch (IOException ex) {
             out.format(err_get_info, mountPoint, dev.getId(), ex.getLocalizedMessage());
         }
     }
-
+    
     private void format(PrintWriter out, String str, boolean left) {
         int ln;
         ln = 15 - str.length();
@@ -137,5 +217,71 @@ public class DFCommand extends AbstractCommand {
             out.print(str);
         }
         out.print(' ');
+    }
+    
+    private String valueOf(long size, boolean up) {
+        switch(outputType) {
+            case OUT_DECIMAL :
+                return NumberUtils.toDecimalByte(size, 0);
+            case OUT_BINARY :
+                return NumberUtils.toBinaryByte(size, 0);
+            case OUT_BLOCKS :
+                return toBlock(size, blockSize, up);
+            default :
+                return String.valueOf(size);
+        }
+    }
+    
+    private String toBlock(long size, long blockSize, boolean up) {
+        return String.valueOf(size / blockSize + ((up && ((size % blockSize) > 0)) ? 1 : 0));
+    }
+    
+    private void parseOptions() {
+        if (argReadDec.isSet()) {
+            outputType = OUT_DECIMAL;
+        } else if (argReadBin.isSet()) {
+            outputType = OUT_BINARY;
+        } else {
+            outputType = OUT_BLOCKS;
+        }
+        
+        all = argAll.isSet();
+        
+        if (argBlock1k.isSet()) {
+            blockSize = 1024;
+        } else if (argBlock.isSet()) {
+            blockSize = argBlock.getValue();
+        } else {
+            blockSize = getDefaultBlock();
+        }
+    }
+    
+    private int getDefaultBlock() {
+        /* Env vars are broken
+        String DF_BLOCK_SIZE = System.getenv("DF_BLOCK_SIZE");
+        String BLOCK_SIZE = System.getenv("BLOCK_SIZE");
+        String BLOCKSIZE = System.getenv("BLOCKSIZE");
+        String POSIXLY_CORRECT = System.getenv("POSIXLY_CORRECT");
+        
+        String size = null;
+        if (DF_BLOCK_SIZE != null) {
+            size = DF_BLOCK_SIZE;
+        } else if (BLOCK_SIZE != null) {
+            size = BLOCK_SIZE;
+        } else if (BLOCKSIZE != null) {
+            size = BLOCKSIZE;
+        } else if (POSIXLY_CORRECT != null) {
+            return 512;
+        } else {
+            return DEFAULT_BLOCK_SIZE;
+        }
+        
+        try {
+            return Integer.parseInt(size);
+        } catch (NumberFormatException e) {
+            return DEFAULT_BLOCK_SIZE;
+        }
+        */
+        return DEFAULT_BLOCK_SIZE;
     }
 }
