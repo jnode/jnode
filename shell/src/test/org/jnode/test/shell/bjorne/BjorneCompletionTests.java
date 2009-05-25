7133f40b18f20285947899157011de6875ae2694
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-05-25 11:12:34 +0000

    Starting to build unit tests for bjorne completion
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5522 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ shell/src/test/org/jnode/test/shell/bjorne/BjorneCompletionTests.java
@@ -0,0 +1,136 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2003-2009 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+package org.jnode.test.shell.bjorne;
+
+import java.util.Set;
+
+import javax.naming.NamingException;
+
+import junit.framework.TestCase;
+
+import org.jnode.shell.CommandCompletions;
+import org.jnode.shell.Completable;
+import org.jnode.shell.ShellManager;
+import org.jnode.shell.ShellSyntaxException;
+import org.jnode.shell.ShellUtils;
+import org.jnode.shell.alias.AliasManager;
+import org.jnode.shell.bjorne.BjorneInterpreter;
+import org.jnode.shell.help.CompletionException;
+import org.jnode.shell.syntax.ArgumentSyntax;
+import org.jnode.shell.syntax.EmptySyntax;
+import org.jnode.shell.syntax.OptionSyntax;
+import org.jnode.shell.syntax.RepeatSyntax;
+import org.jnode.shell.syntax.SequenceSyntax;
+import org.jnode.shell.syntax.SyntaxBundle;
+import org.jnode.shell.syntax.SyntaxManager;
+import org.jnode.test.shell.Cassowary;
+import org.jnode.test.shell.syntax.TestShell;
+
+public class BjorneCompletionTests extends TestCase {
+    
+    static TestShell shell;
+    static {
+        try {
+            Cassowary.initEnv();
+            shell = new TestShell();
+            ShellUtils.getShellManager().registerShell(shell);
+            
+            AliasManager am = shell.getAliasManager();
+            am.add("gc", "org.jnode.command.system.GcCommand");
+            am.add("cpuid", "org.jnode.command.system.CpuIDCommand");
+            am.add("set", "org.jnode.command.system.SetCommand");
+            am.add("dir", "org.jnode.test.shell.MyDirCommand");
+            am.add("duh", "org.jnode.test.shell.MyDuhCommand");
+            am.add("cat", "org.jnode.test.shell.MyCatCommand");
+            am.add("echo", "org.jnode.test.shell.MyEchoCommand");
+            am.add("alias", "org.jnode.test.shell.MyAliasCommand");
+
+            SyntaxManager sm = shell.getSyntaxManager();
+            sm.add(new SyntaxBundle("set",
+                new SequenceSyntax(new ArgumentSyntax("key"), new ArgumentSyntax("value"))));
+            sm.add(new SyntaxBundle("duh", new ArgumentSyntax("path")));
+            sm.add(new SyntaxBundle("echo", new RepeatSyntax(new ArgumentSyntax("text"))));
+            sm.add(new SyntaxBundle("cpuid", new SequenceSyntax()));
+            sm.add(new SyntaxBundle("alias",
+                new EmptySyntax(null, "Print all available aliases and corresponding classnames"),
+                new SequenceSyntax(null, "Set an aliases for given classnames",
+                    new ArgumentSyntax("alias"), new ArgumentSyntax("classname")),
+                new OptionSyntax("remove", 'r', null, "Remove an alias")));
+            
+        } catch (NamingException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    
+    private static boolean DEBUG = true;
+
+    public void testSimpleCommand() throws ShellSyntaxException, CompletionException {
+        doCompletionTest("echo hi", "TE");
+    }
+
+    private void doCompletionTest(String input, String flags) 
+        throws ShellSyntaxException, CompletionException {
+        BjorneInterpreter interpreter = new BjorneInterpreter();
+        for (int i = 0; i <= input.length(); i++) {
+            String partial = input.substring(0, i);
+            int inWord = 0;
+            int wordStart = 0;
+            for (int j = 0; j < i; j++) {
+                if (partial.charAt(j) == ' ') {
+                    inWord++;
+                    wordStart = j + 1;
+                }
+            }
+            String lastWord = partial.substring(wordStart);
+            Completable completable = interpreter.parsePartial(shell, partial);
+            CommandCompletions completions = new CommandCompletions();
+            completable.complete(completions, shell);
+            Set<String> completionWords = completions.getCompletions();
+            switch (flags.charAt(inWord)) {
+                case 'T':
+                    // Expect completions
+                    assertTrue("got no completions: " + diag(partial, completions), completionWords.size() > 0);
+                    break;
+                case 'F':
+                    // Expect no completions
+                    assertTrue("got unexpected completions: " + diag(partial, completions), completionWords.size() == 0);
+                    break;
+                case 'E':
+                    // Expect completions if the last char is ' ', otherwise not
+                    if (wordStart >= partial.length()) {
+                        assertTrue("got no completions: " + diag(partial, completions), completionWords.size() > 0);
+                    } else {
+                        assertTrue("got unexpected completions: " + diag(partial, completions), completionWords.size() == 0);
+                    }
+                case '?':
+                    // Maybe completions, maybe not
+            }
+            for (String completionWord : completionWords) {
+                assertTrue(completionWord.startsWith(lastWord));
+            }
+        }
+    }
+
+    private String diag(String partial, CommandCompletions completions) {
+        return "partial = '" + partial + "', completions = " + completions; 
+    }
+}
