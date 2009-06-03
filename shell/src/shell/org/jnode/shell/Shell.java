30e2651bf0b0cf7cd09ebdcbdc8927edd317cacc
crawley <crawley@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-06-03 14:47:18 +0000

    Javadoc tweaks
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5542 407389f7-7c16-0410-9eee-ad85acb55d7f

--- shell/src/shell/org/jnode/shell/Shell.java
+++ shell/src/shell/org/jnode/shell/Shell.java
@@ -71,7 +71,10 @@ public interface Shell extends InputCompleter {
      * Run a command file in the shell.  If the first line of the file is of the form
      * "#!&lt;interpreter&gt;", where "&lt;interpreter&gt;" has been registered with
      * the ShellManager, the file will be run using the nominated interpreter.  Otherwise,
-     * the shell gets to decide which interpreter to use.
+     * the shell decides which interpreter to use.
+     * <p>
+     * A command alias and arguments are passed to the interpreter, but it is up to the
+     * interpreter to decide what if anything to do with them.  Some interpreters ignore them.
      * 
      * @param file the command file
      * @param alias the command alias used to launch the script.  If this parameter is
