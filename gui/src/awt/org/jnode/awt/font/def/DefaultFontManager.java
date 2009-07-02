9d09f0373f30fe3e0fa6d5fc388a30b3c2f77899
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-07-02 14:23:34 +0000

    Use format instead of name in message of the exception.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5584 407389f7-7c16-0410-9eee-ad85acb55d7f

--- gui/src/awt/org/jnode/awt/font/def/DefaultFontManager.java
+++ gui/src/awt/org/jnode/awt/font/def/DefaultFontManager.java
@@ -191,7 +191,9 @@ public class DefaultFontManager implements FontManager, ExtensionPointListener {
 
     public Font createFont(int format, InputStream stream) throws FontFormatException, IOException {
         String name = fontTypeToProviderName.get(format);
-        if (name == null) throw new IllegalArgumentException("unknown format " + name);
+        if (name == null) {
+            throw new IllegalArgumentException("unknown format " + format);
+        }
 
         for (FontProvider<?> prv : getProviders()) {
             if (prv.getName().equals(name)) {
