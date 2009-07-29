1957ba6ef3a046204dd02dc937991870c18edc32
fduminy <fduminy@407389f7-7c16-0410-9eee-ad85acb55d7f>
2009-07-29 19:09:34 +0000

    updated to check that JAVA_HOME is set and that %JAVA_HOME%\bin\java.exe exists some credits given to GriffenJBS
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5622 407389f7-7c16-0410-9eee-ad85acb55d7f

--- build.bat
+++ build.bat
@@ -1,3 +1,23 @@
 @echo off
-setlocal
+
+if ("%JAVA_HOME%") == ("") GOTO ERROR1
+echo JAVA_HOME is set to "%JAVA_HOME%"
+
+if NOT EXIST "%JAVA_HOME%\bin\java.exe" GOTO ERROR2
 "%JAVA_HOME%\bin\java" -Xmx768M -Xms256M -jar core\lib\ant-launcher.jar -lib "%JAVA_HOME%\lib" -lib core\lib -f all\build.xml %*
+GOTO :END
+
+:ERROR1
+echo ERROR: JAVA_HOME is not set.
+GOTO :HELP
+
+:ERROR2
+echo ERROR: JAVA_HOME is set to a wrong value, was expecting "%JAVA_HOME%\bin\java.exe" to exist.
+GOTO :HELP
+
+:HELP
+echo A typical java home is C:\Program Files\Java\jdk1.6.0 (or 'jdk1.6.1' ...)
+pause
+GOTO :END
+
+:END
