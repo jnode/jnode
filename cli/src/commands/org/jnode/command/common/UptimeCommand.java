69fac622e4a8a749c78416bac41be6abcd8ffe34
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2010-01-03 11:09:20 +0000

    Updated file headers.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5708 407389f7-7c16-0410-9eee-ad85acb55d7f

--- cli/src/commands/org/jnode/command/common/UptimeCommand.java
+++ cli/src/commands/org/jnode/command/common/UptimeCommand.java
@@ -1,22 +1,23 @@
 /*
- * $Id: UptimeCommand.java 4977 2009-02-02 09:09:41Z lsantha $
+ * $Id$
  *
- * Copyright (C) 2003-2009 JNode.org
+ * Copyright (C) 2003-2010 JNode.org
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but
+ * This library is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library; If not, write to the Free Software Foundation, Inc.,
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+ 
 package org.jnode.command.common;
 
 import java.io.PrintWriter;
