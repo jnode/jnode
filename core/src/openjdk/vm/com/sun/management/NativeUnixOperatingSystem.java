7a57c7bf7341b5d4db55ff73b61a7ee0a733e509
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2010-01-10 14:43:09 +0000

    Added native stub for com.sun.management.UnixOperatingSystem.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5717 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ core/src/openjdk/vm/com/sun/management/NativeUnixOperatingSystem.java
@@ -0,0 +1,69 @@
+package com.sun.management;
+
+import org.jnode.vm.VmSystem;
+
+/**
+ * @see com.sun.management.UnixOperatingSystem
+ */
+class NativeUnixOperatingSystem {
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getCommittedVirtualMemorySize()
+     */
+    private static long getCommittedVirtualMemorySize(UnixOperatingSystem instance) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getTotalSwapSpaceSize()
+     */
+    private static long getTotalSwapSpaceSize(UnixOperatingSystem instance) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getFreeSwapSpaceSize()
+     */
+    private static long getFreeSwapSpaceSize(UnixOperatingSystem instance) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getProcessCpuTime()
+     */
+    private static long getProcessCpuTime(UnixOperatingSystem instance) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getFreePhysicalMemorySize()
+     */
+    private static long getFreePhysicalMemorySize(UnixOperatingSystem instance) {
+        return VmSystem.freeMemory();
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getTotalPhysicalMemorySize()
+     */
+    private static long getTotalPhysicalMemorySize(UnixOperatingSystem instance) {
+        return VmSystem.totalMemory();
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getOpenFileDescriptorCount()
+     */
+    private static long getOpenFileDescriptorCount(UnixOperatingSystem instance) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#getMaxFileDescriptorCount()
+     */
+    private static long getMaxFileDescriptorCount(UnixOperatingSystem instance) {
+        //todo implement it
+        return 0;
+    }
+    /**
+     * @see com.sun.management.UnixOperatingSystem#initialize()
+     */
+    private static void initialize() {
+        //todo implement it
+    }
+}
