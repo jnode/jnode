89ff29cda73844e4e7984d5901be6d949c27e251
Levente Santha <lsantha@jnode.org>
2011-04-27 06:49:49 +0000

    Improved support for java.lang.Package.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5809 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/openjdk/vm/java/lang/NativePackage.java
+++ core/src/openjdk/vm/java/lang/NativePackage.java
@@ -26,10 +26,10 @@ package java.lang;
 class NativePackage {
     private static String getSystemPackage0(String name) {
         //todo implement it
-        throw new UnsupportedOperationException();
+        return "";
     }
     private static String[] getSystemPackages0() {
         //todo implement it
-        throw new UnsupportedOperationException();
+        return new String[0];
     }
 }
