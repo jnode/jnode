c986398ef79d9f5f4fa3606d76c156eb0f7787d1
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2011-06-24 08:53:44 +0000

    Remove commented code.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5825 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/fs/org/jnode/fs/ext2/cache/INodeCache.java
+++ fs/src/fs/org/jnode/fs/ext2/cache/INodeCache.java
@@ -17,7 +17,7 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
+
 package org.jnode.fs.ext2.cache;
 
 import java.util.Hashtable;
@@ -34,52 +34,3 @@ public class INodeCache extends Hashtable<Object, INode> {
         super(initialCapacity, loadFactor);
     }
 }
-
-/*
-public class INodeCache extends LinkedHashMap {
-    // at most MAX_SIZE blocks fit in the cache
-    static final int MAX_SIZE = 10;
-    private final Logger log = Logger.getLogger(getClass());
-
-    public INodeCache(int initialCapacity, float loadFactor) {
-        super(Math.min(MAX_SIZE, initialCapacity), loadFactor, true);
-        // super(initialCapacity,loadFactor);
-        log.setLevel(Level.DEBUG);
-    }
-
-//    public boolean containsKey(Integer key) {
-//        boolean result = super.containsKey(key);
-//        if (result)
-//            log.debug("INODE CACHE HIT, size:" + size());
-//        else
-//            log.debug("INODE CACHE MISS");
-//        return result;
-//    }
-
-    protected synchronized boolean removeEldestEntry(final Map.Entry eldest) {
-        log.debug("INodeCache size: " + size());
-        INode eldestINode = (INode) eldest.getValue();
-        synchronized (eldestINode) {
-            while (eldestINode.isLocked()) {
-                // an other thread has indicated that this inode
-                // should not be removed from the cache
-                try {
-                    log.info("** Waiting for an other thread to unlock inode " + eldestINode);
-                    eldestINode.wait(1000);
-                } catch (InterruptedException iex) {
-                }
-            }
-
-            if (size() > MAX_SIZE) {
-                try {
-                    eldestINode.flush();
-                } catch (Exception e) {
-                    log.error("Can't flush inode!", e);
-                }
-                return true;
-            } else
-                return false;
-        }
-    }
-}
-*/
