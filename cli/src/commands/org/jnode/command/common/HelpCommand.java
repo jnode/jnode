ca356e743a07adb6bb821bd250910621e18f06c8
lsantha <lsantha@407389f7-7c16-0410-9eee-ad85acb55d7f>
2011-08-21 15:23:35 +0000

    Fix to showing aliases in help command.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5852 407389f7-7c16-0410-9eee-ad85acb55d7f

--- cli/src/commands/org/jnode/command/common/HelpCommand.java
+++ cli/src/commands/org/jnode/command/common/HelpCommand.java
@@ -50,7 +50,7 @@ public class HelpCommand extends AbstractCommand {
     private static final String err_no_help = "No help information is available for '%s'%n";
     private static final String err_help_ex = "Error getting help for '%s': %s%n";
     private static final String err_sec_ex = "Security exception while loading the class for '%s'%nReason: %s%n";
-    private static final String fmt_other_alias = "Other aliases %s";
+    private static final String fmt_other_alias = "Other aliases: %s%n";
     
     private final AliasArgument argAlias;
 
