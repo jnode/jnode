157880252bc8c7d1ec8f16ad5d73541f5afe3c96
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2012-06-04 09:05:31 +0000

    Update headers.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5899 407389f7-7c16-0410-9eee-ad85acb55d7f

--- core/src/core/org/jnode/vm/x86/compiler/l1a/L1AHelper.java
+++ core/src/core/org/jnode/vm/x86/compiler/l1a/L1AHelper.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright (C) 2003-2010 JNode.org
+ * Copyright (C) 2003-2012 JNode.org
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -17,7 +17,7 @@
  * along with this library; If not, write to the Free Software Foundation, Inc., 
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
+ 
 package org.jnode.vm.x86.compiler.l1a;
 
 import org.jnode.assembler.x86.X86Assembler;
--- fs/src/driver/org/jnode/driver/block/ide/disk/IDEDeviceBlockAlignmentSupport.java
+++ fs/src/driver/org/jnode/driver/block/ide/disk/IDEDeviceBlockAlignmentSupport.java
@@ -1,3 +1,23 @@
+/*
+ * $Id: header.txt 5714 2010-01-03 13:33:07Z lsantha $
+ *
+ * Copyright (C) 2003-2012 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
 package org.jnode.driver.block.ide.disk;
 
 import org.jnode.driver.block.PartitionableBlockAlignmentSupport;
