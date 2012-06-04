1aedb9dd9651cc0bd78ed121107ade765fce1159
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2012-06-04 09:05:31 +0000

    Update headers.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5899 407389f7-7c16-0410-9eee-ad85acb55d7f

--- fs/src/test/org/jnode/test/fs/LfnTest.java
+++ fs/src/test/org/jnode/test/fs/LfnTest.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright (C) 2003-2010 JNode.org
+ * Copyright (C) 2003-2012 JNode.org
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
