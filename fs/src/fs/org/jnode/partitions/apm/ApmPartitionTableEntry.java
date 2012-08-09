a1292b99c7cb243d5c44ad2ff7d7f39228901f3a
galatnm <galatnm@407389f7-7c16-0410-9eee-ad85acb55d7f>
2012-08-09 14:08:21 +0000

    Initial Apple partition map support (patch from L. Quinane)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jnode/svn/trunk@5904 407389f7-7c16-0410-9eee-ad85acb55d7f

---
+++ fs/src/fs/org/jnode/partitions/apm/ApmPartitionTableEntry.java
@@ -0,0 +1,117 @@
+/*
+ * $Id: $
+ *
+ * Copyright (C) 2003-2012 JNode.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jnode.partitions.apm;
+
+import java.nio.charset.Charset;
+import org.jnode.partitions.PartitionTableEntry;
+import org.jnode.partitions.ibm.IBMPartitionTable;
+import org.jnode.util.BigEndian;
+import org.jnode.util.LittleEndian;
+import org.jnode.util.NumberUtils;
+
+/**
+ * A APM partition table entry.
+ *
+ * @author Luke Quinane
+ */
+public class ApmPartitionTableEntry implements PartitionTableEntry {
+
+    /** The first 16KiB of the drive. */
+    private final byte[] first16KiB;
+
+    /** The offset to this partition table entry. */
+    private final int offset;
+
+    /**
+     * Creates a new entry.
+     *
+     * @param parent the parent table.
+     * @param first16KiB the first 16,384 bytes of the disk.
+     * @param offset the offset of this entry in the table.
+     */
+    public ApmPartitionTableEntry(ApmPartitionTable parent, byte[] first16KiB, int offset) {
+        this.first16KiB = first16KiB;
+        this.offset = offset;
+    }
+
+    public boolean isValid() {
+        return first16KiB.length > offset + 128 && !isEmpty();
+    }
+
+    /**
+     * @see org.jnode.partitions.PartitionTableEntry#getChildPartitionTable()
+     */
+    public IBMPartitionTable getChildPartitionTable() {
+        throw new UnsupportedOperationException("No child partitions.");
+    }
+
+    /**
+     * @see org.jnode.partitions.PartitionTableEntry#hasChildPartitionTable()
+     */
+    public boolean hasChildPartitionTable() {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return "Apple_Scratch".equals(getType());
+    }
+
+    public long getStartOffset() {
+        return BigEndian.getInt32(first16KiB, offset + 0x8) * 0x200L;
+    }
+
+    public long getEndOffset() {
+        return getStartOffset() + (BigEndian.getInt32(first16KiB, offset + 0xc) + 1) * 0x200L;
+    }
+
+    public String getName() {
+        byte[] nameBytes = new byte[31];
+        System.arraycopy(first16KiB, offset + 0x10, nameBytes, 0, nameBytes.length);
+        return new String(nameBytes, Charset.forName("ASCII")).replace("\u0000", "");
+    }
+
+    public String getType() {
+        byte[] nameBytes = new byte[31];
+        System.arraycopy(first16KiB, offset + 0x30, nameBytes, 0, nameBytes.length);
+        return new String(nameBytes, Charset.forName("ASCII")).replace("\u0000", "");
+    }
+
+    public String dump() {
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < 128; i++) {
+            b.append(NumberUtils.hex(LittleEndian.getUInt8(first16KiB, offset + i), 2));
+            b.append(' ');
+        }
+        return b.toString();
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    public String toString() {
+        StringBuilder builder = new StringBuilder(32);
+        builder.append('[').append(getName()).append(' ');
+        builder.append("t:").append(getType()).append(' ');
+        builder.append("s:").append(getStartOffset()).append(' ');
+        builder.append("e:").append(getEndOffset()).append(']');
+        return builder.toString();
+    }
+}
