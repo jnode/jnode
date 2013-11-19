2fc9a165bd3921b1b008ab2fa6c82c761c05f336
Fabien Lesire <galatnm@gmail.com>
2013-11-19 11:25:20 +0100

    NTFS : Update documentation, expose parent sequence number and fix the type for the parent MFT index.

--- fs/src/fs/org/jnode/fs/ntfs/FileRecord.java
+++ fs/src/fs/org/jnode/fs/ntfs/FileRecord.java
@@ -42,7 +42,7 @@ import org.jnode.util.NumberUtils;
 public class FileRecord extends NTFSRecord {
 
     /**
-     * Sequence number of the file within the MFT.
+     * Index of the file record within the MFT.
      */
     private long referenceNumber;
 
