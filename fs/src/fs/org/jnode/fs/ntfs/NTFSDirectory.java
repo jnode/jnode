a86210c838c64d45984ed73dda2d3431ea3def60
Luke Quinane <luke.quinane@gmail.com>
2014-08-27 15:40:24 +1000

    NTFS: Expose the directory's file record.

--- fs/src/fs/org/jnode/fs/ntfs/NTFSDirectory.java
+++ fs/src/fs/org/jnode/fs/ntfs/NTFSDirectory.java
@@ -31,15 +31,29 @@ import org.jnode.fs.ReadOnlyFileSystemException;
 import org.jnode.fs.ntfs.index.NTFSIndex;
 
 /**
+ * A NTFS directory.
+ *
  * @author vali
  * @author Ewout Prangsma (epr@users.sourceforge.net)
+ * @author Luke Quinane
  */
 public class NTFSDirectory implements FSDirectory, FSDirectoryId {
 
     private static final Logger log = Logger.getLogger(NTFSDirectory.class);
 
+    /**
+     * The file record for the directory.
+     */
+    private final FileRecord fileRecord;
+
+    /**
+     * The directory's index.
+     */
     private final NTFSIndex index;
 
+    /**
+     * The associated file system.
+     */
     private final NTFSFileSystem fs;
 
     /**
@@ -54,6 +68,7 @@ public class NTFSDirectory implements FSDirectory, FSDirectoryId {
      */
     public NTFSDirectory(NTFSFileSystem fs, FileRecord record) throws IOException {
         this.fs = fs;
+        this.fileRecord = record;
         this.index = new NTFSIndex(record, "$I30");
         id = Long.toString(record.getReferenceNumber());
     }
@@ -87,8 +102,16 @@ public class NTFSDirectory implements FSDirectory, FSDirectoryId {
     @Override
     public FSEntry getEntryById(String id) throws IOException {
         FileRecord fileRecord = fs.getNTFSVolume().getMFT().getRecord(Long.parseLong(id));
-        long parentId = Long.parseLong(this.id);
-        return new NTFSEntry(fs, fileRecord, parentId);
+        return new NTFSEntry(fs, fileRecord, this.fileRecord.getReferenceNumber());
+    }
+
+    /**
+     * Gets the NTFS file record for the directory.
+     *
+     * @return the file record.
+     */
+    public FileRecord getFileRecord() {
+        return fileRecord;
     }
 
     /**
